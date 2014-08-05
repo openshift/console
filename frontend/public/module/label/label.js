52ed3f5a526f02d13ab4d9c81d26c49b55ce47f1
Ed Rooth <ed.rooth@coreos.com>
2014-08-05 11:55:18 -0700

    frontend: update label styles and entity icon.

--- frontend/public/module/label/label.js
+++ frontend/public/module/label/label.js
@@ -10,26 +10,29 @@ angular.module('app')
     restrict: 'E',
     replace: true,
     link: function(scope, el, attrs) {
-      var ref;
+      var ref, linkEl;
+
       ref = linkTemplate({
         type: attrs.type,
         key: attrs.key,
         value: attrs.value
       });
 
-      el.addClass('co-m-label--' + attrs.type);
+      linkEl = el.find('.co-m-label__link');
+      linkEl.attr('href', ref);
 
-      el.find('.co-m-label__circle')
-        .text(attrs.type[0].toUpperCase());
+      el.addClass('co-m-label--' + attrs.type);
+      if (attrs.selector) {
+        el.addClass('co-m-label--selector');
+        linkEl.prepend('<i class="co-m-label__icon fa fa-search"></i>');
+        linkEl.addClass('co-m-modal-link');
+      }
 
       el.find('.co-m-label__key')
         .text(attrs.key);
 
       el.find('.co-m-label__value')
         .text(attrs.value);
-
-      el.find('.co-m-label__link')
-        .attr('href', ref);
     }
   };
 })
@@ -42,6 +45,7 @@ angular.module('app')
     replace: true,
     scope: {
       type: '@',
+      selector: '@',
       labels: '='
     }
   };
