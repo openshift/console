1a6ece0c0b087e58fb3559bd6d08b5abd6b40afd
Brian Waldon <bcwaldon@gmail.com>
2014-08-20 15:47:34 -0700

    config: ignore missing config.ini

--- config/config.go
+++ config/config.go
@@ -1,14 +1,30 @@
 package config
 
 import (
+	"log"
+	"os"
+
 	"github.com/coreos-inc/bridge/Godeps/_workspace/src/github.com/rakyll/globalconf"
 )
 
+const (
+	cfgLocation = "./config.ini"
+)
+
 func Init() error {
-	conf, err := globalconf.NewWithOptions(&globalconf.Options{
-		Filename:  "./config.ini",
+	opts := globalconf.Options{
 		EnvPrefix: "BRIDGE_",
-	})
+	}
+
+	_, err := os.Stat(cfgLocation)
+	if err == nil {
+		log.Printf("Using config file at %s", cfgLocation)
+		opts.Filename = cfgLocation
+	} else {
+		log.Printf("Not using config file: %v", err)
+	}
+
+	conf, err := globalconf.NewWithOptions(&opts)
 	if err != nil {
 		return err
 	}
