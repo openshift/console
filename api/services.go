76ee264ca33490adde2a2ed3a065fe314ca2f8f2
Ed Rooth <ed.rooth@coreos.com>
2014-08-20 10:41:14 -0700

    api: endpoint to create k8s service

--- api/services.go
+++ api/services.go
@@ -8,11 +8,19 @@ import (
 
 func registerServices(router *mux.Router) {
 	router.HandleFunc("/services", serviceList).Methods("GET")
+	router.HandleFunc("/services", serviceCreate).Methods("POST")
 	router.HandleFunc("/services/{id}", serviceGet).Methods("GET")
 }
 
 // Get Service api endpoint.
 func serviceGet(w http.ResponseWriter, r *http.Request) {
+	err := k8proxy.DoAndRespond(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+	}
+}
+
+func serviceCreate(w http.ResponseWriter, r *http.Request) {
 	err := k8proxy.DoAndRespond(w, r)
 	if err != nil {
 		w.WriteHeader(http.StatusBadGateway)
