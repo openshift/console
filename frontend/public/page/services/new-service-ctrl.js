118da3a599e0b5abe9d06204dc37df34506b2f42
Ed Rooth <ed.rooth@coreos.com>
2014-08-20 10:45:20 -0700

    frontend/services: start of "create service" page

---
+++ frontend/public/page/services/new-service-ctrl.js
@@ -0,0 +1,23 @@
+angular.module('app')
+.controller('NewServiceCtrl', function($scope, $routeParams, ServicesSvc) {
+  'use strict';
+
+  $scope.service = {
+    id: null,
+    port: null,
+    containerPort: null,
+    selector: null,
+    labels: null
+  };
+
+  $scope.save = function() {
+    ServicesSvc.create($scope.service);
+  };
+
+})
+
+.controller('NewServiceFormCtrl', function($scope) {
+  'use strict';
+
+  $scope.submit = $scope.save;
+});
