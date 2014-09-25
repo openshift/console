0465597f58dcf76b1f28f249b5bac8793233fda3
Ed Rooth <ed.rooth@coreos.com>
2014-09-25 11:53:37 -0700

    frontend: better styling/layout of container detail page

--- frontend/public/page/containers/container-ctrl.js
+++ frontend/public/page/containers/container-ctrl.js
@@ -1,17 +1,22 @@
 angular.module('app')
-.controller('ContainerCtrl', function($scope, $routeParams, _, PodsSvc) {
+.controller('ContainerCtrl', function($scope, $routeParams, _, PodsSvc, ModalLauncherSvc) {
   'use strict';
 
   PodsSvc.get({ id: $routeParams.podId }).then(function(result) {
-    var container;
-    $scope.pod = result;
-    container = _.findWhere($scope.pod.desiredState.manifest.containers, { name: $routeParams.name });
+    $scope.pod = result.data;
+    $scope.container = _.findWhere($scope.pod.desiredState.manifest.containers, { name: $routeParams.name });
     if ($scope.pod.currentState && $scope.pod.currentState.info &&
         $scope.pod.currentState.info[$routeParams.name]) {
-      _.extend(container, $scope.pod.currentState.info[$routeParams.name]);
+      _.extend($scope.container, $scope.pod.currentState.info[$routeParams.name]);
     }
-    $scope.container = container;
   });
+
+  $scope.openJsonModal = function() {
+    ModalLauncherSvc.open('view-json', {
+      title: 'Raw Container Data',
+      json: $scope.container,
+    });
+  };
 
   $scope.getState = function() {
     var state = '';
