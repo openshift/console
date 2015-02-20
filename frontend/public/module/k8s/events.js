55bf8376c4e174250ad8a4a539d615babe62b9fb
Ed Rooth <ed.rooth@coreos.com>
2015-02-20 13:33:33 -0800

    frontend/k8s: add "resource updated" event type

--- frontend/public/module/k8s/events.js
+++ frontend/public/module/k8s/events.js
@@ -1,4 +1,5 @@
 angular.module('k8s').constant('k8sEvents', {
   RESOURCE_CREATED: 'k8s-resource-created',
+  RESOURCE_UPDATED: 'k8s-resource-updated',
   RESOURCE_DELETED: 'k8s-resource-deleted',
 });
