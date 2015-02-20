cb565a74052fe0e9eec1cb457bd4a8f2df6fef72
Ed Rooth <ed.rooth@coreos.com>
2015-02-20 13:35:58 -0800

    frontend: ensure resource lists react to updates

--- frontend/public/module/pod-list/pod-list.js
+++ frontend/public/module/pod-list/pod-list.js
@@ -3,7 +3,7 @@
  * List out pods in a table-like view.
  */
 
-angular.module('app').directive('coPodList', function(k8s, arraySvc) {
+angular.module('app').directive('coPodList', function(k8s, arraySvc, resourceMgrSvc) {
   'use strict';
 
   return {
@@ -14,11 +14,19 @@ angular.module('app').directive('coPodList', function(k8s, arraySvc) {
       pods: '='
     },
     controller: function($scope) {
+
       $scope.$on(k8s.events.RESOURCE_DELETED, function(e, data) {
         if (data.kind === k8s.enum.Kind.POD) {
           arraySvc.remove($scope.pods, data.original);
         }
       });
+
+      $scope.$on(k8s.events.RESOURCE_UPDATED, function(e, data) {
+        if (data.kind === k8s.enum.Kind.POD) {
+          resourceMgrSvc.updateInList($scope.pods, data.resource);
+        }
+      });
+
     }
   };
 
