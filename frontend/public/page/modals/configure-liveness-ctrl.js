f429dbb934b660e8f7bcff70ba3d7d51e70da194
Ed Rooth <ed.rooth@coreos.com>
2015-03-06 12:43:10 -0800

    frontend/modals: wire up liveness modal

--- frontend/public/page/modals/configure-liveness-ctrl.js
+++ frontend/public/page/modals/configure-liveness-ctrl.js
@@ -1,13 +1,32 @@
 angular.module('app')
-.controller('ConfigureLivenessCtrl', function() {
+.controller('ConfigureLivenessCtrl', function($scope, $modalInstance, k8s, container) {
   'use strict';
 
-  //TODO
+  var placeholders = {
+    exec: '/bin/my-liveness-check',
+    httpGet: 'http://localhost:8080/my-liveness-check',
+    tcpSocket: '8080',
+  };
+
+  $scope.actionTypes = k8s.enum.HookAction;
+  $scope.fields = k8s.probe.mapLivenessProbeToFields(container.livenessProbe);
+  $scope.getActionLabelById = k8s.probe.getActionLabelById;
+
+  $scope.getPlaceholder = function(type) {
+    return placeholders[type];
+  };
+
+  $scope.save = function() {
+    container.livenessProbe = k8s.probe.mapFieldsToLivenessProbe($scope.fields);
+    $modalInstance.close(container);
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
 
 })
-.controller('ConfigureLivenessFormCtrl', function() {
+.controller('ConfigureLivenessFormCtrl', function($scope) {
   'use strict';
-
-  //TODO
-
+  $scope.submit = $scope.save;
 });
