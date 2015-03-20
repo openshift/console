ebd27fd12fcdce52e56ee7bd412b0dfda336275b
Ed Rooth <ed.rooth@coreos.com>
2015-03-19 17:08:50 -0700

    frontend/replication-controllers: move listing into directive

---
+++ frontend/public/module/ui/resources/replication-controller-list.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview
+ * List RCs in a table-like view.
+ */
+
+angular.module('app.ui')
+.directive('coReplicationControllerList', function(k8s, _, arraySvc, resourceMgrSvc) {
+  'use strict';
+
+  return {
+    templateUrl: '/static/module/ui/resources/replication-controller-list.html',
+    restrict: 'E',
+    replace: true,
+    scope: {
+      rcs: '='
+    },
+    controller: function($scope) {
+
+      $scope.getPods = function(rc) {
+        k8s.pods.list({ns: rc.metadata.namespace, labels: rc.spec.selector })
+          .then(function(pods) {
+            rc.pods = pods;
+          });
+      };
+
+      $scope.$on(k8s.events.RESOURCE_DELETED, function(e, data) {
+        if (data.kind === k8s.enum.Kind.REPLICATIONCONTROLLER) {
+          arraySvc.remove($scope.rcs, data.original);
+        }
+      });
+
+      $scope.$on(k8s.events.RESOURCE_UPDATED, function(e, data) {
+        if (data.kind === k8s.enum.Kind.REPLICATIONCONTROLLER) {
+          resourceMgrSvc.updateInList($scope.rcs, data.resource);
+        }
+      });
+
+    }
+  };
+
+});
