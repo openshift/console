977a82e445ddf19c6be1a37605e908d3338329ec
Ed Rooth <ed.rooth@coreos.com>
2015-03-19 17:01:02 -0700

    frontend/nodes: move listing into directive

---
+++ frontend/public/module/ui/resources/node-list.js
@@ -0,0 +1,47 @@
+/**
+ * @fileoverview
+ * List nodes in a table-like view.
+ */
+
+angular.module('app.ui')
+.directive('coNodeList', function(k8s, _, pkg, arraySvc, resourceMgrSvc) {
+  'use strict';
+
+  return {
+    templateUrl: '/static/module/ui/resources/node-list.html',
+    restrict: 'E',
+    replace: true,
+    scope: {
+      nodes: '='
+    },
+    controller: function($scope) {
+
+      $scope.getStatus = function(node) {
+        if (!pkg.propExists('status.conditions', node) || _.isEmpty(node.status.conditions)) {
+          return 'Unknown';
+        }
+        return node.status.conditions[0].status;
+      };
+
+      $scope.getPods = function(node) {
+        k8s.pods.listByNode(node).then(function(pods) {
+          node.pods = pods;
+        });
+      };
+
+      $scope.$on(k8s.events.RESOURCE_DELETED, function(e, data) {
+        if (data.kind === k8s.enum.Kind.NODE) {
+          arraySvc.remove($scope.nodes, data.original);
+        }
+      });
+
+      $scope.$on(k8s.events.RESOURCE_UPDATED, function(e, data) {
+        if (data.kind === k8s.enum.Kind.NODE) {
+          resourceMgrSvc.updateInList($scope.nodes, data.resource);
+        }
+      });
+
+    }
+  };
+
+});
--- frontend/public/page/machines/machines-ctrl.js
+++ frontend/public/page/machines/machines-ctrl.js
@@ -1,22 +1,9 @@
 angular.module('app')
-.controller('MachinesCtrl', function($scope, _, k8s) {
+.controller('MachinesCtrl', function($scope, k8s) {
   'use strict';
 
   k8s.nodes.list().then(function(nodes) {
-    $scope.machines = nodes;
+    $scope.nodes = nodes;
   });
-
-  $scope.getPods = function(machine) {
-    k8s.pods.listByNode(machine).then(function(pods) {
-      machine.pods = pods;
-    });
-  };
-
-  $scope.getStatus = function(machine) {
-    if (!machine || !machine.status || _.isEmpty(machine.status.conditions)) {
-      return 'Unknown';
-    }
-    return machine.status.conditions[0].status;
-  };
 
 });
