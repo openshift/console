1134e35f7061167521d010e36ce38870114fc0df
Ed Rooth <ed.rooth@coreos.com>
2015-04-28 17:03:40 -0700

    frontend/ui: event-icon directive for cluster events

---
+++ frontend/public/module/ui/sysevents/sysevent-icon.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview
+ * Display an icon based on a kubernetes cluster event.
+ */
+
+angular.module('app.ui')
+.directive('coSyseventIcon', function($compile) {
+  'use strict';
+
+  return {
+    template: '<span></span>',
+    restrict: 'E',
+    replace: true,
+    link: function(scope, elem, attrs) {
+      var tooltipMsg, cssClass, iconEl,
+          reason = attrs.reason,
+          kind = attrs.kind;
+
+      if (!reason || !kind) {
+        return;
+      }
+
+      tooltipMsg = reason + ' (' + kind.toLowerCase() + ')';
+      cssClass = 'fa co-sysevent-icon co-sysevent-icon--' + kind.toLowerCase() + '-' + reason.toLowerCase();
+      iconEl = angular.element('<i tooltip="' + tooltipMsg +
+          '" tooltip-append-to-body="true" tooltip-placement="top" class="' + cssClass + '"></i>');
+      $compile(iconEl)(scope);
+      elem.append(iconEl);
+    },
+  };
+
+});
