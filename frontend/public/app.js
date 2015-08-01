1d272005438d9e8d7d447eda3093f40639c0f83c
Ed Rooth <ed.rooth@coreos.com>
2015-07-31 17:41:20 -0700

    frontend: wire up auth, show user info in nav

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -4,6 +4,7 @@ angular.module('bridge', [
   'ngRoute',
   'ngAnimate',
   'ngSanitize',
+  'ngCookies',
   // other deps
   'ui.bootstrap',
   'underscore',
@@ -27,6 +28,7 @@ angular.module('bridge', [
   $locationProvider.html5Mode(true);
   flagSvcProvider.setGlobalId('SERVER_FLAGS');
   k8sConfigProvider.setBasePath('/api/kubernetes/' + window.SERVER_FLAGS.k8sVersion);
+  $httpProvider.interceptors.push('unauthorizedInterceptorSvc');
 
   configSvcProvider.config({
     siteBasePath: '/',
@@ -53,188 +55,218 @@ angular.module('bridge', [
     return 'An error occurred. Please try again.';
   });
 
-  $routeProvider
-    .when('/', {
-      controller: 'ClusterStatusCtrl',
-      templateUrl: '/static/page/cluster/status.html',
-      title: 'Cluster Status',
-      resolve: {
-        client: 'ClientLoaderSvc'
+  function r(route, config, unprotected) {
+    if (!unprotected) {
+      if (!config.resolve) {
+        config.resolve = {};
       }
-    })
-    .when('/apps', {
-      controller: 'AppsCtrl',
-      templateUrl: '/static/page/apps/apps.html',
-      title: 'Applications',
-    })
-    .when('/ns/:ns/apps', {
-      controller: 'AppsCtrl',
-      templateUrl: '/static/page/apps/apps.html',
-      title: 'Applications',
-    })
-    .when('/ns/:ns/apps/new', {
-      controller: 'NewAppCtrl',
-      templateUrl: '/static/page/apps/new-app.html',
-      title: 'Create New Application',
-    })
-    .when('/events', {
-      controller: 'EventsCtrl',
-      templateUrl: '/static/page/events/events.html',
-      title: 'Events',
-    })
-    .when('/ns/:ns/events', {
-      controller: 'EventsCtrl',
-      templateUrl: '/static/page/events/events.html',
-      title: 'Events',
-    })
-    .when('/services', {
-      controller: 'ServicesCtrl',
-      templateUrl: '/static/page/services/services.html',
-      title: 'Services',
-    })
-    .when('/ns/:ns/services', {
-      controller: 'ServicesCtrl',
-      templateUrl: '/static/page/services/services.html',
-      title: 'Services',
-    })
-    .when('/ns/:ns/services/new', {
-      controller: 'NewServiceCtrl',
-      templateUrl: '/static/page/services/new-service.html',
-      title: 'Create New Service',
-    })
-    .when('/ns/:ns/services/:name', {
-      controller: 'ServiceCtrl',
-      templateUrl: '/static/page/services/service.html',
-      title: 'Service',
-    })
-    .when('/ns/:ns/services/:name/pods', {
-      controller: 'ServicePodsCtrl',
-      templateUrl: '/static/page/services/pods.html',
-      title: 'Service Pods',
-    })
-    .when('/replicationcontrollers', {
-      controller: 'ReplicationcontrollersCtrl',
-      templateUrl: '/static/page/replicationcontrollers/replicationcontrollers.html',
-      title: 'Replication Controllers',
-    })
-    .when('/ns/:ns/replicationcontrollers', {
-      controller: 'ReplicationcontrollersCtrl',
-      templateUrl: '/static/page/replicationcontrollers/replicationcontrollers.html',
-      title: 'Replication Controllers',
-    })
-    .when('/ns/:ns/replicationcontrollers/new', {
-      controller: 'NewReplicationcontrollerCtrl',
-      templateUrl: '/static/page/replicationcontrollers/new-replicationcontroller.html',
-      title: 'New Replication Controller',
-    })
-    .when('/ns/:ns/replicationcontrollers/:name/edit', {
-      controller: 'EditReplicationcontrollerCtrl',
-      templateUrl: '/static/page/replicationcontrollers/edit-replicationcontroller.html',
-      title: 'Edit Replication Controller',
-    })
-    .when('/ns/:ns/replicationcontrollers/:name', {
-      controller: 'ReplicationcontrollerCtrl',
-      templateUrl: '/static/page/replicationcontrollers/replicationcontroller.html',
-      title: 'Replication Controller',
-    })
-    .when('/ns/:ns/replicationcontrollers/:name/pods', {
-      controller: 'ReplicationcontrollerPodsCtrl',
-      templateUrl: '/static/page/replicationcontrollers/pods.html',
-      title: 'Replication Controller Pods',
-    })
-    .when('/pods', {
-      controller: 'PodsCtrl',
-      templateUrl: '/static/page/pods/pods.html',
-      title: 'Pods',
-    })
-    .when('/ns/:ns/pods', {
-      controller: 'PodsCtrl',
-      templateUrl: '/static/page/pods/pods.html',
-      title: 'Pods',
-    })
-    .when('/ns/:ns/pods/new', {
-      controller: 'NewPodCtrl',
-      templateUrl: '/static/page/pods/new-pod.html',
-      title: 'Create New Pod',
-    })
-    .when('/ns/:ns/pods/:name', {
-      controller: 'PodCtrl',
-      templateUrl: '/static/page/pods/pod.html',
-      title: 'Pod',
-    })
-    .when('/ns/:ns/pods/:name/events', {
-      controller: 'PodSyseventsCtrl',
-      templateUrl: '/static/page/pods/sysevents.html',
-      title: 'Pod Events',
-    })
-    .when('/ns/:ns/pods/:podName/containers/:name', {
-      controller: 'ContainerCtrl',
-      templateUrl: '/static/page/containers/container.html',
-      title: 'Container',
-    })
-    .when('/machines', {
-      controller: 'MachinesCtrl',
-      templateUrl: '/static/page/machines/machines.html',
-      title: 'Machines',
-    })
-    .when('/machines/:name', {
-      controller: 'MachineCtrl',
-      templateUrl: '/static/page/machines/machine.html',
-      title: 'Machine',
-    })
-    .when('/machines/:name/events', {
-      controller: 'MachineSyseventsCtrl',
-      templateUrl: '/static/page/machines/sysevents.html',
-      title: 'Machine Events',
-    })
-    .when('/machines/:name/pods', {
-      controller: 'MachinePodsCtrl',
-      templateUrl: '/static/page/machines/pods.html',
-      title: 'Machine Pods',
-    })
-    // Alias for machines (for programatic routing).
-    .when('/nodes', {
-      redirectTo: '/machines',
-    })
-    .when('/nodes/:name', {
-      redirectTo: '/machines/:name',
-    })
-    .when('/search', {
-      controller: 'SearchCtrl',
-      templateUrl: '/static/page/search/search.html',
-      title: 'Search',
-    })
-    .when('/settings/registries', {
-      controller: 'RegistriesCtrl',
-      templateUrl: '/static/page/settings/registries.html',
-      title: 'Configure Registries',
-      resolve: {
-        client: 'ClientLoaderSvc'
-      }
-    })
-    .when('/settings/users', {
-      controller: 'UsersCtrl',
-      templateUrl: '/static/page/settings/users.html',
-      title: 'Users & API Keys',
-      resolve: {
-        client: 'ClientLoaderSvc'
-      }
-    })
-    .when('/welcome', {
-      controller: 'WelcomeCtrl',
-      templateUrl: '/static/page/welcome/welcome.html',
-      title: 'Welcome to your CoreOS Cluster',
-    })
-    .otherwise({
-      templateUrl: '/static/page/error/404.html',
-      title: 'Page Not Found (404)'
-    });
+      config.resolve.ensureLoggedIn = 'ensureLoggedInSvc';
+    }
+    $routeProvider.when(route, config);
+  }
+
+  r('/', {
+    controller: 'ClusterStatusCtrl',
+    templateUrl: '/static/page/cluster/status.html',
+    title: 'Cluster Status',
+    resolve: {
+      client: 'ClientLoaderSvc'
+    }
+  });
+  r('/apps', {
+    controller: 'AppsCtrl',
+    templateUrl: '/static/page/apps/apps.html',
+    title: 'Applications',
+  });
+  r('/ns/:ns/apps', {
+    controller: 'AppsCtrl',
+    templateUrl: '/static/page/apps/apps.html',
+    title: 'Applications',
+  });
+  r('/ns/:ns/apps/new', {
+    controller: 'NewAppCtrl',
+    templateUrl: '/static/page/apps/new-app.html',
+    title: 'Create New Application',
+  });
+  r('/events', {
+    controller: 'EventsCtrl',
+    templateUrl: '/static/page/events/events.html',
+    title: 'Events',
+  });
+  r('/ns/:ns/events', {
+    controller: 'EventsCtrl',
+    templateUrl: '/static/page/events/events.html',
+    title: 'Events',
+  });
+  r('/services', {
+    controller: 'ServicesCtrl',
+    templateUrl: '/static/page/services/services.html',
+    title: 'Services',
+  });
+  r('/ns/:ns/services', {
+    controller: 'ServicesCtrl',
+    templateUrl: '/static/page/services/services.html',
+    title: 'Services',
+  });
+  r('/ns/:ns/services/new', {
+    controller: 'NewServiceCtrl',
+    templateUrl: '/static/page/services/new-service.html',
+    title: 'Create New Service',
+  });
+  r('/ns/:ns/services/:name', {
+    controller: 'ServiceCtrl',
+    templateUrl: '/static/page/services/service.html',
+    title: 'Service',
+  });
+  r('/ns/:ns/services/:name/pods', {
+    controller: 'ServicePodsCtrl',
+    templateUrl: '/static/page/services/pods.html',
+    title: 'Service Pods',
+  });
+  r('/replicationcontrollers', {
+    controller: 'ReplicationcontrollersCtrl',
+    templateUrl: '/static/page/replicationcontrollers/replicationcontrollers.html',
+    title: 'Replication Controllers',
+  });
+  r('/ns/:ns/replicationcontrollers', {
+    controller: 'ReplicationcontrollersCtrl',
+    templateUrl: '/static/page/replicationcontrollers/replicationcontrollers.html',
+    title: 'Replication Controllers',
+  });
+  r('/ns/:ns/replicationcontrollers/new', {
+    controller: 'NewReplicationcontrollerCtrl',
+    templateUrl: '/static/page/replicationcontrollers/new-replicationcontroller.html',
+    title: 'New Replication Controller',
+  });
+  r('/ns/:ns/replicationcontrollers/:name/edit', {
+    controller: 'EditReplicationcontrollerCtrl',
+    templateUrl: '/static/page/replicationcontrollers/edit-replicationcontroller.html',
+    title: 'Edit Replication Controller',
+  });
+  r('/ns/:ns/replicationcontrollers/:name', {
+    controller: 'ReplicationcontrollerCtrl',
+    templateUrl: '/static/page/replicationcontrollers/replicationcontroller.html',
+    title: 'Replication Controller',
+  });
+  r('/ns/:ns/replicationcontrollers/:name/pods', {
+    controller: 'ReplicationcontrollerPodsCtrl',
+    templateUrl: '/static/page/replicationcontrollers/pods.html',
+    title: 'Replication Controller Pods',
+  });
+  r('/pods', {
+    controller: 'PodsCtrl',
+    templateUrl: '/static/page/pods/pods.html',
+    title: 'Pods',
+  });
+  r('/ns/:ns/pods', {
+    controller: 'PodsCtrl',
+    templateUrl: '/static/page/pods/pods.html',
+    title: 'Pods',
+  });
+  r('/ns/:ns/pods/new', {
+    controller: 'NewPodCtrl',
+    templateUrl: '/static/page/pods/new-pod.html',
+    title: 'Create New Pod',
+  });
+  r('/ns/:ns/pods/:name', {
+    controller: 'PodCtrl',
+    templateUrl: '/static/page/pods/pod.html',
+    title: 'Pod',
+  });
+  r('/ns/:ns/pods/:name/events', {
+    controller: 'PodSyseventsCtrl',
+    templateUrl: '/static/page/pods/sysevents.html',
+    title: 'Pod Events',
+  });
+  r('/ns/:ns/pods/:podName/containers/:name', {
+    controller: 'ContainerCtrl',
+    templateUrl: '/static/page/containers/container.html',
+    title: 'Container',
+  });
+  r('/machines', {
+    controller: 'MachinesCtrl',
+    templateUrl: '/static/page/machines/machines.html',
+    title: 'Machines',
+  });
+  r('/machines/:name', {
+    controller: 'MachineCtrl',
+    templateUrl: '/static/page/machines/machine.html',
+    title: 'Machine',
+  });
+  r('/machines/:name/events', {
+    controller: 'MachineSyseventsCtrl',
+    templateUrl: '/static/page/machines/sysevents.html',
+    title: 'Machine Events',
+  });
+  r('/machines/:name/pods', {
+    controller: 'MachinePodsCtrl',
+    templateUrl: '/static/page/machines/pods.html',
+    title: 'Machine Pods',
+  });
+  // Alias for machines (for programatic routing).
+  r('/nodes', {
+    redirectTo: '/machines',
+  });
+  r('/nodes/:name', {
+    redirectTo: '/machines/:name',
+  });
+  r('/search', {
+    controller: 'SearchCtrl',
+    templateUrl: '/static/page/search/search.html',
+    title: 'Search',
+  });
+  r('/settings/registries', {
+    controller: 'RegistriesCtrl',
+    templateUrl: '/static/page/settings/registries.html',
+    title: 'Configure Registries',
+    resolve: {
+      client: 'ClientLoaderSvc'
+    }
+  });
+  r('/settings/users', {
+    controller: 'UsersCtrl',
+    templateUrl: '/static/page/settings/users.html',
+    title: 'Users & API Keys',
+    resolve: {
+      client: 'ClientLoaderSvc'
+    }
+  });
+  r('/welcome', {
+    controller: 'WelcomeCtrl',
+    templateUrl: '/static/page/welcome/welcome.html',
+    title: 'Welcome to your CoreOS Cluster',
+  });
+  r('/error', {
+    controller: 'ErrorCtrl',
+    templateUrl: '/static/page/error/error.html',
+    title: 'Error',
+  }, true);
+
+  $routeProvider.otherwise({
+    templateUrl: '/static/page/error/404.html',
+    title: 'Page Not Found (404)'
+  });
 })
-.run(function($rootScope, CONST, flagSvc, debugSvc, firehose) {
+.run(function($rootScope, $window, CONST, flagSvc, debugSvc, firehose, authSvc) {
   'use strict';
   // Convenience access for temmplates
   $rootScope.CONST = CONST;
   $rootScope.SERVER_FLAGS = flagSvc.all();
   $rootScope.debug = debugSvc;
   firehose.start();
+
+  $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+    switch(rejection) {
+      case 'not-logged-in':
+        $window.location.href = '/auth/login';
+        break;
+    }
+  });
+
+  $rootScope.$on('xhr-error-unauthorized', function(e, rejection) {
+    if (rejection && rejection.status === 401) {
+      authSvc.logout($window.location.pathname);
+    }
+  });
+
 });
