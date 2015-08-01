4768990ca72dbd1a197578831a501bf83cf7538e
Ed Rooth <ed.rooth@coreos.com>
2015-07-31 17:32:16 -0700

    server,cmd: wire up authenticator and auth middleware

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -9,9 +9,11 @@ import (
 	"os"
 	"path"
 
+	"github.com/coreos/go-oidc/oidc"
 	"github.com/coreos/pkg/capnslog"
 	"github.com/coreos/pkg/flagutil"
 
+	"github.com/coreos-inc/bridge/auth"
 	"github.com/coreos-inc/bridge/etcd"
 	"github.com/coreos-inc/bridge/fleet"
 	"github.com/coreos-inc/bridge/server"
@@ -26,15 +28,20 @@ func main() {
 	capnslog.SetFormatter(capnslog.NewStringFormatter(os.Stderr))
 
 	fs := flag.NewFlagSet("bridge", flag.ExitOnError)
-	listen := fs.String("listen", "http://0.0.0.0:9000", "")
+	fs.String("listen", "http://0.0.0.0:9000", "")
 	logLevel := fs.String("log-level", "", "level of logging information by package (pkg=level)")
 	publicDir := fs.String("public-dir", "./frontend/public", "directory containing static web assets")
 	etcdEndpoints := fs.String("etcd-endpoints", "http://localhost:4001", "comma separated list of etcd endpoints")
 	fleetEndpoint := fs.String("fleet-endpoint", "unix://var/run/fleet.sock", "fleet API endpoint")
-	k8sEndpoint := fs.String("k8s-endpoint", "https://172.17.4.101:29101", "URL of the Kubernetes API server")
+	fs.String("k8s-endpoint", "https://172.17.4.101:29101", "URL of the Kubernetes API server")
 	k8sAPIService := fs.String("k8s-api-service", "", "fleet service name to inspect for api server status")
 	k8sControllerManagerService := fs.String("k8s-controller-manager-service", "", "fleet service name to inspect for controller manager status")
 	k8sSchedulerService := fs.String("k8s-scheduler-service", "", "fleet service name to inspect for scheduler status")
+	fs.String("host", "http://127.0.0.1:9000", "The externally visible hostname/port of the service. Used in OIDC/OAuth2 Redirect URL.")
+	disableAuth := fs.Bool("disable-auth", false, "Disable all forms of authentication.")
+	authClientID := fs.String("auth-client-id", "", "The OIDC OAuth2 Client ID.")
+	authClientSecret := fs.String("auth-client-secret", "", "The OIDC/OAuth2 Client Secret.")
+	fs.String("auth-issuer-url", "", "The OIDC/OAuth2 issuer URL")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -56,17 +63,15 @@ func main() {
 		log.Infof("Setting log level to %s", *logLevel)
 	}
 
-	tpls, err := template.ParseFiles(path.Join(*publicDir, server.IndexPageTemplateName))
+	tpl := template.New(server.IndexPageTemplateName)
+	tpl.Delims("[[", "]]")
+	tpls, err := tpl.ParseFiles(path.Join(*publicDir, server.IndexPageTemplateName))
 	if err != nil {
 		fmt.Printf("index.html not found in configured public-dir path: %v", err)
 		os.Exit(1)
 	}
 
-	lu, err := url.Parse(*listen)
-	if err != nil {
-		log.Fatalf("Unable to use --listen flag: %v", err)
-	}
-
+	lu := validateURLFlag(fs, "listen")
 	if lu.Scheme != "http" {
 		log.Fatalf("Unable to listen using scheme: %s", lu.Scheme)
 	}
@@ -81,15 +86,7 @@ func main() {
 		log.Fatalf("Error initializing etcd client: %v", err)
 	}
 
-	k8sURL, err := url.Parse(*k8sEndpoint)
-	if err != nil {
-		log.Fatalf("Unable to use k8s-endpoint flag: %v", err)
-	}
-
-	if k8sURL == nil || k8sURL.String() == "" {
-		log.Fatal("Missing required flag: --k8s-endpoint")
-	}
-
+	k8sURL := validateURLFlag(fs, "k8s-endpoint")
 	kCfg := &server.K8sConfig{
 		Endpoint:                 k8sURL,
 		APIService:               *k8sAPIService,
@@ -105,6 +102,31 @@ func main() {
 		Templates:   tpls,
 	}
 
+	if *disableAuth {
+		log.Warningf("running with AUTHENTICATION DISABLED!")
+	} else {
+		validateFlagNotEmpty(fs, "auth-client-id")
+		validateFlagNotEmpty(fs, "auth-client-secret")
+		iURL := validateURLFlag(fs, "auth-issuer-url")
+		rURL := validateURLFlag(fs, "host")
+		rURL.Path = server.AuthCallbackEndpoint
+
+		ocfg := oidc.ClientConfig{
+			Credentials: oidc.ClientCredentials{
+				ID:     *authClientID,
+				Secret: *authClientSecret,
+			},
+			RedirectURL: rURL.String(),
+		}
+
+		auther, err := auth.NewAuthenticator(ocfg, iURL, server.AuthErrorURL, server.AuthSuccessURL)
+		if err != nil {
+			log.Fatalf("Error initializing authenticator: %v", err)
+		}
+
+		srv.Auther = auther
+	}
+
 	httpsrv := &http.Server{
 		Addr:    lu.Host,
 		Handler: srv.HTTPHandler(),
@@ -112,4 +134,27 @@ func main() {
 
 	log.Infof("Binding to %s...", httpsrv.Addr)
 	log.Fatal(httpsrv.ListenAndServe())
+}
+
+func validateURLFlag(fs *flag.FlagSet, name string) *url.URL {
+	validateFlagNotEmpty(fs, name)
+	flag := fs.Lookup(name)
+	ur, err := url.Parse(flag.Value.String())
+	if err != nil {
+		log.Fatalf("Invalid flag: %s, error: %v", flag.Name, err)
+	}
+	if ur == nil || ur.String() == "" {
+		log.Fatalf("Missing required flag: %s", flag.Name)
+	}
+	if ur.Scheme == "" || ur.Host == "" {
+		log.Fatalf("Invalid flag: %s, error: malformed URL", flag.Name)
+	}
+	return ur
+}
+
+func validateFlagNotEmpty(fs *flag.FlagSet, name string) {
+	flag := fs.Lookup(name)
+	if flag.Value.String() == "" {
+		log.Fatalf("Missing required flag: %s", flag.Name)
+	}
 }
