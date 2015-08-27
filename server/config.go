1180f9da4b9c473478f76c878bffa7707c12a566
Brian Waldon <brian@waldon.cc>
2015-08-26 20:58:15 -0700

    server: drop unused field K8sConfig.APIVersion

--- server/config.go
+++ server/config.go
@@ -4,7 +4,6 @@ import "net/url"
 
 type K8sConfig struct {
 	Endpoint                 *url.URL
-	APIVersion               string
 	APIService               string
 	ControllerManagerService string
 	SchedulerService         string
