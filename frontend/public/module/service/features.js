ea01b93dc0fd3d1134a5aaba617106ec6b616b56
Joe Bowers <joe@coreos.com>
2015-09-23 14:56:14 -0700

    frontend: use strict in all client code

--- frontend/public/module/service/features.js
+++ frontend/public/module/service/features.js
@@ -1,5 +1,6 @@
 angular.module('bridge.service')
 .factory('featuresSvc', function($window) {
+  'use strict';
   return {
     isAuthDisabled: $window.SERVER_FLAGS.authDisabled,
     isUserManagementDisabled: $window.SERVER_FLAGS.userManagementDisabled,
