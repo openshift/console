9947242c8ff4327b287410d5bc806fe48156926c
Derek Parker <derek.parker@coreos.com>
2015-09-23 17:09:34 -0700

    frontend: Lowercase resource names
    Fixes #277

---
+++ frontend/public/module/k8s/resource_test.js
@@ -0,0 +1,24 @@
+describe('k8s.k8sResource', function() {
+  'use strict';
+  var k8s;
+
+  beforeEach(module('k8s'));
+  beforeEach(inject(function(_k8s_) {
+    k8s = _k8s_;
+  }));
+
+
+  describe('create', function() {
+    it('automatically lowercases resource name', function () {
+      var data = { metadata: { name: 'TEST' }, spec: { volumes: [] } };
+
+      inject(function($rootScope) {
+        k8s.pods.create(data);
+        // Since we're passing by reference we
+        // can simply assert about the mutation of
+        // the object here.
+        expect(data.metadata.name).toEqual('test');
+      });
+    });
+  });
+});
