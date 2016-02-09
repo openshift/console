28ddd4f435609e0104b486612f884f3a8e99cfa4
Joe Bowers <joe.bowers@coreos.com>
2016-02-09 09:24:08 -0800

    stats: remove unnecessary indirection in tests

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -2,47 +2,13 @@ package stats
 
 import "testing"
 
-var (
-	goodCfg []byte = []byte(`{
- "quay.io": {
-  "auth": "bmFtZSBwYXJ0Omdvb2Z5IHRyYWlsaW5nIHBhcnQK",
-  "email": ""
- }
-}`)
-
-	notJSONCfg []byte = []byte(`This isn't even JSON`)
-
-	weirdJSONCfg []byte = []byte(`{
- "Nope": {
-  "auth": "bmFtZSBwYXJ0Omdvb2Z5IHRyYWlsaW5nIHBhcnQK",
-  "email": ""
- }
-}`)
-
-	badBase64Cfg []byte = []byte(`{
- "quay.io": {
-  "auth": "+-_/\][bmFtZSBwYXJ0Omdvb2Z5=IHRyYWlsaW5nIHBhcnQK",
-  "email": ""
- }
-}`)
-
-	unexpectedAuthFormatCfg []byte = []byte(`{
- "quay.io": {
-  "auth": "d2l0aG91dCBhIGNvbG9uLCBub3RoaW5nIGludGVyZXN0aW5nIGNhbiBoYXBwZW4K",
-  "email": ""
- }
-}`)
-
-	emptyNameCfg []byte = []byte(`{
- "quay.io": {
-  "auth": "OmEgbGVhZGluZyBjb2xvbiBpcyBwcm9iYWJseSBjb3JydXB0Cg==",
-  "email": ""
- }
-}`)
-)
-
 func TestStatsParseGood(t *testing.T) {
-	name, err := parseNameFromDockercfg(goodCfg)
+	name, err := parseNameFromDockercfg([]byte(`{
+ "quay.io": {
+  "auth": "bmFtZSBwYXJ0Omdvb2Z5IHRyYWlsaW5nIHBhcnQK",
+  "email": ""
+ }
+}`))
 
 	if err != nil {
 		t.Errorf("Expected clean result, got: %v", err)
@@ -54,35 +20,59 @@ func TestStatsParseGood(t *testing.T) {
 }
 
 func TestStatsParseNotJSON(t *testing.T) {
-	name, err := parseNameFromDockercfg(notJSONCfg)
+	name, err := parseNameFromDockercfg([]byte(`This isn't even JSON`))
 	if err == nil {
 		t.Errorf("Expected error, got: %s", name)
 	}
 }
 
 func TestStatsParseWeirdJSON(t *testing.T) {
-	name, err := parseNameFromDockercfg(weirdJSONCfg)
+	name, err := parseNameFromDockercfg([]byte(`{
+ "Nope": {
+  "auth": "bmFtZSBwYXJ0Omdvb2Z5IHRyYWlsaW5nIHBhcnQK",
+  "email": ""
+ }
+}`))
+
 	if err == nil {
 		t.Errorf("Expected error, got: %s", name)
 	}
 }
 
 func TestStatsParseBadBase64(t *testing.T) {
-	name, err := parseNameFromDockercfg(badBase64Cfg)
+	name, err := parseNameFromDockercfg([]byte(`{
+ "quay.io": {
+  "auth": "+-_/\][bmFtZSBwYXJ0Omdvb2Z5=IHRyYWlsaW5nIHBhcnQK",
+  "email": ""
+ }
+}`))
+
 	if err == nil {
 		t.Errorf("Expected error, got: %s", name)
 	}
 }
 
 func TestStatsParseUnexpectedAuthFormat(t *testing.T) {
-	name, err := parseNameFromDockercfg(unexpectedAuthFormatCfg)
+	name, err := parseNameFromDockercfg([]byte(`{
+ "quay.io": {
+  "auth": "d2l0aG91dCBhIGNvbG9uLCBub3RoaW5nIGludGVyZXN0aW5nIGNhbiBoYXBwZW4K",
+  "email": ""
+ }
+}`))
+
 	if err == nil {
 		t.Errorf("Expected error, got: %s", name)
 	}
 }
 
 func TestStatsParseEmptyNameCfg(t *testing.T) {
-	name, err := parseNameFromDockercfg(emptyNameCfg)
+	name, err := parseNameFromDockercfg([]byte(`{
+ "quay.io": {
+  "auth": "OmEgbGVhZGluZyBjb2xvbiBpcyBwcm9iYWJseSBjb3JydXB0Cg==",
+  "email": ""
+ }
+}`))
+
 	if err == nil {
 		t.Errorf("Expected error, got: %s", name)
 	}
