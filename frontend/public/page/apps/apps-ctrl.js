7405c77da0b48fe7d1a6c3c98a3427ddcf9bfd75
Joe Bowers <joe.bowers@coreos.com>
2016-03-02 11:37:27 -0800

    frontend: remove unused code

--- frontend/public/page/apps/apps-ctrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('bridge.page')
-.controller('AppsCtrl', function($scope, $routeParams, $location, k8s) {
-  'use strict';
-
-  $scope.ns = $routeParams.ns || k8s.enum.DefaultNS;
-  $scope.defaultNS = k8s.enum.DefaultNS;
-
-});
