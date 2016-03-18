55ea4d5e35687e36bd249d575f8cd66d5e82089a
Joe Bowers <joe.bowers@coreos.com>
2016-03-18 09:23:13 -0700

    frontend: fixup - why is ns stateful?

--- frontend/public/module/filter/ns.js
+++ frontend/public/module/filter/ns.js
@@ -4,6 +4,10 @@ angular.module('bridge.filter')
   var ret = function(path) {
     return namespacesSvc.formatNamespaceRoute(path);
   };
+
+  // Because formatNamespaceRoute is stateful, this filter is
+  // stateful. If this flag isn't present, Angular will cache the
+  // filter results and navigation won't work.
   ret.$stateful = true;
 
   return ret;
