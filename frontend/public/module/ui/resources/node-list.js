a4a7fd00f6fbcc722b6b0ad7a7eff5fb8c77dd9d
Matt Kaniaris <kans@Matts-MBP.libcore.so>
2016-04-20 15:23:17 -0700

    The first commit's message is:
    Expose node TPM status:
    
    this is based on coreos-hyperkube-v1.2.0-tpm

--- frontend/public/module/ui/resources/node-list.js
+++ frontend/public/module/ui/resources/node-list.js
@@ -3,8 +3,24 @@
  * List nodes in a table-like view.
  */
 
+'use strict';
+
+function hasStatus_ (n, k8s, status) {
+  // tri-bool classic - status (ready state) is 'true', 'false', or undefined!! :(
+  if (!status) {
+    return true;
+  }
+  status = status === 'true';
+  return status === k8s.nodes.isReady(n);
+};
+
+function isNamed_ (n, name) {
+  name = name || '';
+  return -1 !== n.metadata.name.indexOf(name);
+};
+
 angular.module('bridge.ui')
-.directive('coNodeList', function(k8s, _, pkg, arraySvc, resourceMgrSvc) {
+.directive('coNodeList', function(k8s) {
   'use strict';
 
   return {
@@ -12,51 +28,32 @@ angular.module('bridge.ui')
     restrict: 'E',
     replace: true,
     scope: {
+      namefilter: '=',
+      statusFilter: '=filter',
       selector: '=',
+      trusted: '=',
+      nodes: '=',
+      compacted: '=',
     },
-    controller: function($scope, $attrs) {
+    controller: function($scope) {
       $scope.loadError = false;
+      $scope.getPodFieldSelector = k8s.pods.fieldSelectors.node;
+      $scope.isTrusted = k8s.nodes.isTrusted;
+      $scope.isReady = k8s.nodes.isReady;
+      $scope.filteredNodes = [];
 
-      function loadNodes() {
-        var query = {};
-
-        if ($attrs.selectorRequired && _.isEmpty($scope.selector)) {
-          $scope.nodes = [];
-          $scope.loadError = false;
-          return;
-        }
-
-        if (!_.isEmpty($scope.selector)) {
-          query.labelSelector = $scope.selector;
-        }
-
-        k8s.nodes.list(query)
-          .then(function(nodes) {
-            $scope.nodes = nodes;
-            $scope.loadError = false;
-          })
-          .catch(function() {
-            $scope.nodes = null;
-            $scope.loadError = true;
-          });
+      const filterNodes = () => {
+        $scope.filteredNodes = ($scope.nodes || []).filter((n) => {
+          return hasStatus_(n, k8s, $scope.statusFilter) && isNamed_(n, $scope.namefilter);
+        })
+        .sort((a, b) => {
+          return a.metadata.name - b.metadata.name;
+        });
       }
 
-      $scope.getPodFieldSelector = k8s.pods.fieldSelectors.node;
-
-      loadNodes();
-
-      $scope.getReadyStateLabel = k8s.nodes.getReadyStateLabel;
-
-      $scope.$on(k8s.events.NODE_DELETED, function(e, data) {
-        resourceMgrSvc.removeFromList($scope.nodes, data.resource);
-      });
-
-      $scope.$on(k8s.events.NODE_ADDED, loadNodes);
-
-      $scope.$on(k8s.events.NODE_MODIFIED, function(e, data) {
-        resourceMgrSvc.updateInList($scope.nodes, data.resource);
-      });
-
+      $scope.$watch('statusFilter', filterNodes);
+      $scope.$watch('namefilter', filterNodes);
+      $scope.$watch('nodes', filterNodes);
     }
   };
 
