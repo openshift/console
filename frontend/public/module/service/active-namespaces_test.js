eaf240693519d4cc0bcb15b68c29841cc04c2028
Pavel Strashkin <pavel.strashkin@gmail.com>
2016-05-17 11:25:24 -0700

    frontend: fix showing of bullet for "all" namespace
    Fixes #565

---
+++ frontend/public/module/service/active-namespaces_test.js
@@ -0,0 +1,43 @@
+describe('bridge.service.activeNamespaceSvc', function() {
+  'use strict';
+
+  var activeNamespaceSvc;
+
+  beforeEach(module('k8s'));
+  beforeEach(module('bridge.const'));
+  beforeEach(module('bridge.service'));
+
+  beforeEach(inject(function(_activeNamespaceSvc_) {
+    activeNamespaceSvc = _activeNamespaceSvc_;
+  }));
+
+  describe('#isNamespaceActive', function() {
+    describe('given active namespace', function () {
+      beforeEach(function () {
+        activeNamespaceSvc.setActiveNamespace('kube-system');
+      });
+
+      it('returns false for falsy namespace name (virtual "all" namespace)', function () {
+        expect(activeNamespaceSvc.isNamespaceActive()).toEqual(false);
+      });
+
+      it('returns true for meaningful namespace name', function () {
+        expect(activeNamespaceSvc.isNamespaceActive('kube-system')).toEqual(true);
+      });
+    });
+
+    describe('given no active namespace', function () {
+      beforeEach(function () {
+        activeNamespaceSvc.setActiveNamespace();
+      });
+
+      it('returns true for falsy namespace name (virtual "all" namespace)', function () {
+        expect(activeNamespaceSvc.isNamespaceActive()).toEqual(true);
+      });
+
+      it('returns false for meaningful namespace name', function () {
+        expect(activeNamespaceSvc.isNamespaceActive('kube-system')).toEqual(false);
+      });
+    });
+  });
+});
