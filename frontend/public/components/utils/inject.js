7ef5722bee1edc7e16c407b7b71f2c02eceee607
Matt Kaniaris <kans@Matts-MBP.libcore.so>
2016-08-30 15:02:58 -0700

    frontend: avoid stack overflow and/or other recursive strangeness

--- frontend/public/components/utils/inject.js
+++ frontend/public/components/utils/inject.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export const inject = (children, props) => {
+  const safeProps = _.omit(props, ['children']);
   return React.Children.map(children, c => {
     if (!_.isObject(c)) {
       return c;
     }
-    return React.cloneElement(c, props);
+    return React.cloneElement(c, safeProps);
   });
 };
