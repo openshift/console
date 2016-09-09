bb153430f9925589f27ead8b13a65e8bc541cae3
Stuart Olivera <stuart@stuartolivera.com>
2016-09-08 21:59:29 -0700

    frontend: update deployment overview every 5 seconds

--- frontend/public/page/deployments/deployment-ctrl.js
+++ frontend/public/page/deployments/deployment-ctrl.js
@@ -1,20 +1,26 @@
 angular.module('bridge.page')
-.controller('DeploymentCtrl', function($scope, $routeParams, k8s) {
+.controller('DeploymentCtrl', function($scope, $routeParams, k8s, $interval) {
   'use strict';
 
   $scope.ns = $routeParams.ns;
   $scope.deploymentName = $routeParams.name;
   $scope.loadError = false;
 
-  k8s.deployments.get($scope.deploymentName, $scope.ns)
-    .then(function(deployment) {
-      $scope.deployment = deployment;
-      $scope.loadError = false;
-    })
-    .catch(function() {
-      $scope.deployment = null;
-      $scope.loadError = true;
-    });
+  const update = function() {
+    k8s.deployments.get($scope.deploymentName, $scope.ns)
+      .then(function(deployment) {
+        $scope.deployment = deployment;
+        $scope.loadError = false;
+      })
+      .catch(function() {
+        $scope.deployment = null;
+        $scope.loadError = true;
+      });
+  };
+  update();
+
+  let intervalReference = $interval(update, 5000);
+  $scope.$on('$destroy', $interval.cancel.bind(null, intervalReference));
 
   $scope.getPodTemplateJson = function() {
     if (!$scope.deployment) {
