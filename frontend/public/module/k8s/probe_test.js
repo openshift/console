bba6e848d89bae048c53d876e60d27c97267306b
Stuart Olivera <stuart@stuartolivera.com>
2016-10-12 15:54:14 -0700

    frontend: add httpGet tests for liveness probe

--- frontend/public/module/k8s/probe_test.js
+++ frontend/public/module/k8s/probe_test.js
@@ -18,6 +18,28 @@ describe('k8s.k8sProbe', function () {
         expect(k8sProbe.parseCmd('tcpSocket', 'http')).toEqual({port: 'http'});
       });
     });
+
+    describe('for httpGet', function () {
+      it('returns port as number when string looks like number', function () {
+        expect(k8sProbe.parseCmd('httpGet', 'http://localhost:8080/check'))
+          .toEqual({ host: 'http://localhost', path: '/check', port: 8080 });
+      });
+
+      it('returns port as string when string does not look like number', function () {
+        expect(k8sProbe.parseCmd('httpGet', 'http://localhost:foo/check'))
+          .toEqual({ host: 'http://localhost', path: '/check', port: 'foo' });
+      });
+
+      it('returns default port 80 when no port is specified', function () {
+        expect(k8sProbe.parseCmd('httpGet', 'http://localhost/check'))
+          .toEqual({ host: 'http://localhost', path: '/check', port: 80 });
+      });
+
+      it('returns host as https when https is specified', function () {
+        expect(k8sProbe.parseCmd('httpGet', 'https://localhost:1234/check'))
+          .toEqual({ host: 'https://localhost', path: '/check', port: 1234 });
+      });
+    });
   });
 
   describe('#flattenCmd', function () {
