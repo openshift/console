232e35830868203684b2fced69cdab669f0c61ad
Geoff Greer <geoff@greer.fm>
2016-10-17 15:20:30 -0700

    frontend: Make React timestamp widget look and behave like the old Angular timestamp widget.
    Instead of just showing a fixed time, show "x seconds/minutes ago" if time is recent. Auto-update relative timestamps as they become later ("x seconds ago" becomes "y minutes ago").
    
    Part of event stream react-ification (#1057).

--- frontend/public/components/utils/timestamp.jsx
+++ frontend/public/components/utils/timestamp.jsx
@@ -1,19 +1,76 @@
 import React from 'react';
 
-export const Timestamp = ({timestamp}) => {
-  const date = new Date(timestamp);
-  const mdate = moment(date);
-  let invalidDate = false;
-  if (!mdate.isValid()) {
-    invalidDate = true;
+export class Timestamp extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.interval = null;
+    this.mdate = moment(new Date(props.timestamp));
+    this.state = {
+      timestamp: null,
+    };
+    this.timeStr();
   }
 
-  return (
-    <div>
-      {!invalidDate && <i className="fa fa-globe" />}
-      <div className="co-timestamp">
-        {invalidDate ? '-' : mdate.utc().format('MMM DD, H:mm A z')}
+  upsertState (timestamp) {
+    if (this._isMounted) {
+      this.setState({timestamp});
+    } else {
+      this.state.timestamp = timestamp;
+    }
+  }
+
+  timeStr () {
+    if (!this.mdate.isValid()) {
+      return;
+    }
+
+    const minutesAgo = moment().diff(this.mdate, 'minutes', /* return floating point value */true);
+    if (minutesAgo >= 10.5) {
+      this.upsertState(this.mdate.format('MMM DD, h:mm a'));
+      clearInterval(this.interval);
+      return;
+    }
+    // 0-14:  a few seconds ago
+    // 15-44: less than a minute ago
+    // 45-89: a minute ago
+    const secondsAgo = moment().diff(this.mdate, 'seconds', /* return floating point value */true);
+    if (secondsAgo < 45 && secondsAgo >= 15) {
+      this.upsertState('less than a minute ago');
+      return;
+    }
+    this.upsertState(this.mdate.fromNow());
+  }
+
+  componentDidMount () {
+    this._isMounted = true;
+    this.interval = setInterval(() => this.timeStr(), this.props.interval || 5000);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval);
+  }
+
+  render () {
+    const mdate = this.mdate;
+    // Calling mdate.utc() modifies mdate to be UTC. :(
+    const utcdate = moment.utc(mdate);
+
+    if (!mdate.isValid()) {
+      return (
+        <div>
+          <div className="co-timestamp">-</div>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <i className="fa fa-globe" />
+        <div className="co-timestamp" title={utcdate.format('MMM DD, H:mm A z')}>
+          {this.state.timestamp}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
