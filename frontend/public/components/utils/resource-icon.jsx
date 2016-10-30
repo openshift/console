9a211647ecc7f86295fe7043ce44c46bfde749b2
Andy Pickering <andy.nihon@gmail.com>
2016-10-30 15:01:46 +0900

    frontend: Use classNames()
    Let's use classnames() here, like we generally do in other components.

--- frontend/public/components/utils/resource-icon.jsx
+++ frontend/public/components/utils/resource-icon.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import {angulars} from '../react-wrapper';
 import iconLabel from '../../module/ui/icons/resource-icon';
 
 export const ResourceIcon = ({kind, className}) => {
   const label = kind === '*' ? '*' : iconLabel(angulars.k8s.enum.Kind, kind);
-  const klass = ['co-m-resource-icon', `co-m-resource-icon--${kind}`, className].join(' ');
+  const klass = classNames(`co-m-resource-icon co-m-resource-icon--${kind}`, className);
   return <span className={klass}>{label}</span>;
 };
