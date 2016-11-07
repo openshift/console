1468261ca01929d7a3221fbbcc9a2c3fc4a1134b
Stuart Olivera <stuart@stuartolivera.com>
2016-11-07 11:42:25 -0800

    frontend: notify if license has expired or will soon [Fixes #133216891]

---
+++ frontend/public/components/global-notification.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const GlobalNotification = ({title, content}) => <div className="co-global-notification">
+  <div className="co-global-notification__sidebar">
+    <p className="co-global-notification__text">{title}</p>
+  </div>
+  <div className="co-global-notification__content">
+    <p className="co-global-notification__text">{content}</p>
+  </div>
+</div>;
+GlobalNotification.propTypes = {
+  title: React.PropTypes.node,
+  content: React.PropTypes.node
+};
