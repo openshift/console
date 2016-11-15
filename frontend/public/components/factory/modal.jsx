2dd9eda9f462957332aaf1e6077cc9e52ec460a0
Stuart Olivera <stuart@stuartolivera.com>
2016-11-15 13:55:47 -0800

    frontend: remove duplicate .modal-content div from errorModal

--- frontend/public/components/factory/modal.jsx
+++ frontend/public/components/factory/modal.jsx
@@ -19,16 +19,14 @@ export const errorModal = createModalLauncher(
   ({error, cancel}) => {
     return (
       <div role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title">Error</h1>
-          </div>
-          <div className="modal-body">
-            {error}
-          </div>
-          <div className="modal-footer">
-            <button type="button" onClick={cancel} className="btn btn-default">OK</button>
-          </div>
+        <div className="modal-header">
+          <h1 className="modal-title">Error</h1>
+        </div>
+        <div className="modal-body">
+          {error}
+        </div>
+        <div className="modal-footer">
+          <button type="button" onClick={cancel} className="btn btn-default">OK</button>
         </div>
       </div>
     );
