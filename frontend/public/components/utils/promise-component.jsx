da1e805a3dc033e3590fcfc1c81d6935627b2674
Stuart Olivera <stuart@stuartolivera.com>
2016-11-18 10:58:37 -0800

    frontend: support promise components w/o extra state
    Some components that extend PromiseComponent won’t use their own state, and therefore won’t initialize state in their constructor. Therefore we need to make sure state exists in the base class itself.

--- frontend/public/components/utils/promise-component.jsx
+++ frontend/public/components/utils/promise-component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export class PromiseComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
     this.requestPromise = null;
   }
 
