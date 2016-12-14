88ad6f6ecde27c48e6d35d5fa4e71d0088f6af69
Andy Pickering <andy.nihon@gmail.com>
2016-12-14 15:30:57 +0900

    frontend: Use React NavBar component on node pages

--- frontend/public/page/nodes/node-sysevents-ctrl.js
+++ frontend/public/page/nodes/node-sysevents-ctrl.js
@@ -2,6 +2,13 @@ angular.module('bridge.page')
 .controller('nodeSyseventsCtrl', function($scope, $routeParams) {
   'use strict';
   $scope.nodeName = $routeParams.name;
+  $scope.navProps = {
+    pages: [
+      {name: 'Overview', href: 'details'},
+      {name: 'Pods', href: 'pods'},
+      {name: 'Events', href: 'events'},
+    ]
+  };
   $scope.props = {
     filter: {
       name: $scope.nodeName,
--- frontend/public/page/nodes/nodes.js
+++ frontend/public/page/nodes/nodes.js
@@ -1,3 +1,2 @@
-import './node-nav';
 import './pods-ctrl';
 import './node-sysevents-ctrl';
