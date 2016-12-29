1a1b5c6d2fdb7e508125260b9871a011482c352f
Andy Pickering <andy.nihon@gmail.com>
2016-12-29 13:14:41 +0900

    frontend: Migrate Profile (My Account) page to React

--- frontend/public/page/pages.js
+++ frontend/public/page/pages.js
@@ -13,7 +13,6 @@ import './deployments/controllers';
 import './pods/pods';
 import './nodes/nodes';
 import './search/search-ctrl';
-import './settings/settings';
 import './modals/modals';
 import './rules/rules-ctrl';
 
