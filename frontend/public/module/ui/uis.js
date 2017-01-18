81f2bcde5069a38333d08457ef36cc5737ab8b72
Andy Pickering <andy.nihon@gmail.com>
2017-01-18 12:52:05 +0900

    frontend: Remove unused coSelector

--- frontend/public/module/ui/uis.js
+++ frontend/public/module/ui/uis.js
@@ -4,7 +4,6 @@ import './containers/multi-container-input';
 import './forms/number-spinner';
 import './labels/label';
 import './labels/label-input';
-import './selector/selector';
 import './selector/selector-input';
 import './selector/selector-input-service';
 import './nav-title/nav-title';
