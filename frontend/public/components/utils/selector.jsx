c3b734f16c4124991df8c6b8493e50d450e1d21d
Andy Pickering <andy.nihon@gmail.com>
2017-02-17 10:03:11 +0900

    frontend: Fix style of pod's node selector to use the selector style

--- frontend/public/components/utils/selector.jsx
+++ frontend/public/components/utils/selector.jsx
@@ -5,13 +5,13 @@ import classnames from 'classnames';
 import {toRequirements} from '../../module/k8s/selector';
 import {toString} from '../../module/k8s/selector-requirement';
 
-const Requirement = ({requirement, withIcon}) => {
+const Requirement = ({kind, requirement, withIcon}) => {
   const requirementAsString           = toString(requirement);
   const requirementAsUrlEncodedString = encodeURIComponent(requirementAsString);
 
   return (
     <div className="co-m-requirement">
-      <Link className="co-m-requirement__link" to={`search?kind=pod&q=${requirementAsUrlEncodedString}`}>
+      <Link className={`co-m-requirement__${kind}-link`} to={`search?kind=${kind}&q=${requirementAsUrlEncodedString}`}>
         { withIcon &&
           <span>
             <i className="fa fa-search"></i>&nbsp;
@@ -23,12 +23,12 @@ const Requirement = ({requirement, withIcon}) => {
   );
 };
 
-export const Selector = ({expand, selector}) => {
+export const Selector = ({kind, expand, selector}) => {
   const requirements = toRequirements(selector || {});
 
   const reqs = _.map(requirements, (requirement, i) => {
     const className = classnames({'co-m-requirement--last': i === requirements.length - 1});
-    return <Requirement key={i} className={className} requirement={requirement} withIcon={i === 0} />;
+    return <Requirement key={i} className={className} kind={kind || 'pod'} requirement={requirement} withIcon={i === 0} />;
   });
 
   const className = classnames('co-m-selector', {'co-m-selector--expand': expand});
