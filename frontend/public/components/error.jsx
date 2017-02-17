dc4ba1bf73ecbcaa97f8ecca2389c459b682f91d
Matt Kaniaris <mkaniaris@gmail.com>
2017-02-16 16:39:15 -0800

    frontend: minor cleanup...
    one export/one use

--- frontend/public/components/error.jsx
+++ frontend/public/components/error.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import {getMessage} from '../module/service/message.js';
 import {NavTitle} from './utils';
 
+const messages = {
+  auth: {
+    'invalid_code': 'There was an error logging you in. Please log out and try again.',
+    'default': 'There was an authentication error with the system. Please try again or contact support.',
+    'logout_error': 'There was an error logging you out. Please try again.',
+  },
+};
+
+const getMessage = (type, id) => _.get(messages, `${type}.${id}`) || _.get(messages, `${type}.default`) || '';
 const urlMessage = ({error_type: type, error}) => (type && error) ? getMessage(type, error) : '';
 
 const Error = ({title, message}) => <div>
