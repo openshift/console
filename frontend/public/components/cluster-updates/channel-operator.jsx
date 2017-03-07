d05a0c4c055fc124d5195bb8cc3a4471f49b61de
Amruta Chitnis <amruta.chitnis@coreos.com>
2017-03-07 13:05:08 -0800

    frontend: Refactor Cluster Updates components. -Cleanups in TectonicChannel Component -Splits ChannelOperators in separate components -Moves channel state related logic to separate utils file -Handles loadErrors in either appversions or configs

--- frontend/public/components/cluster-updates/channel-operator.jsx
+++ frontend/public/components/cluster-updates/channel-operator.jsx
@@ -1,47 +1,103 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import {LoadingInline} from '../utils';
+import {LoadingInline, taskStatuses, OperatorState, operatorStates, calculateChannelState, StatusBox} from '../utils';
 import {configureOperatorStrategyModal} from '../modals';
 import {DetailConfig} from './detail-config';
 import {DetailStatus} from './detail-status';
 import {SafetyFirst} from '../safety-first';
 
-export const states = {
-  FAILED: 'failed',
-  LOADING: 'loading',
-  NEEDS_ATTENTION: 'needsattention',
-  PAUSED: 'paused',
-  PAUSING: 'pausing',
-  REQUESTED: 'requested',
-  UPDATE_AVAILABLE: 'updateavailable',
-  UPDATING: 'updating',
-  UP_TO_DATE: 'uptodate'
+const Header = ({channelState, primaryComponent, expanded, onClick}) => {
+  return <div className="co-cluster-updates__heading">
+    <div className="co-cluster-updates__heading--name-wrapper">
+      <span className="co-cluster-updates__heading--name">Tectonic</span>
+      { expanded || <span className="co-cluster-updates__heading--version">{primaryComponent.currentVersion}</span> }
+    </div>
+    { expanded ||
+      <div className="co-cluster-updates__heading--updates">
+        <OperatorState state={channelState} version={primaryComponent.desiredVersion} />
+      </div>
+    }
+    <a className="co-cluster-updates__toggle" href="#" onClick={onClick}>{expanded ? 'Collapse' : 'Expand'}</a>
+  </div>;
 };
 
-export const componentStates = {
-  COMPLETE: 'complete',
-  FAILED: 'failed',
-  LOADING: 'loading',
-  NEEDS_ATTENTION: states.NEEDS_ATTENTION,
-  PAUSED: states.PAUSED,
-  PENDING: 'pending',
-  UPDATING: states.UPDATING
+const DetailWrapper = ({title, spacer, children}) => {
+  const spacerClass = spacer ? 'co-cluster-updates__detail--spacer' : null;
+  return <dl className={classNames('co-cluster-updates__detail', spacerClass)}>
+    { spacer || <dt>{title}</dt> }
+    { spacer || <dd>{children}</dd> }
+  </dl>;
+};
+DetailWrapper.propTypes = {
+  title: React.PropTypes.node,
+  spacer: React.PropTypes.bool,
+  children: React.PropTypes.node
 };
 
-export const taskStatuses = {
-  'BACKOFF': 'backoff',
-  'COMPLETED': 'completed',
-  'FAILED': 'failed',
-  'NOTSTARTED': 'notstarted',
-  'RUNNING': 'running'
+const Details = ({config, channelState, primaryComponent}) => {
+  if (config.loadError) {
+    return  <div className="co-cluster-updates__details">
+      <DetailWrapper title="Current Version">
+        {primaryComponent.currentVersion || <LoadingInline />}
+      </DetailWrapper>
+    </div>;
+  }
+
+  return <div className="co-cluster-updates__details">
+    <DetailWrapper title="Status">
+      <DetailStatus config={config} state={channelState} version={primaryComponent.desiredVersion} />
+    </DetailWrapper>
+    <DetailWrapper title="Current Version">
+      {primaryComponent.currentVersion || <LoadingInline />}
+    </DetailWrapper>
+    <DetailWrapper title="Channel">
+      <DetailConfig config={config} field="channel" displayFunction={_.capitalize} />
+    </DetailWrapper>
+    <DetailWrapper title="Strategy">
+      <DetailConfig config={config} field="automaticUpdate" modal={configureOperatorStrategyModal}
+        modalData={{updateAvailable: channelState === 'UpdateAvailable'}}
+        displayFunction={(value) => value ? 'Automatic' : 'Admin Approval'} />
+    </DetailWrapper>
+    <DetailWrapper spacer={true} />
+  </div>;
 };
 
-export const getFailedTypeClassName = (type) => {
-  return _.includes(['Human decision needed', 'Voided warranty'], type) ? 'warning' : 'failed';
+const FailureStatus = ({failureStatus}) => {
+  if (!failureStatus) {
+    return null;
+  }
+  const type = _.includes(['Human decision needed', 'Voided warranty'], failureStatus.type) ? 'warning' : 'failed';
+  return  <div className={`co-cluster-updates__message-box co-cluster-updates__message-box--${type}`}>
+    <span>
+      {failureStatus.type} : {failureStatus.reason}
+    </span>
+  </div>;
 };
 
-export class TaskStatusComponent extends React.Component {
+const Operator = ({component, cols, isPrimaryComponent}) => {
+  const {state, headerText, logsUrl} = component;
+  const suffix = _.get(operatorStates[state], 'suffix', '');
+  const icon = _.get(operatorStates[state], 'icon', '');
+
+  return <div className={`co-cluster-updates__operator-component col-xs-${cols}`}>
+    <div className="co-cluster-updates__operator-step">
+      {(state === 'Complete' || state === 'Pending') && <div className={`co-cluster-updates__operator-icon co-cluster-updates__operator-icon--${suffix}`}>
+        <span className={classNames('fa fa-fw', icon)}></span>
+      </div>}
+      <div className="co-cluster-updates__operator-text">{headerText}</div>
+    </div>
+    {state !== 'Complete' && logsUrl && <div><a className="co-cluster-updates__breakdown-button btn btn-default" href={logsUrl}>View Logs</a></div>}
+    {_.map(component.taskStatuses, (taskStatus, index) => <TaskStatus taskStatus={taskStatus} key={index} isPrimaryComponent={isPrimaryComponent} /> )}
+  </div>;
+};
+Operator.propTypes = {
+  component: React.PropTypes.object,
+  cols: React.PropTypes.number,
+  isPrimaryComponent: React.PropTypes.bool
+};
+
+export class TaskStatus extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -51,90 +107,32 @@ export class TaskStatusComponent extends React.Component {
 
   render() {
     const {name, state, reason} = this.props.taskStatus;
-    const lowerCaseState = state.toLowerCase();
+    const suffix = _.get(taskStatuses[state], 'suffix', '');
+    const icon = _.get(taskStatuses[state], 'icon', '');
 
-    const taskStatusClassName = {
-      'fa-circle-o-notch fa-spin': lowerCaseState === taskStatuses.RUNNING,
-      'fa-ban': lowerCaseState === taskStatuses.FAILED || lowerCaseState === taskStatuses.BACKOFF,
-      'fa-check-circle': lowerCaseState === taskStatuses.COMPLETED,
-      'fa-circle-o': lowerCaseState === taskStatuses.NOTSTARTED,
-    };
-
-    return <div className="co-cluster-updates__breakdown-ts-component">
-      <div className="co-cluster-updates__breakdown-ts-step">
-        <div className={`co-cluster-updates__breakdown-icon co-cluster-updates__breakdown-ts-icon--${lowerCaseState}`}>
-          <span className={classNames('fa fa-fw', taskStatusClassName)}></span>
+    return <div className="co-cluster-updates__operator-ts-component">
+      <div className="co-cluster-updates__operator-ts-step">
+        <div className={`co-cluster-updates__operator-icon co-cluster-updates__operator-ts--${suffix}`}>
+          <span className={classNames('fa fa-fw', icon)}></span>
         </div>
-        <div className={`co-cluster-updates__breakdown-text co-cluster-updates__breakdown-ts-text--${lowerCaseState}`}>{name}</div>
+        <div className={`co-cluster-updates__operator-text co-cluster-updates__operator-ts--${suffix}`}>
+          {name}
+        </div>
       </div>
-      { reason && !this.props.isPrimaryComponent && <div className="co-cluster-updates__breakdown-ts-error-msg-link">
+      { reason && !this.props.isPrimaryComponent && <div className="co-cluster-updates__operator-ts-error-msg-link">
           <a onClick={() => {this.setState({isErrorMsgVisible : !this.state.isErrorMsgVisible});}}>
             {this.state.isErrorMsgVisible ? 'Hide Failed Reason' : 'Show Failed Reason'}
           </a>
         </div>
       }
-      { reason && !this.props.isPrimaryComponent && this.state.isErrorMsgVisible && <div className="co-cluster-updates__breakdown-ts-error-msg">{reason}</div> }
+      { reason && !this.props.isPrimaryComponent && this.state.isErrorMsgVisible && <div className="co-cluster-updates__operator-ts-error-msg">{reason}</div> }
     </div>;
   }
 }
 
-const BreakdownComponent = ({component, cols, isPrimaryComponent}) => {
-
-  const {state, headerText, logsUrl} = component;
-
-  const componentStateClassName = {
-    'fa-circle-o': state === componentStates.PENDING,
-    'fa-pause-circle-o': state === componentStates.PAUSED,
-    'fa-circle-o-notch fa-spin': state === componentStates.UPDATING,
-    'fa-exclamation-triangle': state === componentStates.NEEDS_ATTENTION,
-    'fa-check-circle': state === componentStates.COMPLETE,
-    'fa-ban': state === componentStates.FAILED
-  };
-
-  return <div className={`co-cluster-updates__breakdown-component col-xs-${cols}`}>
-    <div className="co-cluster-updates__breakdown-step">
-      { (state === componentStates.COMPLETE || state === componentStates.PENDING) && <div className={`co-cluster-updates__breakdown-icon co-cluster-updates__breakdown-icon--${state}`}>
-        <span className={classNames('fa fa-fw', componentStateClassName)}></span>
-      </div> }
-      <div className="co-cluster-updates__breakdown-text">{headerText}</div>
-    </div>
-    { state !== componentStates.COMPLETE && logsUrl && <div><a className="co-cluster-updates__breakdown-button btn btn-default" href={logsUrl}>View Logs</a></div> }
-    { _.map(component.taskStatuses, (taskStatus, index) => <TaskStatusComponent taskStatus={taskStatus} key={index} isPrimaryComponent={isPrimaryComponent} /> ) }
-  </div>;
-};
-BreakdownComponent.propTypes = {
-  component: React.PropTypes.object,
-  cols: React.PropTypes.number,
-  isPrimaryComponent: React.PropTypes.bool
-};
-
-const Detail = ({title, spacer, children}) => {
-  const spacerClass = spacer ? 'co-cluster-updates__detail--spacer' : null;
-  return <dl className={classNames('co-cluster-updates__detail', spacerClass)}>
-    { spacer || <dt>{title}</dt> }
-    { spacer || <dd>{children}</dd> }
-  </dl>;
-};
-Detail.propTypes = {
-  title: React.PropTypes.node,
-  spacer: React.PropTypes.bool,
-  children: React.PropTypes.node
-};
-
-// ChannelOperator is intended to be a generic UI component that
-// takes in pre-parsed data and displays it in a consistent manner,
-// regardless of which channel (Tectonic or Container Linux) is
-// being displayed.
-//
-// Ideally, channels operate exactly the same - they all share
-// the same updated/updating/paused/etc states and basic
-// config parameters. ChannelOperator aggregates those states,
-// versions, & configs into the UI.
-//
-// However, ChannelOperator and it's sub-components ended up being
-// built a little less generic than originally intended, so there
-// is a bit of refactoring to be done when the Container Linux
-// channel gets built.
+//Displays details about Channel Operators
+//Primary operator is Tectonic Channel Operator
+//The other operator currently shown is Kubertenes Version Operator
 export class ChannelOperator extends SafetyFirst{
   constructor(props) {
     super(props);
@@ -149,57 +147,30 @@ export class ChannelOperator extends SafetyFirst{
   _initialize(props = this.props) {
     const newState = _.defaults({}, props, {
       primaryComponent: {},
-      state: states.LOADING,
-      updateChannel: null,
+      channelState: 'Loading',
       components: {},
-      updateStrategy: null,
-      allComponents: null
+      allComponents: null,
+      expanded: true
     });
 
-    newState.primaryComponent = _.get(newState.components, props.primaryComponent) || {};
+    if (!props.components.loadError) {
+      newState.primaryComponent = _.get(newState.components, props.primaryOperatorName) || {};
 
-    newState.allComponents = Object.keys(newState.components).reduce((components, key) => {
-      components.push(newState.components[key]);
-      return components;
-    }, []);
-
-    newState.components = Object.keys(newState.components).reduce((components, key) => {
-      if (key !== props.primaryComponent) {
+      newState.allComponents = Object.keys(newState.components).reduce((components, key) => {
         components.push(newState.components[key]);
-      }
-      return components;
-    }, []);
+        return components;
+      }, []);
 
-    const aggregateState = newState.allComponents.reduce((state, component) => {
-      state[component.state] = state[component.state] || 0;
-      state[component.state] += 1;
-      return state;
-    }, {});
+      newState.components = Object.keys(newState.components).reduce((components, key) => {
+        if (key !== props.primaryOperatorName) {
+          components.push(newState.components[key]);
+        }
+        return components;
+      }, []);
 
-    let channelState;
-    if (newState.components.length === 0) {
-      channelState = states.LOADING;
-    } else {
-      const priority = [componentStates.NEEDS_ATTENTION, componentStates.UPDATING, componentStates.PENDING];
-      channelState = _.find(priority, (priority) => aggregateState[priority] > 0);
-
-      // "pending" doesn't match a channel state
-      if (channelState === componentStates.PENDING) {
-        channelState = states.UPDATE_AVAILABLE;
-      }
-
-      if (newState.primaryComponent.pausedStatus && newState.primaryComponent.pausedSpec) {
-        channelState = states.PAUSED;
-      } else if (newState.primaryComponent.pausedSpec) {
-        channelState = states.PAUSING;
-      } else if (newState.primaryComponent.failureStatus) {
-        channelState = states.FAILED;
-      } else if (props.config && props.config.triggerUpdate && channelState === states.UPDATE_AVAILABLE) {
-        channelState = states.REQUESTED;
-      }
+      newState.channelState = newState.components.length === 0 ? 'Loading' : calculateChannelState(newState.allComponents, newState.primaryComponent, props.config);
     }
 
-    newState.state = channelState || states.UP_TO_DATE;
     if (this.isMounted_) {
       this.setState(newState);
     } else {
@@ -215,85 +186,36 @@ export class ChannelOperator extends SafetyFirst{
     event.target.blur();
   }
 
-  _isState(s) {
-    return this.state.state === s;
-  }
-
   render() {
-    let statusText;
     const operatorCols = Math.floor(12/this.state.components.length);
     const failureStatus = this.state.primaryComponent.failureStatus;
 
-    if (this._isState(states.UPDATING)) {
-      statusText = <span><span className="co-cluster-updates__breakdown-icon--updating"><span className="fa fa-circle-o-notch fa-spin fa-fw co-cluster-updates__text-icon"></span></span>Updating</span>;
-    } else if (this._isState(states.UPDATE_AVAILABLE)) {
-      statusText = <span className="co-cluster-updates__update-available-text"><span className="fa fa-arrow-circle-down fa-fw co-icon-space-r"></span>{this.state.primaryComponent.desiredVersion} is available</span>;
-    } else if (this._isState(states.UP_TO_DATE)) {
-      statusText = 'Up to date';
-    } else if (this._isState(states.NEEDS_ATTENTION)) {
-      statusText = <span className="co-cluster-updates__needs-attention-text"><span className="fa fa-exclamation-triangle fa-fw co-icon-space-r"></span>Update Needs Attention</span>;
-    } else if (this._isState(states.PAUSING)) {
-      statusText = 'Pausing...';
-    } else if (this._isState(states.PAUSED)) {
-      statusText = 'Paused';
-    } else if (this._isState(states.REQUESTED)) {
-      statusText = 'Update requested...';
-    } else if (this._isState(states.LOADING)) {
-      statusText = <LoadingInline />;
-    } else if (this._isState(states.FAILED)) {
-      statusText = <span className="co-cluster-updates__update-failed-text"><span className="fa fa-ban co-icon-space-r"></span>Software update failed</span>;
-    } else {
-      statusText = 'Unknown';
+    if (this.props.components.loadError) {
+      return <StatusBox loadError={this.props.components.loadError} label="Operators" />;
     }
 
-    return <div className={classNames('co-cluster-updates__component', {'co-cluster-updates__component--last': this.props.last})}>
-      <div className="co-cluster-updates__heading">
-        <div className="co-cluster-updates__heading--name-wrapper">
-          <span className="co-cluster-updates__heading--name">{this.props.type}</span>
-          { this.state.expanded || <span className="co-cluster-updates__heading--version">{this.state.primaryComponent.currentVersion}</span> }
-        </div>
-        { this.state.expanded ||
-          <div className="co-cluster-updates__heading--updates">
-            {statusText}
-          </div>
-        }
-        <a className="co-cluster-updates__toggle" href="#" onClick={this._toggleExpand}>{this.state.expanded ? 'Collapse' : 'Expand'}</a>
-      </div>
+    return <div>
+      <Header channelState={this.state.channelState}
+        primaryComponent={this.state.primaryComponent}
+        expanded={this.state.expanded}
+        onClick={this._toggleExpand} />
       { this.state.expanded &&
-        <div className="co-cluster-updates__details">
-          <Detail title="Status">
-            <DetailStatus config={this.state.config} statusText={statusText} state={this.state.state} />
-          </Detail>
-          <Detail title="Current Version">
-            {this.state.primaryComponent.currentVersion || <LoadingInline />}
-          </Detail>
-          <Detail title="Channel">
-            <DetailConfig config={this.state.config} field="channel" displayFunction={_.capitalize} />
-          </Detail>
-          <Detail title="Strategy">
-            <DetailConfig config={this.state.config} field="automaticUpdate" modal={configureOperatorStrategyModal} modalData={{updateAvailable: this._isState(states.UPDATE_AVAILABLE)}} displayFunction={(value) => value ? 'Automatic' : 'Admin Approval'} />
-          </Detail>
-          <Detail spacer={true} />
-        </div>
-      }
-      { this.state.expanded && failureStatus &&
-        <div className={`co-cluster-updates__message-box co-cluster-updates__message-box--${getFailedTypeClassName(failureStatus.type)}`}>
-          <span>
-            {failureStatus.type} : {failureStatus.reason}
-          </span>
-        </div>
-      }
-      { this.state.expanded && this.state.primaryComponent && this.state.components.length > 0 &&
-        <div className="co-cluster-updates__breakdown">
-          <BreakdownComponent component={this.state.primaryComponent} cols={operatorCols} isPrimaryComponent={true}/>
-          { _.map(this.state.components, (component, index) => <BreakdownComponent component={component} key={index} cols={operatorCols} isPrimaryComponent={false} /> )}
+        <div>
+          <Details config={this.state.config}
+            channelState={this.state.channelState}
+            primaryComponent={this.state.primaryComponent} />
+          <FailureStatus failureStatus={failureStatus} />
+          <div className="co-cluster-updates__operator">
+            {this.state.primaryComponent && <Operator component={this.state.primaryComponent} cols={operatorCols} isPrimaryComponent={true}/>}
+            {this.state.components.length > 0 && _.map(this.state.components, (component, index) => <Operator component={component} key={index} cols={operatorCols} isPrimaryComponent={false} /> )}
+          </div>
         </div>
       }
     </div>;
   }
 }
 ChannelOperator.propTypes = {
+  primaryOperatorName: React.PropTypes.string,
   config: React.PropTypes.object,
-  last: React.PropTypes.bool,
-  type: React.PropTypes.string.isRequired
+  components: React.PropTypes.object,
 };
--- frontend/public/components/cluster-updates/tectonic-channel.jsx
+++ frontend/public/components/cluster-updates/tectonic-channel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 
-import {MultiFirehose} from '../utils';
-import {ChannelOperator, componentStates} from './channel-operator';
+import {MultiFirehose, determineOperatorState} from '../utils';
+import {ChannelOperator} from './channel-operator';
 import {SafetyFirst} from '../safety-first';
 import * as k8sSelector from '../../module/k8s/selector';
 
@@ -17,6 +18,38 @@ const podNames = {
 
 const clusterAppVersionName = 'tectonic-cluster';
 
+const generateComponents = (components, pods) => {
+  return Object.keys(components).reduce((finalComponents, key) => {
+    const component = components[key];
+
+    //the component has targetVersion if in the process of an upgrade.
+    if (component.currentVersion && (component.desiredVersion || component.targetVersion)) {
+      let logsUrl;
+      const name = componentNames[key] || key;
+      const headerText = <span>{name} {component.currentVersion} &#10141; {component.desiredVersion || component.targetVersion}</span>;
+      const state = determineOperatorState(component);
+      const pod = _.find(pods, p => p.metadata.name.indexOf(podNames[key]) > -1);
+      if (pod) {
+        logsUrl = `ns/tectonic-system/pods/${pod.metadata.name}/logs`;
+      }
+      finalComponents[key] = {
+        currentVersion: component.currentVersion,
+        desiredVersion: component.desiredVersion,
+        targetVersion: component.targetVersion,
+        pausedSpec: component.pausedSpec,
+        pausedStatus: component.pausedStatus,
+        taskStatuses: component.taskStatuses,
+        failureStatus: component.failureStatus,
+        state,
+        logsUrl,
+        headerText
+      };
+    }
+
+    return finalComponents;
+  }, {});
+};
+
 // Consumes component data (TCO third party resources) and
 // prepares it for ChannelOperator to display.
 // This is responsible for determining which components
@@ -28,12 +61,6 @@ export class TectonicChannel extends SafetyFirst {
     super(props);
     const selector = k8sSelector.fromString(`k8s-app in (${_.values(podNames).join()})`);
     this.firehoseResources = [
-      {
-        kind: 'tectonicversion',
-        namespace: 'tectonic-system',
-        isList: true,
-        prop: 'tectonicVersions'
-      },
       {
         kind: 'channeloperatorconfig',
         namespace: 'tectonic-system',
@@ -64,7 +91,6 @@ export class TectonicChannel extends SafetyFirst {
   }
 }
 TectonicChannel.propTypes = {
-  expanded: React.PropTypes.bool,
   last: React.PropTypes.bool
 };
 
@@ -72,9 +98,8 @@ class TectonicChannelWithData extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tectonicVersion: {},
       config: null,
-      components: {}
+      components: null
     };
   }
 
@@ -85,14 +110,21 @@ class TectonicChannelWithData extends React.Component {
       newState.config = _.get(nextProps.configs, 'data[0]');
     }
 
-    if (nextProps.appVersions.loaded && nextProps.tectonicVersions.loaded && nextProps.pods.loaded) {
-      newState.pods = nextProps.pods.data;
-      const tectonicAppVersion = _.find(nextProps.appVersions.data, ['metadata.name', clusterAppVersionName]);
-      const currentVersion = _.get(tectonicAppVersion, 'spec.desiredVersion', '');
-      newState.tectonicVersion = _.find(nextProps.tectonicVersions.data, ['version', currentVersion]) || this.state.tectonicVersion || {};
+    if (nextProps.appVersions.loaded && nextProps.pods.loaded) {
+      const components = nextProps.appVersions.data.reduce(this._createComponentFromData.bind(this), {});
+      newState.components = generateComponents(components, nextProps.pods.data);
+    }
 
-      const desiredVersions = newState.tectonicVersion.desiredVersions || [];
-      newState.components = nextProps.appVersions.data.reduce(this._createComponentFromData.bind(this, desiredVersions), {});
+    if (nextProps.configs.loadError) {
+      newState.config = {
+        loadError : nextProps.configs.loadError
+      };
+    }
+
+    if (nextProps.appVersions.loadError) {
+      newState.components = {
+        loadError: nextProps.appVersions.loadError
+      };
     }
 
 
@@ -102,7 +134,7 @@ class TectonicChannelWithData extends React.Component {
   // Plucks information from third party resources. Uses the
   // desired version from the Tectonic Channel Operator instead
   // of the one on individual operator resources.
-  _createComponentFromData(desiredVersions, components, component) {
+  _createComponentFromData(components, component) {
     const name = component.metadata.name;
 
     components[name] = {
@@ -118,53 +150,14 @@ class TectonicChannelWithData extends React.Component {
     return components;
   }
 
-  _generateComponents() {
-    return Object.keys(this.state.components).reduce((finalComponents, key) => {
-      const component = this.state.components[key];
-
-      //the component has targetVersion if in the process of an upgrade.
-      if (component.currentVersion && (component.desiredVersion || component.targetVersion)) {
-        let state, logsUrl;
-        const name = componentNames[key] || key;
-        const headerText = <span>{name} {component.currentVersion} &#10141; {component.desiredVersion || component.targetVersion}</span>;
-        const pod = _.find(this.state.pods, p => p.metadata.name.indexOf(podNames[key]) > -1);
-        if (pod) {
-          logsUrl = `ns/tectonic-system/pods/${pod.metadata.name}/logs`;
-        }
-
-        if (component.failureStatus) {
-          state = componentStates.FAILED;
-        } else if (component.targetVersion) {
-          state = componentStates.UPDATING;
-        } else if (component.currentVersion !== component.desiredVersion) {
-          state = componentStates.PENDING;
-        } else {
-          state = componentStates.COMPLETE;
-        }
-
-        if (component.pausedStatus) {
-          state = componentStates.PAUSED;
-        }
-
-        finalComponents[key] = {
-          currentVersion: component.currentVersion,
-          desiredVersion: component.desiredVersion,
-          targetVersion: component.targetVersion,
-          pausedSpec: component.pausedSpec,
-          pausedStatus: component.pausedStatus,
-          taskStatuses: component.taskStatuses,
-          failureStatus: component.failureStatus,
-          state,
-          logsUrl,
-          headerText
-        };
-      }
-
-      return finalComponents;
-    }, {});
-  }
-
   render() {
-    return <ChannelOperator type="Tectonic" primaryComponent={clusterAppVersionName} components={this._generateComponents()} config={this.state.config} last={this.props.last} expanded={this.props.expanded} />;
+    return <div className={classNames('co-cluster-updates__component', {'co-cluster-updates__component--last': this.props.last})}>
+      {this.state.config && this.state.components &&
+        <ChannelOperator
+          primaryOperatorName={clusterAppVersionName}
+          components={this.state.components}
+          config={this.state.config} />
+      }
+    </div>;
   }
 }
