7cbe5414e3ee9459ee6b5a7e6a59f18075a990aa
Amruta Chitnis <amruta.chitnis@coreos.com>
2017-03-21 17:09:28 -0700

    frontend: Reorganizes channel operator

--- frontend/public/components/cluster-updates/cluster-updates.jsx
+++ frontend/public/components/cluster-updates/cluster-updates.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {TectonicChannel} from './tectonic-channel';
+import {TectonicChannel} from '../channel-operators/tectonic-channel';
 
 export const ClusterUpdates = () => <div className="co-cluster-updates">
   <TectonicChannel last={true} />
