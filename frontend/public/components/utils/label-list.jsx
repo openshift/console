4508f072b82bcf2532d9acfeb99c254f511b23d3
Andy Pickering <andy.nihon@gmail.com>
2017-06-21 11:04:35 +0900

    frontend: Refactor away linkEncode()
    Was only being used by the Label component.

--- frontend/public/components/utils/label-list.jsx
+++ frontend/public/components/utils/label-list.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import { Link } from 'react-router';
 import classNames from 'classnames';
 
-import {linkEncode} from '../../module/k8s/labels';
-
 const Label = ({kind, name, value, expand}) => {
-  const labelObj = {[name]: value};
-  const query = linkEncode(labelObj);
-  const href = `search?kind=${kind}&q=${query}`;
+  const href = `search?kind=${kind}&q=${name}%3D${value}`;
   const klass = classNames('co-m-label', {'co-m-label--expand': expand});
 
   return (
--- frontend/public/module/k8s/labels.js
+++
@@ -1,6 +0,0 @@
-export const linkEncode = labels => {
-  var result = _.map(_.keys(labels), function(key) {
-    return `${key}%3D${labels[key]}`;
-  });
-  return result.join(',');
-};
