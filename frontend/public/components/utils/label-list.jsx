3699768e520cca57325f67dbf2de04faa89de096
Andy Pickering <andy.nihon@gmail.com>
2017-06-21 15:44:02 +0900

    frontend: Fix Label component's handling of valueless labels

--- frontend/public/components/utils/label-list.jsx
+++ frontend/public/components/utils/label-list.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router';
 import classNames from 'classnames';
 
 const Label = ({kind, name, value, expand}) => {
-  const href = `search?kind=${kind}&q=${name}%3D${value}`;
+  const href = `search?kind=${kind}&q=${value ? encodeURIComponent(`${name}=${value}`) : name}`;
   const klass = classNames('co-m-label', {'co-m-label--expand': expand});
 
   return (
     <Link className={`co-text-${kind}`} to={href}>
       <div className={klass}>
         <span className="co-m-label__key">{name}</span>
-        <span className="co-m-label__eq">=</span>
-        <span className="co-m-label__value">{value}</span>
+        {value && <span className="co-m-label__eq">=</span>}
+        {value && <span className="co-m-label__value">{value}</span>}
       </div>
     </Link>
   );
