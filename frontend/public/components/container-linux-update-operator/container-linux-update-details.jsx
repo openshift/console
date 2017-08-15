6bb71c3fd6b0d681487180f21b3a8aeed4417a00
Amruta Chitnis <amruta.chitnis@coreos.com>
2017-08-15 14:05:49 -0700

    frontend: Restyle Container linux updates section

--- frontend/public/components/container-linux-update-operator/container-linux-update-details.jsx
+++ frontend/public/components/container-linux-update-operator/container-linux-update-details.jsx
@@ -1,75 +1,154 @@
 import * as React from 'react';
-import { Link } from 'react-router';
 
+import { Link } from 'react-router';
 import { containerLinuxUpdateOperator } from '../utils';
 import { SafetyFirst } from '../safety-first';
 
-const Status = ({status}) => {
+const Status = ({status, upgradeCount}) => {
   return <div className="co-cluster-updates__details">
     <dl className="co-cluster-updates__detail">
       <dt>Status</dt>
-      <dd>{status}</dd>
+      <dd>
+        {upgradeCount > 0 && <span className="co-cluster-updates__text-icon fa fa-circle-o-notch fa-spin co-cluster-updates__operator-icon--updating"></span>}
+        <span className={upgradeCount > 0 ? 'co-cluster-updates--updating' : 'co-cluster-updates--up-to-date'}>{status}</span>
+      </dd>
     </dl>
   </div>;
 };
 
-/** This component can show as two sections
-"Download Completed" section:
-When a node enters 'Downloading', 'Verifying", 'Finalizing', light up (i.e. spinner shows up and showing "0 of [NUMBER OF NODES NEED UPDATE]") the "Download Completed" section.
-Only update/add up the node count for nodes in "Updated_Need_Reboot" state.
+const getProgressIconsClass = (dependentCount, total, nodes) => {
+  if (dependentCount === total) {
+    return 'fa fa-circle-o co-cl-operator--pending';
+  }
+  return _.isEmpty(nodes) ? 'fa fa-check-circle co-cl-operator--downloaded' : 'fa fa-spin fa-circle-o-notch co-cl-operator-spinner--downloading';
+};
 
-"Update Completed" section:
-When a node enters 'Rebooting', light up (i.e. spinner shows up and showing "0 of [NUMBER OF NODES NEED UPDATE]") the "Update Completed" section.
-Only update/add up the node count for up-to-date nodes.
-**/
-const Breakdown = ({iconClass, text, count, total, textClass}) => {
-  return <div className="co-cluster-updates__operator-ts-component">
+const Breakdown = ({iconClass, text, count, total, style}) => {
+  return <div className="co-cluster-updates__operator-ts-component" style={style}>
     <div className="co-cluster-updates__operator-ts-step">
       <div className="co-cluster-updates__operator-icon"><span className={iconClass}></span></div>
       <div className="co-cluster-updates__operator-text">
-        <div className={textClass}>
-          {text}
-        </div>
-        <div>
-          <Link to="nodes">{count} of {total}</Link>
-        </div>
+        {text} {count > 0 && <span className="text-muted">({count} of {total})</span>}
       </div>
-
     </div>
   </div>;
 };
 
-const Details = ({nodeListUpdateStatus}) => {
-  const totalNodes = nodeListUpdateStatus.count;
-  const upgradeCount = nodeListUpdateStatus.upgradeCount;
+class UpToDateState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false
+    };
+  }
 
-  return <div className="co-cluster-updates__operator-component">
-    <div className="co-cluster-updates__operator-step">
-      <div className="co-cluster-updates__operator-text">
-        {nodeListUpdateStatus.isSoftwareUpgarding ? <span><span className="co-cluster-updates__operator-subheader">Updated Software is available</span> <span className="text-muted">({upgradeCount} nodes need updating)</span></span> : <span>Container Linux &#10141; Latest Update</span>}
+  render() {
+    const {iconClass, text, total, textClass, versions} = this.props;
+
+    return <div className="co-cluster-updates__operator-ts-component">
+      <div className="co-cluster-updates__operator-ts-step">
+        <div className="co-cluster-updates__operator-icon"><span className={iconClass}></span></div>
+        <div className="co-cluster-updates__operator-text">
+          <div className={textClass}>
+            {text}
+          </div>
+          <button className="btn btn-link" onClick={() => this.setState({showDetails: !this.state.showDetails})}>
+            {this.state.showDetails ? 'Hide Details' : 'Show Details'}
+          </button>
+          {this.state.showDetails && <ul className="co-cluster-updates__operator-list">
+            {_.map(_.countBy(versions.sort()), (count, version) => <li key={version}>
+              <span>{version} ({count} of {total})</span>
+            </li>)}
+          </ul>}
+        </div>
       </div>
-    </div>
-    {(nodeListUpdateStatus.downloading.length > 0 || nodeListUpdateStatus.downloadCompleted.length > 0) &&
-      <Breakdown text="Download Completed"
-        count={nodeListUpdateStatus.downloadCompleted.length}
-        iconClass={containerLinuxUpdateOperator.getDownloadCompletedIconClass(nodeListUpdateStatus)}
-        total={upgradeCount} />
-    }
-    {(nodeListUpdateStatus.downloading.length > 0 || nodeListUpdateStatus.rebooting.length > 0) &&
-      <Breakdown text="Update Completed"
-        iconClass={containerLinuxUpdateOperator.getUpdateCompletedIconClass(nodeListUpdateStatus)}
-        count={nodeListUpdateStatus.upToDate.length}
-        textClass={nodeListUpdateStatus.downloading.length ? 'co-cl-operator--pending' : ''}
-        total={upgradeCount} />
-    }
-    {nodeListUpdateStatus.upToDate.length === totalNodes &&
-      <Breakdown text="Container Linux is up to date"
-        iconClass="fa fa-check-circle co-cl-operator--up-to-date"
-        count={nodeListUpdateStatus.upToDate.length}
-        total={totalNodes} />
-    }
-  </div>;
+    </div>;
+  }
+}
+UpToDateState.propTypes = {
+  iconClass: React.PropTypes.string,
+  textClass: React.PropTypes.string,
+  total: React.PropTypes.number,
+  text: React.PropTypes.string,
+  versions: React.PropTypes.array,
 };
+
+class UpdateProgress extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false
+    };
+  }
+
+  render() {
+    const {nodeListUpdateStatus} = this.props;
+    const {downloading, verifying, finalizing, upgradeCount, count, updatedNeedsReboot, rebooting} = nodeListUpdateStatus;
+    const headerCountText = upgradeCount === count ? count : `${upgradeCount} out of ${count}`;
+    const style = {padding: '0 0 10px 20px'};
+
+    return <div>
+      <div className="co-cluster-updates__operator-step">
+        <div className="co-cluster-updates__operator-text">
+          <span className="co-cluster-updates__operator-subheader">
+            Updates are available for {headerCountText}
+          </span>
+        </div>
+      </div>
+      <button className="btn btn-link" onClick={() => this.setState({showDetails: !this.state.showDetails})}>
+        {this.state.showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {this.state.showDetails && <div>
+        <div className="co-cluster-updates__operator-logs">
+          <Link to="nodes" className="co-cluster-updates__breakdown-button btn btn-default" target="_blank">View Logs</Link>
+        </div>
+        <div className="co-cluster-updates__operator-ts-component">
+          <Breakdown text="Download and finalize updates"
+            style={{paddingBottom: '10px'}}
+            iconClass={containerLinuxUpdateOperator.getDownloadCompletedIconClass(nodeListUpdateStatus)} />
+          {(downloading.length > 0 || verifying.length > 0 || finalizing.length > 0) && <div>
+            <Breakdown text="Downloading..."
+              style={style}
+              iconClass={getProgressIconsClass(0, upgradeCount, downloading)}
+              count={downloading.length}
+              total={upgradeCount} />
+            <Breakdown text="Verifying..."
+              iconClass={getProgressIconsClass(downloading.length, upgradeCount, verifying)}
+              style={style}
+              count={verifying.length}
+              total={upgradeCount} />
+            <Breakdown text="Finalizing..."
+              style={style}
+              iconClass={getProgressIconsClass(downloading.length + verifying.length, upgradeCount, finalizing)}
+              count={finalizing.length}
+              total={upgradeCount} />
+          </div>}
+          <Breakdown text="Apply Container Linux updates"
+            style={{paddingBottom: '10px'}}
+            iconClass={containerLinuxUpdateOperator.getUpdateCompletedIconClass(nodeListUpdateStatus)}
+            textClass={nodeListUpdateStatus.downloading.length ? 'co-cl-operator--pending' : ''}
+            total={upgradeCount} />
+          {(updatedNeedsReboot.length > 0 || rebooting.length > 0) && <div>
+            <Breakdown text="Updated need reboot"
+              iconClass={getProgressIconsClass(0, upgradeCount, updatedNeedsReboot)}
+              style={style}
+              count={updatedNeedsReboot.length}
+              total={upgradeCount} />
+            <Breakdown text="Reboot Success"
+              iconClass={getProgressIconsClass(updatedNeedsReboot, upgradeCount, rebooting)}
+              style={style}
+              count={rebooting.length}
+              total={upgradeCount} />
+          </div>}
+        </div>
+      </div>}
+    </div>;
+  }
+}
+UpdateProgress.propTypes = {
+  nodeListUpdateStatus: React.PropTypes.object,
+};
+
 
 export class ContainerLinuxUpdateDetails extends SafetyFirst {
   constructor(props) {
@@ -90,23 +169,34 @@ export class ContainerLinuxUpdateDetails extends SafetyFirst {
 
   render() {
     const {nodeListUpdateStatus, isOperatorInstalled} = this.props;
+    const {count, overallState, upgradeCount} = nodeListUpdateStatus;
+
     return <div>
       { isOperatorInstalled && <div className="co-cluster-updates__component">
         <div className="co-cluster-updates__heading">
           <div className="co-cluster-updates__heading--name-wrapper">
             <span className="co-cluster-updates__heading--name">Container Linux</span>
           </div>
-          { this.state.expanded ||
+          { !this.state.expanded &&
             <div className="co-cluster-updates__heading--updates">
-              <div className="co-cluster-updates__heading--updates">{nodeListUpdateStatus.overallState}</div>
+              <div className="co-cluster-updates__heading--updates">{overallState}</div>
             </div>
           }
           <a className="co-cluster-updates__toggle" onClick={this._toggleExpand}>{this.state.expanded ? 'Collapse' : 'Expand'}</a>
         </div>
         { this.state.expanded && <div>
-          <Status status={nodeListUpdateStatus.overallState} />
+          <Status status={overallState} upgradeCount={upgradeCount}/>
           <div className="co-cluster-updates__operator">
-            <Details nodeListUpdateStatus={nodeListUpdateStatus} />
+            {nodeListUpdateStatus.upToDate.length === count &&
+              <UpdateProgress nodeListUpdateStatus={nodeListUpdateStatus} />
+            }
+            {nodeListUpdateStatus.upToDate.length !== count &&
+              <UpToDateState text="Container Linux is up to date"
+                iconClass="fa fa-check-circle co-cl-operator--up-to-date"
+                count={nodeListUpdateStatus.upToDate.length}
+                versions={nodeListUpdateStatus.versions}
+                total={count} />
+            }
           </div>
         </div> }
       </div> }
