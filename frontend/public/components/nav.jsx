999c11dfc0327947a489cf430a6ad27aabf1641c
Rob Szumski <rob@robszumski.com>
2017-08-14 19:43:28 -0700

    Use correct marketing name

--- frontend/public/components/nav.jsx
+++ frontend/public/components/nav.jsx
@@ -127,7 +127,7 @@ export const Nav = () => <div id="sidebar" className="co-img-bg-cells">
       <NavLink resource="secrets" name="Secrets" />
     </NavSection>
 
-    <NavSection required={['ETCD_OPERATOR', 'PROMETHEUS']} text="Operators" img="static/imgs/operator-logo.svg">
+    <NavSection required={['ETCD_OPERATOR', 'PROMETHEUS']} text="Open Cloud Services" img="static/imgs/operator-logo.svg">
       <NavLink resource="etcdclusters" name="etcd Clusters" required="ETCD_OPERATOR" />
       <NavLink resource="prometheuses" name="Prometheus Instances" required="PROMETHEUS" />
     </NavSection>
