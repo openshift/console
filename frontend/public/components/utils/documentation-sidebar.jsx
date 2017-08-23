c4f2600934f301acc16f5ed175ee7fab29fba739
Geoff Greer <geoff@greer.fm>
2017-08-23 09:47:28 -0700

    frontend: replace anchor tags with <Link>s. Fixes links when console is run in a sub-path.

--- frontend/public/components/utils/documentation-sidebar.jsx
+++ frontend/public/components/utils/documentation-sidebar.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 export const DocumentationSidebar = () => <div className="co-p-cluster__sidebar">
   <div className="co-m-pane__body">
@@ -10,7 +11,7 @@ export const DocumentationSidebar = () => <div className="co-p-cluster__sidebar"
       <dd className="co-p-cluster__doc-description">End-users of Tectonic are expected to deploy applications directly in Kubernetes. Your application's architecture will drive how you assemble these components together.</dd>
     </dl>
     <h1 className="co-p-cluster__sidebar-heading">Additional Support</h1>
-    <p><a href="/start-guide" className="co-p-cluster__sidebar-link"><span className="fa fa-fw fa-info-circle co-p-cluster__sidebar-link-icon"></span>Quick Start Guide</a></p>
+    <p><Link to="/start-guide" className="co-p-cluster__sidebar-link"><span className="fa fa-fw fa-info-circle co-p-cluster__sidebar-link-icon"></span>Quick Start Guide</Link></p>
     <p><a href="https://coreos.com/tectonic/docs/latest/" target="_blank" className="co-p-cluster__sidebar-link"><span className="fa fa-fw fa-book co-p-cluster__sidebar-link-icon"></span>Full Documentation</a></p>
     <p><a href="https://github.com/coreos/tectonic-forum" target="_blank" className="co-p-cluster__sidebar-link"><span className="fa fa-fw fa-comments-o co-p-cluster__sidebar-link-icon"></span>Tectonic Forum</a></p>
     <p><a href="mailto:tectonic-feedback@coreos.com" className="co-p-cluster__sidebar-link"><span className="fa fa-fw fa-envelope-o co-p-cluster__sidebar-link-icon"></span>tectonic-feedback@coreos.com</a></p>
