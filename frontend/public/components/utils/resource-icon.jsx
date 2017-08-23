6c98e1104d8b42a027feb09c39a285f9d86f905c
Matt Kaniaris <mkaniaris@gmail.com>
2017-08-23 14:12:12 -0700

    simplify and de-alias enum usage

--- frontend/public/components/utils/resource-icon.jsx
+++ frontend/public/components/utils/resource-icon.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as classNames from'classnames';
-import { k8sEnum } from '../../module/k8s/enum';
+import { k8sKinds } from '../../module/k8s/enum';
 
 export const ResourceIcon = ({kind, className}) => {
-  const k = k8sEnum.Kind[kind];
+  const k = k8sKinds[kind];
   const klass = classNames(`co-m-resource-icon co-m-resource-${kind.toLowerCase()}`, className);
   const iconLabel = k && k.abbr ? k.abbr : kind.toUpperCase().slice(0, 2);
   return <span className={klass}>{iconLabel}</span>;
