cc4b6f64db06ce5ef60a62ebdfe70d7dab949717
Geoff Greer <geoff@greer.fm>
2017-09-13 16:08:59 -0700

    Async load graph components.

--- frontend/public/components/graphs/index.jsx
+++ frontend/public/components/graphs/index.jsx
@@ -1,5 +1,10 @@
-export { Bar } from './bar';
-export { Gauge } from './gauge';
-export { Line } from './line';
-export { Scalar } from './scalar';
+import * as React from 'react';
+
+import { AsyncComponent } from '../utils/async';
+
 export { Status } from './status';
+
+export const Bar = props => <AsyncComponent loader={() => System.import('./graph-loader').then(c => c.Bar)} {...props} />;
+export const Gauge = props => <AsyncComponent loader={() => System.import('./graph-loader').then(c => c.Gauge)} {...props} />;
+export const Line = props => <AsyncComponent loader={() => System.import('./graph-loader').then(c => c.Line)} {...props} />;
+export const Scalar = props => <AsyncComponent loader={() => System.import('./graph-loader').then(c => c.Scalar)} {...props} />;
