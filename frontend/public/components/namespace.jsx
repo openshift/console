29d03e07277f67508c379e683189ed39d44cbbfd
Geoff Greer <geoff@greer.fm>
2017-11-07 15:57:30 -0800

    If namespace dropdown is initialized to 'all', highlight it.

--- frontend/public/components/namespace.jsx
+++ frontend/public/components/namespace.jsx
@@ -168,7 +168,7 @@ const NamespaceDropdown = connect(() => ({activeNamespace: getActiveNamespace()}
   };
 
   return <div className="co-namespace-selector">
-    Namespace: <Dropdown className="co-namespace-selector__dropdown" noButton={true} items={items} title={title} onChange={onChange} selectedKey={activeNamespace} />
+    Namespace: <Dropdown className="co-namespace-selector__dropdown" noButton={true} items={items} title={title} onChange={onChange} selectedKey={activeNamespace || allNamespacesKey} />
   </div>;
 });
 
