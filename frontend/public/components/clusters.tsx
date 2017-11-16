771a382791388df25c0130b2046a19b336879476
Geoff Greer <geoff@greer.fm>
2017-11-16 15:05:44 -0800

    Show cluster type and feedback message on cluster list page. Improve styling to look like mocks.

--- frontend/public/components/clusters.tsx
+++ frontend/public/components/clusters.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as moment from 'moment';
 import * as _ from 'lodash';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
@@ -9,9 +8,9 @@ import { Cog, detailsPage, navFactory, ResourceCog, Heading, ResourceLink, Resou
 const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
 
 const ClustersHeader = props => <ListHeader>
-  <ColHead {...props} className="col-xs-4" sortField="metadata.name">Name</ColHead>
-  <ColHead {...props} className="col-xs-4">URL</ColHead>
-  <ColHead {...props} className="col-xs-4" sortField="metadata.creationTimestamp">Created</ColHead>
+  <ColHead {...props} className="col-xs-4" sortField="metadata.name">Cluster Name</ColHead>
+  <ColHead {...props} className="col-xs-3">Type</ColHead>
+  <ColHead {...props} className="col-xs-5">Addresses</ColHead>
 </ListHeader>;
 
 // eslint-disable-next-line no-unused-vars
@@ -21,17 +20,23 @@ type ClustersRowProps = {
 };
 const ClustersRow: React.StatelessComponent<ClustersRowProps> = ({obj}) => {
   const clusterLink = _.get(obj, ['metadata', 'annotations', 'multicluster.coreos.com/console-url']);
+  const clusterType = _.get(obj, ['metadata', 'annotations', 'multicluster.coreos.com/directory']);
 
   return <div className="row co-resource-list__item">
     <div className="col-xs-4">
       <ResourceCog actions={menuActions} kind="Cluster" resource={obj} />
       <ResourceLink kind="Cluster" name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
     </div>
-    <div className="col-xs-4">
-      {clusterLink ? <a href={clusterLink.toString()} target="_blank" rel="noopener noreferrer">{clusterLink} <i className="fa fa-external-link"></i></a> : '—'}
+    <div className="col-xs-3">
+      { clusterType ? 'Directory': 'Replica' } Cluster
     </div>
-    <div className="col-xs-4">
-      { moment(obj.metadata.creationTimestamp).fromNow() }
+    <div className="col-xs-5">
+      {clusterLink
+        ? <span className="text-muted">Console:&nbsp;
+          <a href={clusterLink.toString()} target="_blank" rel="noopener noreferrer">{clusterLink} <i className="fa fa-external-link"></i></a>
+        </span>
+        : '—'
+      }
     </div>
   </div>;
 };
@@ -62,7 +67,12 @@ type ClustersPageProps = {
   // eslint-disable-next-line no-undef
   filterLabel: string,
 };
-export const ClustersPage: React.StatelessComponent<ClustersPageProps> = props => <ListPage {...props} kind="Cluster" ListComponent={ClustersList} canCreate={true} filterLabel={props.filterLabel} />;
+export const ClustersPage: React.StatelessComponent<ClustersPageProps> = props => <div>
+  <div className="co-well" style={{marginBottom: 0}}>
+    Thanks for trying out the Multi-Cluster Directory. Future updates will enable add/remove and other policy features. Feedback and questions are encouraged: <a href="mailto:tectonic-alpha-feedback@coreos.com">tectonic-alpha-feedback@coreos.com</a>
+  </div>
+  <ListPage {...props} title="Cluster Directory" kind="Cluster" ListComponent={ClustersList} canCreate={false} filterLabel={props.filterLabel} />
+</div>;
 ClustersPage.displayName = 'ClustersPage';
 
 const pages = [navFactory.details(detailsPage(ClustersDetails)), navFactory.editYaml()];
