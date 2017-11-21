f60f02d28c546a9017f4256169aea55cb88bcb79
Geoff Greer <geoff@greer.fm>
2017-11-21 12:16:56 -0800

    Only warn once for each kind.
    Fixes an issue where lots of log warnings polluted the dev console on list views.

--- frontend/public/components/utils/inject.js
+++ frontend/public/components/utils/inject.js
@@ -14,18 +14,18 @@ export const inject = (children, props) => {
   });
 };
 
-let lastKind = '';
+const lastKind = new Set();
 
 /**
  * @deprecated: Use `modelFor` or `connectToModel`.
  * Provides a synchronous way to acquire a statically-defined Kubernetes model.
  * NOTE: This will not work for CRDs defined at runtime, use `connectToModels` instead.
- */ 
+ */
 export const kindObj = (kind) => {
-  if (kindForReference(kind) === kind && kind !== lastKind) {
+  if (kindForReference(kind) === kind && !lastKind.has(kind)) {
     // eslint-disable-next-line no-console
     console.warn(`Attempting to get Kubernetes object model using string kind: ${kind}, which is not guaranteed to be unique!`);
-    lastKind = kind;
+    lastKind.add(kind);
   }
   // FIXME(alecmerdler): Remove synchronous `store.getState()` call here, should be using `connectToModels` instead, only here for backwards-compatibility
   return modelFor(kind) || store.getState().KINDS.get('kinds').get(kind) || {};
