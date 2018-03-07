c21ef991a0bd939a1dae11065233088eabae20af
Geoff Greer <geoff@greer.fm>
2018-03-06 16:46:37 -0800

    Minor cleanup. No need for auth variable. Reconnect function checks _connectionAttempt.

--- frontend/public/module/ws-factory.js
+++ frontend/public/module/ws-factory.js
@@ -138,8 +138,7 @@ WebSocketWrapper.prototype._connect = function() {
   const that = this;
   this._state = 'init';
   this._buffer = [];
-  const auth = [];
-  this.ws = new WebSocket(this.url, auth);
+  this.ws = new WebSocket(this.url);
 
   this.ws.onopen = function() {
     console.log(`websocket open: ${that.id}`);
@@ -154,9 +153,7 @@ WebSocketWrapper.prototype._connect = function() {
     console.log(`websocket closed: ${that.id}`, evt);
     that._state = 'closed';
     that._triggerEvent({ type: 'close', args: [evt] });
-    if (!that._connectionAttempt) {
-      that._reconnect();
-    }
+    that._reconnect();
   };
   this.ws.onerror = function (evt) {
     console.log(`websocket error: ${that.id}`);
