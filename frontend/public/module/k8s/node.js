54c96cf8285ba52dbb0c3ebf6142ec410f4d1535
Matt Kaniaris <mkaniaris@gmail.com>
2018-04-03 12:43:25 -0700

    Fixes MarkAsUnschedulable/MarkAsSchedulable 500...
    The 'path' must exist for patches which 'replace' a 'path'; otherwise, use add.
    Also, a bit of cleanup around the implementations.

--- frontend/public/module/k8s/node.js
+++ frontend/public/module/k8s/node.js
@@ -1,5 +1,18 @@
 import * as _ from 'lodash-es';
 
+import { k8sPatch } from './resource';
+import { k8sKinds } from './enum';
+
+export const makeNodeUnschedulable = resource => {
+  const op = _.has(resource, 'spec.unschedulable') ? 'replace' : 'add';
+  return k8sPatch(k8sKinds.Node, resource, [{ op, path: '/spec/unschedulable', value: true }]);
+};
+
+export const makeNodeSchedulable = resource => {
+  const op = _.has(resource, 'spec.unschedulable') ? 'replace' : 'add';
+  return k8sPatch(k8sKinds.Node, resource, [{ op, path: '/spec/unschedulable', value: false }]);
+};
+
 export const isNodeReady = (node) => {
   if (!node || !node.status || !node.status.conditions || !node.status.conditions.length) {
     return false;
