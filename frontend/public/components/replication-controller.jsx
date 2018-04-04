afc2c179561de24a805b3381b279d5d371c9376f
Samuel Padgett <spadgett@redhat.com>
2018-04-04 15:52:56 -0400

    Add breadcrumbs for ReplicationController -> DeploymentConfig

--- frontend/public/components/replication-controller.jsx
+++ frontend/public/components/replication-controller.jsx
@@ -5,6 +5,7 @@ import { DetailsPage, List, ListPage, WorkloadListHeader, WorkloadListRow } from
 import { replicaSetMenuActions } from './replicaset';
 import { navFactory, Heading, ResourceSummary, ResourcePodCount } from './utils';
 import { registerTemplate } from '../yaml-templates';
+import { modelFor, referenceForOwnerRef } from '../module/k8s/k8s-models';
 
 registerTemplate('v1.ReplicationController', `apiVersion: v1
 kind: ReplicationController
@@ -47,6 +48,9 @@ const {details, editYaml, pods, events} = navFactory;
 
 export const ReplicationControllersDetailsPage = props => <DetailsPage
   {...props}
+  breadcrumbsFor={(obj) => (obj.metadata.ownerReferences || []).map(ref => ({
+    name: ref.name, path: `/k8s/ns/${obj.metadata.namespace}/${modelFor(referenceForOwnerRef(ref)).plural}/${ref.name}`
+  })).concat([{name: 'ReplicationController Details', path: props.match.url}])}
   menuActions={replicaSetMenuActions}
   pages={[details(Details), editYaml(), pods(), events(ResourceEventStream)]}
 />;
