49f6bf37d8ccd1a671febb9618795f7a8a9395cb
Matt Kaniaris <mkaniaris@gmail.com>
2018-04-10 17:47:22 -0700

    Clean up DeploymentPodCounts with regards to state management:
    state should pushed down into lowest possible component to reduce complexity.
    Also, favor 'getDerivedStateFromProps' over 'componentWillReceiveProps'.

--- frontend/public/components/utils/deployment-pod-counts.tsx
+++ frontend/public/components/utils/deployment-pod-counts.tsx
@@ -1,63 +1,114 @@
+import * as _ from 'lodash-es';
 import * as React from 'react';
 import { Tooltip } from './tooltip';
 
 import { K8sKind, K8sResourceKind } from '../../module/k8s';
+import { SafetyFirst } from '../safety-first';
+import { configureReplicaCountModal } from '../modals';
 import { LoadingInline, pluralize } from './';
 
-// Common representation of desired / up-to-date / matching pods for both k8s Deployments and OpenShift Deployment Configs.
-export const DeploymentPodCounts: React.SFC<DeploymentPodCountsProps> = ({ resource, resourceKind, desiredCountOutdated, openReplicaCountModal }) => {
-  const { spec, status } = resource;
 
-  return <div className="co-m-pane__body-group">
-    <div className="row no-gutter">
-      <div className="co-detail-table">
-        <div className="co-detail-table__row row">
-          <div className="co-detail-table__section col-sm-3">
-            <dl className="co-m-pane__details">
-              <dt className="co-detail-table__section-header">Desired Count</dt>
-              <dd>{desiredCountOutdated ? <LoadingInline /> : <a className="co-m-modal-link" href="#" onClick={openReplicaCountModal}>{pluralize(spec.replicas, 'pod')}</a>}</dd>
-            </dl>
-          </div>
-          <div className="co-detail-table__section col-sm-3">
-            <dl className="co-m-pane__details">
-              <dt className="co-detail-table__section-header">Up-to-date Count</dt>
-              <dd>
-                <Tooltip content={`Total number of non-terminated pods targeted by this ${resourceKind.label} that have the desired template spec.`}>
-                  {pluralize(status.updatedReplicas, 'pod')}
+/* eslint-disable no-undef, no-unused-vars */
+type DPCProps = {
+  resource: K8sResourceKind;
+  resourceKind: K8sKind;
+};
+
+type DPCState = {
+  desiredCount: number,
+  waitingForUpdate: boolean,
+};
+/* eslint-enable no-undef, no-unused-vars */
+
+// Common representation of desired / up-to-date / matching pods for Deployment like things
+export class DeploymentPodCounts extends SafetyFirst<DPCProps, DPCState> {
+  /* eslint-disable no-undef, no-unused-vars */
+  openReplicaCountModal: any;
+  /* eslint-enable no-undef, no-unused-vars */
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      desiredCount: -1,
+      waitingForUpdate: false
+    };
+
+    this.openReplicaCountModal = event => {
+      event.preventDefault();
+      event.target.blur();
+
+      const {resourceKind, resource} = this.props;
+
+      configureReplicaCountModal({
+        resourceKind, resource,
+        invalidateState: (waitingForUpdate, desiredCount) => this.setState({waitingForUpdate, desiredCount}),
+      });
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, nextState) {
+    if (!nextState.waitingForUpdate) {
+      return null;
+    }
+
+    if (_.get(nextProps, 'resource.spec.replicas') !== nextState.desiredCount) {
+      return null;
+    }
+
+    return { waitingForUpdate: false, desiredCount: -1 };
+  }
+
+  render () {
+    const { resource, resourceKind } = this.props;
+    const { spec, status } = resource;
+
+    return <div className="co-m-pane__body-group">
+      <div className="row no-gutter">
+        <div className="co-detail-table">
+          <div className="co-detail-table__row row">
+            <div className="co-detail-table__section col-sm-3">
+              <dl className="co-m-pane__details">
+                <dt className="co-detail-table__section-header">Desired Count</dt>
+                <dd>
+                  {
+                    this.state.waitingForUpdate
+                      ? <LoadingInline />
+                      : <a className="co-m-modal-link" href="#" onClick={this.openReplicaCountModal}>{ pluralize(spec.replicas, 'pod') }</a>
+                  }
+                </dd>
+              </dl>
+            </div>
+            <div className="co-detail-table__section col-sm-3">
+              <dl className="co-m-pane__details">
+                <dt className="co-detail-table__section-header">Up-to-date Count</dt>
+                <dd>
+                  <Tooltip content={`Total number of non-terminated pods targeted by this ${resourceKind.label} that have the desired template spec.`}>
+                    {pluralize(status.updatedReplicas, 'pod')}
+                  </Tooltip>
+                </dd>
+              </dl>
+            </div>
+            <div className="co-detail-table__section co-detail-table__section--last col-sm-6">
+              <dl className="co-m-pane__details">
+                <dt className="co-detail-table__section-header">Matching Pods</dt>
+                <dd>
+                  <Tooltip content={`Total number of non-terminated pods targeted by this ${resourceKind.label} (their labels match the selector).`}>
+                    {pluralize(status.replicas, 'pod')}
+                  </Tooltip>
+                </dd>
+              </dl>
+              <div className="co-detail-table__bracket"></div>
+              <div className="co-detail-table__breakdown">
+                <Tooltip content="Total number of available pods (ready for at least minReadySeconds) targeted by this deployment.">
+                  {status.availableReplicas || 0} available
                 </Tooltip>
-              </dd>
-            </dl>
-          </div>
-          <div className="co-detail-table__section co-detail-table__section--last col-sm-6">
-            <dl className="co-m-pane__details">
-              <dt className="co-detail-table__section-header">Matching Pods</dt>
-              <dd>
-                <Tooltip content={`Total number of non-terminated pods targeted by this ${resourceKind.label} (their labels match the selector).`}>
-                  {pluralize(status.replicas, 'pod')}
-                </Tooltip>
-              </dd>
-            </dl>
-            <div className="co-detail-table__bracket"></div>
-            <div className="co-detail-table__breakdown">
-              <Tooltip content="Total number of available pods (ready for at least minReadySeconds) targeted by this deployment.">
-                {status.availableReplicas || 0} available
-              </Tooltip>
-              <Tooltip content={`Total number of unavailable pods targeted by this ${resourceKind.label}.`}>{status.unavailableReplicas || 0} unavailable</Tooltip>
+                <Tooltip content={`Total number of unavailable pods targeted by this ${resourceKind.label}.`}>{status.unavailableReplicas || 0} unavailable</Tooltip>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>;
-};
-
-/* eslint-disable no-undef */
-export type DeploymentPodCountsProps = {
-  resource: K8sResourceKind;
-  resourceKind: K8sKind;
-  desiredCountOutdated: boolean;
-  openReplicaCountModal: (event: React.MouseEvent<HTMLElement>) => void;
-};
-/* eslint-enable no-undef */
-
-DeploymentPodCounts.displayName = 'DeploymentPodCounts';
+    </div>;
+  }
+}
