b3619dc1e9adc61680110b89014ad4d306528115
Eric Chiang <eric.chiang.m@gmail.com>
2018-04-19 14:49:43 -0700

    cleanup documentation of local development

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
-OPENSHIFT_API="https://ec2-54-91-246-166.compute-1.amazonaws.com:8443"
-
-../../bin/bridge \
+./bin/bridge \
     --base-address=http://localhost:9000 \
-    --ca-file=ca.crt \
+    --ca-file=examples/ca.crt \
     --k8s-auth=openshift \
     --k8s-mode=off-cluster \
     --k8s-mode-off-cluster-endpoint=$OPENSHIFT_API \
     --k8s-mode-off-cluster-skip-verify-tls=true \
     --listen=http://127.0.0.1:9000 \
-    --public-dir=../../frontend/public/dist \
+    --public-dir=./frontend/public/dist \
     --user-auth=openshift \
     --user-auth-oidc-client-id=tectonic-console \
     --user-auth-oidc-client-secret=$OAUTH_SECRET \
