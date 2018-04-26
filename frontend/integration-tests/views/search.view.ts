3aced48f55b3192a76071c528c11e91dbdad7f1c
Alejandro Rosero V <aroserov@coreos.com>
2018-04-26 11:56:00 -0500

    CONSOLE-368: Add Modal anotations tests

--- frontend/integration-tests/views/search.view.ts
+++ frontend/integration-tests/views/search.view.ts
@@ -2,7 +2,7 @@ import { $, $$, browser, ExpectedConditions as until } from 'protractor';
 
 const BROWSER_TIMEOUT = 15000;
 
-export const dropdown = $('.co-type-selector');
+export const dropdown = $('.btn--dropdown__content-wrap');
 export const dropdownLinks = $$('.dropdown-menu a');
 export const labelFilter = $('.co-m-selector-input input');
 
