2a344c012ae598bef6878c10c5590ae737272d07
Amruta Chitnis <amruta.chitnis@coreos.com>
2018-05-01 15:24:43 -0700

    frontend: Readd account site details

--- frontend/public/components/utils/documentation-sidebar.jsx
+++ frontend/public/components/utils/documentation-sidebar.jsx
@@ -7,6 +7,7 @@ export const DocumentationSidebar = props => <div className="co-p-cluster__sideb
     <h1 className="co-p-cluster__sidebar-heading co-p-cluster__sidebar-heading--first">Documentation</h1>
     <dl>
       <dt className="co-p-cluster__doc-title"><a href="https://coreos.com/tectonic/docs/latest/account/manage-account.html" target="_blank" rel="noopener">Manage Your Account</a></dt>
+      <dd className="co-p-cluster__doc-description">You can manage your Tectonic account at <a href="https://account.coreos.com" target="_blank" rel="noopener">account.coreos.com</a> for access to licenses, billing details, invoices, and account users.</dd>
       <dt className="co-p-cluster__doc-title"><a href="https://coreos.com/tectonic/docs/latest/usage/" target="_blank" rel="noopener">End User Guide</a></dt>
       <dd className="co-p-cluster__doc-description">End-users of Tectonic are expected to deploy applications directly in Kubernetes. Your application&rsquo;s architecture will drive how you assemble these components together.</dd>
     </dl>
