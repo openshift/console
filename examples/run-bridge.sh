334cad852b6527f145514535169d7fd569c4de08
xiaocwan <xiaocwan@redhat.com>
2018-07-10 17:36:51 +0800

    Make up attributes

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
-
+set -x
 ./bin/bridge \
     --base-address=http://localhost:9000 \
     --ca-file=examples/ca.crt \
     --k8s-auth=openshift \
     --k8s-mode=off-cluster \
     --k8s-mode-off-cluster-endpoint=$OPENSHIFT_API \
+    --k8s-mode-off-cluster-skip-verify-tls=true \
     --listen=http://127.0.0.1:9000 \
     --public-dir=./frontend/public/dist \
     --user-auth=openshift \
     --user-auth-oidc-client-id=console-oauth-client \
-    --user-auth-oidc-client-secret-file=examples/console-client-secret
-    --user-auth-oidc-ca-file=examples/ca.crt
-    --logo-image-name=origin
+    --user-auth-oidc-client-secret-file=examples/console-client-secret \
+    --user-auth-oidc-ca-file=examples/ca.crt \
+    --logo-image-name=origin 
