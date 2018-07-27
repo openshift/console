3f047968e11596b2bce3174ae2feee5d1c7777f7
Samuel Padgett <spadgett@redhat.com>
2018-07-27 10:04:15 -0400

    Fix filter test failure

--- frontend/integration-tests/views/search.view.ts
+++ frontend/integration-tests/views/search.view.ts
@@ -1,17 +1,18 @@
-import { $, $$, browser, ExpectedConditions as until } from 'protractor';
+import { $, $$, browser, by, element, ExpectedConditions as until } from 'protractor';
 
 const BROWSER_TIMEOUT = 15000;
 
 export const dropdown = $('.btn--dropdown__content-wrap');
 export const dropdownLinks = $$('.dropdown-menu a');
 export const labelFilter = $('.co-m-selector-input input');
+export const linkForType = type => element(by.partialLinkText(type));
 
 export const selectSearchType = async (objectType: string) => {
   const isPresent = await dropdownLinks.isPresent();
   if (!isPresent) {
     await dropdown.click();
-    await browser.wait(until.elementToBeClickable(dropdownLinks.first()), BROWSER_TIMEOUT);
+    await browser.wait(until.elementToBeClickable(linkForType(objectType)), BROWSER_TIMEOUT);
   }
-  await dropdownLinks.filter(elem => elem.getText().then(text => text.toLowerCase().includes(objectType.toLowerCase()))).first().click();
+  await linkForType(objectType).click();
   await browser.wait(until.elementToBeClickable(labelFilter), BROWSER_TIMEOUT);
 };
