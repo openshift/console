e396b76a207780a48e629526c024bb7befd2845f
Rebecca Alpert <7014965+rebeccaalpert@users.noreply.github.com>
2018-08-06 11:36:36 -0400

    frontend: Show both ClusterResourceQuotas and ResourceQuotas
    Modified ResourceQuotaPage to show both ClusterResourceQuotas and
    ResourceQuotas.
    
    Fixes https://jira.coreos.com/browse/CONSOLE-653

--- frontend/public/components/resource-quota.jsx
+++ frontend/public/components/resource-quota.jsx
@@ -1,23 +1,30 @@
+import * as _ from 'lodash-es';
 import * as React from 'react';
 
-import { ColHead, DetailsPage, List, ListHeader, ListPage } from './factory';
+import { ColHead, DetailsPage, List, ListHeader, MultiListPage } from './factory';
 import { Cog, SectionHeading, navFactory, ResourceCog, ResourceLink, ResourceSummary } from './utils';
 
 const menuActions = [Cog.factory.ModifyLabels, Cog.factory.ModifyAnnotations, Cog.factory.Edit, Cog.factory.Delete];
+
+export const QuotaName = ({quota: quota}) => {
+  const kind = quota.metadata.namespace ? 'ResourceQuota' : 'quota.openshift.io:v1:ClusterResourceQuota';
+  return <React.Fragment>
+    <ResourceCog actions={menuActions} kind={kind} resource={quota} />
+    <ResourceLink kind={kind} name={quota.metadata.name} namespace={quota.metadata.namespace} className="co-resource-link__resource-name" />
+  </React.Fragment>;
+};
 
 const Header = props => <ListHeader>
   <ColHead {...props} className="col-md-5 col-xs-6" sortField="metadata.name">Name</ColHead>
   <ColHead {...props} className="col-md-7 col-xs-6" sortField="metadata.namespace">Namespace</ColHead>
 </ListHeader>;
 
-const kind = 'ResourceQuota';
 const Row = ({obj: rq}) => <div className="row co-resource-list__item">
   <div className="col-md-5 col-xs-6 co-resource-link-wrapper">
-    <ResourceCog actions={menuActions} kind={kind} resource={rq} />
-    <ResourceLink kind={kind} name={rq.metadata.name} namespace={rq.metadata.namespace} title={rq.metadata.name} />
+    <QuotaName quota={rq} />
   </div>
   <div className="col-md-7 col-xs-6 co-break-word">
-    <ResourceLink kind="Namespace" name={rq.metadata.namespace} title={rq.metadata.namespace} />
+    {rq.metadata.namespace ? <ResourceLink kind="Namespace" name={rq.metadata.namespace} title={rq.metadata.namespace} /> : 'all'}
   </div>
 </div>;
 
@@ -29,7 +36,54 @@ const Details = ({obj: rq}) => <React.Fragment>
 </React.Fragment>;
 
 export const ResourceQuotasList = props => <List {...props} Header={Header} Row={Row} />;
-export const ResourceQuotasPage = props => <ListPage {...props} ListComponent={ResourceQuotasList} kind={kind} canCreate={true} />;
+
+export const quotaType = quota => {
+  if (!quota) {
+    return undefined;
+  }
+  return quota.metadata.namespace ? 'namespace' : 'cluster';
+};
+
+// Split each resource quota into one row per subject
+export const flatten = resources => _.flatMap(resources, resource => {
+  const rows = [];
+
+  _.each(resource.data, quota => {
+    if (!quota) {
+      return undefined;
+    }
+    rows.push(quota);
+  });
+
+  return rows;
+});
+
+export const ResourceQuotasPage = props => {
+  const {match: {params: {name, ns}}} = props;
+  let resources = [{kind: 'ResourceQuota', namespaced: true}, {kind: 'quota.openshift.io:v1:ClusterResourceQuota', namespaced: false, optional: true}];
+  return <MultiListPage
+    canCreate={true}
+    createButtonText="Create Resource Quota"
+    createProps={{to: '/k8s/ns/tectonic-system/resourcequotas/new'}}
+    ListComponent={ResourceQuotasList}
+    staticFilters={[{'resource-quota-roleRef': name}]}
+    resources={resources}
+    filterLabel="Resource Quotas by name"
+    label="Resource Quotas"
+    namespace={ns}
+    flatten={flatten}
+    title="Resource Quotas"
+    rowFilters={[{
+      type: 'role-kind',
+      selected: ['cluster', 'namespace'],
+      reducer: quotaType,
+      items: [
+        {id: 'cluster', title: 'Cluster-wide Resource Quotas'},
+        {id: 'namespace', title: 'Namespace Resource Quotas'},
+      ],
+    }]}
+  />;
+};
 
 export const ResourceQuotasDetailsPage = props => <DetailsPage
   {...props}
