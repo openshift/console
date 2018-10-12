09b51ec766eb424b4ab0f48384880706da6689b7
alecmerdler <alecmerdler@gmail.com>
2018-10-11 20:15:10 -0400

    adding more spec/status descriptor components

--- frontend/public/components/operator-lifecycle-manager/descriptors/status/index.tsx
+++ frontend/public/components/operator-lifecycle-manager/descriptors/status/index.tsx
@@ -8,30 +8,47 @@ import { ResourceLink } from '../../../utils';
 import { StatusCapability, CapabilityProps, DescriptorProps } from '../types';
 import { Phase } from './phase';
 import { PodStatusChart } from './pods';
+import { Tooltip } from '../../../utils/tooltip';
+import { Conditions } from '../../../conditions';
 
-// TODO(alecmerdler): Move these to separate module?
-const Default: React.SFC<StatusCapabilityProps> = ({value}) => _.isEmpty(value) && !_.isNumber(value)
-  ? <span className="text-muted">None</span>
-  : <span>{value}</span>;
+const Invalid: React.SFC<StatusCapabilityProps> = (props) => <span className="text-muted">
+  <i className="fa fa-exclamation-triangle text-warning" aria-hidden="true" />&nbsp;&nbsp;The field <code>status.{props.descriptor.path}</code> is invalid
+</span>;
 
-const PodStatuses: React.SFC<StatusCapabilityProps> = (props) => <PodStatusChart fetcher={() => props.value} statusDescriptor={props.descriptor} />;
+const Default: React.SFC<StatusCapabilityProps> = ({value}) => {
+  if (_.isEmpty(value) && !_.isNumber(value)) {
+    return <span className="text-muted">None</span>;
+  } else if (_.isObject(value)) {
+    return <div className="row">{_.map(value, (v, k) => <span key={k}>{k}: {v}</span>)}</div>;
+  }
+  return <span>{_.toString(value)}</span>;
+};
 
-// FIXME(alecmerdler): Need to revisit this component to show a table of conditions
-const Conditions: React.SFC<StatusCapabilityProps> = ({value}) => !_.isEmpty(value)
-  ? <span>{value.reduce((latest, next) => new Date(latest.lastUpdateTime) < new Date(next.lastUpdateTime) ? latest : next).type}</span>
-  : <span className="text-muted">None</span>;
+const PodStatuses: React.SFC<StatusCapabilityProps> = (props) => {
+  return (!_.isObject(props.value) || _.some(props.value, v => !_.isArray(v))) && <Invalid {...props} />
+    || _.every(props.value, v => _.isArray(v) && v.length === 0) && <span className="text-muted">No members</span>
+    || <PodStatusChart fetcher={() => props.value} statusDescriptor={props.descriptor} />;
+};
+
+const StatusConditions: React.SFC<StatusCapabilityProps> = (props) => {
+  return !_.isArray(props.value) && <Invalid {...props} />
+    || props.value.length === 0 && <span className="text-muted">No conditions present</span>
+    || <Conditions conditions={props.value} />;
+};
 
 const Link: React.SFC<StatusCapabilityProps> = ({value}) => <a href={value}>{value.replace(/https?:\/\//, '')}</a>;
 
 const K8sPhase: React.SFC<StatusCapabilityProps> = ({value}) => <Phase status={value} />;
 
-const K8sPhaseReason: React.SFC<StatusCapabilityProps> = ({value}) => <pre>{value}</pre>;
+const K8sPhaseReason: React.SFC<StatusCapabilityProps> = ({value}) => _.isEmpty(value)
+  ? <span className="text-muted">None</span>
+  : <pre style={{width: 'max-content'}}>{value}</pre>;
 
 const K8sResourceLink: React.SFC<StatusCapabilityProps> = (props) => <ResourceLink kind={props.capability.substr(StatusCapability.k8sResourcePrefix.length)} name={props.value} namespace={props.namespace} title={props.value} />;
 
 const capabilityComponents = ImmutableMap<StatusCapability, React.ComponentType<StatusCapabilityProps>>()
   .set(StatusCapability.podStatuses, PodStatuses)
-  .set(StatusCapability.conditions, Conditions)
+  .set(StatusCapability.conditions, StatusConditions)
   .set(StatusCapability.w3Link, Link)
   .set(StatusCapability.k8sPhase, K8sPhase)
   .set(StatusCapability.k8sPhaseReason, K8sPhaseReason)
@@ -57,7 +74,11 @@ export const StatusDescriptor: React.SFC<DescriptorProps> = (props) => {
   const Capability = capabilityFor(capability);
 
   return <dl>
-    <dt>{descriptor.displayName}</dt>
+    <div style={{width: 'max-content'}}>
+      <Tooltip content={descriptor.description}>
+        <dt>{descriptor.displayName}</dt>
+      </Tooltip>
+    </div>
     <dd><Capability descriptor={descriptor} capability={capability} value={value} namespace={namespace} /></dd>
   </dl>;
 };
