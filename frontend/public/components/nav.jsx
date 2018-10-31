69506da04d070b8bc4ea2bb9b49ddb3df1ece41e
Andy Pickering <anpicker@redhat.com>
2018-10-31 14:38:41 +0900

    Use SERVER_FLAGS.prometheusBaseURL to show / hide Monitoring UI
    Use the existence of SERVER_FLAGS.prometheusBaseURL to determine whether
    to show the Monitoring UI. This is the URL of the Prometheus API, which
    provides the data for the Monitoring UI. The MonitoringRoutes.Prometheus
    URL just provides the Prometheus UI URL, which is not essential for
    displaying the Monitoring UI.

--- frontend/public/components/nav.jsx
+++ frontend/public/components/nav.jsx
@@ -280,9 +280,9 @@ const ClusterPickerNavSection = connectToFlags(FLAGS.OPENSHIFT)(({flags}) => {
 const MonitoringNavSection_ = ({urls, closeMenu}) => {
   const prometheusURL = urls[MonitoringRoutes.Prometheus];
   const grafanaURL = urls[MonitoringRoutes.Grafana];
-  return prometheusURL || grafanaURL
+  return window.SERVER_FLAGS.prometheusBaseURL || prometheusURL || grafanaURL
     ? <NavSection text="Monitoring" icon="pficon pficon-screen">
-      {prometheusURL && <HrefLink href="/monitoring" name="Alerts" onClick={closeMenu} />}
+      {window.SERVER_FLAGS.prometheusBaseURL && <HrefLink href="/monitoring" name="Alerts" onClick={closeMenu} />}
       {prometheusURL && <HrefLink href={prometheusURL} target="_blank" name="Metrics" onClick={closeMenu} isExternal={true} />}
       {grafanaURL && <HrefLink href={grafanaURL} target="_blank" name="Dashboards" onClick={closeMenu} isExternal={true} />}
     </NavSection>
