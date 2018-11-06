03e042235f1f35f00c264b03a2c15fc46fa4f4a2
Samuel Padgett <spadgett@redhat.com>
2018-11-06 10:37:01 -0500

    Add details about resources created for source-to-image

--- frontend/public/components/source-to-image.tsx
+++ frontend/public/components/source-to-image.tsx
@@ -18,6 +18,20 @@ const getSampleRepo = tag => _.get(tag, 'annotations.sampleRepo');
 const getSampleRef = tag => _.get(tag, 'annotations.sampleRef');
 const getSampleContextDir = tag => _.get(tag, 'annotations.sampleContextDir');
 
+export const SourceToImageResourceDetails = () => <React.Fragment>
+  <hr />
+  <p>
+    The following resources will be created:
+  </p>
+  <ul>
+    <li>A <span className="co-catalog-item-details__kind-label">build config</span> to build source from a Git repository.</li>
+    <li>An <span className="co-catalog-item-details__kind-label">image stream</span> to track built images.</li>
+    <li>A <span className="co-catalog-item-details__kind-label">deployment config</span> to rollout new revisions when the image changes.</li>
+    <li>A <span className="co-catalog-item-details__kind-label">service</span> to expose your workload inside the cluster.</li>
+    <li>An optional <span className="co-catalog-item-details__kind-label">route</span> to expose your workload outside the cluster.</li>
+  </ul>
+</React.Fragment>;
+
 // Transform image ports to k8s structure.
 // `{ '3306/tcp': {} }` -> `{ containerPort: 3306, protocol: 'TCP' }`
 const portsFromSpec = (portSpec: any): ContainerPort[] => {
@@ -67,6 +81,7 @@ const ImageStreamInfo: React.SFC<ImageStreamInfoProps> = ({imageStream, tag}) =>
     </div>
     {description && <p className="co-catalog-item-details__description">{description}</p>}
     {sampleRepo && <p>Sample repository: <a href={sampleRepo} target="_blank" rel="noopener noreferrer">{sampleRepo}</a></p>}
+    <SourceToImageResourceDetails />
   </div>;
 };
 
