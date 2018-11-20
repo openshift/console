0d0b462886bef8cb8ff7c65c63fa448a8916019a
Robb Hamilton <rhamilto@redhat.com>
2018-11-20 09:41:06 -0500

    Remove contextselector
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1651086

--- frontend/public/components/utils/dropdown.jsx
+++ frontend/public/components/utils/dropdown.jsx
@@ -317,7 +317,6 @@ export class Dropdown extends DropdownMixin {
 
     _.each(items, (v, k) => addItem(k, v));
 
-    //Adding `dropDownClassName` specifically to use patternfly's context selector component, which expects `bootstrap-select` class on the dropdown. We can remove this additional property if that changes in upcoming patternfly versions.
     return <div className={classNames(className)} ref={this.dropdownElement} style={this.props.style}>
       <div className={classNames('dropdown', dropDownClassName, {'open': active})}>
         <button aria-haspopup="true" onClick={this.toggle} onKeyDown={this.onKeyDown} type="button" className={classNames('btn', 'btn-dropdown', 'dropdown-toggle', buttonClassName ? buttonClassName : 'btn-default')} id={this.props.id} aria-describedby={describedBy} >
