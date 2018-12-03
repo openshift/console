46d9c514ee7f0fd3551d38e941ae30d79ef87ec4
David Taylor <dtaylor@redhat.com>
2018-12-03 10:26:33 -0500

    Added Resource Quota Charts, Scopes, and Help Infotip

--- frontend/public/components/resource-quota.jsx
+++ frontend/public/components/resource-quota.jsx
@@ -1,9 +1,11 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
+import { FieldLevelHelp } from 'patternfly-react';
 
 import { ColHead, DetailsPage, List, ListHeader, MultiListPage } from './factory';
-import { Kebab, SectionHeading, navFactory, ResourceKebab, ResourceLink, ResourceSummary, convertToBaseValue } from './utils';
+import { Kebab, SectionHeading, navFactory, ResourceKebab, ResourceLink, ResourceSummary, convertToBaseValue, helpLink, HELP_TOPICS } from './utils';
 import { FLAGS, connectToFlags, flagPending } from '../features';
+import { Gauge } from './graphs';
 import { LoadingBox } from './utils/status-box';
 import { referenceForModel } from '../module/k8s';
 import { ResourceQuotaModel, ClusterResourceQuotaModel } from '../models';
@@ -12,10 +14,29 @@ const { common } = Kebab.factory;
 const menuActions = [...common];
 
 const quotaKind = quota => quota.metadata.namespace ? referenceForModel(ResourceQuotaModel) : referenceForModel(ClusterResourceQuotaModel);
+const gaugeChartThresholds = {warn: 90,error: 101};
+
+const quotaScopes = Object.freeze({
+  'Terminating': {label: 'Terminating', description: 'Affects pods that have an active deadline. These pods usually include builds, deployers, and jobs.'},
+  'NotTerminating': {label: 'Not Terminating', description: 'Affects pods that do not have an active deadline. These pods usually include your applications.'},
+  'BestEffort': {label: 'Best Effort', description: 'Affects pods that do not have resource limits set. These pods have a best effort quality of service.'},
+  'NotBestEffort': {label: 'Not Best Effort', description: 'Affects pods that have at least one resource limit set. These pods do not have a best effort quality of service.'},
+});
 
 const getResourceTypes = (quota) => {
   const specHard = _.get(quota, 'spec.hard');
   return _.keys(specHard).sort();
+};
+
+const getResourceUsage = (quota, resourceType) => {
+  const max = _.get(quota, ['status', 'hard', resourceType]) || _.get(quota, ['spec', 'hard', resourceType]);
+  const used = _.get(quota, ['status', 'used', resourceType]);
+  const percent = (!max || !used) ? 0 : convertToBaseValue(used) / convertToBaseValue(max) * 100;
+  return {
+    used,
+    max,
+    percent,
+  };
 };
 
 const Header = props => <ListHeader>
@@ -35,64 +56,119 @@ const Row = ({obj: rq}) => <div className="row co-resource-list__item">
   </div>
 </div>;
 
-const ProportionIcon = ({proportion}) => {
-  let proportionIconClass = 'pficon pficon-unknown';
-  if (proportion === 0) {
-    proportionIconClass = 'fa fa-circle-thin co-resource-quota-empty';
-  } else if (proportion > 0 && proportion < .5) {
-    proportionIconClass = 'pficon pficon-resources-almost-empty';
-  } else if (proportion >= .5 && proportion < 1){
-    proportionIconClass = 'pficon pficon-resources-almost-full';
-  } else if (proportion === 1) {
-    proportionIconClass = 'pficon pficon-resources-full';
-  } else if (proportion > 1) {
-    proportionIconClass = 'pficon pficon-warning-triangle-o';
+const UsageIcon = ({percent}) => {
+  let usageIconClass = 'pficon pficon-unknown';
+  if (percent === 0) {
+    usageIconClass = 'fa fa-circle-thin co-resource-quota-empty';
+  } else if (percent > 0 && percent < 50) {
+    usageIconClass = 'pficon pficon-resources-almost-empty';
+  } else if (percent >= 50 && percent < 100){
+    usageIconClass = 'pficon pficon-resources-almost-full';
+  } else if (percent === 100) {
+    usageIconClass = 'pficon pficon-resources-full';
+  } else if (percent > 100) {
+    usageIconClass = 'pficon pficon-warning-triangle-o';
   }
-  return <i className={proportionIconClass} aria-hidden="true" />;
-};
-
-const getProportionUsed = (used, max) => {
-  if (!max || !used) {
-    return 0;
-  }
-
-  const usedNum = convertToBaseValue(used);
-  const maxNum = convertToBaseValue(max);
-  return usedNum / maxNum;
+  return <i className={usageIconClass} aria-hidden="true" />;
 };
 
 const ResourceUsageRow = ({quota, resourceType}) => {
-  const max = _.get(quota, ['status', 'hard', resourceType]) || _.get(quota, ['spec', 'hard', resourceType]);
-  const used = _.get(quota, ['status', 'used', resourceType]);
-  const proportionUsed = getProportionUsed(used, max);
+  const { used, max, percent } = getResourceUsage(quota, resourceType);
   return <div className="row co-m-row">
     <div className="col-sm-4 col-xs-6 co-break-word">{resourceType}</div>
-    <div className="col-sm-2 hidden-xs co-resource-quota-icon"><ProportionIcon proportion={proportionUsed} /></div>
+    <div className="col-sm-2 hidden-xs co-resource-quota-icon"><UsageIcon percent={percent} /></div>
     <div className="col-sm-3 col-xs-3">{used}</div>
     <div className="col-sm-3 col-xs-3">{max}</div>
   </div>;
 };
 
-const Details = ({obj: rq}) => <React.Fragment>
-  <div className="co-m-pane__body">
-    <SectionHeading text="Resource Quota Overview" />
-    <ResourceSummary resource={rq} showPodSelector={false} showNodeSelector={false} />
-  </div>
-  <div className="co-m-pane__body">
-    <SectionHeading text="Resource Usage" />
-    <div className="co-m-table-grid co-m-table-grid--bordered">
-      <div className="row co-m-table-grid__head">
-        <div className="col-sm-4 col-xs-6">Resource Type</div>
-        <div className="col-sm-2 hidden-xs">Capacity</div>
-        <div className="col-sm-3 col-xs-3">Used</div>
-        <div className="col-sm-3 col-xs-3">Max</div>
-      </div>
-      <div className="co-m-table-grid__body">
-        {getResourceTypes(rq).map(type => <ResourceUsageRow key={type} quota={rq} resourceType={type} />)}
+const QuotaScopes = ({quota}) => {
+  const scopes = _.get(quota, ['spec', 'scopes']);
+  if (_.isEmpty(scopes)) {
+    return <dd>-</dd>;
+  }
+  return scopes.map(scope => {
+    const scopeObj = _.get(quotaScopes, scope);
+    return scopeObj ?
+      <dd key={scope}>
+        <div className="co-resource-quota-scope__label">{scopeObj.label}</div>
+        <div className="co-resource-quota-scope__description">{scopeObj.description}</div>
+      </dd>
+      : <dd key={scope} className="co-resource-quota-scope__label">{scope}</dd>;
+  });
+};
+
+const showCharts = resourceTypes => {
+  const chartResourceTypes = ['requests.cpu', 'cpu', 'limits.cpu', 'requests.memory', 'memory', 'limits.memory'];
+  return _.intersection(resourceTypes, chartResourceTypes).length > 0;
+};
+
+const Details = ({obj: rq}) => {
+  const resourceTypes = getResourceTypes(rq);
+  const resourceTypesSet = new Set(resourceTypes);
+  const showChartRow = showCharts(resourceTypes);
+  return <React.Fragment>
+    <div className="co-m-pane__body">
+      <SectionHeading text="Resource Quota Overview" />
+      {showChartRow && <div className="co-resource-quota-chart-row">
+        {(resourceTypesSet.has('requests.cpu') || resourceTypesSet.has('cpu')) &&
+          <div className="co-resource-quota-chart">
+            <Gauge title="CPU Request" thresholds={gaugeChartThresholds}
+              percent={getResourceUsage(rq, resourceTypesSet.has('requests.cpu') ? 'requests.cpu' : 'cpu').percent} />
+          </div>}
+        {resourceTypesSet.has('limits.cpu') &&
+          <div className="co-resource-quota-chart">
+            <Gauge title="CPU Limit" thresholds={gaugeChartThresholds}
+              percent={getResourceUsage(rq, 'limits.cpu').percent} />
+          </div>}
+        {(resourceTypesSet.has('requests.memory') || resourceTypesSet.has('memory')) &&
+          <div className="co-resource-quota-chart">
+            <Gauge title="Memory Request" thresholds={gaugeChartThresholds}
+              percent={getResourceUsage(rq, resourceTypesSet.has('requests.memory') ? 'requests.memory' : 'memory').percent} />
+          </div>}
+        {resourceTypesSet.has('limits.memory') &&
+          <div className="co-resource-quota-chart">
+            <Gauge title="Memory Limit" thresholds={gaugeChartThresholds}
+              percent={getResourceUsage(rq, 'limits.memory').percent} />
+          </div>}
+      </div>}
+      <div className="row">
+        <div className="col-sm-6">
+          <ResourceSummary resource={rq} showPodSelector={false} showNodeSelector={false} />
+        </div>
+        <div className="col-sm-6">
+          <dl className="co-m-pane__details">
+            <dt>Scopes</dt>
+            <QuotaScopes quota={rq} />
+          </dl>
+        </div>
       </div>
     </div>
-  </div>
-</React.Fragment>;
+    <div className="co-m-pane__body">
+      <SectionHeading text="Resource Quota Details" style={{display: 'block', marginBottom: '20px'}}>
+        <FieldLevelHelp content={
+          <div>
+            <p>Requests are the amount of resources you expect to use. These are used when establishing if the cluster can fulfill your Request.</p>
+            <p>Limits are a maximum amount of a resource you can consume. Applications consuming more than the Limit may be terminated.</p>
+            <p>A cluster administrator can establish limits on both the amount you can Request and your Limits with a &nbsp;
+              <a href={helpLink(HELP_TOPICS.COMPUTE_RESOURCES_QUOTA)} target="_blank" rel="noopener">Resource Quota</a>.</p>
+          </div>
+        } />
+      </SectionHeading>
+      <div className="co-m-table-grid co-m-table-grid--bordered">
+        <div className="row co-m-table-grid__head">
+          <div className="col-sm-4 col-xs-6">Resource Type</div>
+          <div className="col-sm-2 hidden-xs">Capacity</div>
+          <div className="col-sm-3 col-xs-3">Used</div>
+          <div className="col-sm-3 col-xs-3">Max</div>
+        </div>
+        <div className="co-m-table-grid__body">
+          {resourceTypes.map(type => <ResourceUsageRow key={type} quota={rq} resourceType={type} />)}
+        </div>
+      </div>
+    </div>
+  </React.Fragment>;
+};
 
 export const ResourceQuotasList = props => <List {...props} Header={Header} Row={Row} />;
 
