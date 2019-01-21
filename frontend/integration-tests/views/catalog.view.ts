f3fb95253dda8081486a04278b03a2c0a7db206c
David Taylor <dtaylor@redhat.com>
2019-01-21 12:57:28 -0500

    Added 'creates a service instance and binding' test to dev catalog e2e test suite

--- frontend/integration-tests/views/catalog.view.ts
+++ frontend/integration-tests/views/catalog.view.ts
@@ -1,4 +1,4 @@
-import { $, $$, browser, ExpectedConditions as until } from 'protractor';
+import { $, $$, browser, by, ExpectedConditions as until } from 'protractor';
 
 export const categoryTabs = $$('.vertical-tabs-pf-tab > a');
 export const pageHeading = $('.co-catalog-page__heading');
@@ -6,3 +6,6 @@ export const pageNumberItemsHeading = $('.co-catalog-page__num-items');
 export const pageHeadingNumberOfItems = () => pageNumberItemsHeading.getText()
   .then(text => parseInt(text.substring(0, text.indexOf(' items')), 10));
 export const catalogDetailsLoaded = () => browser.wait(until.presenceOf($('.modal-content')), 10000).then(() => browser.sleep(1000));
+export const createServiceInstanceButton = $('.co-catalog-page__overlay-create');
+export const createServiceInstanceForm = $('.co-create-service-instance');
+export const createServiceBindingButton = $('.co-well').element(by.buttonText('Create Service Binding'));
