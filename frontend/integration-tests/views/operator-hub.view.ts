bb6092c09d8f477a85c4b785c68dcfdf58d8e832
Jeffrey Phillips <jephilli@redhat.com>
2019-01-23 09:04:09 -0500

    Update operator hub scenario tests and add to E2E tests.

--- frontend/integration-tests/views/operator-hub.view.ts
+++ frontend/integration-tests/views/operator-hub.view.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef, no-unused-vars */
 
-import { browser, $, ExpectedConditions as until } from 'protractor';
+import {browser, $, ExpectedConditions as until, $$} from 'protractor';
 
 // Modal views
 export const operatorModal = $('.modal-content');
@@ -10,3 +10,12 @@ export const operatorModalTitle = operatorModal.$('.catalog-item-header-pf-title
 export const closeOperatorModal = () => operatorModal.$('.close').click();
 export const operatorModalIsClosed = () => browser.wait(until.not(until.presenceOf(operatorModal)), 1000)
   .then(() => browser.sleep(500));
+
+export const communityWarningModal = $('.co-modal-ignore-warning');
+export const clickShowCommunityToggle = () => $$('.co-catalog-page__filter-toggle').click();
+export const operatorCommunityWarningIsLoaded = () => browser.wait(until.presenceOf(communityWarningModal), 1000)
+  .then(() => browser.sleep(500));
+export const operatorCommunityWarningIsClosed = () => browser.wait(until.not(until.presenceOf(communityWarningModal)), 1000)
+  .then(() => browser.sleep(500));
+export const closeCommunityWarningModal = () => communityWarningModal.$('.close').click();
+export const acceptCommunityWarningModal = () => communityWarningModal.$('.btn-primary').click();
