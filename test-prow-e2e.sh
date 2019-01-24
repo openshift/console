c66b96e0a95482a2ef6db3ff1837b6704cdc9668
Joseph Caiani <jcaiani@redhat.com>
2019-01-24 14:02:55 -0500

    Combine all scripts to single e2e test script

---
+++ test-prow-e2e.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+ARTIFACT_DIR=/tmp/artifacts
+export ARTIFACT_DIR
+
+./build.sh
+
+oc login -u kubeadmin -p $(cat "${ARTIFACT_DIR}/installer/auth/kubeadmin-password")
+
+source ./contrib/oc-environment.sh
+
+kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_06-rh-operators.configmap.yaml
+kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_09-rh-operators.catalogsource.yaml
+
+./test-gui.sh e2e
+
+cp -rv ./frontend/gui_test_screenshots "${ARTIFACT_DIR}/gui_test_screenshots"
