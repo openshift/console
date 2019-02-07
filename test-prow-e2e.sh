e526436432272e5114a191ff4d68ab929e2755c4
Joseph Caiani <jcaiani@redhat.com>
2019-02-07 12:30:22 -0500

    use trap to copy test files to artifacts dir on exit

--- test-prow-e2e.sh
+++ test-prow-e2e.sh
@@ -5,6 +5,13 @@ set -exuo pipefail
 ARTIFACT_DIR=/tmp/artifacts
 export ARTIFACT_DIR
 
+function copyArtifacts {
+  echo "Copying artifacts from $(pwd)..."
+  cp -rv ./gui_test_screenshots "${ARTIFACT_DIR}/gui_test_screenshots"
+}
+
+trap copyArtifacts EXIT
+
 ./build.sh
 
 oc login -u kubeadmin -p $(cat "${ARTIFACT_DIR}/installer/auth/kubeadmin-password")
@@ -14,6 +21,4 @@ source ./contrib/oc-environment.sh
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_06-rh-operators.configmap.yaml
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_09-rh-operators.catalogsource.yaml
 
-./test-gui.sh e2e
-
-cp -rv ./frontend/gui_test_screenshots "${ARTIFACT_DIR}/gui_test_screenshots"
+. ./test-gui.sh e2e
