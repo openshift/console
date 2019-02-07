b13b685203e8889f68d6993aca96f37454916a3f
Joseph Caiani <jcaiani@redhat.com>
2019-02-07 15:52:21 -0500

    update cp src and remove extra source

--- test-prow-e2e.sh
+++ test-prow-e2e.sh
@@ -7,7 +7,7 @@ export ARTIFACT_DIR
 
 function copyArtifacts {
   echo "Copying artifacts from $(pwd)..."
-  cp -rv ./gui_test_screenshots "${ARTIFACT_DIR}/gui_test_screenshots"
+  cp -rv ./frontend/gui_test_screenshots "${ARTIFACT_DIR}/gui_test_screenshots"
 }
 
 trap copyArtifacts EXIT
@@ -21,4 +21,4 @@ source ./contrib/oc-environment.sh
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_06-rh-operators.configmap.yaml
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_09-rh-operators.catalogsource.yaml
 
-. ./test-gui.sh e2e
+./test-gui.sh e2e
