08ab5b1b9556d3164a6a23703c186cf555135254
Joseph Caiani <jcaiani@redhat.com>
2019-02-11 14:28:17 -0500

    Check for dist folder and index.html before running e2e tests

--- test-prow-e2e.sh
+++ test-prow-e2e.sh
@@ -21,4 +21,13 @@ source ./contrib/oc-environment.sh
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_06-rh-operators.configmap.yaml
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/okd/manifests/0.8.0/0000_30_09-rh-operators.catalogsource.yaml
 
+echo "Checking for dist files:"
+while [ ! -f ./frontend/public/dist/index.html ]
+do
+ echo "#"
+ sleep 2
+done
+
+echo "Build complete and dist folder ready...running e2e tests"
+
 ./test-gui.sh e2e
