a9b8d6577cff947e68f7c2681f00169b62a8c205
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2019-02-25 21:16:03 +0530

    feat(perspective-switcher): add switcher menu with navigational links (#3)

--- frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
+++ frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
@@ -1,7 +1,42 @@
 import * as React from 'react';
+import { Modal } from '@patternfly/react-core';
+import { NavLink } from 'react-router-dom';
+import './PerspectiveSwitcher.scss'
 
-const PerspectiveSwitcher: React.SFC = () => (
-  <h1>This is Perspective Switcher Component.</h1>
+export interface PerspectiveSwitcherProps {
+  isNavOpen: boolean,
+  onNavToggle: (MouseEvent) => void,
+}
+
+const PerspectiveSwitcher: React.SFC<PerspectiveSwitcherProps> = (props: PerspectiveSwitcherProps) => (
+  <Modal
+    isLarge
+    title="Switcher Menu"
+    isOpen={props.isNavOpen}
+    onClose={props.onNavToggle}
+    className="devops-perspective-switcher">
+    <ul>
+      <li>
+        <NavLink
+          to="/devconsole"
+          onClick={props.onNavToggle}
+          activeClassName="devops-perspective-switcher__active-link"
+        >
+          DevOps Console
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/k8s/cluster/projects"
+          onClick={props.onNavToggle}
+          isActive={(match, { pathname }):boolean => !pathname.startsWith('/devconsole')}
+          activeClassName="devops-perspective-switcher__active-link"
+        >
+          Admin Console
+        </NavLink>
+      </li>
+    </ul>
+  </Modal>
 );
 
 export default PerspectiveSwitcher;
