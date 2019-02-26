2296efc47cd50ba7f0e816f048fe5c29557b4e44
Robb Hamilton <rhamilto@redhat.com>
2019-02-26 15:38:59 -0500

    Add missing OpenShift Dedicated option to getProductName

--- frontend/public/branding.ts
+++ frontend/public/branding.ts
@@ -4,6 +4,8 @@ const getProductName = () => {
       return 'OpenShift Container Platform';
     case 'online':
       return 'OpenShift Online';
+    case 'dedicated':
+      return 'OpenShift Dedicated';
     default:
       return 'OKD';
   }
