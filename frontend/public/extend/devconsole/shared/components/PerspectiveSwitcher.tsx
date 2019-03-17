3bb9041ae669b18bf4733501f30a9860e5131657
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2019-03-17 23:33:50 +0530

    Improve the design of the perspective switcher (#17)

--- frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
+++ frontend/public/extend/devconsole/shared/components/PerspectiveSwitcher.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Modal } from '@patternfly/react-core';
 import { NavLink } from 'react-router-dom';
 import './PerspectiveSwitcher.scss';
+import * as openshiftIconImg from '../../../../imgs/openshift-favicon.png';
 
 export interface PerspectiveSwitcherProps {
   isNavOpen: boolean,
@@ -12,31 +13,35 @@ export interface PerspectiveSwitcherProps {
 const PerspectiveSwitcher: React.SFC<PerspectiveSwitcherProps> = (props: PerspectiveSwitcherProps) => (
   <Modal
     isLarge
-    title="Switcher Menu"
+    title=""
     isOpen={props.isNavOpen}
     onClose={props.onNavToggle}
     className="devops-perspective-switcher">
-    <ul>
-      <li>
-        <NavLink
-          to="/devops"
-          onClick={props.onNavToggle}
-          activeClassName="devops-perspective-switcher__active-link"
-        >
-          DevOps Console
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/k8s/cluster/projects"
-          onClick={props.onNavToggle}
-          isActive={(match, { pathname }):boolean => !pathname.startsWith('/devops')}
-          activeClassName="devops-perspective-switcher__active-link"
-        >
-          Admin Console
-        </NavLink>
-      </li>
-    </ul>
+    <nav className="pf-c-nav">
+      <ul className="pf-c-nav__simple-list">
+        <li className="pf-c-nav__item">
+          <NavLink
+            to="/devops"
+            onClick={props.onNavToggle}
+            className="pf-c-nav__link"
+            activeClassName="pf-m-current"
+          >
+          <img src={openshiftIconImg} alt="DevOps Console" className="devops-perspective-switcher__openshift-logo"/>  DevOps Console
+          </NavLink>
+        </li>
+        <li className="pf-c-nav__item">
+          <NavLink
+            to="/k8s/cluster/projects"
+            onClick={props.onNavToggle}
+            className="pf-c-nav__link"
+            isActive={(match, { pathname }):boolean => !pathname.startsWith('/devops')}
+            activeClassName="pf-m-current"
+          >
+            <img src={openshiftIconImg} alt="Admin Console" className="devops-perspective-switcher__openshift-logo"/> Admin Console
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   </Modal>
 );
 
