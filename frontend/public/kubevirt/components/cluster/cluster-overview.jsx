6762b34df55c07b441790b158c1b27cd22dad07a
Marek Libra <marek.libra@gmail.com>
2019-04-11 10:09:47 +0200

    Utilization: Add Disk Usage chart (#270)
    and refactor prometheus queries of disk capacity so they
    can be reused for utilization.

--- frontend/public/kubevirt/components/cluster/cluster-overview.jsx
+++ frontend/public/kubevirt/components/cluster/cluster-overview.jsx
@@ -5,6 +5,7 @@ import {
   getResource,
   ClusterOverviewContext,
   complianceData,
+  getCapacityStats,
 } from 'kubevirt-web-ui-components';
 
 import {
@@ -33,16 +34,18 @@ const OPENSHIFT_VERSION_QUERY = 'openshift_build_info{job="apiserver"}';
 
 const CAPACITY_MEMORY_TOTAL_QUERY = 'sum(kube_node_status_capacity_memory_bytes)';
 
-const CAPACITY_STORAGE_TOTAL_QUERY = 'sum(kubelet_volume_stats_capacity_bytes)'; // available with Ceph
-const CAPACITY_STORAGE_USED_QUERY = 'sum(kubelet_volume_stats_used_bytes)';
+const CAPACITY_STORAGE_TOTAL_BASE_CEPH_METRIC = 'ceph_cluster_total_bytes'; // available with Ceph only
+const CAPACITY_STORAGE_TOTAL_QUERY = CAPACITY_STORAGE_TOTAL_BASE_CEPH_METRIC;
 const CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY = 'sum(node_filesystem_avail_bytes)';
-const CAPACITY_STORAGE_USED_DEFAULT_QUERY = 'sum(node_filesystem_avail_bytes) - sum(node_filesystem_free_bytes)';
 
 const CAPACITY_NETWORK_TOTAL_QUERY = 'sum(avg by(instance)(node_network_speed_bytes))'; // TODO(mlibra): needs to be refined
 const CAPACITY_NETWORK_USED_QUERY = 'sum(node:node_net_utilisation:sum_irate)';
 
 const UTILIZATION_CPU_USED_QUERY = '((sum(node:node_cpu_utilisation:avg1m) / count(node:node_cpu_utilisation:avg1m)) * 100)[60m:5m]';
 const UTILIZATION_MEMORY_USED_QUERY = '(sum(kube_node_status_capacity_memory_bytes) - sum(kube_node_status_allocatable_memory_bytes))[60m:5m]'; // TOTAL is reused from CAPACITY_MEMORY_TOTAL_QUERY
+
+const UTILIZATION_STORAGE_USED_QUERY = 'ceph_cluster_total_used_bytes[60m:5m]';
+const UTILIZATION_STORAGE_USED_DEFAULT_QUERY = '(sum(node_filesystem_avail_bytes) - sum(node_filesystem_free_bytes))[60m:5m]';
 
 const REFRESH_TIMEOUT = 5000;
 
@@ -95,6 +98,7 @@ export class ClusterOverview extends React.Component {
     this.setDetailsOpenshiftResponse = this._setDetailsOpenshiftResponse.bind(this);
     this.setCapacityData = this._setCapacityData.bind(this);
     this.setUtilizationData = this._setUtilizationData.bind(this);
+    this.getStorageMetrics = this._getStorageMetrics.bind(this);
   }
 
   _setConsumersData(key, response) {
@@ -147,14 +151,42 @@ export class ClusterOverview extends React.Component {
     }));
   }
 
-  getPrometheusMetrics() {
+  async _getStorageMetrics() {
+    try {
+      let queryTotal = CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY;
+      let queryUsed = UTILIZATION_STORAGE_USED_DEFAULT_QUERY;
+
+      const metrics = await this.getPrometheusMetrics();
+      if (_.get(metrics, 'data', []).find(metric => metric === CAPACITY_STORAGE_TOTAL_BASE_CEPH_METRIC)) {
+        const cephData = await this.getPrometheusQuery(CAPACITY_STORAGE_TOTAL_QUERY);
+        if (getCapacityStats(cephData)) { // Ceph data are available
+          queryTotal = CAPACITY_STORAGE_TOTAL_QUERY;
+          queryUsed = UTILIZATION_STORAGE_USED_QUERY;
+        }
+      }
+
+      this.fetchPrometheusQuery(queryTotal, response => this.setCapacityData('storageTotal', response));
+      this.fetchPrometheusQuery(queryUsed, response => this.setUtilizationData('storageUsed', response));
+    } catch (error) {
+      if (this._isMounted) {
+        this.setCapacityData('storageTotal', error);
+        this.setUtilizationData('storageUsed', error);
+      }
+    }
+  }
+
+  async getPrometheusMetrics() {
     const url = `${getPrometheusBaseURL()}/api/v1/label/__name__/values`;
     return coFetchJSON(url);
   }
 
-  fetchPrometheusQuery(query, callback) {
+  async getPrometheusQuery(query) {
     const url = `${getPrometheusBaseURL()}/api/v1/query?query=${encodeURIComponent(query)}`;
-    coFetchJSON(url).then(result => {
+    return coFetchJSON(url);
+  }
+
+  fetchPrometheusQuery(query, callback) {
+    this.getPrometheusQuery(query).then(result => {
       if (this._isMounted) {
         callback(result);
       }
@@ -200,20 +232,7 @@ export class ClusterOverview extends React.Component {
     this.fetchPrometheusQuery(CAPACITY_NETWORK_TOTAL_QUERY, response => this.setCapacityData('networkTotal', response));
     this.fetchPrometheusQuery(CAPACITY_NETWORK_USED_QUERY, response => this.setCapacityData('networkUsed', response));
 
-    this.getPrometheusMetrics().then(metrics => {
-      if (_.get(metrics, 'data', []).find(metric => metric === 'kubelet_volume_stats_capacity_bytes')) {
-        this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_QUERY, response => this.setCapacityData('storageTotal', response));
-        this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_QUERY, response => this.setCapacityData('storageUsed', response));
-      } else {
-        this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY, response => this.setCapacityData('storageTotal', response));
-        this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_DEFAULT_QUERY, response => this.setCapacityData('storageUsed', response));
-      }
-    }).catch(error => {
-      if (this._isMounted) {
-        this.setCapacityData('storageTotal', error);
-        this.setCapacityData('storageUsed', error);
-      }
-    });
+    this.getStorageMetrics();
 
     this.fetchPrometheusQuery(UTILIZATION_CPU_USED_QUERY, response => this.setUtilizationData('cpuUtilization', response));
     this.fetchPrometheusQuery(UTILIZATION_MEMORY_USED_QUERY, response => this.setUtilizationData('memoryUtilization', response));
