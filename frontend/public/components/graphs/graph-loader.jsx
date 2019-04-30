c5c00825a743f39fb7b8620adecf45ad431f9426
Samuel Padgett <spadgett@redhat.com>
2019-04-30 08:57:28 -0400

    Remove unused cluster-health component

--- frontend/public/components/graphs/graph-loader.jsx
+++ frontend/public/components/graphs/graph-loader.jsx
@@ -2,5 +2,4 @@ export { Bar } from './bar';
 export { Gauge } from './gauge';
 export { Line } from './line';
 export { QueryBrowser } from './query-browser';
-export { Scalar } from './scalar';
 export { Donut } from './donut';
