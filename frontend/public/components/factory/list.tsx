6f89f2bece98f63c8ec95caf2f401c62129e46a4
sg00dwin <sgoodwin@redhat.com>
2019-05-07 10:43:49 -0400

    Add skeleton screen for list view
    https://jira.coreos.com/browse/CONSOLE-1037

--- frontend/public/components/factory/list.tsx
+++ frontend/public/components/factory/list.tsx
@@ -490,7 +490,7 @@ export const List = connect(stateToProps, {sortList: UIActions.sortList})(
       </React.Fragment>;
 
       return <div className="co-m-table-grid co-m-table-grid--bordered">
-        { mock ? children : <StatusBox {...this.props}>{children}</StatusBox> }
+        { mock ? children : <StatusBox skeletonClass="loading-skeleton--table" {...this.props}>{children}</StatusBox> }
       </div>;
     }
   });
