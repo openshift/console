0ecd7d6e95d7160429b88797c9789bb269950dfe
Jon Jackson <jonjacks@redhat.com>
2019-05-09 12:09:03 -0400

    Fix hot looping issue with prometheus poll effect and ref width effect

--- frontend/public/components/utils/use-ref-width.ts
+++ frontend/public/components/utils/use-ref-width.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
 import { useSafetyFirst } from '../safety-first';
 
@@ -15,7 +16,7 @@ export const useRefWidth = () => {
       window.removeEventListener('resize', handleResize);
       window.removeEventListener('nav_toggle', handleResize);
     };
-  }, [setWidth]);
+  }, []);
 
   return [ref, width] as [React.Ref<HTMLDivElement>, number];
 };
