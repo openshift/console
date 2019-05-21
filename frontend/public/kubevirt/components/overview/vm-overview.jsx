2b8e8a577b6e50cc064325ff642e4297234f34e3
Yaacov Zamir <kobi.zamir@gmail.com>
2019-05-21 12:36:16 +0300

    Add missing actions props to overview header (#377)
    * Add missing action args to vm overview

--- frontend/public/kubevirt/components/overview/vm-overview.jsx
+++ frontend/public/kubevirt/components/overview/vm-overview.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import * as _ from 'lodash-es';
+import { getResource } from 'kubevirt-web-ui-components';
 
-import { VirtualMachineModel } from '../../../models';
+import { VirtualMachineModel, PodModel, VirtualMachineInstanceMigrationModel, VirtualMachineInstanceModel } from '../../../models';
 import { ResourceOverviewDetails } from '../okdcomponents';
 
 import { menuActions } from '../vm/menu-actions';
 import { ConnectedVmDetails } from '../vm';
+import { WithResources } from '../utils/withResources';
 
 const VirtualMachineOverviewDetails = ({ item }) =>
   <div className="overview__sidebar-pane-body resource-overview__body">
@@ -18,10 +21,44 @@ const tabs = [
   },
 ];
 
-export const VirtualMachineOverviewPage = ({ item }) =>
-  <ResourceOverviewDetails
-    item={item}
-    kindObj={VirtualMachineModel}
-    menuActions={menuActions}
-    tabs={tabs}
-  />;
+const ConnctedResourceOverviewDetails = ({ item, pods, migrations, vmi }) => {
+  const actionArgs = {};
+  actionArgs[PodModel.kind] = pods;
+  actionArgs[VirtualMachineInstanceMigrationModel.kind] = migrations;
+  actionArgs[VirtualMachineInstanceModel.kind] = vmi;
+
+  const menuActions_ = _.map(menuActions, m => (kind, vm) => m(kind, vm, actionArgs));
+  return (
+    <ResourceOverviewDetails
+      item={item}
+      kindObj={VirtualMachineModel}
+      menuActions={menuActions_}
+      tabs={tabs}
+    />
+  );
+};
+
+export const VirtualMachineOverviewPage = ({ item }) => {
+  const vm = item.obj;
+  const { name, namespace } = vm.metadata;
+  const resourceMap = {
+    vmi: {
+      resource: getResource(VirtualMachineInstanceModel, {name, namespace, isList: false}),
+      ignoreErrors: true,
+    },
+    pods: {
+      resource: getResource(PodModel, { namespace }),
+    },
+    migrations: {
+      resource: getResource(VirtualMachineInstanceMigrationModel, {namespace}),
+    },
+  };
+
+  return (
+    <WithResources resourceMap={resourceMap}>
+      <ConnctedResourceOverviewDetails
+        item={item}
+      />
+    </WithResources>
+  );
+};
