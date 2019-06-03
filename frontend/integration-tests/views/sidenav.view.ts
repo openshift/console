96ffc3f15c6e3927afb61d9e1b6a92515223eae5
Jadhaj <jhajyahy@redhat.com>
2019-06-03 18:04:50 +0300

    fixed sidenav.view.ts navigation issue fixed lint checks removed empty lines check elent visibility instead of aria-expanded limit searching of sidenav locators

--- frontend/integration-tests/views/sidenav.view.ts
+++ frontend/integration-tests/views/sidenav.view.ts
@@ -1,10 +1,16 @@
+/* eslint-disable no-undef, no-unused-vars */
+
 import { $$, by, browser, element, ExpectedConditions as until } from 'protractor';
 
-export const navSectionFor = (name: string) => element(by.cssContainingText('.pf-c-nav > .pf-c-nav__list > .pf-c-nav__item', name));
+export const navSectionFor = (name: string) =>  element(by.css('.pf-c-nav__list')).element(by.linkText(name));
 
 export const clickNavLink = (path: [string, string]) => browser.wait(until.visibilityOf(navSectionFor(path[0])))
-  .then(() => navSectionFor(path[0]).click())
-  .then(() => browser.wait(until.visibilityOf(navSectionFor(path[0]).element(by.linkText(path[1])))))
-  .then(() => navSectionFor(path[0]).element(by.linkText(path[1])).click());
+  .then(() => navSectionFor(path[1]).isDisplayed().then((isVisible: boolean) => {
+    if (!isVisible) {
+        navSectionFor(path[0]).click();
+        browser.wait(until.visibilityOf(navSectionFor(path[1])));
+    }
+  }))
+  .then(() => navSectionFor(path[1]).click());
 
 export const activeLink = $$('.pf-c-nav__link.pf-m-current');
