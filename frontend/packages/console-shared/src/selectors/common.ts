a1be33827dfd0bba6ac0eee26dea29a1d747d959
suomiy <suomiy@gmail.com>
2019-06-19 11:44:38 +0200

    introduce types to @console/shared

--- frontend/packages/console-shared/src/selectors/common.ts
+++ frontend/packages/console-shared/src/selectors/common.ts
@@ -2,5 +2,11 @@ import * as _ from 'lodash';
 
 import { K8sResourceKind } from '@console/internal/module/k8s';
 
-export const getName = (value: K8sResourceKind): string => _.get(value, 'metadata.name');
-export const getNamespace = (value: K8sResourceKind): string => _.get(value, 'metadata.namespace');
+export const getName = (value: K8sResourceKind) =>
+  _.get(value, 'metadata.name') as K8sResourceKind['metadata']['name'];
+export const getNamespace = (value: K8sResourceKind) =>
+  _.get(value, 'metadata.namespace') as K8sResourceKind['metadata']['namespace'];
+export const getUid = (value: K8sResourceKind) =>
+  _.get(value, 'metadata.uid') as K8sResourceKind['metadata']['uid'];
+export const getDeletetionTimestamp = (value: K8sResourceKind) =>
+  _.get(value, 'metadata.deletionTimestamp') as K8sResourceKind['metadata']['deletionTimestamp'];
