3677866567befe02f162590ef442e9bad4b468fb
xiaocwan <xiaocwan@localhost.localdomain>
2019-06-21 15:52:49 +0800

    Add Unit tests for QuotaGaugeCharts and QuotaScopesList

--- frontend/__tests__/components/resource-quota.spec.tsx
+++ frontend/__tests__/components/resource-quota.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
-import { ResourceQuotaTableRow, UsageIcon, ResourceUsageRow } from '../../public/components/resource-quota';
+import { QuotaGaugeCharts, QuotaScopesList, ResourceQuotaTableRow, ResourceUsageRow, UsageIcon } from '../../public/components/resource-quota';
 
 describe(ResourceQuotaTableRow.displayName, () => {
   let wrapper: ShallowWrapper;
@@ -41,13 +41,9 @@ describe(ResourceQuotaTableRow.displayName, () => {
   });
 });
 
-describe('Check quota table columns by ResourceUsageRow', () => {
-  let wrapper: ShallowWrapper;
+describe('Renders quota table columns by ResourceUsageRow', () => {
   const quota = { 'status': {'hard': {'limits.cpu' : 2}, 'used': {'limits.cpu' : 1} } };
-
-  beforeEach(() => {
-    wrapper = shallow(<ResourceUsageRow resourceType={'limits.cpu'} quota={quota} />);
-  });
+  const wrapper = shallow(<ResourceUsageRow resourceType={'limits.cpu'} quota={quota} />);
 
   it('renders ResourceUsageRow for each columns', () => {
     const col0 = wrapper.childAt(0);
@@ -61,5 +57,63 @@ describe('Check quota table columns by ResourceUsageRow', () => {
 
     const col3 = wrapper.childAt(3);
     expect(col3.text()).toBe('2');
+  });
+});
+
+describe('Renders QuotaGaugeCharts', () => {
+  const resourceTypes = ['limits.cpu','requests.memory'];
+  const quota = {
+    'status': {
+      'hard': {
+        'limits.cpu': '2',
+        'requests.memory': '1Gi',
+      },
+      'used': {
+        'limits.cpu': '0',
+        'requests.memory': '2Gi',
+      },
+    },
+  };
+  const wrapper = shallow(<QuotaGaugeCharts resourceTypes={resourceTypes} quota={quota} />);
+  const chartRow = wrapper.find('.co-resource-quota-chart-row');
+
+  it('renders QuotaGaugeCharts for CPU Request', () => {
+    expect(chartRow.find({title:'CPU Request'}).find({centerText:'No Request'}).exists()).toBe(true);
+  });
+
+  it('renders QuotaGaugeCharts for CPU Limit', () => {
+    expect(chartRow.find({title:'CPU Limit'}).prop('percent')).toBe(0);
+  });
+
+  it('renders QuotaGaugeCharts for Memory Request', () => {
+    expect(chartRow.find({title:'Memory Request'}).prop('percent')).toBe(200);
+  });
+
+  it('renders QuotaGaugeCharts for Memory Limit', () => {
+    expect(chartRow.find({title:'Memory Limit'}).find({centerText:'No Limit'}).exists()).toBe(true);
+  });
+});
+
+describe('Renders quota scopes by QuotaScopesList', () => {
+  let wrapper: ShallowWrapper;
+  const scopes = ['NotTerminating','NotBestEffort'];
+  beforeEach(() => {
+    wrapper = shallow(<QuotaScopesList scopes={scopes} />);
+  });
+
+  it('renders label for the first scope', () => {
+    expect(wrapper.find('.co-resource-quota-scope__label').at(0).text()).toBe('Not Terminating');
+  });
+
+  it('renders label for the first description', () => {
+    expect(wrapper.find('.co-resource-quota-scope__description').at(0).text()).toContain('pods that do not have an active deadline');
+  });
+
+  it('renders label for the second scope', () => {
+    expect(wrapper.find('.co-resource-quota-scope__label').at(1).text()).toBe('Not Best Effort');
+  });
+
+  it('renders label for the second description', () => {
+    expect(wrapper.find('.co-resource-quota-scope__description').at(1).text()).toContain('pods that have at least one resource limit set');
   });
 });
