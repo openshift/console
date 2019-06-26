9f3ace33286580a3be7b8aa418c65ebf6d2652f1
invinciblejai <mailjai.vardhan@gmail.com>
2019-06-26 18:06:26 +0530

    fix(knative): adds utils to create knative resources

---
+++ frontend/packages/dev-console/src/utils/create-knative-utils.ts
@@ -0,0 +1,41 @@
+import { k8sCreate, K8sResourceKind } from '@console/internal/module/k8s';
+import { KsServiceModel } from '../models';
+
+export const createKnativeService = (
+  name: string,
+  namespace: string,
+  imageStreamName: string,
+  imageStreamTag: string,
+  cpuResource: string = '100m',
+  memoryResource: string = '100Mi',
+): Promise<K8sResourceKind> => {
+  const knativeDeployResource = {
+    kind: 'Service',
+    apiVersion: 'serving.knative.dev/v1alpha1',
+    metadata: {
+      name,
+      namespace,
+    },
+    spec: {
+      runLatest: {
+        configuration: {
+          revisionTemplate: {
+            spec: {
+              container: {
+                image: `${imageStreamName}:${imageStreamTag}`,
+                resources: {
+                  requests: {
+                    cpu: `${cpuResource}`,
+                    memory: `${memoryResource}`,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return k8sCreate(KsServiceModel, knativeDeployResource);
+};
