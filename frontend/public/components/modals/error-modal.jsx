ce6a36f6fe8f15937df5c65e27f21054b14ea4f9
Marek Libra <mlibra@redhat.com>
2019-06-26 10:36:56 +0200

    errorModal: refactor to use React Component
    No API or functional change.
    
    The component is intended to be reused by other commits.

--- frontend/public/components/modals/error-modal.jsx
+++ frontend/public/components/modals/error-modal.jsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 
 import {createModalLauncher, ModalTitle, ModalBody, ModalFooter} from '../factory/modal';
 
-export const errorModal = createModalLauncher(
-  ({error, cancel}) => {
-    return (
-      <div className="modal-content">
-        <ModalTitle>Error</ModalTitle>
-        <ModalBody>{error}</ModalBody>
-        <ModalFooter inProgress={false} errorMessage=""><button type="button" onClick={(e) => cancel(e)} className="btn btn-default">OK</button></ModalFooter>
-      </div>
-    );
-  }
-);
+export const ModalErrorContent = ({ error, title = 'Error', cancel }) => (
+  <div className="modal-content">
+    <ModalTitle>{title}</ModalTitle>
+    <ModalBody>{error}</ModalBody>
+    <ModalFooter inProgress={false} errorMessage="">
+      <button type="button" onClick={cancel} className="btn btn-default">OK</button>
+    </ModalFooter>
+  </div>);
+
+export const errorModal = createModalLauncher(ModalErrorContent);
