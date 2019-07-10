f87d9199504859bc77015f6f345742b8fddef977
yaacov <kobi.zamir@gmail.com>
2019-07-10 12:18:56 +0300

    Edit vm template yaml redirect

--- frontend/packages/kubevirt-plugin/src/components/vm-templates/vm-template-create-yaml.tsx
+++ frontend/packages/kubevirt-plugin/src/components/vm-templates/vm-template-create-yaml.tsx
@@ -1,13 +1,59 @@
 import * as React from 'react';
+import { safeLoad } from 'js-yaml';
 
-import { CreateYAML } from '@console/internal/components/create-yaml';
 import { TemplateModel } from '@console/internal/models';
+import { connectToPlural } from '@console/internal/kinds';
+import { CreateYAMLProps } from '@console/internal/components/create-yaml';
+import {
+  LoadingBox,
+  AsyncComponent,
+  resourcePathFromModel,
+} from '@console/internal/components/utils';
+import { ErrorPage404 } from '@console/internal/components/error';
+import { getNamespace, getName } from '@console/shared';
+
 import { VMTemplateYAMLTemplates } from '../../models/templates';
+import { VM_TEMPLATE_CREATE_HEADER } from '../../constants/vm-templates';
+
+const CreateVMTemplateYAMLConnected = connectToPlural(
+  ({ match, kindsInFlight, kindObj }: CreateYAMLProps) => {
+    if (!kindObj) {
+      if (kindsInFlight) {
+        return <LoadingBox />;
+      }
+      return <ErrorPage404 />;
+    }
+
+    const template = VMTemplateYAMLTemplates.getIn(['vm-template']);
+    const obj = safeLoad(template);
+    obj.kind = kindObj.kind;
+    obj.metadata = obj.metadata || {};
+    obj.metadata.namespace = match.params.ns || 'default';
+
+    const vmTemplateObjPath = (o) =>
+      resourcePathFromModel(
+        { ...TemplateModel, plural: 'vmtemplates' },
+        getName(o),
+        getNamespace(o),
+      );
+
+    return (
+      <AsyncComponent
+        loader={() =>
+          import('@console/internal/components/droppable-edit-yaml').then(
+            (c) => c.DroppableEditYAML,
+          )
+        }
+        obj={obj}
+        create
+        kind={kindObj.kind}
+        resourceObjPath={vmTemplateObjPath}
+        header={VM_TEMPLATE_CREATE_HEADER}
+      />
+    );
+  },
+);
 
 export const CreateVMTemplateYAML = (props: any) => (
-  <CreateYAML
-    {...props as any}
-    plural={TemplateModel.plural}
-    template={VMTemplateYAMLTemplates.getIn(['vm-template'])}
-  />
+  <CreateVMTemplateYAMLConnected {...props as any} plural={TemplateModel.plural} />
 );
