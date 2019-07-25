475d670c88c55ae390b57e48b5bf2ff00b20266a
Rohit Rai <rohitkrai03@gmail.com>
2019-07-26 04:16:11 +0530

    Update FormSection to control width, add pf4 form class and some cleanup

--- frontend/packages/dev-console/src/components/import/section/FormSection.tsx
+++ frontend/packages/dev-console/src/components/import/section/FormSection.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
-import FormSectionHeading from './FormSectionHeading';
-import FormSectionDivider from './FormSectionDivider';
-import FormSectionSubHeading from './FormSectionSubHeading';
+import cx from 'classnames';
+import { SectionHeading } from '@console/internal/components/utils';
+import { FormHelperText } from '@patternfly/react-core';
 
 export interface FormSectionProps {
-  title: string;
+  title?: string;
   subTitle?: string;
-  divider?: boolean;
+  fullWidth?: boolean;
   children: React.ReactNode;
 }
 
-const FormSection: React.FC<FormSectionProps> = ({ title, subTitle, divider, children }) => (
-  <React.Fragment>
-    <FormSectionHeading title={title} />
-    {subTitle && <FormSectionSubHeading subTitle={subTitle} />}
+const FormSection: React.FC<FormSectionProps> = ({ title, subTitle, fullWidth, children }) => (
+  <div className={cx('pf-c-form', { 'co-m-pane__form': !fullWidth })}>
+    {title && (
+      <SectionHeading
+        text={title}
+        style={{ margin: 0, fontWeight: 'var(--pf-global--FontWeight--semi-bold)' }}
+      />
+    )}
+    {subTitle && <FormHelperText isHidden={false}>{subTitle}</FormHelperText>}
     {children}
-    {divider && <FormSectionDivider />}
-  </React.Fragment>
+  </div>
 );
 
 export default FormSection;
