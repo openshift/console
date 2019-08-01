f734d88454010aa26c9d50135e1bec5fc468fe10
Abhi <abhinandan13jan@gmail.com>
2019-08-01 17:20:54 +0530

    enhance(pipeline): Expand the Pipeline Run Column

--- frontend/packages/dev-console/src/components/pipelines/pipeline-table.ts
+++ frontend/packages/dev-console/src/components/pipelines/pipeline-table.ts
@@ -1,9 +1,9 @@
 import { Kebab } from '@console/internal/components/utils';
 
 export const tableColumnClasses = [
-  'col-lg-2 col-md-2 col-sm-3 col-xs-5',
-  'col-lg-2 col-md-2 col-sm-4 col-xs-5',
-  'col-lg-2 col-md-2 col-sm-3 hidden-xs',
+  'col-lg-3 col-md-3 col-sm-5 col-xs-6',
+  'col-lg-3 col-md-3 col-sm-5 col-xs-6',
+  'col-lg-2 col-md-2 col-sm-2 hidden-xs',
   'col-lg-2 col-md-2 hidden-sm hidden-xs',
   'col-lg-2 col-md-2 hidden-sm hidden-xs',
   Kebab.columnClass,
