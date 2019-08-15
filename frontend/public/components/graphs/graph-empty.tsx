2748b313b7ece35235d27cd5939361712131643f
Jon Jackson <jonjacks@redhat.com>
2019-08-15 12:31:00 -0400

    Fix wrapping issue with node details graphs

--- frontend/public/components/graphs/graph-empty.tsx
+++ frontend/public/components/graphs/graph-empty.tsx
@@ -3,16 +3,16 @@ import { ChartAreaIcon } from '@patternfly/react-icons';
 import { EmptyStateIcon, EmptyState, EmptyStateVariant, Title } from '@patternfly/react-core';
 import { LoadingBox } from '../utils';
 
-export const GraphEmpty: React.FC<GraphEmptyProps> = ({height = 180, icon = ChartAreaIcon, loading = false}) => loading ? (
-  <div style={{height, width: '100%'}} >
-    <LoadingBox />
-  </div>
-) : (
-  <EmptyState className="graph-empty-state" variant={EmptyStateVariant.full} >
-    <EmptyStateIcon size="sm" icon={icon} />
-    <Title size="sm">No datapoints found.</Title>
-  </EmptyState>
-);
+export const GraphEmpty: React.FC<GraphEmptyProps> = ({height = 180, icon = ChartAreaIcon, loading = false}) => <div style={{height, width: '100%'}} >
+  {
+    loading ? <LoadingBox /> : (
+      <EmptyState className="graph-empty-state" variant={EmptyStateVariant.full}>
+        <EmptyStateIcon size="sm" icon={icon} />
+        <Title size="sm">No datapoints found.</Title>
+      </EmptyState>
+    )
+  }
+</div>;
 
 type GraphEmptyProps = {
   icon?: React.SFC<any>;
