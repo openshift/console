51e1d4c071f05d1345605914faad83ef7783b80e
Robb Hamilton <rhamilto@redhat.com>
2019-08-19 10:29:52 -0400

    Migrate copy to clipboard tooltip to PF4

--- frontend/public/components/utils/copy-to-clipboard.tsx
+++ frontend/public/components/utils/copy-to-clipboard.tsx
@@ -1,49 +1,10 @@
 import * as React from 'react';
 import * as _ from 'lodash-es';
 import { CopyToClipboard as CTC } from 'react-copy-to-clipboard';
-import { Tooltip } from 'react-lightweight-tooltip';
+import { Tooltip } from '@patternfly/react-core';
 import { PasteIcon } from '@patternfly/react-icons';
 
 export const CopyToClipboard: React.FC<CopyToClipboardProps> = React.memo((props) => {
-  const overrides = Object.freeze({
-    wrapper: {
-      position: 'absolute',
-      right: '0',
-      top: '0',
-      height: '42px',
-      padding: '0',
-    } as React.CSSProperties,
-    tooltip: {
-      position: 'absolute',
-      right: '55px',
-      bottom: '-6px',
-      left: 'auto',
-      maxWidth: '170px',
-      minWidth: 'auto',
-      padding: '0',
-      textAlign: 'center',
-      width: 'auto',
-      transform: 'translateX(0px)',
-    } as React.CSSProperties,
-    content: {
-      display: 'block',
-      fontFamily: '"Open Sans",Helvetica,Arial,sans-serif',
-      fontSize: '12px',
-      padding: '7px 12px',
-      whiteSpace: 'normal',
-    } as React.CSSProperties,
-    arrow: {
-      position: 'absolute',
-      bottom: '8px',
-      right: '-13px',
-      left: 'auto',
-      borderTop: '8px solid transparent',
-      borderBottom: '8px solid transparent',
-      borderLeft: '8px solid #000',
-    } as React.CSSProperties,
-    gap: {},
-  });
-
   const [copied, setCopied] = React.useState(false);
 
   const tooltipText = copied ? 'Copied' : 'Copy to Clipboard';
@@ -54,7 +15,7 @@ export const CopyToClipboard: React.FC<CopyToClipboardProps> = React.memo((props
 
   return <div className="co-copy-to-clipboard">
     <pre className="co-pre-wrap co-copy-to-clipboard__text">{visibleValue}</pre>
-    <Tooltip content={tooltipContent} styles={overrides}>
+    <Tooltip content={tooltipContent} trigger="click mouseenter focus" exitDelay={1250}>
       <CTC text={props.value} onCopy={() => setCopied(true)}>
         <button onMouseEnter={() => setCopied(false)} className="btn btn-default co-copy-to-clipboard__btn fix" type="button">
           <PasteIcon />
