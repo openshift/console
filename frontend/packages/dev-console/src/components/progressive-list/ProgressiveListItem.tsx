b7be579ceffa1cbc1f1153cbb2e5022b01dfaffa
Rohit Rai <rohitkrai03@gmail.com>
2019-08-22 01:15:06 +0530

    Fix use of eslint-disable for dependency array in useEffect for ProgressiveListItem

--- frontend/packages/dev-console/src/components/progressive-list/ProgressiveListItem.tsx
+++ frontend/packages/dev-console/src/components/progressive-list/ProgressiveListItem.tsx
@@ -5,12 +5,9 @@ export interface ProgressiveListItemProps {
 }
 
 const ProgressiveListItem: React.FC<ProgressiveListItemProps> = ({ children }) => {
-  const element = React.createRef<HTMLDivElement>();
+  const element = React.useRef<HTMLDivElement>();
   React.useEffect(() => {
     element.current.scrollIntoView({ behavior: 'smooth' });
-    // this effect needs to run only on first render that why dependency array is kept empty
-    // Error: React Hook React.useEffect has a missing dependency: 'element'. Either include it or remove the dependency array.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div ref={element}>{children}</div>;
 };
