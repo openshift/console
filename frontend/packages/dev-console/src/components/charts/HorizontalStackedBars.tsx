5717f195b44c6937cbb333e0006606cc83b6a52f
Andrew Ballantyne <aballant@redhat.com>
2019-08-26 19:27:01 -0400

    Make Pipeline Task Status bar use space more efficiently
    Also adds animation

---
+++ frontend/packages/dev-console/src/components/charts/HorizontalStackedBars.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './HorizontalStackedBars.scss';
+
+export type StackedValue = {
+  color: string;
+  name: string;
+  size: number;
+};
+
+export type HorizontalStackedBarsProps = {
+  disableAnimation?: boolean;
+  height?: number | string;
+  values: StackedValue[];
+  width?: number | string;
+};
+
+const HorizontalStackedBars: React.FC<HorizontalStackedBarsProps> = ({
+  disableAnimation,
+  height,
+  values,
+  width,
+}) => {
+  return (
+    <div className="odc-horizontal-stacked-bars" style={{ height, width }}>
+      {values.map(({ color, name, size }) => (
+        <div
+          key={name}
+          className="odc-horizontal-stacked-bars__data-bar"
+          style={{
+            background: color,
+            flexGrow: size,
+            transition: disableAnimation ? 'initial' : undefined,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default HorizontalStackedBars;
