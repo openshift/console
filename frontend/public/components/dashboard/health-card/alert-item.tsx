0440a06be1910a806b10b81dfb621b6856683c67
Rastislav Wagner <rawagner@redhat.com>
2019-08-30 21:36:02 +0200

    Add left margin to alerts text

--- frontend/public/components/dashboard/health-card/alert-item.tsx
+++ frontend/public/components/dashboard/health-card/alert-item.tsx
@@ -14,14 +14,14 @@ const getSeverityIcon = (severity: string) => {
     default:
       icon = <YellowExclamationTriangleIcon />;
   }
-  return <div className="co-dashboard-icon co-health-card__icon">{icon}</div>;
+  return <div className="co-dashboard-icon">{icon}</div>;
 };
 
 export const AlertItem: React.FC<AlertItemProps> = ({ alert }) => {
   return (
     <div className="co-health-card__alerts-item">
       {getSeverityIcon(getAlertSeverity(alert))}
-      {getAlertDescription(alert) || getAlertMessage(alert)}
+      <span className="co-health-card__text">{getAlertDescription(alert) || getAlertMessage(alert)}</span>
     </div>
   );
 };
