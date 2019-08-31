9b621a16e083ea2fdc1cb00c2343195158b1e53f
Vikram Raj <vikrmrj@gmail.com>
2019-08-31 07:34:12 +0530

    remove required * notation from application dropdown

--- frontend/packages/dev-console/src/components/dropdown/ResourceDropdown.tsx
+++ frontend/packages/dev-console/src/components/dropdown/ResourceDropdown.tsx
@@ -62,6 +62,8 @@ class ResourceDropdown extends React.Component<ResourceDropdownProps, State> {
   componentWillReceiveProps(nextProps: ResourceDropdownProps) {
     const { loaded, loadError, autoSelect, selectedKey, placeholder, onLoad } = nextProps;
 
+    const { onLoad } = this.props;
+
     if (!loaded) {
       this.setState({ title: <LoadingInline /> });
       return;
