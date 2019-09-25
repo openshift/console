13632b6494ac81a993cb2ebd695f40765158fbf5
Elena Bondarenko <ebondare@redhat.com>
2019-09-25 16:29:20 +0200

    Make extra waiting time a constant

--- frontend/integration-tests/views/dashboard-shared.view.ts
+++ frontend/integration-tests/views/dashboard-shared.view.ts
@@ -1,4 +1,5 @@
 import { browser, ExpectedConditions as until } from 'protractor';
 import { untilNoLoadersPresent } from '../views/crud.view';
 
-export const dashboardIsLoaded = () => browser.wait(until.and(untilNoLoadersPresent)).then(() => browser.sleep(1000));
+const extraWaitingLime = 2000;
+export const dashboardIsLoaded = () => browser.wait(until.and(untilNoLoadersPresent)).then(() => browser.sleep(extraWaitingLime));
