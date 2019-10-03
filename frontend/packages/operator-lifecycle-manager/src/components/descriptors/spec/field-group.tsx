b6679cde00ebd40cb3967753963aa8bb5fd746db
Yu Zhao <yzha@redhat.com>
2019-10-03 14:36:29 -0400

    parent a0b9a63794805ac5a21ba4ef7a06112cbe730a77 author Yu Zhao <yzha@redhat.com> 1570127789 -0400 committer Yu Zhao <yzha@redhat.com> 1572456179 -0400
    Apply PF4 Accordion component to fieldGroup descriptor and integrated it with create operand form

---
+++ frontend/packages/operator-lifecycle-manager/src/components/descriptors/spec/field-group.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { ExpandableSection } from '@console/internal/components/utils/expandable-section';
+
+export const FieldGroup: React.FC<FieldGroupProps> = ({ groupName, defaultExpand, children }) => {
+  const [expand, setExpand] = React.useState<boolean>(defaultExpand);
+
+  const onExpandableSectionToggle = (event) => {
+    event.preventDefault();
+    setExpand(!expand);
+  };
+
+  return (
+    <div className="co-field-group">
+      <ExpandableSection
+        id={groupName}
+        key={groupName}
+        isExpanded={expand}
+        listTitle={groupName}
+        onToggle={onExpandableSectionToggle}
+      >
+        {children}
+      </ExpandableSection>
+    </div>
+  );
+};
+FieldGroup.displayName = 'FieldGroup';
+
+export type FieldGroupProps = {
+  defaultExpand: boolean;
+  groupName: string;
+};
