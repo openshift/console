8066005e36c831dbc87c8a48140876d07f6ed609
invinciblejai <mailjai.vardhan@gmail.com>
2019-10-04 13:45:54 +0530

    adds spec to get knative resources utils

---
+++ frontend/packages/knative-plugin/src/utils/__tests__/get-knative-resources.spec.ts
@@ -0,0 +1,49 @@
+import { MockKnativeResources } from '@console/dev-console/src/components/topology/__tests__/topology-knative-test-data';
+import { MockResources } from '@console/dev-console/src/components/topology/__tests__/topology-test-data';
+import {
+  getKnativeServingRevisions,
+  getKnativeServingConfigurations,
+  getKnativeServingRoutes,
+} from '../get-knative-resources';
+import { deploymentData, deploymentKnativeData } from './knative-serving-data';
+
+describe('Get knative resources', () => {
+  describe('knative Serving Resources', () => {
+    it('expect getKnativeServingRevisions to return revision data', () => {
+      const knServingRevResource = getKnativeServingRevisions(
+        deploymentKnativeData,
+        MockKnativeResources,
+      );
+      expect(knServingRevResource.revisions).toBeDefined();
+      expect(knServingRevResource.revisions).toHaveLength(1);
+    });
+    it('expect getKnativeServingRevisions to return revision as undefined', () => {
+      const knServingResource = getKnativeServingRevisions(deploymentData, MockResources);
+      expect(knServingResource).toBeUndefined();
+    });
+    it('expect getKnativeServingConfigurations to return configuration data', () => {
+      const knServingResource = getKnativeServingConfigurations(
+        deploymentKnativeData,
+        MockKnativeResources,
+      );
+      expect(knServingResource.configurations).toBeDefined();
+      expect(knServingResource.configurations).toHaveLength(1);
+    });
+    it('expect getKnativeServingConfigurations to return configuration as undefined', () => {
+      const knServingResource = getKnativeServingConfigurations(deploymentData, MockResources);
+      expect(knServingResource).toBeUndefined();
+    });
+    it('expect getKnativeServingRoutes to return route data', () => {
+      const knServingResource = getKnativeServingRoutes(
+        deploymentKnativeData,
+        MockKnativeResources,
+      );
+      expect(knServingResource.ksroutes).toBeDefined();
+      expect(knServingResource.ksroutes).toHaveLength(1);
+    });
+    it('expect getKnativeServingRoutes to return route as undefined', () => {
+      const knServingResource = getKnativeServingRoutes(deploymentData, MockResources);
+      expect(knServingResource).toBeUndefined();
+    });
+  });
+});
