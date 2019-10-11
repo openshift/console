3b6cd4d4a1d74bd230bb3db90478b4d423633175
Neha <gnehapk@gmail.com>
2019-10-11 18:24:02 +0900

    Bug 1752845: Modified the text to remove tainting of nodes on ocs install flow

--- frontend/packages/ceph-storage-plugin/src/components/ocs-install/create-form.tsx
+++ frontend/packages/ceph-storage-plugin/src/components/ocs-install/create-form.tsx
@@ -26,10 +26,7 @@ export const CreateOCSServiceForm: React.FC<CreateOCSServiceFormProps> = (props)
           <p className="co-m-pane__explanation">
             Selected nodes will be labeled with
             <code>cluster.ocs.openshift.io/openshift-storage=&quot;&quot;</code> to create the OCS
-            Service. These nodes will also be tainted with
-            <code>node.ocs.openshift.io/storage=true:NoSchedule</code> to dedicate these nodes to
-            allow only OCS components to be scheduled on them. Note: Ensure you have additional
-            worker nodes that are not tainted to run other workloads in your OpenShift cluster.
+            Service.
           </p>
           <Alert
             className="co-alert"
