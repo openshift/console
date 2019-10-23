b9ceb45d26ec425625400835374aa57f6f16fffa
Marek Libra <marek.libra@gmail.com>
2019-10-23 08:42:25 +0200

    Remove unused podIpAddress() selector

--- frontend/packages/console-shared/src/selectors/pod.ts
+++ frontend/packages/console-shared/src/selectors/pod.ts
@@ -2,8 +2,6 @@ import { PodKind } from '@console/internal/module/k8s';
 
 export const getNodeName = (pod: PodKind): PodKind['spec']['nodeName'] =>
   pod && pod.spec ? pod.spec.nodeName : undefined;
-export const podIpAddress = (pod: PodKind): PodKind['status']['podIP'] =>
-  pod && pod.status ? pod.status.podIP : undefined;
 
 export const getPodContainers = (pod: PodKind): PodKind['spec']['containers'] =>
   pod && pod.spec && pod.spec.containers ? pod.spec.containers : [];
