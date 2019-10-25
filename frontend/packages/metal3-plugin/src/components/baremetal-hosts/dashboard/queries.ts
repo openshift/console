f496fac77dbef35215fa364e3717bdf4575afac1
Honza Pokorny <honza@redhat.com>
2019-10-25 09:54:24 -0300

    [metal3] Add top consumer popovers to utilization card

--- frontend/packages/metal3-plugin/src/components/baremetal-hosts/dashboard/queries.ts
+++ frontend/packages/metal3-plugin/src/components/baremetal-hosts/dashboard/queries.ts
@@ -13,6 +13,14 @@ export enum HostQuery {
   NUMBER_OF_PODS_SIMPLE = 'NUMBER_OF_PODS_SIMPLE',
   NUMBER_OF_FANS = 'NUMBER_OF_FANS',
   NUMBER_OF_PSUS = 'NUMBER_OF_PSUS',
+
+  // popover queries
+  PODS_BY_CPU = 'PODS_BY_CPU',
+  PODS_BY_MEMORY = 'PODS_BY_MEMORY',
+  PODS_BY_STORAGE = 'PODS_BY_STORAGE',
+  PROJECTS_BY_CPU = 'PROJECTS_BY_CPU',
+  PROJECTS_BY_MEMORY = 'PROJECTS_BY_MEMORY',
+  PROJECTS_BY_STORAGE = 'PROJECTS_BY_STORAGE',
 }
 
 const hostQueriesByHostName = {
@@ -43,6 +51,24 @@ const hostQueriesByIP = {
   ),
   [HostQuery.NUMBER_OF_PODS_SIMPLE]: _.template(
     `kubelet_running_pod_count{instance=~'<%= host %>:.*'}`,
+  ),
+  [HostQuery.PODS_BY_CPU]: _.template(
+    `topk(25, sort_desc(sum(rate(container_cpu_usage_seconds_total{instance=~"<%= host %>:.*",container_name="",pod!=""}[5m])) BY (pod, namespace)))`,
+  ),
+  [HostQuery.PROJECTS_BY_CPU]: _.template(
+    `topk(25, sort_desc(sum(rate(container_cpu_usage_seconds_total{instance=~"<%= host %>:.*",container_name="",pod!=""}[5m])) BY (namespace)))`,
+  ),
+  [HostQuery.PODS_BY_MEMORY]: _.template(
+    `topk(25, sort_desc(sum(container_memory_working_set_bytes{instance=~"<%= host %>:.*",container="",pod!=""}) BY (pod, namespace)))`,
+  ),
+  [HostQuery.PROJECTS_BY_MEMORY]: _.template(
+    `topk(25, sort_desc(sum(container_memory_working_set_bytes{instance=~"<%= host %>:.*",container="",pod!=""}) BY (namespace)))`,
+  ),
+  [HostQuery.PODS_BY_STORAGE]: _.template(
+    `topk(25, sort_desc(avg by (pod, namespace)(irate(container_fs_io_time_seconds_total{instance=~"<%= host %>:.*",container="POD", pod!=""}[1m]))))`,
+  ),
+  [HostQuery.PROJECTS_BY_STORAGE]: _.template(
+    `topk(25, sort_desc(avg by (namespace)(irate(container_fs_io_time_seconds_total{instance=~"<%= host %>:.*",container="POD", pod!=""}[1m]))))`,
   ),
 };
 
@@ -98,3 +124,20 @@ type HostQueryType = {
 
 export const getHostQueryResultError = (result: PrometheusResponse): boolean =>
   _.get(result, 'status', '') !== 'success';
+
+export const getTopConsumerQueries = (hostName: string, hostIP: string): HostQueryType => ({
+  [HostQuery.PODS_BY_CPU]: getQuery(hostIP, hostQueriesByIP[HostQuery.PODS_BY_CPU], ''),
+  [HostQuery.PROJECTS_BY_CPU]: getQuery(hostIP, hostQueriesByIP[HostQuery.PROJECTS_BY_CPU], ''),
+  [HostQuery.PODS_BY_MEMORY]: getQuery(hostIP, hostQueriesByIP[HostQuery.PODS_BY_MEMORY], ''),
+  [HostQuery.PROJECTS_BY_MEMORY]: getQuery(
+    hostIP,
+    hostQueriesByIP[HostQuery.PROJECTS_BY_MEMORY],
+    '',
+  ),
+  [HostQuery.PODS_BY_STORAGE]: getQuery(hostIP, hostQueriesByIP[HostQuery.PODS_BY_STORAGE], ''),
+  [HostQuery.PROJECTS_BY_STORAGE]: getQuery(
+    hostIP,
+    hostQueriesByIP[HostQuery.PROJECTS_BY_STORAGE],
+    '',
+  ),
+});
