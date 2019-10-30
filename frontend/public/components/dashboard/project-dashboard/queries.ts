e1085119413cea32d5baf7d1f001599b3eeef49f
Rastislav Wagner <rawagner@redhat.com>
2019-10-30 13:15:37 +0100

    Add top consumers to project dashboard

--- frontend/public/components/dashboard/project-dashboard/queries.ts
+++ frontend/public/components/dashboard/project-dashboard/queries.ts
@@ -4,6 +4,8 @@ export enum ProjectQueries {
   CPU_USAGE = 'CPU_USAGE',
   MEMORY_USAGE = 'MEMORY_USAGE',
   POD_COUNT = 'POD_COUNT',
+  PODS_BY_CPU = 'PODS_BY_CPU',
+  PODS_BY_MEMORY = 'PODS_BY_MEMORY',
 }
 
 const queries = {
@@ -18,8 +20,22 @@ const queries = {
   ),
 };
 
+const top25Queries = {
+  [ProjectQueries.PODS_BY_CPU]: _.template(
+    `topk(25, sort_desc(sum(rate(container_cpu_usage_seconds_total{container="",pod!="",namespace='<%= project %>'}[5m])) BY (pod, namespace)))`,
+  ),
+  [ProjectQueries.PODS_BY_MEMORY]: _.template(
+    `topk(25, sort_desc(sum(container_memory_working_set_bytes{container="",pod!="",namespace='<%= project %>'}) BY (pod, namespace)))`,
+  ),
+};
+
 export const getUtilizationQueries = (project: string, duration: string) => ({
   [ProjectQueries.CPU_USAGE]: queries[ProjectQueries.CPU_USAGE]({ project, duration }),
   [ProjectQueries.MEMORY_USAGE]: queries[ProjectQueries.MEMORY_USAGE]({ project, duration }),
   [ProjectQueries.POD_COUNT]: queries[ProjectQueries.POD_COUNT]({ project, duration }),
+});
+
+export const getTopConsumerQueries = (project: string) => ({
+  [ProjectQueries.PODS_BY_CPU]: top25Queries[ProjectQueries.PODS_BY_CPU]({ project }),
+  [ProjectQueries.PODS_BY_MEMORY]: top25Queries[ProjectQueries.PODS_BY_MEMORY]({ project }),
 });
