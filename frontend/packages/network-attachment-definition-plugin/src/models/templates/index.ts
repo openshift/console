e12a058cc47c057b586a65ddda4882abc903a8a1
Phillip Bailey <phbailey@redhat.com>
2019-11-01 11:09:24 -0400

    Update the network attachment definition YAML template

--- frontend/packages/network-attachment-definition-plugin/src/models/templates/index.ts
+++ frontend/packages/network-attachment-definition-plugin/src/models/templates/index.ts
@@ -11,17 +11,6 @@ kind: ${NetworkAttachmentDefinitionModel.kind}
 metadata:
   name: example
 spec:
-  config: '{
-    "cniVersion": "0.3.1",
-    "name": "a-bridge-network",
-    "type": "bridge",
-    "bridge": "br0",
-    "isGateway": true,
-    "ipam": {
-      "type": "host-local",
-      "subnet": "192.168.5.0/24",
-      "dataDir": "/mnt/cluster-ipam"
-    }
-  }'
+  config: '{}'
 `,
 );
