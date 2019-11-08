baf8038e192e7c3e38df3447346c8905ff650895
Elena Bondarenko <ebondare@redhat.com>
2019-11-08 14:26:41 +0100

    Fix style issues

--- frontend/integration-tests/tests/storage-dashboard.scenario.ts
+++ frontend/integration-tests/tests/storage-dashboard.scenario.ts
@@ -5,27 +5,30 @@ import { serviceName, clusterHealth, clusterName, allNodes } from '../views/stor
 import { execSync } from 'child_process';
 
 describe('Check data on Storage Dashboard.', () => {
-  beforeAll(async() => {
+  beforeAll(async () => {
     await browser.get(`${appHost}/dashboards/persistent-storage`);
     await dashboardIsLoaded();
   });
 
-  it('Check cluster health is OK', async() => {
+  it('Check cluster health is OK', async () => {
     expect(clusterHealth.getText()).toContain('is healthy');
   });
 
-  it('Check service name is OCS', async() => {
+  it('Check service name is OCS', async () => {
     expect(serviceName.getText()).toEqual('OpenShift Container Storage');
   });
 
-  it('Check that cluster name is correct', async() => {
-    const cephClusterName = execSync("kubectl get cephcluster -n openshift-storage -o jsonpath='{.items..metadata.name}'");
+  it('Check that cluster name is correct', async () => {
+    const cephClusterName = execSync(
+      "kubectl get cephcluster -n openshift-storage -o jsonpath='{.items..metadata.name}'",
+    );
     expect(clusterName.getText()).toEqual(cephClusterName.toString().trim());
   });
 
-  it('Check the total number of OCS nodes', async() => {
-    const ocsNodesNumber = execSync("kubectl get nodes -l cluster.ocs.openshift.io/openshift-storage -o json | jq '.items | length'");
+  it('Check the total number of OCS nodes', async () => {
+    const ocsNodesNumber = execSync(
+      "kubectl get nodes -l cluster.ocs.openshift.io/openshift-storage -o json | jq '.items | length'",
+    );
     expect(allNodes.getText()).toEqual(`${ocsNodesNumber.toString().trim()} Nodes`);
   });
-
 });
--- frontend/integration-tests/views/dashboard-shared.view.ts
+++ frontend/integration-tests/views/dashboard-shared.view.ts
@@ -2,4 +2,7 @@ import { browser, ExpectedConditions as until } from 'protractor';
 import { untilNoLoadersPresent } from '../views/crud.view';
 
 const DASHBOARD_LOAD_WAIT_TIME = 2000;
-export const dashboardIsLoaded = () => browser.wait(until.and(untilNoLoadersPresent)).then(() => browser.sleep(DASHBOARD_LOAD_WAIT_TIME));
+export const dashboardIsLoaded = () =>
+  browser
+    .wait(until.and(untilNoLoadersPresent))
+    .then(() => browser.sleep(DASHBOARD_LOAD_WAIT_TIME));
