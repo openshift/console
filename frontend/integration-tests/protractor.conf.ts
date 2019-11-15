4c374890e769adca455abf458f48cf3cd2ffa12c
Elena Bondarenko <ebondare@redhat.com>
2019-11-15 11:10:01 +0100

    Revert the unneeded changes to protractor.conf.ts

--- frontend/integration-tests/protractor.conf.ts
+++ frontend/integration-tests/protractor.conf.ts
@@ -92,7 +92,6 @@ const testSuites = {
     '../packages/operator-lifecycle-manager/integration-tests/scenarios/global-installmode.scenario.ts',
     '../packages/operator-lifecycle-manager/integration-tests/scenarios/single-installmode.scenario.ts',
   ]),
-  'ceph-storage-plugin': suite(['tests/storage-dashboard.scenario.ts']),
   release: suite([
     'tests/crud.scenario.ts',
     'tests/secrets.scenario.ts',
