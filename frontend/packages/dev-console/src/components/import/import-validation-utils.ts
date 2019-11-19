f83df04385d2af774e186dc9ca7c88f169d4dcd0
Vikram Raj <vikrmrj@gmail.com>
2019-11-19 10:14:03 +0530

    Bug fix: parse the git url correctly to detect the git type

--- frontend/packages/dev-console/src/components/import/import-validation-utils.ts
+++ frontend/packages/dev-console/src/components/import/import-validation-utils.ts
@@ -32,18 +32,22 @@ export const validationSchema = yup.object().shape({
   resources: resourcesValidationSchema,
 });
 
+const hasDomain = (url: string, domain: string): boolean => {
+  return url.includes(`https://${domain}/`) || url.includes(`@${domain}:`);
+};
+
 export const detectGitType = (url: string): string => {
   if (!gitUrlRegex.test(url)) {
     // Not a URL
     return GitTypes.invalid;
   }
-  if (url.includes('github.com')) {
+  if (hasDomain(url, 'github.com')) {
     return GitTypes.github;
   }
-  if (url.includes('bitbucket.org')) {
+  if (hasDomain(url, 'bitbucket.org')) {
     return GitTypes.bitbucket;
   }
-  if (url.includes('gitlab.com')) {
+  if (hasDomain(url, 'gitlab.com')) {
     return GitTypes.gitlab;
   }
   // Not a known URL
