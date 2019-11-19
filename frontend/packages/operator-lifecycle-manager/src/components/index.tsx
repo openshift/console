607dbb4b30f96475b2f70f08a4ba408308d9dafe
Samuel Padgett <spadgett@redhat.com>
2019-11-19 15:12:29 -0500

    Bug 1757526: Handle cluster-scoped operands

--- frontend/packages/operator-lifecycle-manager/src/components/index.tsx
+++ frontend/packages/operator-lifecycle-manager/src/components/index.tsx
@@ -113,16 +113,14 @@ export const OperandLink: React.SFC<OperandLinkProps> = (props) => {
           allParts[i - 1] === ClusterServiceVersionModel.plural,
       );
 
+  const reference = referenceFor(props.obj);
+  const to = namespace
+    ? `/k8s/ns/${namespace}/${ClusterServiceVersionModel.plural}/${csvName()}/${reference}/${name}`
+    : `/k8s/cluster/${reference}/${name}`;
   return (
     <span className="co-resource-item">
       <ResourceIcon kind={referenceFor(props.obj)} />
-      <Link
-        to={`/k8s/ns/${namespace}/${ClusterServiceVersionModel.plural}/${csvName()}/${referenceFor(
-          props.obj,
-        )}/${name}`}
-        className="co-resource-item__resource-name"
-        data-test-operand-link={name}
-      >
+      <Link to={to} className="co-resource-item__resource-name" data-test-operand-link={name}>
         {name}
       </Link>
     </span>
