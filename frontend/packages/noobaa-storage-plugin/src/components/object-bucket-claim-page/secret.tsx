2db918bff820cc9c46051fb5414e91183eaf2ce8
bipuladh <badhikar@redhat.com>
2019-11-20 22:31:10 +0530

    Fixed issues of order in secret page for obc

--- frontend/packages/noobaa-storage-plugin/src/components/object-bucket-claim-page/secret.tsx
+++ frontend/packages/noobaa-storage-plugin/src/components/object-bucket-claim-page/secret.tsx
@@ -1,41 +1,82 @@
 import * as React from 'react';
 import * as _ from 'lodash';
-import { Base64 } from 'js-base64';
-import { SecretData } from '@console/internal/components/configmap-and-secret-data';
+import { SecretValue } from '@console/internal/components/configmap-and-secret-data';
 import { ConfigMapModel, SecretModel } from '@console/internal/models';
 import { k8sGet, K8sResourceKind } from '@console/internal/module/k8s';
 import { getName, getNamespace } from '@console/shared';
-import { isBound } from '../../utils';
+import { SectionHeading, EmptyBox } from '@console/internal/components/utils';
+import { Button } from '@patternfly/react-core';
+import { EyeSlashIcon, EyeIcon } from '@patternfly/react-icons';
 
 export const GetSecret: React.FC<GetSecretProps> = ({ obj }) => {
-  const secretData = {
-    BUCKET_NAME: Base64.encode(getName(obj)),
-    ACCESS_KEY: Base64.encode('loading..'),
-    SECRET_KEY: Base64.encode('loading..'),
-    ENDPOINT: Base64.encode('http://loading.com'),
-  };
-  if (isBound(obj)) {
-    const secret = k8sGet(SecretModel, getName(obj), getNamespace(obj));
-    const configMap = k8sGet(ConfigMapModel, getName(obj), getNamespace(obj));
+  const [reveal, setReveal] = React.useState(false);
+  const [secretData, setSecretData] = React.useState([]);
+
+  const name = getName(obj);
+  const namespace = getNamespace(obj);
+
+  React.useEffect(() => {
+    const secret = k8sGet(SecretModel, name, namespace);
+    const configMap = k8sGet(ConfigMapModel, name, namespace);
     Promise.all([secret, configMap])
       .then((data) => {
-        secretData.ACCESS_KEY = Base64.encode(_.get(data[0], 'data.AWS_ACCESS_KEY_ID'));
-        secretData.SECRET_KEY = Base64.encode(_.get(data[0], 'data.AWS_SECRET_ACCESS_KEY'));
         const endpoint = `${_.get(data[1], 'data.BUCKET_HOST')}:${_.get(
           data[1],
           'data.BUCKET_PORT',
         )}`;
-        secretData.ENDPOINT = Base64.encode(endpoint);
+        const accessKey = _.get(data[0], 'data.AWS_ACCESS_KEY_ID');
+        const secretKey = _.get(data[0], 'data.AWS_SECRET_ACCESS_KEY');
+        const secretValues = [
+          { field: 'Endpoint', value: endpoint },
+          { field: 'Bucket Name', value: name },
+          { field: 'Access Key', value: accessKey },
+          { field: 'Secret Key', value: secretKey },
+        ];
+        setSecretData(secretValues);
       })
       .catch(() => undefined);
-    return (
-      <div className="co-m-pane__body">
-        <SecretData title="Object Bucket Claim Data" data={secretData} />
-      </div>
-    );
-  }
+  }, [name, namespace]);
 
-  return null;
+  const dl = secretData.length
+    ? secretData.reduce((acc, datum) => {
+        const { field, value } = datum;
+        acc.push(<dt key={`${field}-k`}>{field}</dt>);
+        acc.push(
+          <dd key={`${field}-v`}>
+            <SecretValue value={value} reveal={reveal} encoded={false} />
+          </dd>,
+        );
+        return acc;
+      }, [])
+    : [];
+
+  return dl.length ? (
+    <div className="co-m-pane__body">
+      <SectionHeading text="Object Bucket Claim Data">
+        {secretData.length ? (
+          <Button
+            type="button"
+            onClick={() => setReveal(!reveal)}
+            variant="link"
+            className="pf-m-link--align-right"
+          >
+            {reveal ? (
+              <>
+                <EyeSlashIcon className="co-icon-space-r" />
+                Hide Values
+              </>
+            ) : (
+              <>
+                <EyeIcon className="co-icon-space-r" />
+                Reveal Values
+              </>
+            )}
+          </Button>
+        ) : null}
+      </SectionHeading>
+      {dl.length ? <dl className="secret-data">{dl}</dl> : <EmptyBox label="Data" />}
+    </div>
+  ) : null;
 };
 
 type GetSecretProps = {
