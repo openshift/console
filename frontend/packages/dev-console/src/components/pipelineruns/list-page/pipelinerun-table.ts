7ee4a1e5100c8f3b02fe439805de8e6ccc20fb2f
Robb Hamilton <rhamilto@redhat.com>
2019-12-10 10:47:10 -0500

    Improve display of Pipeline Runs cols

--- frontend/packages/dev-console/src/components/pipelineruns/list-page/pipelinerun-table.ts
+++ frontend/packages/dev-console/src/components/pipelineruns/list-page/pipelinerun-table.ts
@@ -1,11 +1,11 @@
 import { Kebab } from '@console/internal/components/utils';
 
 export const tableColumnClasses = [
-  'col-lg-3 col-md-4 col-sm-4 col-xs-4', // name
-  'col-lg-2 col-md-3 col-sm-3 col-xs-3', // namespace
-  'col-lg-2 col-md-2 col-sm-2 col-xs-2', // task status
-  'col-lg-2 col-md-3 col-sm-3 col-xs-3', // started
-  'col-lg-1 hidden-md hidden-sm hidden-xs', // duration
-  'col-lg-2 hidden-md hidden-sm hidden-xs', // status
+  '', // name
+  '', // namespace
+  'pf-m-hidden pf-m-visible-on-sm', // status
+  'pf-m-hidden pf-m-visible-on-lg', // task status
+  'pf-m-hidden pf-m-visible-on-lg', // started
+  'pf-m-hidden pf-m-visible-on-xl', // duration
   Kebab.columnClass,
 ];
