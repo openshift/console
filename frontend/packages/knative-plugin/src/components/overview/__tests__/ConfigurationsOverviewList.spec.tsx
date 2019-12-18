c0996139572add2913f5f4e69dfcbc7af90a57e0
Aritra Roy <ari.roy09@gmail.com>
2019-12-18 09:25:31 +0530

    add unit test to ConfigurationsOverviewList

---
+++ frontend/packages/knative-plugin/src/components/overview/__tests__/ConfigurationsOverviewList.spec.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { sampleKnativeConfigurations } from '@console/dev-console/src/components/topology/__tests__/topology-knative-test-data';
+import ConfigurationsOverviewList from '../ConfigurationsOverviewList';
+import ConfigurationsOverviewListItem from '../ConfigurationsOverviewListItem';
+
+describe('ConfigurationsOverviewList', () => {
+  it('should render error Message when configurations array is empty', () => {
+    const wrapper = shallow(<ConfigurationsOverviewList configurations={[]} />);
+    expect(wrapper.text().includes('No Configurations found for this resource.')).toBe(true);
+  });
+
+  it('should render ConfigurationsOverviewListItem', () => {
+    const wrapper = shallow(
+      <ConfigurationsOverviewList configurations={sampleKnativeConfigurations.data} />,
+    );
+    expect(wrapper.find(ConfigurationsOverviewListItem)).toHaveLength(1);
+  });
+});
