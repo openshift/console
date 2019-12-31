7713f3bd8d940f2cd6a4a5d9cf0af143b3ce9619
gigner <gigner@redhat.com>
2019-12-31 10:41:00 +0200

    add install scenario

---
+++ frontend/packages/kubevirt-plugin/integration-tests/installation.scenario.ts
@@ -0,0 +1,25 @@
+import * as sidenavView from '@console/internal-integration-tests/views/sidenav.view';
+import { isLoaded } from '@console/internal-integration-tests/views/crud.view';
+import { $, browser, by, element } from 'protractor';
+import { click } from '@console/shared/src/test-utils/utils';
+
+const namespaceButton = $('.co-namespace-selector button');
+const openshiftNamespaceButton = $('#openshift-cnv-link');
+
+ 
+describe('******Install Kubevirt', () => {
+ 
+  beforeAll(async () => {
+    await sidenavView.clickNavLink(['Operators', 'OperatorHub']);
+    await isLoaded();
+    await click(namespaceButton);
+    await click(openshiftNamespaceButton);
+  });
+ 
+  it('install Kubevirt', async () => {
+    await isLoaded();
+    element(by.cssContainingText('.catalog-tile-pf-title', 'Container-native virtualization Operator')).click();
+    element(by.linkText('Install')).click()
+    await browser.sleep(20000);  
+  })
+ })
