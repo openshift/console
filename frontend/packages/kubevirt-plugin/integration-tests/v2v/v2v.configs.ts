2dd65a46c8792dfb023c9ba0fed6313c074dbf2b
ibragins <59690591+ibragins@users.noreply.github.com>
2020-01-26 11:29:18 +0200

    Add files via upload

--- frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.configs.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.configs.ts
@@ -5,8 +5,23 @@ import { OperatingSystem, WorkloadProfile } from '../tests/utils/constants/wizar
 const { V2V_INSTANCE_HOSTNAME, V2V_INSTANCE_USERNAME, V2V_INSTANCE_PASSWORD } = process.env;
 
 export const vmwareVMConfig: VMImportConfig = {
-  name: 'v2v-rhel7-mini-imported',
-  sourceVMName: 'v2v-rhel7-mini',
+  name: 'v2v-rhel7-igor-imported',
+  sourceVMName: 'v2v-rhel7-igor',
+  provider: 'VMware',
+  instanceConfig: {
+    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
+    hostname: V2V_INSTANCE_HOSTNAME,
+    username: V2V_INSTANCE_USERNAME,
+    password: V2V_INSTANCE_PASSWORD,
+    saveInstance: false,
+  },
+  operatingSystem: OperatingSystem.RHEL7_6,
+  workloadProfile: WorkloadProfile.DESKTOP,
+};
+
+export const vmwareVMMultiNicConfig: VMImportConfig = {
+  name: 'v2v-rhel7-2nic-2disk-igor-imported',
+  sourceVMName: 'v2v-rhel7-2nic-2disk-igor',
   provider: 'VMware',
   instanceConfig: {
     instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
