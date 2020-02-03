975c251a1228508b7007dc3b3708b1f01151bdf5
guy9050 <guy9050@gmail.com>
2020-02-03 17:53:27 +0200

    add missing things

--- frontend/packages/kubevirt-plugin/integration-tests/installation/cnv-installation.scenario.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/installation/cnv-installation.scenario.ts
@@ -1,24 +1,24 @@
-import * as cnvView from '../views/containerNativeVirtualization.view';
 import { click } from '@console/shared/src/test-utils/utils';
 import { by, element } from 'protractor';
 import { isLoaded } from '@console/internal-integration-tests/views/crud.view';
 import * as sidenavView from '@console/internal-integration-tests/views/sidenav.view';
+import * as cnvView from '../views/containerNativeVirtualization.view';
 import { waitFor } from '../tests/utils/utils';
 
 describe('Kubevirt Installation', () => {
- 
   beforeAll(async () => {
     await sidenavView.clickNavLink(['Operators', 'OperatorHub']);
     await isLoaded();
     await click(cnvView.namespaceButton);
     await click(cnvView.openshiftNamespaceButton);
   });
- 
+
   it('Install Kubevirt', async () => {
     await isLoaded();
-    element(by.cssContainingText('.catalog-tile-pf-title', 'Container-native virtualization Operator')).click();
+    element(
+      by.cssContainingText('.catalog-tile-pf-title', 'Container-native virtualization Operator'),
+    ).click();
     element(by.linkText('Install')).click();
     await waitFor(cnvView.kubevirtOperatorStatus, 'Succeeded', 5);
-  })
- });
- 
+  });
+});
--- frontend/packages/kubevirt-plugin/integration-tests/installation/cnv-uninstallation.scenario.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/installation/cnv-uninstallation.scenario.ts
@@ -1,20 +1,19 @@
-import * as cnvView from '../views/containerNativeVirtualization.view';
 import { click } from '@console/shared/src/test-utils/utils';
 import { browser, by, element } from 'protractor';
 import { isLoaded } from '@console/internal-integration-tests/views/crud.view';
 import * as sidenavView from '@console/internal-integration-tests/views/sidenav.view';
+import * as cnvView from '../views/containerNativeVirtualization.view';
 import { confirmAction } from '../views/vm.actions.view';
 import { waitFor } from '../tests/utils/utils';
 
 describe('Uninstall Kubevirt', () => {
- 
   beforeAll(async () => {
     await sidenavView.clickNavLink(['Operators', 'Installed Operators']);
     await isLoaded();
     await click(cnvView.namespaceButton);
     await click(cnvView.openshiftNamespaceButton);
   });
- 
+
   it('Uninstall Kubevirt', async () => {
     await isLoaded();
     element(by.xpath("//button[@data-test-id='kebab-button']")).click();
@@ -22,5 +21,5 @@ describe('Uninstall Kubevirt', () => {
     element(by.xpath("//button[@data-test-action='Uninstall Operator']")).click();
     await waitFor(cnvView.kubevirtOperatorStatus, 'Succeeded', 5);
     await confirmAction();
-  })
- });
+  });
+});
