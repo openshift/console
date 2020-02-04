12410d7daf16445d0fb9c51b046d6eda938fe1c7
yaacov <kobi.zamir@gmail.com>
2020-02-04 13:31:12 +0200

    VM details, Align href to page name

--- frontend/packages/kubevirt-plugin/src/components/vms/vmi-details-page.tsx
+++ frontend/packages/kubevirt-plugin/src/components/vms/vmi-details-page.tsx
@@ -11,7 +11,7 @@ import {
 } from '../../models';
 import { getResource } from '../../utils';
 import {
-  VM_DETAIL_OVERVIEW_HREF,
+  VM_DETAIL_DETAILS_HREF,
   VM_DETAIL_DISKS_HREF,
   VM_DETAIL_NETWORKS_HREF,
   VM_DETAIL_CONSOLES_HREF,
@@ -44,7 +44,7 @@ export const VirtualMachinesInstanceDetailsPage: React.FC<VirtualMachinesInstanc
   };
 
   const overviewPage = {
-    href: VM_DETAIL_OVERVIEW_HREF,
+    href: VM_DETAIL_DETAILS_HREF,
     name: 'Overview',
     component: VMDetailsFirehose,
   };
