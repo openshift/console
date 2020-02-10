58523f12d9bdd812467c033b866adc062c8a640b
Cyril <cyril.ajieh@gmail.com>
2020-02-10 16:20:33 -0500

    Operators not filtered for all projects

--- frontend/public/components/utils/tile-view-page.jsx
+++ frontend/public/components/utils/tile-view-page.jsx
@@ -780,8 +780,10 @@ export class TileViewPage extends React.Component {
   renderItems(items, renderTile) {
     return (
       <Gallery gutter="sm" className="catalog-tile-view-pf catalog-tile-view-pf-no-categories">
-        {_.map(items, (item) => (
-          <GalleryItem key={item.uid ? `gallery-${item.uid}` : `gallery-${item.obj.metadata.uid}`}>
+        {_.map(items, (item, index) => (
+          <GalleryItem
+            key={item.uid ? `gallery-${item.uid}-${index}` : `gallery-${item.obj.metadata.uid}`}
+          >
             {renderTile(item)}
           </GalleryItem>
         ))}
