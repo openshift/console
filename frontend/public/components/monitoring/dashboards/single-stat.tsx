71ae5630b1575f650e9dff191fce1cede5323954
sg00dwin <sgoodwin@redhat.com>
2020-02-12 13:39:57 -0500

    Correct monitoring dashboard panel issues Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1799490

--- frontend/public/components/monitoring/dashboards/single-stat.tsx
+++ frontend/public/components/monitoring/dashboards/single-stat.tsx
@@ -9,7 +9,9 @@ import { getPrometheusURL, PrometheusEndpoint } from '../../graphs/helpers';
 import { LoadingInline, usePoll, useSafeFetch } from '../../utils';
 
 const Body: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <Bullseye className="monitoring-dashboards__single-stat">{children}</Bullseye>
+  <Bullseye className="monitoring-dashboards__single-stat query-browser__wrapper">
+    {children}
+  </Bullseye>
 );
 
 const SingleStat: React.FC<Props> = ({
