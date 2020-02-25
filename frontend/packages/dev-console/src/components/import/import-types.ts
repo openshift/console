27e019cb0a5a4353743bf8293cdceda2f2545dbd
Andrew Ballantyne <aballant@redhat.com>
2020-02-25 11:30:11 -0500

    Adds a filter down to a specific Pipeline for the resource type and runtime

--- frontend/packages/dev-console/src/components/import/import-types.ts
+++ frontend/packages/dev-console/src/components/import/import-types.ts
@@ -223,8 +223,8 @@ export enum ImportTypes {
 
 export enum Resources {
   OpenShift = 'openshift',
-  Kubernetes = 'k8s',
-  KnativeService = 'knativeservice',
+  Kubernetes = 'kubernetes',
+  KnativeService = 'knative',
 }
 
 export interface ImportData {
