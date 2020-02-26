97b73bccdbe19af0f01a8ffa009055b6fc2ef527
Jeffrey Phillips <jephilli@redhat.com>
2020-02-26 18:42:04 -0500

    Remove refactor to CreateConnectorWidget component

--- frontend/packages/topology/src/components/index.ts
+++ frontend/packages/topology/src/components/index.ts
@@ -5,5 +5,4 @@ export { default as ConnectorArrow } from './ConnectorArrow';
 export { default as EdgeConnectorArrow } from './EdgeConnectorArrow';
 export { default as GraphComponent } from './GraphComponent';
 export { default as VisualizationSurface } from './VisualizationSurface';
-export * from './CreateConnectorWidget';
 export { default as DefaultCreateConnector } from './DefaultCreateConnector';
