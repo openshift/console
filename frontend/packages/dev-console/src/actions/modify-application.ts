ed1f1963d7ba14fa3e3ec00b2786f3bfa4d61629
Vikram Raj <vikrmrj@gmail.com>
2020-02-28 11:52:50 +0530

    hide edit option for DS, SS and knative services created via CLI

--- frontend/packages/dev-console/src/actions/modify-application.ts
+++ frontend/packages/dev-console/src/actions/modify-application.ts
@@ -1,7 +1,6 @@
 import { KebabOption } from '@console/internal/components/utils';
 import { truncateMiddle } from '@console/internal/components/utils/truncate-middle';
 import { K8sResourceKind, K8sKind } from '@console/internal/module/k8s';
-import { ServiceModel } from '@console/knative-plugin';
 import { RESOURCE_NAME_TRUNCATE_LENGTH } from '../const';
 import { editApplicationModal } from '../components/modals';
 
@@ -29,9 +28,7 @@ export const EditApplication = (model: K8sKind, obj: K8sResourceKind): KebabOpti
   const annotation = obj?.metadata?.annotations?.['openshift.io/generated-by'];
   return {
     label: `Edit ${truncateMiddle(obj.metadata.name, { length: RESOURCE_NAME_TRUNCATE_LENGTH })}`,
-    hidden:
-      (obj.kind !== ServiceModel.kind && !annotation) ||
-      (annotation && annotation !== 'OpenShiftWebConsole'),
+    hidden: annotation !== 'OpenShiftWebConsole',
     href: `/edit/ns/${obj.metadata.namespace}?name=${obj.metadata.name}&kind=${obj.kind ||
       model.kind}`,
     accessReview: {
