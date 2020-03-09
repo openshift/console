fdd219217764047c9a329d97dea736fd6795b43f
Gilad Lekner <gilad215@gmail.com>
2020-03-09 15:56:01 +0200

    add windows guest tools cd test

---
+++ frontend/packages/kubevirt-plugin/integration-tests/tests/vm.windows-guest-tools.scenario.ts
@@ -0,0 +1,48 @@
+import { browser, ExpectedConditions as until } from 'protractor';
+import { testName } from '@console/internal-integration-tests/protractor.conf';
+import { withResource } from '@console/shared/src/test-utils/utils';
+import { isWinToolsImage, getVolumeContainerImage, getVolumes } from '../../src/selectors/vm';
+import { VirtualMachine } from './models/virtualMachine';
+import { VM_BOOTUP_TIMEOUT_SECS } from './utils/consts';
+import { getProvisionConfigs, vmConfig } from './vm.wizard.configs';
+import { ProvisionConfigName } from './utils/constants/wizard';
+import { widowsVMConfig } from './utils/mocks';
+import { getResourceObject } from './utils/utils';
+import { windowsGuestToolsCDElement } from '../views/editCDView';
+
+describe('Kubevirt Windows Guest tools', () => {
+  const leakedResources = new Set<string>();
+  const provisionConfigs = getProvisionConfigs();
+
+  const configName = ProvisionConfigName.CONTAINER;
+  const provisionConfig = provisionConfigs.get(configName);
+
+  provisionConfig.networkResources = [];
+  provisionConfig.storageResources = [];
+
+  it(
+    'Checks that Guest Tools CD is mounted after Windows VM creation',
+    async () => {
+      const windowsConfig = vmConfig(
+        configName.toLowerCase(),
+        testName,
+        provisionConfig,
+        widowsVMConfig,
+        false, // dont startOnCreation
+      );
+      const vm = new VirtualMachine(windowsConfig);
+
+      const isWindowsCDMounted = () =>
+        !!getVolumes(getResourceObject(vm.name, vm.namespace, vm.kind)).some((volume) =>
+          isWinToolsImage(getVolumeContainerImage(volume)),
+        );
+
+      await withResource(leakedResources, vm.asResource(), async () => {
+        await vm.create(windowsConfig);
+        await browser.wait(until.presenceOf(windowsGuestToolsCDElement));
+        expect(isWindowsCDMounted()).toBeTruthy();
+      });
+    },
+    VM_BOOTUP_TIMEOUT_SECS,
+  );
+});
