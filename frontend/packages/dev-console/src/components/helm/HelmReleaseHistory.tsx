4ffb26d44a3dff86d256038841eaefaaf2313088
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2020-03-23 20:11:44 +0530

    Add chart version and description columns in helm release history list

--- frontend/packages/dev-console/src/components/helm/HelmReleaseHistory.tsx
+++ frontend/packages/dev-console/src/components/helm/HelmReleaseHistory.tsx
@@ -1,23 +1,40 @@
 import * as React from 'react';
+import { match as RMatch } from 'react-router';
+import { coFetchJSON } from '@console/internal/co-fetch';
+import { SortByDirection } from '@patternfly/react-table';
 import CustomResourceList from '../custom-resource-list/CustomResourceList';
-import { getHelmReleaseRevisions, helmRowFilters, getFilteredItems } from './helm-utils';
-import HelmReleaseRow from './HelmReleaseRow';
-import HelmReleaseHeader from './HelmReleaseHeader';
+import { helmRowFilters, getFilteredItems } from './helm-utils';
+import HelmReleaseHistoryRow from './HelmReleaseHistoryRow';
+import HelmReleaseHistoryHeader from './HelmReleaseHistoryHeader';
+import { HelmRelease } from './helm-types';
 
 interface HelmReleaseHistoryProps {
-  helmReleaseName: string;
-  namespace: string;
+  match: RMatch<{
+    ns?: string;
+    name?: string;
+  }>;
 }
 
-const HelmReleaseHistory: React.FC<HelmReleaseHistoryProps> = ({ helmReleaseName, namespace }) => (
-  <CustomResourceList
-    items={getHelmReleaseRevisions(namespace, helmReleaseName)}
-    queryArg="rowFilter-helm-release-status"
-    rowFilters={helmRowFilters}
-    getFilteredItems={getFilteredItems}
-    resourceRow={HelmReleaseRow}
-    resourceHeader={HelmReleaseHeader}
-  />
-);
+const HelmReleaseHistory: React.FC<HelmReleaseHistoryProps> = ({ match }) => {
+  const namespace = match.params.ns;
+  const helmReleaseName = match.params.name;
+
+  const getHelmReleaseRevisions = (): Promise<HelmRelease[]> => {
+    return coFetchJSON(`/api/helm/release/history?ns=${namespace}&name=${helmReleaseName}`);
+  };
+
+  return (
+    <CustomResourceList
+      fetchCustomResources={getHelmReleaseRevisions}
+      queryArg="rowFilter-helm-release-status"
+      rowFilters={helmRowFilters}
+      sortBy="version"
+      sortOrder={SortByDirection.desc}
+      rowFilterReducer={getFilteredItems}
+      resourceRow={HelmReleaseHistoryRow}
+      resourceHeader={HelmReleaseHistoryHeader}
+    />
+  );
+};
 
 export default HelmReleaseHistory;
