c13b2ea54c155e779a3317307d09ec5e157e0fc1
yaacov <kobi.zamir@gmail.com>
2020-03-26 17:32:23 +0200

    fix xvi

--- frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/redux/stateUpdate/vmSettings/vm-settings-tab-state-update.ts
+++ frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/redux/stateUpdate/vmSettings/vm-settings-tab-state-update.ts
@@ -26,6 +26,9 @@ import { prefillVmTemplateUpdater } from './prefill-vm-template-state-update';
 export const selectUserTemplateOnLoadedUpdater = (options: UpdateOptions) => {
   const { id, dispatch, getState } = options;
   const state = getState();
+  if (options.changedCommonData.has(VMWizardProps.userTemplates)) {
+    return;
+  }
 
   if (iGetVmSettingAttribute(state, id, VMSettingsField.USER_TEMPLATE, 'initialized')) {
     return;
