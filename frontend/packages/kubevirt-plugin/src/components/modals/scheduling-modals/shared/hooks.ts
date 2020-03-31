c6f9d111f15f5671429290703057221df0ce3e4d
Gilad Lekner <glekner@Gilads-iMac.local>
2020-03-31 20:39:05 +0300

    fix node qualifier

--- frontend/packages/kubevirt-plugin/src/components/modals/scheduling-modals/shared/hooks.ts
+++ frontend/packages/kubevirt-plugin/src/components/modals/scheduling-modals/shared/hooks.ts
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { FirehoseResult } from '@console/internal/components/utils';
 import { NodeKind, K8sResourceKind } from '@console/internal/module/k8s';
 import { getLabels } from '@console/shared';
@@ -20,7 +21,12 @@ export const useNodeQualifier = <T extends IDLabel = IDLabel>(
       const newNodes = [];
       loadedNodes.forEach((node) => {
         const nodeLabels = getLabels(node);
-        if (nodeLabels && filteredLabels.every(({ key, value }) => nodeLabels[key] === value)) {
+        if (
+          nodeLabels &&
+          filteredLabels.every(({ key, value }) =>
+            value ? nodeLabels[key] === value : _.has(nodeLabels, key),
+          )
+        ) {
           newNodes.push(node);
         }
       });
