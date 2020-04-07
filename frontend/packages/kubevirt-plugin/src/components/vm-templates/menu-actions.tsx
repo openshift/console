e8188333df08d56c752b4c921ef5d85b59c744e9
yaacov <kobi.zamir@gmail.com>
2020-04-07 09:16:25 +0300

    Add create vm dropdown action

--- frontend/packages/kubevirt-plugin/src/components/vm-templates/menu-actions.tsx
+++ frontend/packages/kubevirt-plugin/src/components/vm-templates/menu-actions.tsx
@@ -1,6 +1,8 @@
 import { getName, getNamespace } from '@console/shared';
 import { K8sKind, TemplateKind } from '@console/internal/module/k8s';
 import { asAccessReview, Kebab } from '@console/internal/components/utils';
+import { VMWizardName, VMWizardMode } from '../../constants/vm';
+import { getVMWizardCreateLink } from '../../utils/url';
 
 const vmTemplateEditAction = (kind: K8sKind, obj: TemplateKind) => ({
   label: `Edit VM Template`,
@@ -8,9 +10,20 @@ const vmTemplateEditAction = (kind: K8sKind, obj: TemplateKind) => ({
   accessReview: asAccessReview(kind, obj, 'update'),
 });
 
+const vmTemplateCreateVMAction = (kind: K8sKind, obj: TemplateKind) => ({
+  label: `Create Virtual Machine`,
+  href: getVMWizardCreateLink(
+    getNamespace(obj),
+    VMWizardName.WIZARD,
+    VMWizardMode.VM,
+    getName(obj),
+  ),
+});
+
 export const menuActions = [
   Kebab.factory.ModifyLabels,
   Kebab.factory.ModifyAnnotations,
   vmTemplateEditAction,
+  vmTemplateCreateVMAction,
   Kebab.factory.Delete,
 ];
