102399d73af0cd90dadd5caccabbca7f777962f2
yaacov <kobi.zamir@gmail.com>
2020-04-07 11:06:35 +0300

    add accessReview

--- frontend/packages/kubevirt-plugin/src/components/vm-templates/menu-actions.tsx
+++ frontend/packages/kubevirt-plugin/src/components/vm-templates/menu-actions.tsx
@@ -2,6 +2,7 @@ import { getName, getNamespace } from '@console/shared';
 import { K8sKind, TemplateKind } from '@console/internal/module/k8s';
 import { asAccessReview, Kebab } from '@console/internal/components/utils';
 import { VMWizardName, VMWizardMode } from '../../constants/vm';
+import { VirtualMachineModel } from '../../models';
 import { getVMWizardCreateLink } from '../../utils/url';
 
 const vmTemplateEditAction = (kind: K8sKind, obj: TemplateKind) => ({
@@ -18,6 +19,7 @@ const vmTemplateCreateVMAction = (kind: K8sKind, obj: TemplateKind) => ({
     VMWizardMode.VM,
     getName(obj),
   ),
+  accessReview: { model: VirtualMachineModel, namespace: getNamespace(obj), verb: 'create' },
 });
 
 export const menuActions = [
