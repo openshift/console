49d02a0ba7bc1c666220088099a4d74082ddda66
Vojtech Szocs <vszocs@redhat.com>
2020-04-09 19:50:42 +0200

    Remove plugin-stats script

--- frontend/packages/console-plugin-sdk/src/codegen/index.ts
+++ frontend/packages/console-plugin-sdk/src/codegen/index.ts
@@ -1,4 +1,3 @@
 export { resolvePluginPackages } from './plugin-resolver';
 export { loadActivePlugins, getActivePluginsModule } from './active-plugins';
 export { reducePluginTestSuites, mergeTestSuites } from './plugin-integration-tests';
-export { printPluginStats } from './plugin-stats';
--- frontend/packages/console-plugin-sdk/src/codegen/plugin-stats.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-import chalk from 'chalk';
-import { ActivePlugin } from '../typings';
-
-export const printPluginStats = (plugins: ActivePlugin[]) => {
-  console.info(`Active plugins: [${plugins.map((p) => `${chalk.green(p.name)}`).join(', ')}]`);
-  // TODO(vojtech): print extension summary per each plugin
-};
