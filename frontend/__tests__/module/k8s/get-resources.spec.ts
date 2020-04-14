686e5bc88cf5603693147b766e1cc2ff513c2eda
Robb Hamilton <rhamilto@redhat.com>
2020-04-14 09:40:37 -0400

    Bug 1823714: Removing humanizing of CRDs

--- frontend/__tests__/module/k8s/get-resources.spec.ts
+++ frontend/__tests__/module/k8s/get-resources.spec.ts
@@ -1,18 +1,4 @@
-import { kindToLabel, pluralizeKind } from '../../../public/module/k8s/get-resources';
-
-describe('kindToLabel', () => {
-  const testKindToLabel = (kind: string, expected: string) => {
-    it(`${kind} into ${expected}`, () => {
-      expect(kindToLabel(kind)).toEqual(expected);
-    });
-  };
-
-  testKindToLabel('DNS', 'DNS');
-  testKindToLabel('DNSRecord', 'DNS Record');
-  testKindToLabel('DeploymentConfig', 'Deployment Config');
-  testKindToLabel('OAuth', 'OAuth');
-  testKindToLabel('OAuthAccessToken', 'OAuth Access Token');
-});
+import { pluralizeKind } from '../../../public/module/k8s/get-resources';
 
 describe('pluralizeKind', () => {
   const testPluralizeKind = (kind: string, expected: string) => {
@@ -23,15 +9,15 @@ describe('pluralizeKind', () => {
 
   testPluralizeKind('DB', 'DBs');
   testPluralizeKind('DNS', 'DNS');
-  testPluralizeKind('DNSRecord', 'DNS Records');
-  testPluralizeKind('DeploymentConfig', 'Deployment Configs');
+  testPluralizeKind('DNSRecord', 'DNSRecords');
+  testPluralizeKind('DeploymentConfig', 'DeploymentConfigs');
   testPluralizeKind('Endpoints', 'Endpoints');
   testPluralizeKind('Identity', 'Identities');
-  testPluralizeKind('ImageContentSourcePolicy', 'Image Content Source Policies');
+  testPluralizeKind('ImageContentSourcePolicy', 'ImageContentSourcePolicies');
   testPluralizeKind('Ingress', 'Ingresses');
   testPluralizeKind('OAuth', 'OAuths');
-  testPluralizeKind('OAuthAccessToken', 'OAuth Access Tokens');
-  testPluralizeKind('PodMetrics', 'Pod Metrics');
+  testPluralizeKind('OAuthAccessToken', 'OAuthAccessTokens');
+  testPluralizeKind('PodMetrics', 'PodMetrics');
   testPluralizeKind('Prometheus', 'Prometheuses');
   testPluralizeKind('Proxy', 'Proxies');
 });
