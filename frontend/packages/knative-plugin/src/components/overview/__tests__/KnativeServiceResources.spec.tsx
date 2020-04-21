adfb77f3691665ea03f02c458e341b8218927f6e
sahil143 <sahilbudhwar143@gmail.com>
2020-04-21 18:44:06 +0530

    fix KnativeServiceResources unit tests

--- frontend/packages/knative-plugin/src/components/overview/__tests__/KnativeServiceResources.spec.tsx
+++ frontend/packages/knative-plugin/src/components/overview/__tests__/KnativeServiceResources.spec.tsx
@@ -7,20 +7,20 @@ import {
   sampleKnativeRevisions,
   knativeServiceObj,
 } from '@console/dev-console/src/components/topology/__tests__/topology-knative-test-data';
+import { OverviewItem } from '@console/shared';
 import KnativeServiceResources from '../KnativeServiceResources';
 import KSRoutesOverviewList from '../RoutesOverviewList';
 import RevisionsOverviewList from '../RevisionsOverviewList';
 
 describe('KnativeServiceResources', () => {
   it('should render KnativeServiceResources', () => {
-    const wrapper = shallow(
-      <KnativeServiceResources
-        revisions={sampleKnativeRevisions.data}
-        ksroutes={sampleKnativeRoutes.data}
-        obj={knativeServiceObj}
-        pods={sampleKnativePods.data}
-      />,
-    );
+    const item = {
+      revisions: sampleKnativeRevisions.data,
+      ksroutes: sampleKnativeRoutes.data,
+      obj: knativeServiceObj,
+      pods: sampleKnativePods.data,
+    } as OverviewItem;
+    const wrapper = shallow(<KnativeServiceResources item={item} />);
     expect(wrapper.find(PodsOverview)).toHaveLength(1);
     expect(wrapper.find(KSRoutesOverviewList)).toHaveLength(1);
     expect(wrapper.find(RevisionsOverviewList)).toHaveLength(1);
