94df8dbef2f4531a627ffd66ea55b5d7a58afb1c
Rebecca Alpert <ralpert@redhat.com>
2020-04-30 15:43:18 -0400

    frontend: Replace dropdown with select
    Replaced ResourceListDropdown implementation with a PatternFly Select.

--- frontend/public/components/resource-dropdown.tsx
+++ frontend/public/components/resource-dropdown.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import * as _ from 'lodash-es';
 import { connect } from 'react-redux';
-import { Map as ImmutableMap, OrderedMap, Set as ImmutableSet } from 'immutable';
+import { Map as ImmutableMap, Set as ImmutableSet } from 'immutable';
 import * as classNames from 'classnames';
 import * as fuzzy from 'fuzzysearch';
 
-import { Dropdown, ResourceIcon } from './utils';
+import { ResourceIcon } from './utils';
 import {
   apiVersionForReference,
   K8sKind,
   K8sResourceKindReference,
-  modelFor,
   referenceForModel,
 } from '../module/k8s';
-import { Badge, Checkbox } from '@patternfly/react-core';
+import { Select, SelectVariant, SelectOption } from '@patternfly/react-core';
 
 // Blacklist known duplicate resources.
 const blacklistGroups = ImmutableSet([
@@ -26,34 +25,9 @@ const blacklistResources = ImmutableSet([
   'events.k8s.io/v1beta1.Event',
 ]);
 
-const DropdownItem: React.SFC<DropdownItemProps> = ({ model, showGroup, checked }) => (
-  <>
-    <span className={'co-resource-item'}>
-      <Checkbox
-        tabIndex={-1}
-        id={`${model.apiGroup}:${model.apiVersion}:${model.kind}`}
-        isChecked={checked}
-      />
-      <span className="co-resource-icon--fixed-width">
-        <ResourceIcon kind={referenceForModel(model)} />
-      </span>
-      <span className="co-resource-item__resource-name">
-        <span>
-          {model.kind}
-          {model.badge && <span className="co-resource-item__tech-dev-preview">{model.badge}</span>}
-        </span>
-        {showGroup && (
-          <span className="co-resource-item__resource-api text-muted co-truncate show co-nowrap small">
-            {model.apiGroup || 'core'}/{model.apiVersion}
-          </span>
-        )}
-      </span>
-    </span>
-  </>
-);
-
 const ResourceListDropdown_: React.SFC<ResourceListDropdownProps> = (props) => {
   const { selected, onChange, allModels, showAll, className, preferredVersions } = props;
+  const [isExpanded, setExpanded] = React.useState(false);
 
   const resources = allModels
     .filter(({ apiGroup, apiVersion, kind, verbs }) => {
@@ -90,63 +64,82 @@ const ResourceListDropdown_: React.SFC<ResourceListDropdownProps> = (props) => {
   const isKindSelected = (kind: string) => {
     return _.includes(selected, kind);
   };
-  // Create dropdown items for each resource.
-  const items = resources.map((model) => (
-    <DropdownItem
+
+  // Create select option for each resource.
+  const items = resources.toArray().map((model) => (
+    <SelectOption
       key={referenceForModel(model)}
-      model={model}
-      showGroup={isDup(model.kind)}
-      checked={isKindSelected(referenceForModel(model))}
-    />
-  )) as OrderedMap<string, JSX.Element>;
-  // Add an "All" item to the top if `showAll`.
-  const allItems = (showAll
-    ? OrderedMap({
-        All: (
-          <>
-            <span className="co-resource-item">
-              <Checkbox id="all-resources" isChecked={isKindSelected('All')} />
-              <span className="co-resource-icon--fixed-width">
-                <ResourceIcon kind="All" />
-              </span>
-              <span className="co-resource-item__resource-name">All Resources</span>
+      value={model}
+      isChecked={isKindSelected(referenceForModel(model))}
+    >
+      <span className={'co-resource-item'}>
+        <span className="co-resource-icon--fixed-width">
+          <ResourceIcon kind={referenceForModel(model)} />
+        </span>
+        <span className="co-resource-item__resource-name">
+          <span>
+            {model.kind}
+            {model.badge && (
+              <span className="co-resource-item__tech-dev-preview">{model.badge}</span>
+            )}
+          </span>
+          {isDup(model.kind) && (
+            <span className="co-resource-item__resource-api text-muted co-truncate show co-nowrap small">
+              {model.apiGroup || 'core'}/{model.apiVersion}
             </span>
-          </>
-        ),
-      }).concat(items)
-    : items
-  ).toJS() as { [s: string]: JSX.Element };
+          )}
+        </span>
+      </span>
+    </SelectOption>
+  ));
 
-  const autocompleteFilter = (text, item) => {
-    const { model } = item.props;
-    if (!model) {
-      return false;
+  // Add an "All" item to the top if `showAll`.
+  const allItems = showAll
+    ? [
+        <SelectOption key="All" value="All" isChecked={isKindSelected('All')}>
+          <span className={'co-resource-item'}>
+            <span className="co-resource-icon--fixed-width">
+              <ResourceIcon kind="All" />
+            </span>
+            <span className="co-resource-item__resource-name">All Resources</span>
+          </span>
+        </SelectOption>,
+      ].concat(items)
+    : items;
+
+  const onFilter = (event) => {
+    const textInput = event.target.value;
+    if (textInput === '') {
+      return allItems;
     }
-
-    return fuzzy(_.toLower(text), _.toLower(model.kind));
+    return allItems.filter((item) => fuzzy(_.toLower(textInput), _.toLower(item.props.value.kind)));
   };
 
-  const handleSelected = (value: string) => {
-    value === 'All' ? onChange('All') : onChange(referenceForModel(modelFor(value)));
+  const onToggle = (isOpen) => {
+    setExpanded(isOpen);
+  };
+
+  const handleSelected = (event, value: K8sKind) => {
+    value === 'All' ? onChange(value) : onChange(referenceForModel(value));
   };
 
   return (
-    <Dropdown
-      menuClassName="dropdown-menu--text-wrap"
+    <Select
+      variant={SelectVariant.checkbox}
+      placeholderText="Resources"
+      selections={selected}
+      isExpanded={isExpanded}
+      ariaLabelledBy="Resources"
+      isGrouped
+      hasInlineFilter
+      onToggle={onToggle}
+      onFilter={onFilter}
+      onSelect={handleSelected}
       className={classNames('co-type-selector', className)}
-      items={allItems}
-      title={
-        <div key="title-resource">
-          Resources{' '}
-          <Badge isRead>
-            {selected.length === 1 && selected[0] === 'All' ? 'All' : selected.length}
-          </Badge>
-        </div>
-      }
-      onChange={handleSelected}
-      autocompleteFilter={autocompleteFilter}
-      autocompletePlaceholder="Select Resource"
-    />
+      noResultsFoundText=""
+    >
+      {allItems}
+    </Select>
   );
 };
 
@@ -161,16 +154,10 @@ export const ResourceListDropdown = connect(resourceListDropdownStateToProps)(
 
 export type ResourceListDropdownProps = {
   selected: K8sResourceKindReference[];
-  onChange: (value: string) => void;
+  onChange: (selection: string) => void;
   allModels: ImmutableMap<K8sResourceKindReference, K8sKind>;
   preferredVersions: { groupVersion: string; version: string }[];
   className?: string;
   id?: string;
   showAll?: boolean;
-};
-
-type DropdownItemProps = {
-  model: K8sKind;
-  showGroup?: boolean;
-  checked?: boolean;
 };
