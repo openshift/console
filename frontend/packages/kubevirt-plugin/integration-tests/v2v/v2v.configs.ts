8de3735a14d47610d912e7d91b32a5c96ce15024
Igor Braginsky <ibragins@redhat.com>
2020-05-11 13:26:46 +0300

    Fixed config for multi flavors

--- frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.configs.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.configs.ts
@@ -79,222 +79,34 @@ export const import2VMsConfigs = [
 //Configurations for importing VMs with different flavors and workload profiles
 const importedVmName = 'smal-rhel7-imported';
 const sourceVMName = 'smal-rhel7';
-export const tinyDesktop: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.TINY},
-};
 
-export const smallDesktop: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.SMALL},
-};
+export const flavorWorkloadConfigs = []
 
-export const mediumDesktop: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.MEDIUM},
-};
+const flavors = [Flavor.TINY, Flavor.SMALL, Flavor.MEDIUM, Flavor.LARGE]
+const profiles = [WorkloadProfile.DESKTOP, WorkloadProfile.SERVER, WorkloadProfile.HIGH_PERFORMANCE]
+flavors.forEach(currentFlavor => {
+  profiles.forEach(currentProfile => {
+     flavorWorkloadConfigs.push(getFlavorConfig(currentFlavor, currentProfile))
+  })
+});
 
-export const largeDesktop: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.LARGE},
-};
+function getFlavorConfig(currentFlavor: Flavor, currentProfile: WorkloadProfile){
+  var currentFlavorWorkloadConfig = {
+    name: importedVmName,
+    sourceVMName: sourceVMName,
+    provider: 'VMware',
+    instanceConfig: {
+      instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
+      hostname: V2V_INSTANCE_HOSTNAME,
+      username: V2V_INSTANCE_USERNAME,
+      password: V2V_INSTANCE_PASSWORD,
+      saveInstance: false,
+    },
+    operatingSystem: OperatingSystem.RHEL7_6,
+    workloadProfile: currentProfile,
+    flavorConfig: {
+      flavor: currentFlavor},    
+  }
+  return currentFlavorWorkloadConfig
+}
 
-export const tinyServer: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.TINY},
-};
-
-export const smallServer: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.SMALL},
-};
-
-export const mediumServer: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.MEDIUM},
-};
-
-export const largeServer: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.LARGE},
-};
-
-export const tinyHighperformance: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.TINY},
-};
-
-export const smallHighperformance: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.SMALL},
-};
-
-export const mediumHighperformance: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.MEDIUM},
-};
-
-export const largeHighperformance: VMImportConfig = {
-  name: importedVmName,
-  sourceVMName: sourceVMName,
-  provider: 'VMware',
-  instanceConfig: {
-    instance: IMPORT_WIZARD_CONN_TO_NEW_INSTANCE,
-    hostname: V2V_INSTANCE_HOSTNAME,
-    username: V2V_INSTANCE_USERNAME,
-    password: V2V_INSTANCE_PASSWORD,
-    saveInstance: false,
-  },
-  operatingSystem: OperatingSystem.RHEL7_6,
-  workloadProfile: WorkloadProfile.DESKTOP,
-  flavorConfig: {
-    flavor: Flavor.LARGE},
-};
-
-
-export const flavorConfigs = [
-  tinyDesktop, 
-  smallDesktop,
-  mediumDesktop,
-  largeDesktop,
-  tinyServer, 
-  smallServer,
-  mediumServer,
-  largeServer,
-  tinyHighperformance, 
-  smallHighperformance,
-  mediumHighperformance,
-  largeHighperformance
-];
--- frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.import.all.flavors.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/v2v/v2v.import.all.flavors.ts
@@ -2,22 +2,22 @@ import { withResource } from '@console/shared/src/test-utils/utils';
 import { testName } from '@console/internal-integration-tests/protractor.conf';
 import { V2V_VM_IMPORT_TIMEOUT, VM_STATUS } from '../tests/utils/consts';
 import { VirtualMachine } from '../tests/models/virtualMachine';
-import { flavorConfigs } from './v2v.configs';
+import { flavorWorkloadConfigs } from './v2v.configs';
 
 describe('Kubevirt create VMs using wizard, all flavors and profiles', () => {
   const leakedResources = new Set<string>();
 //   const notCleanEnv = true;
 
-  flavorConfigs.forEach( (importConfig) => {
+  flavorWorkloadConfigs.forEach( (currentConfig) => {
     it(
-      `Imports VM ${importConfig.name} from VMware Instance`,
+      `Imports VM ${currentConfig.name} from VMware Instance`,
       async () => {
-        const vm = new VirtualMachine({ name: importConfig.name, namespace: testName });
+        const vm = new VirtualMachine({ name: currentConfig.name, namespace: testName });
         await withResource(
             leakedResources, 
             vm.asResource(), 
             async () => {
-                await vm.import(importConfig);
+                await vm.import(currentConfig);
                 await vm.waitForStatus(VM_STATUS.Off, V2V_VM_IMPORT_TIMEOUT);
             }
         );
