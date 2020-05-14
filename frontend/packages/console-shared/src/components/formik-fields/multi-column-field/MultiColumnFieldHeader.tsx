3a9d1070b2f035aca5af9487ffd86dd6f2cf91fa
Aritra Roy <ari.roy09@gmail.com>
2020-05-14 19:03:39 +0530

    fix traffic splitting required fields

--- frontend/packages/console-shared/src/components/formik-fields/multi-column-field/MultiColumnFieldHeader.tsx
+++ frontend/packages/console-shared/src/components/formik-fields/multi-column-field/MultiColumnFieldHeader.tsx
@@ -3,7 +3,7 @@ import { Grid, GridItem, gridItemSpanValueShape } from '@patternfly/react-core';
 import './MultiColumnField.scss';
 
 export interface MultiColumnFieldHeaderProps {
-  headers: string[];
+  headers: ({ name: string; required: boolean } | string)[];
   spans: gridItemSpanValueShape[];
 }
 
@@ -11,8 +11,24 @@ const MultiColumnFieldHeader: React.FC<MultiColumnFieldHeaderProps> = ({ headers
   <div className="odc-multi-column-field__row">
     <Grid className="odc-multi-column-field__row">
       {headers.map((header, i) => (
-        <GridItem span={spans[i]} key={header}>
-          <div className="odc-multi-column-field__col">{header}</div>
+        <GridItem span={spans[i]} key={typeof header === 'string' ? header : header.name}>
+          <div className="odc-multi-column-field__col">
+            {typeof header === 'string' ? (
+              header
+            ) : (
+              <>
+                {header.name}
+                {header.required && (
+                  <span
+                    className="odc-multi-column-field__header--required-label"
+                    aria-hidden="true"
+                  >
+                    *
+                  </span>
+                )}
+              </>
+            )}
+          </div>
         </GridItem>
       ))}
     </Grid>
