6f42ea4b78d6685646a1662041489ae7ff2f5541
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2020-05-14 14:02:47 +0530

    fixes eventsource icon in context menu

--- frontend/packages/knative-plugin/src/actions/add-event-source.tsx
+++ frontend/packages/knative-plugin/src/actions/add-event-source.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { HelpIcon } from '@patternfly/react-icons';
 import {
   createKebabAction,
   KebabAction,
 } from '@console/dev-console/src/utils/add-resources-menu-utils';
 import { ImportOptions } from '@console/dev-console/src/components/import/import-types';
+import * as eventSourceImg from '../imgs/event-source.svg';
+
+const eventSourceIconStyle = {
+  width: '1em',
+  height: '1em',
+};
+const EventSourceIcon: React.FC = () => {
+  return <img style={eventSourceIconStyle} src={eventSourceImg} alt="" />;
+};
 
 export const addEventSource: KebabAction = createKebabAction(
   'Event Source',
-  <HelpIcon />,
+  <EventSourceIcon />,
   ImportOptions.EVENTSOURCE,
 );
