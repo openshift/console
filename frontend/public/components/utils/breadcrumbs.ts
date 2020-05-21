261ed930016a04868fedebbf53a52fdad7eb5076
Rebecca Alpert <ralpert@redhat.com>
2020-05-21 17:00:30 -0400

    frontend: Use last namespace for nav/breadcrumbs
    Updated breadcrumbs and resource nav items to use the last namespace (if available) when generating the path.
    
    Fixes https://issues.redhat.com/projects/CONSOLE/issues/CONSOLE-2228

--- frontend/public/components/utils/breadcrumbs.ts
+++ frontend/public/components/utils/breadcrumbs.ts
@@ -1,11 +1,21 @@
 import { K8sKind } from '../../module/k8s';
+import { LAST_NAMESPACE_NAME_LOCAL_STORAGE_KEY } from '@console/shared/src/constants';
+import { ALL_NAMESPACES_KEY } from '@console/shared/src/constants/common';
+
+export const getBreadcrumbPath = (match: any, customPlural?: string) => {
+  const lastNamespace = localStorage.getItem(LAST_NAMESPACE_NAME_LOCAL_STORAGE_KEY);
+  if (match.params.ns) {
+    return lastNamespace === ALL_NAMESPACES_KEY
+      ? `/k8s/all-namespaces/${customPlural || match.params.plural}`
+      : `/k8s/ns/${match.params.ns}/${customPlural || match.params.plural}`;
+  }
+  return `/k8s/cluster/${customPlural || match.params.plural}`;
+};
 
 export const breadcrumbsForDetailsPage = (kindObj: K8sKind, match: any) => () => [
   {
     name: `${kindObj.labelPlural}`,
-    path: match.params.ns
-      ? `/k8s/ns/${match.params.ns}/${match.params.plural}`
-      : `/k8s/cluster/${match.params.plural}`,
+    path: getBreadcrumbPath(match),
   },
   { name: `${kindObj.label} Details`, path: `${match.url}` },
 ];
