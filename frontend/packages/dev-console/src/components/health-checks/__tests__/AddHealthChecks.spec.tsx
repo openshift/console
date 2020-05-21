e2f809326e973d1f3b29416bf658b766c7adea4a
Mohammed Saud <md.saud020@gmail.com>
2020-05-21 16:06:19 +0530

    mock useFormikContext

--- frontend/packages/dev-console/src/components/health-checks/__tests__/AddHealthChecks.spec.tsx
+++ frontend/packages/dev-console/src/components/health-checks/__tests__/AddHealthChecks.spec.tsx
@@ -9,6 +9,8 @@ import { getHealthChecksData } from '../create-health-checks-probe-utils';
 import { getResourcesType } from '../../edit-application/edit-application-utils';
 import HealthChecks from '../HealthChecks';
 
+jest.mock('formik', () => ({ useFormikContext: () => ({ values: [] }) }));
+
 let addHealthCheckProbs: React.ComponentProps<typeof AddHealthChecks>;
 
 describe('AddHealthCheck', () => {
