63633b681706bf796e919ae3ef4cb8718f457548
Mohammed Saud <md.saud020@gmail.com>
2020-05-27 13:43:47 +0530

    Squash: max value checks
    warn on entering max int value
    
    test for max int value warning
    
    add max value check to successThreshold
    
    update warning message
    
    add integer checks

--- frontend/packages/dev-console/src/components/health-checks/health-checks-probe-validation-utils.ts
+++ frontend/packages/dev-console/src/components/health-checks/health-checks-probe-validation-utils.ts
@@ -2,6 +2,8 @@ import * as yup from 'yup';
 
 const pathRegex = /^\/.*$/;
 
+export const MAX_INT32 = 2147483647;
+
 export const healthChecksValidationSchema = yup.object().shape({
   containerName: yup.string(),
   showForm: yup.boolean(),
@@ -9,15 +11,30 @@ export const healthChecksValidationSchema = yup.object().shape({
   data: yup.object().when('showForm', {
     is: true,
     then: yup.object().shape({
-      periodSeconds: yup.number().min(1, 'Period must be greater than or equal to 1.'),
-      initialDelaySeconds: yup.number().min(0, 'Initial Delay must be greater than or equal to 0.'),
+      periodSeconds: yup
+        .number()
+        .integer('Value must be an integer.')
+        .min(1, 'Period must be greater than or equal to 1.')
+        .max(MAX_INT32, 'Value is larger than maximum value allowed.'),
+      initialDelaySeconds: yup
+        .number()
+        .integer('Value must be an integer.')
+        .min(0, 'Initial Delay must be greater than or equal to 0.')
+        .max(MAX_INT32, 'Value is larger than maximum value allowed.'),
       failureThreshold: yup
         .number()
+        .integer('Value must be an integer.')
         .min(1, 'Failure Threshold must be greater than or equal to 1.'),
-      timeoutSeconds: yup.number().min(1, 'Timeout must be greater than or equal to 1.'),
+      timeoutSeconds: yup
+        .number()
+        .integer('Value must be an integer.')
+        .min(1, 'Timeout must be greater than or equal to 1.')
+        .max(MAX_INT32, 'Value is larger than maximum value allowed.'),
       successThreshold: yup
         .number()
-        .min(1, 'Success Threshold must be greater than or equal to 1.'),
+        .integer('Value must be an integer.')
+        .min(1, 'Success Threshold must be greater than or equal to 1.')
+        .max(MAX_INT32, 'Value is larger than maximum value allowed.'),
       requestType: yup.string(),
       httpGet: yup.object().when('requestType', {
         is: 'httpGet',
