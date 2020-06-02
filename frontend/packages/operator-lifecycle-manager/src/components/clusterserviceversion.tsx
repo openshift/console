20f5147a839fe3299ac2f63f364821a1a8c901ea
Cyril <cyril.ajieh@gmail.com>
2020-06-02 10:11:56 -0400

    Operand's tab for Operand list view is missing

--- frontend/packages/operator-lifecycle-manager/src/components/clusterserviceversion.tsx
+++ frontend/packages/operator-lifecycle-manager/src/components/clusterserviceversion.tsx
@@ -980,7 +980,7 @@ export const ClusterServiceVersionsDetailsPage: React.FC<ClusterServiceVersionsD
                   href: referenceForProvidedAPI(desc),
                   name: ['Details', 'YAML', 'Subscription', 'Events'].includes(desc.displayName)
                     ? `${desc.displayName} Operand`
-                    : desc.displayName,
+                    : desc.displayName || desc.kind,
                   component: ProvidedAPIPage,
                   pageData: {
                     csv: obj,
