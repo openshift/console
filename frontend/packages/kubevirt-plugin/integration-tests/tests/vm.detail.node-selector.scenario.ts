bdd9ae9fe5ddd6325e07050fd801541bd0df9726
Radim Hrazdil <rhrazdil@redhat.com>
2020-06-15 23:06:19 +0200

    Refactor VM node selector scenario

--- frontend/packages/kubevirt-plugin/integration-tests/tests/vm.detail.node-selector.scenario.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/tests/vm.detail.node-selector.scenario.ts
@@ -1,17 +1,20 @@
 import { browser, ExpectedConditions as until } from 'protractor';
 import { testName } from '@console/internal-integration-tests/protractor.conf';
-import { createResource, deleteResource, click } from '@console/shared/src/test-utils/utils';
-import * as editNodeSelectorView from '../views/dialogs/editNodeSelectorView';
+import { createResource, deleteResource } from '@console/shared/src/test-utils/utils';
 import * as virtualMachineView from '../views/virtualMachine.view';
-import { saveButton } from '../views/kubevirtUIResource.view';
 import { VM_CREATE_AND_EDIT_TIMEOUT_SECS } from './utils/consts';
 import { VirtualMachine } from './models/virtualMachine';
 import { getVMManifest } from './utils/mocks';
 import { getRandStr } from './utils/utils';
+import { MatchLabels } from '@console/internal/module/k8s';
 
 describe('KubeVirt VM detail - edit Node Selector', () => {
   const testVM = getVMManifest('Container', testName, `node-selector-vm-${getRandStr(5)}`);
-  const vm = new VirtualMachine(testVM.metadata);
+  const vm: VirtualMachine = new VirtualMachine(testVM.metadata);
+  const labels: MatchLabels = {
+    key1: 'value1',
+    key2: 'value2',
+  };
 
   beforeAll(async () => {
     createResource(testVM);
@@ -24,23 +27,20 @@ describe('KubeVirt VM detail - edit Node Selector', () => {
   it(
     'ID(CNV-4133) Adds a Node Selector, then removes it',
     async () => {
-      await vm.navigateToDetail();
-      await vm.modalEditNodeSelector();
-      await click(editNodeSelectorView.addLabelBtn);
-      await editNodeSelectorView.labelKeyInputByID(0).sendKeys('key');
-      await editNodeSelectorView.labelValueInputByID(0).sendKeys('value');
-      await click(saveButton);
-
+      await vm.addNodeSelectors(labels);
       await browser.wait(
-        until.textToBePresentInElement(
-          virtualMachineView.vmDetailNodeSelector(vm.namespace, vm.name),
-          'key=value',
+        until.and(
+          until.textToBePresentInElement(
+            virtualMachineView.vmDetailNodeSelector(vm.namespace, vm.name),
+            `key1=${labels.key1}`,
+          ),
+          until.textToBePresentInElement(
+            virtualMachineView.vmDetailNodeSelector(vm.namespace, vm.name),
+            `key2=${labels.key2}`,
+          ),
         ),
       );
-
-      await vm.modalEditNodeSelector();
-      await click(editNodeSelectorView.deleteBtnByID(0));
-      await click(saveButton);
+      await vm.deleteNodeSelectors(Object.keys(labels));
       await browser.wait(
         until.textToBePresentInElement(
           virtualMachineView.vmDetailNodeSelector(vm.namespace, vm.name),
