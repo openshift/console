dcd217a676316ff74e5f3cae8671ec49d9ad3abb
yaacov <kobi.zamir@gmail.com>
2020-06-15 09:43:28 +0300

    Use guest agent types

--- frontend/packages/kubevirt-plugin/src/hooks/use-guest-agent-info.ts
+++ frontend/packages/kubevirt-plugin/src/hooks/use-guest-agent-info.ts
@@ -1,7 +1,8 @@
 import { VMIKind } from '../types';
 import { useURLPoll } from '@console/internal/components/utils/url-poll-hook';
-import { getVMIApiPath, getVMISubresourcePath } from '../selectors/vmi/selectors';
 import { isGuestAgentInstalled } from '../components/dashboards-page/vm-dashboard/vm-alerts';
+import { getVMIApiPath, getVMISubresourcePath } from '../selectors/vmi/selectors';
+import { V1VirtualMachineInstanceGuestAgentInfo } from '../types/vmi-guest-data-info/vmi-guest-agent-info';
 
 const getGuestAgentURL = (vmi: VMIKind) =>
   vmi &&
@@ -9,56 +10,11 @@ const getGuestAgentURL = (vmi: VMIKind) =>
   `/${getVMISubresourcePath()}/${getVMIApiPath(vmi)}/guestosinfo`;
 
 const useGuestAgentInfo = ({ vmi, delay }: GuestAgentInfoProps) =>
-  useURLPoll<VirtualMachineInstanceGuestAgentInfo>(getGuestAgentURL(vmi), delay);
+  useURLPoll<V1VirtualMachineInstanceGuestAgentInfo>(getGuestAgentURL(vmi), delay);
 
 type GuestAgentInfoProps = {
   vmi: VMIKind;
   delay?: number;
-};
-
-// https://kubevirt.io/api-reference/master/definitions.html#_v1_virtualmachineinstancefilesystem
-type VirtualMachineInstanceFileSystem = {
-  diskName: string;
-  fileSystemType: string;
-  mountPoint: string;
-  totalBytes: number;
-  usedBytes: number;
-};
-
-// https://kubevirt.io/api-reference/master/definitions.html#_v1_virtualmachineinstancefilesysteminfo
-type VirtualMachineInstanceFileSystemInfo = {
-  disks: VirtualMachineInstanceFileSystem[];
-};
-
-// https://kubevirt.io/api-reference/master/definitions.html#_v1_virtualmachineinstanceguestagentinfo
-type VirtualMachineInstanceGuestOSInfo = {
-  id?: string;
-  kernelRelease?: string;
-  kernelVersion?: string;
-  machine?: string;
-  name?: string;
-  prettyName?: string;
-  version?: string;
-  versionId?: string;
-};
-
-// https://kubevirt.io/api-reference/master/definitions.html#_v1_virtualmachineinstanceguestosuser
-type VirtualMachineInstanceGuestOSUser = {
-  userName: string;
-  domain?: string;
-  loginTime?: number;
-};
-
-// https://kubevirt.io/api-reference/master/definitions.html#_v1_virtualmachineinstancefilesysteminfo
-type VirtualMachineInstanceGuestAgentInfo = {
-  apiVersion?: string;
-  fsInfo?: VirtualMachineInstanceFileSystemInfo;
-  guestAgentVersion?: string;
-  hostname?: string;
-  kind?: string;
-  os?: VirtualMachineInstanceGuestOSInfo;
-  timezone?: string;
-  userList?: VirtualMachineInstanceGuestOSUser[];
 };
 
 export { useGuestAgentInfo, GuestAgentInfoProps };
