9983fcd892b067b2512be3954900a4f3e2d00d62
Guohua Ouyang <gouyang@redhat.com>
2020-06-30 13:49:43 +0800

    Bug 1852301: Add integration test for Utilization and Events

--- frontend/packages/kubevirt-plugin/integration-tests/tests/vm.tab.dashboard.scenario.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/tests/vm.tab.dashboard.scenario.ts
@@ -1,32 +1,31 @@
 import { browser, ExpectedConditions as until } from 'protractor';
 import { testName } from '@console/internal-integration-tests/protractor.conf';
-import { createResources, deleteResources } from '@console/shared/src/test-utils/utils';
-import { VirtualMachineModel } from '@console/kubevirt-plugin/src/models';
 import {
-  vmDetailsName,
-  vmDetailsNamespace,
-  vmDetailsNode,
-  vmDetailsIPAddress,
-  vmStatus,
-  vmInventoryNICs,
-  vmInventoryDisks,
-} from '../views/dashboard.view';
+  click,
+  createResources,
+  deleteResources,
+  waitForStringInElement,
+} from '@console/shared/src/test-utils/utils';
+import { VirtualMachineModel } from '@console/kubevirt-plugin/src/models';
 import { getVMManifest, hddDisk, multusNetworkInterface, multusNAD } from './utils/mocks';
 import { VirtualMachine } from './models/virtualMachine';
-import { waitForStringInElement } from '../../../console-shared/src/test-utils/utils';
 import {
-  VM_STATUS,
-  VM_BOOTUP_TIMEOUT_SECS,
-  VM_ACTION,
-  VM_IMPORT_TIMEOUT_SECS,
-  TAB,
-  VM_STOP_TIMEOUT_SECS,
+  JASMINE_EXTENDED_TIMEOUT_INTERVAL,
   NOT_AVAILABLE,
   PAGE_LOAD_TIMEOUT_SECS,
+  TAB,
+  VM_ACTION,
+  VM_STATUS,
+  VM_BOOTUP_TIMEOUT_SECS,
+  VM_CREATE_AND_EDIT_AND_CLOUDINIT_TIMEOUT_SECS,
+  VM_IMPORT_TIMEOUT_SECS,
+  VM_STOP_TIMEOUT_SECS,
 } from './utils/consts';
+import * as dashboardView from '../views/dashboard.view';
 
 describe('Test VM dashboard', () => {
-  const testVM = getVMManifest('URL', testName, null, 'foo');
+  const cloudInit = `#cloud-config\nuser: cloud-user\npassword: atomic\nchpasswd: {expire: False}\nruncmd:\n- dnf install -y qemu-guest-agent\n- systemctl start qemu-guest-agent`;
+  const testVM = getVMManifest('Container', testName, null, cloudInit);
 
   let vm: VirtualMachine;
 
@@ -36,7 +35,7 @@ describe('Test VM dashboard', () => {
     await vm.navigateToOverview();
     try {
       await browser.wait(
-        until.not(until.textToBePresentInElement(vmStatus, VM_STATUS.Off)),
+        until.not(until.textToBePresentInElement(dashboardView.vmStatus, VM_STATUS.Off)),
         PAGE_LOAD_TIMEOUT_SECS,
       );
     } catch (ex) {
@@ -44,7 +43,7 @@ describe('Test VM dashboard', () => {
       // we want to wait for import to start but in some cases it may have already completed
     }
     await browser.wait(
-      until.textToBePresentInElement(vmStatus, VM_STATUS.Off),
+      until.textToBePresentInElement(dashboardView.vmStatus, VM_STATUS.Off),
       VM_IMPORT_TIMEOUT_SECS,
     );
   }, VM_IMPORT_TIMEOUT_SECS);
@@ -54,12 +53,12 @@ describe('Test VM dashboard', () => {
   });
 
   it('ID(CNV-3333) Inventory card', async () => {
-    expect(vmInventoryNICs.getText()).toEqual('1 NIC');
-    expect(vmInventoryNICs.$('a').getAttribute('href')).toMatch(
+    expect(dashboardView.vmInventoryNICs.getText()).toEqual('1 NIC');
+    expect(dashboardView.vmInventoryNICs.$('a').getAttribute('href')).toMatch(
       new RegExp(`.*/k8s/ns/${vm.namespace}/${VirtualMachineModel.plural}/${vm.name}/nics`),
     );
-    expect(vmInventoryDisks.getText()).toEqual('2 Disks');
-    expect(vmInventoryDisks.$('a').getAttribute('href')).toMatch(
+    expect(dashboardView.vmInventoryDisks.getText()).toEqual('2 Disks');
+    expect(dashboardView.vmInventoryDisks.$('a').getAttribute('href')).toMatch(
       new RegExp(`.*/k8s/ns/${vm.namespace}/${VirtualMachineModel.plural}/${vm.name}/disks`),
     );
 
@@ -67,33 +66,88 @@ describe('Test VM dashboard', () => {
     await vm.addNIC(multusNetworkInterface);
     await vm.navigateToTab(TAB.Overview);
 
-    expect(vmInventoryNICs.getText()).toEqual('2 NICs');
-    expect(vmInventoryDisks.getText()).toEqual('3 Disks');
+    expect(dashboardView.vmInventoryNICs.getText()).toEqual('2 NICs');
+    expect(dashboardView.vmInventoryDisks.getText()).toEqual('3 Disks');
 
     await vm.removeDisk(hddDisk.name);
     await vm.removeNIC(multusNetworkInterface.name);
   });
 
-  it('ID(CNV-3330) Status card', async () => {
-    await vm.waitForStatus(VM_STATUS.Off);
-    await vm.navigateToOverview();
-    expect(vmStatus.getText()).toEqual(VM_STATUS.Off);
+  it(
+    'ID(CNV-3330) Status card',
+    async () => {
+      await vm.waitForStatus(VM_STATUS.Off);
+      await vm.navigateToOverview();
+      expect(dashboardView.vmStatus.getText()).toEqual(VM_STATUS.Off);
 
-    await vm.action(VM_ACTION.Start, true, VM_BOOTUP_TIMEOUT_SECS);
-    await vm.navigateToTab(TAB.Overview);
-    expect(vmStatus.getText()).toEqual(VM_STATUS.Running);
-  });
+      await vm.action(VM_ACTION.Start, true, VM_BOOTUP_TIMEOUT_SECS);
+      await vm.navigateToTab(TAB.Overview);
+      expect(dashboardView.vmStatus.getText()).toEqual(VM_STATUS.Running);
+      await browser.wait(until.stalenessOf(dashboardView.vmStatusAlert));
+    },
+    VM_CREATE_AND_EDIT_AND_CLOUDINIT_TIMEOUT_SECS,
+  );
 
   it('ID(CNV-3332) Details card', async () => {
-    expect(vmDetailsName.getText()).toEqual(vm.name);
-    expect(vmDetailsNamespace.getText()).toEqual(vm.namespace);
-    expect(vmDetailsNode.getText()).not.toEqual(NOT_AVAILABLE);
-    expect(vmDetailsIPAddress.getText()).not.toEqual(NOT_AVAILABLE);
+    await browser.wait(waitForStringInElement(dashboardView.vmDetailsHostname, vm.name));
+    expect(dashboardView.vmDetailsName.getText()).toEqual(vm.name);
+    expect(dashboardView.vmDetailsNamespace.getText()).toEqual(vm.namespace);
+    expect(dashboardView.vmDetailsNode.getText()).not.toEqual(NOT_AVAILABLE);
+    expect(dashboardView.vmDetailsIPAddress.getText()).not.toEqual(NOT_AVAILABLE);
+    expect(dashboardView.vmDetailsOS.getText()).toContain('Fedora');
+    expect(dashboardView.vmDetailsTZ.getText()).toContain('UTC');
+    expect(dashboardView.vmDetailsLoggedUser.getText()).toEqual('No users logged in');
+
+    await click(dashboardView.vmDetailsViewAll);
+    const currentUrl = await browser.getCurrentUrl();
+    expect(currentUrl).toMatch(
+      new RegExp(`.*/k8s/ns/${vm.namespace}/${VirtualMachineModel.plural}/${vm.name}/details`),
+    );
 
     await vm.action(VM_ACTION.Stop, true, VM_STOP_TIMEOUT_SECS);
     await vm.navigateToTab(TAB.Overview);
 
-    await browser.wait(waitForStringInElement(vmDetailsNode, NOT_AVAILABLE));
-    await browser.wait(waitForStringInElement(vmDetailsIPAddress, NOT_AVAILABLE));
+    expect(dashboardView.vmDetailsNode.getText()).toEqual(NOT_AVAILABLE);
+    expect(dashboardView.vmDetailsIPAddress.getText()).toEqual(NOT_AVAILABLE);
+    expect(dashboardView.vmDetailsHostname.getText()).toEqual('VM not running');
+    expect(dashboardView.vmDetailsOS.getText()).toEqual('Red Hat Enterprise Linux 7.0 or higher');
+    expect(dashboardView.vmDetailsTZ.getText()).toEqual('VM not running');
+    expect(dashboardView.vmDetailsLoggedUser.getText()).toEqual('VM not running');
+  });
+
+  it(
+    'ID(CNV-3331) Utilization card',
+    async () => {
+      expect(dashboardView.vmUtilizationItemUsage(0).getText()).toContain('CPU');
+      expect(dashboardView.vmUtilizationItemUsage(0).getText()).toContain(NOT_AVAILABLE);
+      expect(dashboardView.vmUtilizationItemMetrics(0).getText()).toContain('No datapoints found');
+
+      await vm.action(VM_ACTION.Start, true, VM_BOOTUP_TIMEOUT_SECS);
+      await vm.navigateToTab(TAB.Overview);
+      expect(dashboardView.vmStatus.getText()).toEqual(VM_STATUS.Running);
+      // CPU metrics takes very long time to show up
+      await browser.wait(
+        until.not(
+          until.textToBePresentInElement(dashboardView.vmUtilizationItemUsage(0), NOT_AVAILABLE),
+        ),
+        150 * 1000,
+      );
+      for (let i = 0; i < dashboardView.vmUtilizationItems.count(); i++) {
+        expect(dashboardView.vmUtilizationItemUsage(i).getText()).not.toContain(NOT_AVAILABLE);
+        expect(dashboardView.vmUtilizationItemMetrics(i).getText()).not.toContain(
+          'No datapoints found',
+        );
+      }
+    },
+    JASMINE_EXTENDED_TIMEOUT_INTERVAL,
+  );
+
+  it('ID(CNV-3329) Events card', async () => {
+    await browser.wait(until.presenceOf(dashboardView.vmEvents));
+    await click(dashboardView.vmEventsViewAll);
+    const currentUrl = await browser.getCurrentUrl();
+    expect(currentUrl).toMatch(
+      new RegExp(`.*/k8s/ns/${vm.namespace}/${VirtualMachineModel.plural}/${vm.name}/events`),
+    );
   });
 });
--- frontend/packages/kubevirt-plugin/integration-tests/views/dashboard.view.ts
+++ frontend/packages/kubevirt-plugin/integration-tests/views/dashboard.view.ts
@@ -4,11 +4,26 @@ export const vmDetails = $$('.co-details-card__body dd');
 export const vmDetailsName = vmDetails.get(0);
 export const vmDetailsNamespace = vmDetails.get(1).$('.co-resource-item__resource-name');
 export const vmDetailsCreated = vmDetails.get(2);
-export const vmDetailsNode = vmDetails.get(3);
-export const vmDetailsIPAddress = vmDetails.get(4);
+export const vmDetailsHostname = vmDetails.get(3);
+export const vmDetailsNode = vmDetails.get(4);
+export const vmDetailsIPAddress = vmDetails.get(5);
+export const vmDetailsOS = vmDetails.get(6);
+export const vmDetailsTZ = vmDetails.get(7);
+export const vmDetailsLoggedUser = vmDetails.get(8);
+export const vmDetailsViewAll = $$('.co-dashboard-card__link').get(0);
 
 export const vmStatus = $('.co-status-card__health-body > span');
+export const vmStatusAlert = $('.co-status-card__alert-item');
 
 export const vmInventoryItems = $$('.co-inventory-card__item');
 export const vmInventoryNICs = vmInventoryItems.get(0);
 export const vmInventoryDisks = vmInventoryItems.get(1);
+
+export const vmEvents = $('.co-activity-card__recent-accordion');
+export const vmEventsViewAll = $$('.co-dashboard-card__link').get(1);
+
+export const vmUtilizationItems = $$('[data-test-id=utilization-item]');
+export const vmUtilizationItemUsage = (index: number) =>
+  vmUtilizationItems.get(index).$('.co-utilization-card__item-section');
+export const vmUtilizationItemMetrics = (index: number) =>
+  vmUtilizationItems.get(index).$('.co-utilization-card__item-chart');
