a714cdb2131774ae91e2b1160fb302fe4570fa5c
abhi <abhinandan13jan>
2020-07-14 18:50:17 +0530

    add serverless tour data

---
+++ frontend/packages/console-app/src/components/quick-starts/utils/quick-start-data.ts
@@ -0,0 +1,11 @@
+import { exploreServerlessQuickStart } from './explore-serverless-quickstart';
+import { explorePipelinesQuickStart } from './explore-pipeline-quickstart';
+import { installAssociatePipelineQuickStart } from './install-associate-pipeline-quickstart';
+
+const quickStartData = [
+  exploreServerlessQuickStart,
+  explorePipelinesQuickStart,
+  installAssociatePipelineQuickStart,
+];
+
+export default quickStartData;
