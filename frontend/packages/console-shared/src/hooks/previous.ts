006564e1e521b90a463a3438eef49a94991d275e
Rastislav Wagner <rawagner@redhat.com>
2020-07-15 20:54:12 +0200

    use value as default dependency array for usePreviousHook

--- frontend/packages/console-shared/src/hooks/previous.ts
+++ frontend/packages/console-shared/src/hooks/previous.ts
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
 
-export const usePrevious = <P = any>(value: P, deps: any[] = []): P =>
+export const usePrevious = <P = any>(value: P, deps: any[] = [value]): P =>
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useMemo(() => value, deps);
