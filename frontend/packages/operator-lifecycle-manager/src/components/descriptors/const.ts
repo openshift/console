9f0b12d21988bfd4a998ad920cb464b82d6a7cb1
Jon Jackson <jonjacks@redhat.com>
2020-07-24 16:55:39 -0400

    Support schema-grouped descriptor arrays on operand details page
    Implement descriptor groups for descriptors which reference array values.
    
    Story: https://issues.redhat.com/browse/CONSOLE-2283
    Sub-task: https://issues.redhat.com/browse/CONSOLE-2328

--- frontend/packages/operator-lifecycle-manager/src/components/descriptors/const.ts
+++ frontend/packages/operator-lifecycle-manager/src/components/descriptors/const.ts
@@ -12,3 +12,20 @@ export const REGEXP_SELECT_OPTION = new RegExp(`${REGEXP_SELECT_CAPABILITY}(.*)$
 export const REGEXP_FIELD_DEPENDENCY_PATH_VALUE = new RegExp(
   `^${REGEXP_FIELD_DEPENDENCY_CAPABILITY}([^:]*):(.*)$`,
 );
+
+// Matches a path string containing an array reference. Captures
+// the segment before the array reference, and the segment after.
+// For example:
+//    path[0].element.property -> [path, element.property]
+export const REGEXP_ARRAY_PATH = /^(.*)\[\d+\]\.?(.*)$/;
+
+// Matches a path string with multiple array references.
+// e.g.:  nested[0].array[0].property
+export const REGEXP_NESTED_ARRAY_PATH = /^.*\[\d+\]\.?.*\[\d+\]\.?.*$/;
+
+//  Captures the root segment of a path string, and all desscendent segments as
+// a single string.
+// For example:
+//    'this.is.a.really.long.path' -> ['this', 'is.a.really.long.path'])
+//    'this'  -> ['this']
+export const REGEXP_CAPTURE_GROUP_SUBGROUP = /^([^.]*)\.?(.*)$/;
