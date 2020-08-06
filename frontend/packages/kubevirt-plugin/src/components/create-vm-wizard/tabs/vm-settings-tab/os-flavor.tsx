7ab0ad2a0a13b8723868358c38e62f129fd0ffac
yaacov <kobi.zamir@gmail.com>
2020-08-06 18:37:23 +0300

    use different pluralize method

--- frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/tabs/vm-settings-tab/os-flavor.tsx
+++ frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/tabs/vm-settings-tab/os-flavor.tsx
@@ -8,7 +8,6 @@ import {
   Button,
   ButtonVariant,
 } from '@patternfly/react-core';
-import { pluralize } from '@console/internal/components/utils';
 import { ValidationErrorType, asValidationObject } from '@console/shared/src/utils/validation';
 import {
   concatImmutableLists,
@@ -26,6 +25,7 @@ import {
   getWorkloadProfiles,
 } from '../../../../selectors/vm-template/combined-dependent';
 import { flavorSort, ignoreCaseSort } from '../../../../utils/sort';
+import { pluralize } from '../../../../utils/strings';
 import { VMSettingsField } from '../../types';
 import { iGetFieldValue } from '../../selectors/immutable/field';
 import { getPlaceholder, getFieldId } from '../../utils/renderable-field-utils';
@@ -151,7 +151,7 @@ export const OSFlavor: React.FC<OSFlavorProps> = React.memo(
     const numOfMountedDisks = cloneBaseDiskImage + mountWindowsGuestTools; // using boolean addition operator to count true
     const mountedDisksHelpMsg = numOfMountedDisks > 0 && (
       <Text className="kv-create-vm__input-text-help-msg">
-        View the mounted {pluralize(numOfMountedDisks, 'disk', 'disks', false)} in the{' '}
+        View the mounted {pluralize(numOfMountedDisks, 'disk')} in the{' '}
         <Button
           isDisabled={!goToStorageStep}
           isInline
