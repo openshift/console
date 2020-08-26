7db54ae309cbde7a446338d8e317f412c1d8c1ac
yaacov <kobi.zamir@gmail.com>
2020-08-26 10:57:24 +0300

    move devel logic into the compare method

--- frontend/packages/kubevirt-plugin/src/selectors/immutable/template/combined.ts
+++ frontend/packages/kubevirt-plugin/src/selectors/immutable/template/combined.ts
@@ -8,7 +8,7 @@ import {
 import { iGetName } from '../../../components/create-vm-wizard/selectors/immutable/selectors';
 import { ITemplate } from '../../../types/template';
 import { iGetCreationTimestamp, iGetLabels } from '../common';
-import { compareVersions, splitVersion } from '../../../utils/sort';
+import { compareVersions } from '../../../utils/sort';
 
 type FindTemplateOptions = {
   userTemplateName?: string;
@@ -83,16 +83,9 @@ export const iGetRelevantTemplates = (
         const aVersion = aLabels?.get(TEMPLATE_VERSION_LABEL);
         const bVersion = bLabels?.get(TEMPLATE_VERSION_LABEL);
 
-        const versionCMP = compareVersions(splitVersion(aVersion), splitVersion(bVersion)) * -1; // descending
+        const versionCMP = compareVersions(aVersion, bVersion) * -1; // descending
 
         if (versionCMP !== 0) {
-          // 'devel' version if exist is always the highst version.
-          if (aVersion === 'devel') {
-            return -1;
-          }
-          if (bVersion === 'devel') {
-            return 1;
-          }
           return versionCMP;
         }
 
