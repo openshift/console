eaf6a1428a8938d706d5b2fbc461acb1fa44ca1b
yaacov <kobi.zamir@gmail.com>
2020-08-31 16:32:33 +0300

    deprecate none suported network cards

--- frontend/packages/kubevirt-plugin/src/constants/vm/network/network-interface-model.ts
+++ frontend/packages/kubevirt-plugin/src/constants/vm/network/network-interface-model.ts
@@ -4,11 +4,21 @@ import { READABLE_VIRTIO } from '../constants';
 
 export class NetworkInterfaceModel extends ObjectEnum<string> {
   static readonly VIRTIO = new NetworkInterfaceModel('virtio');
-  static readonly E1000 = new NetworkInterfaceModel('e1000');
+  static readonly E1000 = new NetworkInterfaceModel('e1000', { isDeprecated: true });
   static readonly E1000E = new NetworkInterfaceModel('e1000e');
-  static readonly NE2kPCI = new NetworkInterfaceModel('ne2kPCI');
-  static readonly PCNET = new NetworkInterfaceModel('pcnet');
-  static readonly RTL8139 = new NetworkInterfaceModel('rtl8139');
+  static readonly NE2kPCI = new NetworkInterfaceModel('ne2kPCI', { isDeprecated: true });
+  static readonly PCNET = new NetworkInterfaceModel('pcnet', { isDeprecated: true });
+  static readonly RTL8139 = new NetworkInterfaceModel('rtl8139', { isDeprecated: true });
+
+  private readonly deprecated: boolean;
+
+  protected constructor(
+    value: string,
+    { isDeprecated = false }: NetworkInterfaceModelConstructorOpts = {},
+  ) {
+    super(value);
+    this.deprecated = isDeprecated;
+  }
 
   private static readonly ALL = Object.freeze(
     ObjectEnum.getAllClassEnumProperties<NetworkInterfaceModel>(NetworkInterfaceModel),
@@ -27,6 +37,8 @@ export class NetworkInterfaceModel extends ObjectEnum<string> {
   static fromString = (model: string): NetworkInterfaceModel =>
     NetworkInterfaceModel.stringMapper[model];
 
+  isDeprecated = () => this.deprecated;
+
   toString() {
     if (this === NetworkInterfaceModel.VIRTIO) {
       return READABLE_VIRTIO;
@@ -34,3 +46,7 @@ export class NetworkInterfaceModel extends ObjectEnum<string> {
     return this.value;
   }
 }
+
+type NetworkInterfaceModelConstructorOpts = {
+  isDeprecated?: boolean;
+};
