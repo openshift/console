c715bde1bc90f92dd46385c8491deec89683dc3a
Jeffrey Phillips <jephilli@redhat.com>
2020-09-03 08:05:09 -0400

    Fix to show topology list view filtered items correctly

--- frontend/packages/dev-console/src/components/topology/filters/useSearchFilter.ts
+++ frontend/packages/dev-console/src/components/topology/filters/useSearchFilter.ts
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import * as fuzzy from 'fuzzysearch';
 import { toLower } from 'lodash';
-import { getTopologySearchQuery } from './filter-utils';
+import { useQueryParams } from '@console/shared/src';
 
 const fuzzyCaseInsensitive = (a: string, b: string): boolean => fuzzy(toLower(a), toLower(b));
 
 const useSearchFilter = (text: string): [boolean, string] => {
-  const searchQuery = getTopologySearchQuery();
+  const queryParams = useQueryParams();
+  const searchQuery = queryParams.get('searchQuery');
   const filtered = React.useMemo(() => fuzzyCaseInsensitive(searchQuery, text), [
     searchQuery,
     text,
