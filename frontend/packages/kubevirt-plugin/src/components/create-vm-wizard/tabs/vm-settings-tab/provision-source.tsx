17e3bb341c636a2fac5bf99a3e9161b74282b9c7
yaacov <kobi.zamir@gmail.com>
2020-09-09 17:39:35 +0300

    PXE rebase

--- frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/tabs/vm-settings-tab/provision-source.tsx
+++ frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/tabs/vm-settings-tab/provision-source.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { Button, ButtonVariant, SelectOption, Text } from '@patternfly/react-core';
+import { NetworkAttachmentDefinitionModel } from '@console/network-attachment-definition-plugin';
+import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
+import { K8sResourceKind, referenceForModel } from '@console/internal/module/k8s';
 import { ProvisionSource } from '../../../../constants/vm/provision-source';
 import { FormFieldRow } from '../../form/form-field-row';
 import { FormField, FormFieldType } from '../../form/form-field';
 import { iGetFieldValue } from '../../selectors/immutable/field';
 import { VMSettingsField } from '../../types';
 import { iGet } from '../../../../utils/immutable';
+import { connect } from 'react-redux';
+import { RootState } from '@console/internal/redux';
+import { SELECT_PXE_NAD_ERROR, PXE_NAD_NOT_FOUND_INFO } from '../../strings/networking';
 import { FormPFSelect } from '../../../form/form-pf-select';
 import { EXAMPLE_CONTAINER, RHEL_IMAGE_LINK, FEDORA_IMAGE_LINK } from '../../../../utils/strings';
 
@@ -87,10 +93,21 @@ const ProvisionSourceNetHelpMsg: React.FC<ProvisionSourceNetHelpMsgProps> = ({
   );
 };
 
-export const ProvisionSourceComponent: React.FC<ProvisionSourceComponentProps> = React.memo(
-  ({ provisionSourceField, onChange, goToStorageStep, goToNetworkingStep }) => {
+export const ProvisionSourceComponentWithNamespace: React.FC<ProvisionSourceComponentProps> = React.memo(
+  ({ provisionSourceField, onChange, goToStorageStep, goToNetworkingStep, activeNamespace }) => {
     const provisionSourceValue = iGetFieldValue(provisionSourceField);
     const sources = iGet(provisionSourceField, 'sources');
+
+    const networkAttachmentDefinitionModelResource = {
+      kind: referenceForModel(NetworkAttachmentDefinitionModel),
+      isList: true,
+      optional: true,
+      namespace: activeNamespace,
+    };
+    const [nads, nadsLoaded] = useK8sWatchResource<K8sResourceKind[]>(
+      networkAttachmentDefinitionModelResource,
+    );
+    const hasNADs = nadsLoaded && nads?.length > 0;
 
     return (
       <FormFieldRow field={provisionSourceField} fieldType={FormFieldType.PF_SELECT}>
@@ -103,15 +120,29 @@ export const ProvisionSourceComponent: React.FC<ProvisionSourceComponentProps> =
             {(sources || [])
               .map(ProvisionSource.fromString)
               .sort((a, b) => a.getOrder() - b.getOrder())
-              .map((source) => (
-                <SelectOption
-                  key={source.getValue()}
-                  value={source.getValue()}
-                  description={source.getDescription()}
-                >
-                  {source.toString()}
-                </SelectOption>
-              ))}
+              .map((source) => {
+                if (source.getValue() === 'PXE' && !hasNADs) {
+                  return (
+                    <SelectOption
+                      key={source.getValue()}
+                      value={SELECT_PXE_NAD_ERROR}
+                      description={`${source.getDescription()}\n${PXE_NAD_NOT_FOUND_INFO}`}
+                    >
+                      {source.toString()}
+                    </SelectOption>
+                  );
+                }
+
+                return (
+                  <SelectOption
+                    key={source.getValue()}
+                    value={source.getValue()}
+                    description={source.getDescription()}
+                  >
+                    {source.toString()}
+                  </SelectOption>
+                );
+              })}
           </FormPFSelect>
         </FormField>
         {[
@@ -146,4 +177,15 @@ type ProvisionSourceComponentProps = {
   onChange: (key: string, value: string | boolean) => void;
   goToStorageStep: () => void;
   goToNetworkingStep: () => void;
+  activeNamespace: string;
 };
+
+const mapStateToProps = (state: RootState): { activeNamespace: string } => {
+  return {
+    activeNamespace: state.UI.get('activeNamespace'),
+  };
+};
+
+export const ProvisionSourceComponent = connect(mapStateToProps)(
+  ProvisionSourceComponentWithNamespace,
+);
