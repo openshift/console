1cad081c0d21c4734d7657124cca4afebb644796
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2020-09-17 14:56:02 +0900

    [feat] edit run-bridge.sh

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -2,30 +2,23 @@
 
 set -exuo pipefail
 
-myIP=$(ipconfig | grep "IPv4" -a | head -1 | awk '{print $NF}')
+myIP=$(hostname -I | awk '{print $1}')
 k8sIP='172.22.6.2'
 
-nodePorts=$(ssh root@$k8sIP "
-    HC_PORT=\$(kubectl get svc -n hypercloud4-system hypercloud4-operator-service | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    PROM_PORT=\$(kubectl get svc -n monitoring prometheus-k8s | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    GRAFANA_PORT=\$(kubectl get svc -n monitoring grafana | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    KIALI_PORT=\$(kubectl get svc -n istio-system kiali | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    JAEGER_PORT=\$(kubectl get svc -n istio-system tracing | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    APPROVAL_PORT=\$(kubectl get svc -n approval-system approval-proxy-server | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    VNC_PORT=\$(kubectl get svc -n kubevirt virtvnc | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
-    echo \"HC_PORT=\$HC_PORT PROM_PORT=\$PROM_PORT GRAFANA_PORT=\$GRAFANA_PORT KIALI_PORT=\$KIALI_PORT JAEGER_PORT=\$JAEGER_PORT APPROVAL_PORT=\$APPROVAL_PORT VNC_PORT=\$VNC_PORT;\"
-")
+BRIDGE_K8S_AUTH_BEARER_TOKEN=$(ssh root@$k8sIP "secretname=\$(kubectl get serviceaccount default --namespace=kube-system -o jsonpath='{.secrets[0].name}'); kubectl get secret "\$secretname" --namespace=kube-system -o template --template='{{.data.token}}' | base64 --decode; ")
+
+PROM_PORT='9090'
 
 ./bin/bridge \
-    --listen=http://$myIP:9000 \
-    --base-address=http://$myIP:9000 \
+    --listen=https://$myIP:9000 \
+    --base-address=https://$myIP:9000 \
     --tls-cert-file=tls/tls.crt \
     --tls-key-file=tls/tls.key \
     --k8s-mode=off-cluster \
     --k8s-mode-off-cluster-endpoint=https://$k8sIP:6443 \
     --k8s-mode-off-cluster-skip-verify-tls=true \
     --k8s-auth=bearer-token \
-    --k8s-auth-bearer-token=@@ \
+    --k8s-auth-bearer-token=${BRIDGE_K8S_AUTH_BEARER_TOKEN} \
     --public-dir=./frontend/public/dist \
     --user-auth=disabled \
     --k8s-mode-off-cluster-prometheus=http://$k8sIP:$PROM_PORT/api  \
