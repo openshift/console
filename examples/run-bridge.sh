c0fcfae1e76053f331cca1da957c0bef0041bb84
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2020-09-17 14:07:39 +0900

    [feat] run-bridge 값 추가

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -2,19 +2,32 @@
 
 set -exuo pipefail
 
+myIP=$(ipconfig | grep "IPv4" -a | head -1 | awk '{print $NF}')
+k8sIP='172.22.6.2'
+
+nodePorts=$(ssh root@$k8sIP "
+    HC_PORT=\$(kubectl get svc -n hypercloud4-system hypercloud4-operator-service | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    PROM_PORT=\$(kubectl get svc -n monitoring prometheus-k8s | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    GRAFANA_PORT=\$(kubectl get svc -n monitoring grafana | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    KIALI_PORT=\$(kubectl get svc -n istio-system kiali | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    JAEGER_PORT=\$(kubectl get svc -n istio-system tracing | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    APPROVAL_PORT=\$(kubectl get svc -n approval-system approval-proxy-server | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    VNC_PORT=\$(kubectl get svc -n kubevirt virtvnc | awk '{print \$5}' | awk 'match(\$0, /:[0-9]+\//){print substr(\$0,RSTART+1,RLENGTH-2)}');
+    echo \"HC_PORT=\$HC_PORT PROM_PORT=\$PROM_PORT GRAFANA_PORT=\$GRAFANA_PORT KIALI_PORT=\$KIALI_PORT JAEGER_PORT=\$JAEGER_PORT APPROVAL_PORT=\$APPROVAL_PORT VNC_PORT=\$VNC_PORT;\"
+")
+
 ./bin/bridge \
-    --base-address=http://localhost:9000 \
-    --ca-file=examples/ca.crt \
-    --k8s-auth=openshift \
+    --listen=http://$myIP:9000 \
+    --base-address=http://$myIP:9000 \
+    --tls-cert-file=tls/tls.crt \
+    --tls-key-file=tls/tls.key \
     --k8s-mode=off-cluster \
-    --k8s-mode-off-cluster-endpoint="$(oc whoami --show-server)" \
+    --k8s-mode-off-cluster-endpoint=https://$k8sIP:6443 \
     --k8s-mode-off-cluster-skip-verify-tls=true \
-    --listen=http://127.0.0.1:9000 \
+    --k8s-auth=bearer-token \
+    --k8s-auth-bearer-token=@@ \
     --public-dir=./frontend/public/dist \
-    --user-auth=openshift \
-    --user-auth-oidc-client-id=console-oauth-client \
-    --user-auth-oidc-client-secret-file=examples/console-client-secret \
-    --user-auth-oidc-ca-file=examples/ca.crt \
-    --k8s-mode-off-cluster-prometheus="$(oc -n openshift-monitoring get configmap sharing-config -o jsonpath='{.data.prometheusURL}')"  \
-    --k8s-mode-off-cluster-alertmanager="$(oc -n openshift-monitoring get configmap sharing-config -o jsonpath='{.data.alertmanagerURL}')" \
-    --k8s-mode-off-cluster-thanos="$(oc -n openshift-monitoring get configmap sharing-config -o jsonpath='{.data.prometheusURL}')"
+    --user-auth=disabled \
+    --k8s-mode-off-cluster-prometheus=http://$k8sIP:$PROM_PORT/api  \
+    --k8s-mode-off-cluster-alertmanager=http://$k8sIP:$PROM_PORT/api \
+    --k8s-mode-off-cluster-thanos=http://$k8sIP:$PROM_PORT/api
