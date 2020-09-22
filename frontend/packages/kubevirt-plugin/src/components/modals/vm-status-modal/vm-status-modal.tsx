4ed82289f629484c78fc9123e3f1eb707fe9f4b1
Ido Rosenzwig <irosenzw@redhat.com>
2020-09-22 16:30:09 +0300

    Bug 1880217: Refactor vm-status-modal in VM details page
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- frontend/packages/kubevirt-plugin/src/components/modals/vm-status-modal/vm-status-modal.tsx
+++ frontend/packages/kubevirt-plugin/src/components/modals/vm-status-modal/vm-status-modal.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { Modal } from '@console/shared';
 import { HandlePromiseProps, withHandlePromise } from '@console/internal/components/utils';
-import { ModalComponentProps } from '@console/internal/components/factory';
+import {
+  ModalComponentProps,
+  createModalLauncher,
+  ModalTitle,
+  ModalBody,
+} from '@console/internal/components/factory';
 import { ModalFooter } from '../modal/modal-footer';
 import { PAUSED_VM_MODAL_MESSAGE } from '../../../strings/vm/messages';
 import { VMIKind } from '../../../types';
@@ -9,41 +13,40 @@ import { unpauseVMI } from '../../../k8s/requests/vmi/actions';
 
 const modalTitle = 'Edit pause state';
 
-const VMStatusModal = withHandlePromise<VMStatusModalProps>(
-  ({ vmi, isOpen, setOpen, title = modalTitle, handlePromise, inProgress, errorMessage }) => {
+export const VMStatusModal = withHandlePromise(
+  ({
+    vmi,
+    title = modalTitle,
+    cancel,
+    close,
+    handlePromise,
+    inProgress,
+    errorMessage,
+  }: VMStatusModalProps) => {
     const [showPatchError, setPatchError] = React.useState<boolean>(false);
 
     const onSubmit = async (event) => {
       event.preventDefault();
 
-      const promise = unpauseVMI(vmi);
       handlePromise(
-        promise,
-        () => setOpen(false),
+        unpauseVMI(vmi),
+        () => close(),
         () => setPatchError(true),
       );
     };
 
-    const footer = (
-      <ModalFooter
-        errorMessage={showPatchError && errorMessage}
-        inProgress={inProgress}
-        onSubmit={onSubmit}
-        onCancel={() => setOpen(false)}
-        submitButtonText="Unpause"
-      />
-    );
-
     return (
-      <Modal
-        title={title}
-        isOpen={isOpen}
-        variant="small"
-        onClose={() => setOpen(false)}
-        footer={footer}
-      >
-        <div>{PAUSED_VM_MODAL_MESSAGE}</div>
-      </Modal>
+      <div className="modal-content">
+        <ModalTitle>{title}</ModalTitle>
+        <ModalBody>{PAUSED_VM_MODAL_MESSAGE}</ModalBody>
+        <ModalFooter
+          errorMessage={showPatchError && errorMessage}
+          inProgress={inProgress}
+          onSubmit={onSubmit}
+          onCancel={() => cancel()}
+          submitButtonText="Unpause"
+        />
+      </div>
     );
   },
 );
@@ -52,8 +55,6 @@ export type VMStatusModalProps = HandlePromiseProps &
   ModalComponentProps & {
     vmi: VMIKind;
     title?: string;
-    isOpen: boolean;
-    setOpen: (isOpen: boolean) => void;
   };
 
-export default VMStatusModal;
+export const vmStatusModal = createModalLauncher(VMStatusModal);
