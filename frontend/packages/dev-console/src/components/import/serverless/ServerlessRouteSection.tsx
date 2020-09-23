c52dc9c0c8f68b00e66ea3e06fdad8b6c05a0e6c
Mohammed Saud <md.saud020@gmail.com>
2020-09-23 15:23:00 +0530

    replace route input with typeahead select menu

--- frontend/packages/dev-console/src/components/import/serverless/ServerlessRouteSection.tsx
+++ frontend/packages/dev-console/src/components/import/serverless/ServerlessRouteSection.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import * as _ from 'lodash';
-import { TextInputTypes } from '@patternfly/react-core';
-import { useFormikContext, FormikValues } from 'formik';
-import { InputField, DropdownField } from '@console/shared';
+import { useFormikContext } from 'formik';
 import FormSection from '../section/FormSection';
-import { RouteData } from '../import-types';
+import { RouteData, GitImportFormData, DeployImageFormData } from '../import-types';
+import PortInputField from '../route/PortInputField';
 
 export interface ServerlessRouteSectionProps {
   route: RouteData;
@@ -14,40 +12,21 @@ const ServerlessRouteSection: React.FC<ServerlessRouteSectionProps> = ({ route }
   const {
     values: {
       image: { ports },
-      route: { defaultUnknownPort, targetPort: routeTargetPort },
+      route: { defaultUnknownPort },
     },
-  } = useFormikContext<FormikValues>();
-  const targetPort = routeTargetPort.split('-')[0];
-  const portOptions = ports.reduce((acc, port) => {
-    const name = port?.containerPort;
-    if (name) {
-      acc[name] = <>{port.containerPort}</>;
-    }
-    return acc;
-  }, {});
+  } = useFormikContext<DeployImageFormData | GitImportFormData>();
+  const placeholderPort = defaultUnknownPort;
+  const portOptions = ports.map((port) => port?.containerPort.toString());
   return (
     <FormSection title="Routing">
       {route.create && (
-        <>
-          {_.isEmpty(ports) ? (
-            <InputField
-              type={TextInputTypes.text}
-              name="route.unknownTargetPort"
-              label="Target Port"
-              placeholder={defaultUnknownPort}
-              helpText="Target port for traffic."
-            />
-          ) : (
-            <DropdownField
-              name="route.targetPort"
-              label="Target Port"
-              items={portOptions}
-              title={portOptions[targetPort] || 'Select target port'}
-              helpText="Target port for traffic."
-              fullWidth
-            />
-          )}
-        </>
+        <PortInputField
+          name="route.unknownTargetPort"
+          label="Target Port"
+          placeholderText={placeholderPort.toString()}
+          helpText="Target port for traffic."
+          options={portOptions}
+        />
       )}
     </FormSection>
   );
