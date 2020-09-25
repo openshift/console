39b58cd9380b07646fff798a015a7027680a19e0
suomiy <suomiy@gmail.com>
2020-09-25 19:42:51 +0200

    Bug 1880083: do not support VMware import for VMs with uefi

--- frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/redux/validations/providers/v2vvmware-tab-validation.ts
+++ frontend/packages/kubevirt-plugin/src/components/create-vm-wizard/redux/validations/providers/v2vvmware-tab-validation.ts
@@ -1,17 +1,18 @@
 import * as _ from 'lodash';
-import { asValidationObject } from '@console/shared';
+import { asValidationObject, ValidationErrorType } from '@console/shared';
 import { UpdateOptions, InternalActionType, ValidationConfig } from '../../types';
 import {
   iGetVMwareData,
-  iGetVMWareFieldAttribute,
-  iGetVMWareFieldValue,
   isVMWareProvider,
   hasVMWareSettingsChanged,
+  iGetVMWareField,
 } from '../../../selectors/immutable/provider/vmware/selectors';
 import { VMWareProviderField, VMImportProvider } from '../../../types';
 import { getValidationUpdate } from '../utils';
 import { vmWizardInternalActions } from '../../internal-actions';
 import { iGetFieldValue } from '../../../selectors/immutable/field';
+import { iGet, iGetIn } from '../../../../../utils/immutable';
+import { VMwareFirmware } from '../../../../../constants/v2v-import/vmware/vmware-firmware';
 
 const validationConfig: ValidationConfig<VMWareProviderField> = {
   [VMWareProviderField.HOSTNAME]: {
@@ -21,6 +22,24 @@ const validationConfig: ValidationConfig<VMWareProviderField> = {
 
       if (hostname && /\s/.test(hostname)) {
         return asValidationObject('Hostname must not contain white spaces');
+      }
+      return null;
+    },
+  },
+  [VMWareProviderField.VM]: {
+    detectValueChanges: [VMWareProviderField.VM],
+    validator: (field) => {
+      const vm = iGet(field, 'vm');
+
+      if (vm) {
+        const raw = vm.getIn(['detail', 'raw']);
+
+        const parsedVM = JSON.parse(raw);
+        const firmware = VMwareFirmware.fromString(parsedVM?.Config?.Firmware);
+
+        if (!firmware?.isSupported()) {
+          return asValidationObject(`VM has unsupported firmware: ${firmware}`);
+        }
       }
       return null;
     },
@@ -65,16 +84,24 @@ export const getV2VVMwareImportProvidersTabValidity = (
     return null;
   }
 
-  const hasSelectedVM = !!iGetVMWareFieldValue(state, id, VMWareProviderField.VM);
-  const hasLoadedVM = !!iGetVMWareFieldAttribute(state, id, VMWareProviderField.VM, 'vm');
+  const vmField = iGetVMWareField(state, id, VMWareProviderField.VM);
+  const hasSelectedVM = !!iGet(vmField, 'value');
+  const hasLoadedVM = !!iGet(vmField, 'vm');
+  const hasValidVM = iGetIn(vmField, ['validation', 'type']) !== ValidationErrorType.Error;
+
+  let error = null;
+
+  if (!hasSelectedVM) {
+    error = 'Please select a VM to import.';
+  } else if (!hasLoadedVM) {
+    error = 'Please wait for a VM to load.';
+  } else if (!hasValidVM) {
+    error = 'Please select a valid VM.';
+  }
 
   return {
     hasAllRequiredFilled: hasSelectedVM,
-    isValid: hasLoadedVM,
-    error: hasLoadedVM
-      ? null
-      : hasSelectedVM
-      ? 'Please wait for a VM to load.'
-      : 'Please select a VM to import.',
+    isValid: hasLoadedVM && hasValidVM,
+    error,
   };
 };
