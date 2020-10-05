f219a0a873a8064cd9e6fd7b110739fb93929a56
Jakub Hadvig <jhadvig@redhat.com>
2020-10-05 13:04:05 +0200

    [release-4.4] Bug 1881058: Use library-go crypto to setup default TLSConfig

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -565,6 +565,7 @@ func main() {
 		Handler: srv.HTTPHandler(),
 		// Disable HTTP/2, which breaks WebSockets.
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+		TLSConfig:    oscrypto.SecureTLSConfig(&tls.Config{}),
 	}
 
 	log.Infof("Binding to %s...", httpsrv.Addr)
