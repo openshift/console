997436354010e7a5dc8abd37c61a13aad3ca9bc4
gyoungjun_lee <injxgj@gmail.com>
2020-10-07 18:29:57 +0900

    [feat][patch] 공통 Form 컴포넌트에 'react-hook-form'적용

--- frontend/public/components/hypercloud/form/create-form.tsx
+++ frontend/public/components/hypercloud/form/create-form.tsx
@@ -1,215 +1,96 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
-import { Base64 } from 'js-base64';
+import { useForm, Controller } from "react-hook-form"
+import { DevTool } from "@hookform/devtools";
 import { ActionGroup, Button } from '@patternfly/react-core';
-import { pluralToKind } from './';
-import { modelFor } from '@console/internal/module/k8s';
-import { k8sCreate, k8sUpdate, referenceFor, K8sResourceKind } from '../../../module/k8s';
+// import { k8sCreate, k8sUpdate, referenceFor, K8sResourceKind } from '../../../module/k8s';
+import { k8sCreate, referenceFor, K8sResourceKind, modelFor } from '../../../module/k8s';
+import { pluralToKind } from './'
+// import { NamespaceModel } from '../../../models';
+// import {
+//   ButtonBar,
+//   history,
+//   resourceObjPath,
+// } from '../../utils';
 import {
   ButtonBar,
   history,
-  resourceObjPath,
+  resourceObjPath
 } from '../../utils';
-import { ModalBody, ModalTitle, ModalSubmitFooter } from '../../factory/modal';
-// import { AsyncComponent } from '../../utils/async';
-// import { SecretModel } from '../../../models';
+
+export const WithCommonForm = (SubForm, params?: any, modal?: boolean) => {
+  const FormComponent: React.FC<CommonFormProps_> = props => {
+    const { register, control, watch, handleSubmit } = useForm();
+    const kind = pluralToKind(params.plural);
+    const title = `${props.titleVerb} ${params.type} ${kind}`;
+
+    const [inProgress] = React.useState(false); // onSubmit이나 나중에 Error관련 메서드에서 inProgress를 false로 변경해줘야함.
+
+    const onSubmit = handleSubmit((data) => {
+      let inDo = _.defaultsDeep(props.fixed, data);
+      inDo = props.onSubmitCallback(inDo);
+      console.log(watch());
+      // if (false) {
+      k8sCreate(modelFor(kind), inDo)
+      history.push(resourceObjPath(inDo, referenceFor(modelFor(kind))));
+      // }
+    });
 
 
-// withCommonForm returns SubForm which is a Higher Order Component for all the types of secret forms.
-export const withCommonForm = (SubForm, params?: any, modal?: boolean) =>
-  class FormComponent extends React.Component<CommonFormProps_, CommonFormState_> {
-    constructor(props) {
-      super(props);
-      const existingData = _.pick(props.obj, ['metadata', 'type']);
-      const kind = pluralToKind(params.plural);
-      const defaultType = params.type || '';  // toDefaultType 대신 index.tsx에 plural에 따른 kind, type값 저장해야 겠음.
-      const data = _.defaultsDeep({}, props.fixed, existingData, {
-        apiVersion: 'v1',
-        data: {},
-        kind: kind,
-        metadata: {
-          name: '',
-        },
-        type: defaultType || ''
-      })
-
-      this.state = {
-        // typeAbstraction: this.props.typeAbstraction,
-        data,
-        kind,
-        inProgress: false,
-        type: defaultType,
-        stringData: _.mapValues(_.get(props.obj, 'data'), (value) => {
-          return value ? Base64.decode(value) : '';
-        }),
-        disableForm: false,
-      };
-      this.onDataChanged = this.onDataChanged.bind(this);
-      this.onNameChanged = this.onNameChanged.bind(this);
-      this.onError = this.onError.bind(this);
-      this.onFormDisable = this.onFormDisable.bind(this);
-      this.save = this.save.bind(this);
-    }
-    onDataChanged(data) {
-      this.setState({
-        stringData: { ...data.stringData },
-        type: data.type,
-      });
-    }
-    onError(err) {
-      this.setState({
-        error: err,
-        inProgress: false,
-      });
-    }
-    onNameChanged(event) {
-      const name = event.target.value;
-      this.setState((state: CommonFormState_) => {
-        const data = _.cloneDeep(state.data);
-        data.metadata.name = name;
-        return { data };
-      });
-    }
-    onFormDisable(disable) {
-      this.setState({
-        disableForm: disable,
-      });
-    }
-    save(e) {
-      e.preventDefault();
-      const { metadata } = this.state.data;
-      let kindObj = modelFor(this.state.kind);
-      this.setState({ inProgress: true });
-      const newData = _.assign(
-        {},
-        this.state.data,
-        { stringData: this.state.stringData },
-        { type: this.state.type },
-      );
-      (this.props.isCreate
-        ? k8sCreate(kindObj, newData)
-        : k8sUpdate(kindObj, newData, metadata.namespace, newData.metadata.name)
-      ).then(
-        (data) => {
-          this.setState({ inProgress: false });
-          if (this.props.onSave) {
-            this.props.onSave(data.metadata.name);
-          }
-          if (!modal) {
-            history.push(resourceObjPath(data, referenceFor(data)));
-          }
-        },
-        (err) => this.setState({ error: err.message, inProgress: false }),
-      );
-    }
-
-    renderBody = () => {
-      return (
-        <>
-          <fieldset disabled={!this.props.isCreate}>
+    return (
+      <div className="co-m-pane__body" onSubmit={onSubmit}>
+        <form
+          className="co-m-pane__body-group co-create-secret-form co-m-pane__form">
+          <h1 className="co-m-pane__heading">{title}</h1>
+          <p className="co-m-pane__explanation">{props.explanation}</p>
+          <fieldset>
             <div className="form-group">
-              <label className="control-label co-required" htmlFor="secret-name">
-                Name
-              </label>
-              <div>
-                <input
-                  className="pf-c-form-control"
-                  type="text"
-                  onChange={this.onNameChanged}
-                  value={this.state.data.metadata.name}
-                  aria-describedby="secret-name-help"
-                  id="secret-name"
-                  required
-                />
-                <p className="help-block" id="secret-name-help">
-                  Unique name.
-                </p>
-              </div>
+              <label className="control-label co-required" htmlFor="secret-name">Name</label>
+              <input className="pf-c-form-control" name='metadata.name' ref={register} />
             </div>
           </fieldset>
-          <SubForm
-            onChange={this.onDataChanged}
-            onError={this.onError}
-            onFormDisable={this.onFormDisable}
-            stringData={this.state.stringData}
-            // secretType={this.state.data.type}
-            isCreate={this.props.isCreate}
-          />
-        </>
-      );
-    };
-
-    render() {
-      // const { secretTypeAbstraction } = this.state;
-      const { onCancel = history.goBack } = this.props;
-      const title = `${this.props.titleVerb} ${this.state.type} ${this.state.kind}`;  // titleVerb + type + kind
-      return modal ? (
-        <form className="co-create-secret-form modal-content" onSubmit={this.save}>
-          <ModalTitle>{title}</ModalTitle>
-          <ModalBody>{this.renderBody()}</ModalBody>
-          <ModalSubmitFooter
-            errorMessage={this.state.error || ''}
-            inProgress={this.state.inProgress}
-            submitText="Create"
-            cancel={this.props.onCancel}
-          />
+          <SubForm isCreate={props.isCreate} register={register} control={control} Controller={Controller} />
+          <ButtonBar inProgress={inProgress}>
+            <ActionGroup className="pf-c-form">
+              <Button
+                type="submit"
+                variant="primary"
+                id="save-changes"
+              >
+                {props.saveButtonText || 'Create'}
+              </Button>
+              <Button type="button" variant="secondary" id="cancel" onClick={props.onCancel}>
+                Cancel
+              </Button>
+            </ActionGroup>
+          </ButtonBar>
         </form>
-      ) : (
-          <div className="co-m-pane__body">
-            <Helmet>
-              <title>{title}</title>
-            </Helmet>
-            <form
-              className="co-m-pane__body-group co-create-secret-form co-m-pane__form"
-              onSubmit={this.save}
-            >
-              <h1 className="co-m-pane__heading">{title}</h1>
-              <p className="co-m-pane__explanation">{this.props.explanation}</p>
-              {this.renderBody()}
-              <ButtonBar errorMessage={this.state.error} inProgress={this.state.inProgress}>
-                <ActionGroup className="pf-c-form">
-                  <Button
-                    type="submit"
-                    isDisabled={this.state.disableForm}
-                    variant="primary"
-                    id="save-changes"
-                  >
-                    {this.props.saveButtonText || 'Create'}
-                  </Button>
-                  <Button type="button" variant="secondary" id="cancel" onClick={onCancel}>
-                    Cancel
-                </Button>
-                </ActionGroup>
-              </ButtonBar>
-            </form>
-          </div>
-        );
-    }
-  };
-
-type CommonFormState_ = {
-  inProgress: boolean;
-  kind: string;
-  type?: string;
-  data: K8sResourceKind;
-  stringData: {
-    [key: string]: string;
-  };
-  error?: any;
-  disableForm: boolean;
-};
+        <DevTool control={control} />
+      </div>
+    )
+  }
+  return FormComponent;
+}
 
 type CommonFormProps_ = {
   obj?: K8sResourceKind;
-  type: string;
-  plural: string;
-  fixed: any;
-  kind?: string;
+  fixed: object;
   isCreate: boolean;
   titleVerb: string;
+  onSubmitCallback: Function;
   saveButtonText?: string;
   explanation?: string;
   onCancel?: () => void;
   onSave?: (name: string) => void;
 };
+
+// type DefaultInputForm_ = {
+//   apiVersiont: string;
+//   kind: string;
+//   type?: string;
+//   metadata: {
+//     name: '',
+//     [propName: string]: any;
+//   }
+//   [propName: string]: any;
+// }
