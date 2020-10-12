a90f424312d62451a6fe0aed1ff8c6e7e16bd4b2
David Taylor <dtaylor@redhat.com>
2020-10-12 16:59:03 -0400

    Changed react-modal's setAppElement(...) to be 'app-container' element instead of 'modal-container'. setAppElement(...) will set 'aria-hidden=true' in attempt to hide app behind the open modal.

--- frontend/public/components/app.jsx
+++ frontend/public/components/app.jsx
@@ -148,7 +148,7 @@ class App_ extends React.PureComponent {
     const content = (
       <>
         <Helmet titleTemplate={`%s · ${productName}`} defaultTitle={productName} />
-        <QuickStartDrawer>
+        <QuickStartDrawer id="app-container">
           <ConsoleNotifier location="BannerTop" />
           <Page
             header={<Masthead onNavToggle={this._onNavToggle} />}
@@ -167,11 +167,11 @@ class App_ extends React.PureComponent {
               <AppContents />
             </ConnectedNotificationDrawer>
           </Page>
-          <div id="modal-container" />
           <CloudShell />
           <GuidedTour />
           <ConsoleNotifier location="BannerBottom" />
         </QuickStartDrawer>
+        <div id="modal-container" />
       </>
     );
 
