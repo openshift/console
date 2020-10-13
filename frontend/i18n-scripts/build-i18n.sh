1a1ecec72c7e861e099bf06c701bf8d1aa68d6bf
Vojtech Szocs <vszocs@redhat.com>
2020-10-13 18:34:40 +0200

    Avoid traversing node_modules when running i18next on packages/*

--- frontend/i18n-scripts/build-i18n.sh
+++ frontend/i18n-scripts/build-i18n.sh
@@ -2,9 +2,11 @@
 
 set -exuo pipefail
 
-i18next "public/!(dist)**/**/*.{js,jsx,ts,tsx}" [-oc] -c "./public/i18next-parser.config.js" -o "public/locales/\$LOCALE/\$NAMESPACE.json"
+FILE_PATTERN="!(dist|node_modules)**/**/*.{js,jsx,ts,tsx}"
+
+i18next "public/${FILE_PATTERN}" [-oc] -c "./public/i18next-parser.config.js" -o "public/locales/\$LOCALE/\$NAMESPACE.json"
 
 cd packages
 for d in */ ; do
-  i18next "${d}!(dist)**/**/*.{js,jsx,ts,tsx}" [-oc] -c "./../public/i18next-parser.config.js" -o "${d}locales/\$LOCALE/\$NAMESPACE.json"
+  i18next "${d}${FILE_PATTERN}" [-oc] -c "./../public/i18next-parser.config.js" -o "${d}locales/\$LOCALE/\$NAMESPACE.json"
 done
