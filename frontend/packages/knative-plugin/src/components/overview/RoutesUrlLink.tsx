3dbf9f01aef416f40d0f3b9efdd1a2f6ed5d407f
Christoph Jerolimov <jerolimov+github@redhat.com>
2020-10-21 00:47:46 +0200

    Add missing key when mapping urls

--- frontend/packages/knative-plugin/src/components/overview/RoutesUrlLink.tsx
+++ frontend/packages/knative-plugin/src/components/overview/RoutesUrlLink.tsx
@@ -10,10 +10,14 @@ const RoutesUrlLink: React.FC<RoutesUrlLinkProps> = ({ urls = [], title }) =>
   urls.length > 0 && (
     <>
       {title && <span className="text-muted">{title}: </span>}
-      {urls.length > 0 &&
-        urls.map((url) => (
-          <ExternalLink href={url} additionalClassName="co-external-link--block" text={url} />
-        ))}
+      {urls.map((url) => (
+        <ExternalLink
+          key={url}
+          href={url}
+          text={url}
+          additionalClassName="co-external-link--block"
+        />
+      ))}
     </>
   );
 
