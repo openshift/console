49e5503c85b66475c03df02672e6e16b091422a3
Jon Jackson <jonjacks@redhat.com>
2020-10-21 18:03:32 -0400

    Bug 1887465: Migrate to typescript

--- frontend/public/components/modals/delete-namespace-modal.tsx
+++ frontend/public/components/modals/delete-namespace-modal.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore: FIXME missing exports due to out-of-sync @types/react-redux version
 import { useDispatch } from 'react-redux';
-import { k8sKill } from '@console/internal/module/k8s';
+import { k8sKill, K8sKind, K8sResourceKind } from '@console/internal/module/k8s';
 import {
   createModalLauncher,
   ModalTitle,
   ModalBody,
   ModalSubmitFooter,
+  ModalComponentProps,
 } from '@console/internal/components/factory/modal';
 import { history } from '@console/internal/components/utils';
 import { setActiveNamespace } from '@console/internal/actions/ui';
@@ -13,7 +16,12 @@ import { ALL_NAMESPACES_KEY, YellowExclamationTriangleIcon } from '@console/shar
 import { useActiveNamespace } from '@console/shared/src/hooks/redux-selectors';
 import { usePromiseHandler } from '@console/shared/src/hooks/promise-handler';
 
-export const DeleteNamespaceModal = ({ cancel, close, kind, resource }) => {
+export const DeleteNamespaceModal: React.FC<DeleteNamespaceModalProps> = ({
+  cancel,
+  close,
+  kind,
+  resource,
+}) => {
   const activeNamespace = useActiveNamespace();
   const dispatch = useDispatch();
   const [handlePromise, inProgress, errorMessage] = usePromiseHandler();
@@ -21,13 +29,17 @@ export const DeleteNamespaceModal = ({ cancel, close, kind, resource }) => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    handlePromise(k8sKill(kind, resource)).then(() => {
-      if (resource.metadata.name === activeNamespace) {
-        dispatch(setActiveNamespace(ALL_NAMESPACES_KEY));
-      }
-      close?.();
-      history.push(`/k8s/cluster/${kind.plural}`);
-    });
+    handlePromise(k8sKill(kind, resource))
+      .then(() => {
+        if (resource.metadata.name === activeNamespace) {
+          dispatch(setActiveNamespace(ALL_NAMESPACES_KEY));
+        }
+        close?.();
+        history.push(`/k8s/cluster/${kind.plural}`);
+      })
+      .catch(() => {
+        /* do nothing */
+      });
   };
 
   const onKeyUp = (e) => {
@@ -71,3 +83,8 @@ export const DeleteNamespaceModal = ({ cancel, close, kind, resource }) => {
 };
 
 export const deleteNamespaceModal = createModalLauncher(DeleteNamespaceModal);
+
+type DeleteNamespaceModalProps = {
+  resource: K8sResourceKind;
+  kind: K8sKind;
+} & ModalComponentProps;
