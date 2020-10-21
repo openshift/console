5b0c4c1b9e22cc532ce27fe87f65250a15f1604c
Jon Jackson <jonjacks@redhat.com>
2020-10-21 18:03:32 -0400

    Bug 1887465: Migrate to typescript

--- frontend/packages/console-shared/src/hooks/promise-handler.ts
+++ frontend/packages/console-shared/src/hooks/promise-handler.ts
@@ -1,17 +1,24 @@
 import * as React from 'react';
 
-export const usePromiseHandler: PromisHandlerHook = () => {
+export const usePromiseHandler: PromisHandlerHook = <T extends unknown = any>() => {
   const [inProgress, setInProgress] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState('');
-  const handlePromise = (promise) => {
+  const handlePromise: PromiseHandlerCallback<T> = (promise) => {
     setInProgress(true);
     return promise
-      .then(() => setErrorMessage(''))
-      .catch((reason) => setErrorMessage(reason || 'An error occurred. Please try again.'))
+      .then((res) => {
+        setErrorMessage('');
+        return res;
+      })
+      .catch((reason) => {
+        const error = reason || 'An error occurred. Please try again.';
+        setErrorMessage(error);
+        return Promise.reject(error);
+      })
       .finally(() => setInProgress(false));
   };
   return [handlePromise, inProgress, errorMessage];
 };
 
 type PromiseHandlerCallback<T> = (promise: Promise<T>) => Promise<T>;
-type PromisHandlerHook = <T = any>() => [PromiseHandlerCallback<T>, boolean, string];
+type PromisHandlerHook = <T>() => [PromiseHandlerCallback<T>, boolean, string];
