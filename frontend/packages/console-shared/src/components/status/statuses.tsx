eb0127ee286b2ce7bef41555db8e310b36130d02
Cyril <cyril.ajieh@gmail.com>
2020-10-21 09:23:07 -0400

    The symbols e.g checkmark in the Node overview

--- frontend/packages/console-shared/src/components/status/statuses.tsx
+++ frontend/packages/console-shared/src/components/status/statuses.tsx
@@ -10,31 +10,51 @@ import GenericStatus from './GenericStatus';
 import { StatusComponentProps } from './types';
 
 export const ErrorStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={RedExclamationCircleIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => <RedExclamationCircleIcon className="co-icon-and-text__icon" title="Error" />}
+  />
 );
 ErrorStatus.displayName = 'ErrorStatus';
 
 export const InfoStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={BlueInfoCircleIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => <BlueInfoCircleIcon className="co-icon-and-text__icon" title="Information" />}
+  />
 );
 InfoStatus.displayName = 'InfoStatus';
 
 export const PendingStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={HourglassHalfIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => <HourglassHalfIcon className="co-icon-and-text__icon" title="Pending" />}
+  />
 );
 PendingStatus.displayName = 'PendingStatus';
 
 export const ProgressStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={InProgressIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => <InProgressIcon className="co-icon-and-text__icon" title="In progress" />}
+  />
 );
 ProgressStatus.displayName = 'ProgressStatus';
 
 export const SuccessStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={GreenCheckCircleIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => <GreenCheckCircleIcon className="co-icon-and-text__icon" title="Healthy" />}
+  />
 );
 SuccessStatus.displayName = 'SuccessStatus';
 
 export const WarningStatus: React.FC<StatusComponentProps> = (props) => (
-  <GenericStatus {...props} Icon={YellowExclamationTriangleIcon} />
+  <GenericStatus
+    {...props}
+    Icon={() => (
+      <YellowExclamationTriangleIcon className="co-icon-and-text__icon" title="Warning" />
+    )}
+  />
 );
 WarningStatus.displayName = 'WarningStatus';
