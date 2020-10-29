172da3ef1cc543f697969dd6a0abe94acfa7cb65
zherman0 <zherman@redhat.com>
2020-10-29 12:29:45 -0600

    i18n for jobs

--- frontend/public/components/job.tsx
+++ frontend/public/components/job.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import * as _ from 'lodash';
 import { Link } from 'react-router-dom';
 import * as classNames from 'classnames';
 import { sortable } from '@patternfly/react-table';
-
+import { useTranslation } from 'react-i18next';
 import { Status } from '@console/shared';
 import { getJobTypeAndCompletions, K8sKind, JobKind, K8sResourceKind } from '../module/k8s';
 import { Conditions } from './conditions';
@@ -22,13 +21,13 @@ import {
   SectionHeading,
   Timestamp,
   navFactory,
-  pluralize,
 } from './utils';
 import { ResourceEventStream } from './events';
 import { JobModel } from '../models';
 
 const ModifyJobParallelism: KebabAction = (kind: K8sKind, obj: JobKind) => ({
-  label: 'Edit Parallelism',
+  // t('workload~Edit parallelism')
+  labelKey: 'workload~Edit parallelism',
   callback: () =>
     configureJobParallelismModal({
       resourceKind: kind,
@@ -58,47 +57,6 @@ const tableColumnClasses = [
   classNames('col-lg-2', 'hidden-md', 'hidden-sm', 'hidden-xs'),
   Kebab.columnClass,
 ];
-
-const JobTableHeader = () => {
-  return [
-    {
-      title: 'Name',
-      sortField: 'metadata.name',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[0] },
-    },
-    {
-      title: 'Namespace',
-      sortField: 'metadata.namespace',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[1] },
-      id: 'namespace',
-    },
-    {
-      title: 'Labels',
-      sortField: 'metadata.labels',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[2] },
-    },
-    {
-      title: 'Completions',
-      sortFunc: 'jobCompletions',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[3] },
-    },
-    {
-      title: 'Type',
-      sortFunc: 'jobType',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[4] },
-    },
-    {
-      title: '',
-      props: { className: tableColumnClasses[5] },
-    },
-  ];
-};
-JobTableHeader.displayName = 'JobTableHeader';
 
 const JobTableRow: RowFunction<JobKind> = ({ obj: job, index, key, style }) => {
   const { type, completions } = getJobTypeAndCompletions(job);
@@ -138,89 +96,153 @@ const JobTableRow: RowFunction<JobKind> = ({ obj: job, index, key, style }) => {
   );
 };
 
-const jobStatus = (job: JobKind): string => {
-  return job && job.status ? _.get(job, 'status.conditions[0].type', 'In Progress') : null;
-};
-
-export const JobDetails: React.FC<JobsDetailsProps> = ({ obj: job }) => (
-  <>
-    <div className="co-m-pane__body">
-      <div className="row">
-        <div className="col-md-6">
-          <SectionHeading text="Job Details" />
-          <ResourceSummary resource={job} showPodSelector>
-            <DetailsItem label="Desired Completions" obj={job} path="spec.completions" />
-            <DetailsItem label="Parallelism" obj={job} path="spec.parallelism" />
-            <DetailsItem
-              label="Active Deadline Seconds"
-              obj={job}
-              path="spec.activeDeadlineSeconds"
-            >
-              {job.spec.activeDeadlineSeconds
-                ? pluralize(job.spec.activeDeadlineSeconds, 'second')
-                : 'Not Configured'}
-            </DetailsItem>
-          </ResourceSummary>
-        </div>
-        <div className="col-md-6">
-          <SectionHeading text="Job Status" />
-          <dl className="co-m-pane__details">
-            <dt>Status</dt>
-            <dd>
-              <Status status={jobStatus(job)} />
-            </dd>
-            <DetailsItem label="Start Time" obj={job} path="status.startTime">
-              <Timestamp timestamp={job.status.startTime} />
-            </DetailsItem>
-            <DetailsItem label="Completion Time" obj={job} path="status.completionTime">
-              <Timestamp timestamp={job.status.completionTime} />
-            </DetailsItem>
-            <DetailsItem
-              label="Succeeded Pods"
-              obj={job}
-              path="status.succeeded"
-              defaultValue="0"
-            />
-            <DetailsItem label="Active Pods" obj={job} path="status.active" defaultValue="0" />
-            <DetailsItem label="Failed Pods" obj={job} path="status.failed" defaultValue="0" />
-          </dl>
+export const JobDetails: React.FC<JobsDetailsProps> = ({ obj: job }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className="co-m-pane__body">
+        <div className="row">
+          <div className="col-md-6">
+            <SectionHeading text={t('workload~Job details')} />
+            <ResourceSummary resource={job} showPodSelector>
+              <DetailsItem
+                label={t('workload~Desired completions')}
+                obj={job}
+                path="spec.completions"
+              />
+              <DetailsItem label={t('workload~Parallelism')} obj={job} path="spec.parallelism" />
+              <DetailsItem
+                label={t('workload~Active deadline seconds')}
+                obj={job}
+                path="spec.activeDeadlineSeconds"
+              >
+                {job.spec.activeDeadlineSeconds
+                  ? t('workload~second', { count: job.spec.activeDeadlineSeconds })
+                  : t('workload~Not configured')}
+              </DetailsItem>
+            </ResourceSummary>
+          </div>
+          <div className="col-md-6">
+            <SectionHeading text={t('workload~Job status')} />
+            <dl className="co-m-pane__details">
+              <dt>{t('workload~Status')}</dt>
+              <dd>
+                <Status
+                  status={job?.status ? job?.status?.conditions?.[0]?.type || 'In progress' : null}
+                />
+              </dd>
+              <DetailsItem label={t('workload~Start time')} obj={job} path="status.startTime">
+                <Timestamp timestamp={job.status.startTime} />
+              </DetailsItem>
+              <DetailsItem
+                label={t('workload~Completion time')}
+                obj={job}
+                path="status.completionTime"
+              >
+                <Timestamp timestamp={job.status.completionTime} />
+              </DetailsItem>
+              <DetailsItem
+                label={t('workload~Succeeded pods')}
+                obj={job}
+                path="status.succeeded"
+                defaultValue="0"
+              />
+              <DetailsItem
+                label={t('workload~Active pods')}
+                obj={job}
+                path="status.active"
+                defaultValue="0"
+              />
+              <DetailsItem
+                label={t('workload~Failed pods')}
+                obj={job}
+                path="status.failed"
+                defaultValue="0"
+              />
+            </dl>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="co-m-pane__body">
-      <SectionHeading text="Containers" />
-      <ContainerTable containers={job.spec.template.spec.containers} />
-    </div>
-    <div className="co-m-pane__body">
-      <SectionHeading text="Conditions" />
-      <Conditions conditions={job.status.conditions} />
-    </div>
-  </>
-);
+      <div className="co-m-pane__body">
+        <SectionHeading text={t('workload~Containers')} />
+        <ContainerTable containers={job.spec.template.spec.containers} />
+      </div>
+      <div className="co-m-pane__body">
+        <SectionHeading text={t('workload~Conditions')} />
+        <Conditions conditions={job.status.conditions} />
+      </div>
+    </>
+  );
+};
 
 const JobPods: React.FC<JobPodsProps> = (props) => (
   <PodsComponent {...props} customData={{ showNodes: true }} />
 );
 
 const { details, pods, editYaml, events } = navFactory;
-const JobsDetailsPage: React.FC<JobsDetailsPageProps> = (props) => (
-  <DetailsPage
-    {...props}
-    getResourceStatus={jobStatus}
-    kind={kind}
-    menuActions={menuActions}
-    pages={[details(JobDetails), editYaml(), pods(JobPods), events(ResourceEventStream)]}
-  />
-);
-const JobsList: React.FC = (props) => (
-  <Table
-    {...props}
-    aria-label={JobModel.labelPlural}
-    Header={JobTableHeader}
-    Row={JobTableRow}
-    virtualize
-  />
-);
+const JobsDetailsPage: React.FC<JobsDetailsPageProps> = (props) => {
+  return (
+    <DetailsPage
+      {...props}
+      getResourceStatus={(job: JobKind) =>
+        job?.status ? job?.status?.conditions?.[0]?.type || 'In progress' : null
+      }
+      kind={kind}
+      menuActions={menuActions}
+      pages={[details(JobDetails), editYaml(), pods(JobPods), events(ResourceEventStream)]}
+    />
+  );
+};
+const JobsList: React.FC = (props) => {
+  const { t } = useTranslation();
+  const JobTableHeader = () => [
+    {
+      title: t('workload~Name'),
+      sortField: 'metadata.name',
+      transforms: [sortable],
+      props: { className: tableColumnClasses[0] },
+    },
+    {
+      title: t('workload~Namespace'),
+      sortField: 'metadata.namespace',
+      transforms: [sortable],
+      props: { className: tableColumnClasses[1] },
+      id: 'namespace',
+    },
+    {
+      title: t('workload~Labels'),
+      sortField: 'metadata.labels',
+      transforms: [sortable],
+      props: { className: tableColumnClasses[2] },
+    },
+    {
+      title: t('workload~Completions'),
+      sortFunc: 'jobCompletions',
+      transforms: [sortable],
+      props: { className: tableColumnClasses[3] },
+    },
+    {
+      title: t('workload~Type'),
+      sortFunc: 'jobType',
+      transforms: [sortable],
+      props: { className: tableColumnClasses[4] },
+    },
+    {
+      title: '',
+      props: { className: tableColumnClasses[5] },
+    },
+  ];
+
+  return (
+    <Table
+      {...props}
+      aria-label={JobModel.labelPlural}
+      Header={JobTableHeader}
+      Row={JobTableRow}
+      virtualize
+    />
+  );
+};
 
 const JobsPage: React.FC<JobsPageProps> = (props) => (
   <ListPage ListComponent={JobsList} kind={kind} canCreate={true} {...props} />
