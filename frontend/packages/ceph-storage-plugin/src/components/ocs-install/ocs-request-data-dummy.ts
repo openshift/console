e09326a97bf3f6c1fc2c5a1dab14f25bcc49598c
bipuladh <badhikar@redhat.com>
2020-11-03 23:23:32 +0530

    Revert deletion of create-form file and associated helpers

---
+++ frontend/packages/ceph-storage-plugin/src/components/ocs-install/ocs-request-data-dummy.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
+export const getOCSRequestData = (
+  _scName: any,
+  _osdSize: any,
+  _isEncrypted: any,
+  _isBaremetal: any,
+  _isMinimal: any,
+  _isEncryptionSupported: any,
+) => ({});
