9dc98d2ea8303833aef09a6f0d5a8de417022cc8
Regina Scott <rescott@redhat.com>
2020-11-05 15:26:32 -0500

    added argocd dashboard link to GitOpsListPage

--- frontend/packages/dev-console/src/components/gitops/GitOpsListPage.tsx
+++ frontend/packages/dev-console/src/components/gitops/GitOpsListPage.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
-import { PageHeading, LoadingBox } from '@console/internal/components/utils';
-import { ProjectModel } from '@console/internal/models';
+import { PageHeading, LoadingBox, ExternalLink } from '@console/internal/components/utils';
+import { ProjectModel, ConsoleLinkModel } from '@console/internal/models';
 import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
-import { K8sResourceKind } from '@console/internal/module/k8s';
+import { K8sResourceKind, referenceForModel } from '@console/internal/module/k8s';
 import { DevPreviewBadge } from '@console/shared';
 import GitOpsList from './list/GitOpsList';
 import { fetchAllAppGroups, getManifestURLs, getPipelinesBaseURI } from './utils/gitops-utils';
 import useDefaultSecret from './utils/useDefaultSecret';
+import { Split } from '@patternfly/react-core';
+import * as _ from 'lodash-es';
+import './GitOpsListPage.scss';
 
 const projectRes = { isList: true, kind: ProjectModel.kind, optional: true };
 
@@ -38,12 +41,36 @@ const GitOpsListPage: React.FC = () => {
     };
   }, [baseURL, namespaces, nsError, nsLoaded]);
 
+  const [consoleLinks] = useK8sWatchResource({
+    isList: true,
+    kind: referenceForModel(ConsoleLinkModel),
+    optional: true,
+  });
+  let aLink;
+  _.filter(consoleLinks, (link: any) => {
+    if (link.spec.location === 'ApplicationMenu') {
+      if (link.metadata.name === "argocd") {
+        aLink = link;
+        return true;
+      }
+      return false;
+    }
+  });
+
   return (
     <>
       <Helmet>
         <title>Application Stages</title>
       </Helmet>
-      <PageHeading title="Application Stages" badge={<DevPreviewBadge />} />
+      <PageHeading 
+        title="Application Stages" 
+        badge={
+            <Split className="odc-gitops-list-page-heading">
+              {aLink && <ExternalLink href={aLink.spec.href} text="Argo CD" additionalClassName="odc-gitops-list-page-heading__argocd"/>}
+              <DevPreviewBadge />
+            </Split>
+        }>
+      </PageHeading> 
       {!appGroups && !emptyStateMsg ? (
         <LoadingBox />
       ) : (
