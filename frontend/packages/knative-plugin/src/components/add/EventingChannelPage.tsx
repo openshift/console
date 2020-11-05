ea95007924a9723c623ac4be380a004c63257dbb
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2020-11-05 11:45:58 +0530

    removes techpreview badge from eventing

--- frontend/packages/knative-plugin/src/components/add/EventingChannelPage.tsx
+++ frontend/packages/knative-plugin/src/components/add/EventingChannelPage.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { RouteComponentProps } from 'react-router';
-import { PageBody, getBadgeFromType } from '@console/shared';
+import { PageBody } from '@console/shared';
 import { PageHeading } from '@console/internal/components/utils';
 import NamespacedPage, {
   NamespacedPageVariants,
 } from '@console/dev-console/src/components/NamespacedPage';
 import { QUERY_PROPERTIES } from '@console/dev-console/src/const';
-import { KnativeEventingModel } from '../../models';
 import { useChannelList } from '../../utils/create-channel-utils';
 import AddChannel from './channels/AddChannel';
 
@@ -22,7 +21,7 @@ const EventingChannelPage: React.FC<EventingChannelPageProps> = ({ match, locati
       <Helmet>
         <title>Channel</title>
       </Helmet>
-      <PageHeading badge={getBadgeFromType(KnativeEventingModel.badge)} title="Channel">
+      <PageHeading title="Channel">
         Create a Knative Channel to create an event forwarding and persistence layer with in-memory
         and reliable implementations
       </PageHeading>
