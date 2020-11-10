09d77639439cc1a032df086f78ca098fee292c0c
mjsso <minjin_so@tmax.co.kr>
2020-11-10 17:54:40 +0900

    [refactor] Number Spinner 컴포넌트에 initialValue속성 추가

--- frontend/public/components/hypercloud/utils/number-spinner.tsx
+++ frontend/public/components/hypercloud/utils/number-spinner.tsx
@@ -3,62 +3,36 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { Button } from '@patternfly/react-core';
 import { MinusSquareIcon, PlusSquareIcon } from '@patternfly/react-icons';
-import { useFormContext } from "react-hook-form";
+import { useFormContext } from 'react-hook-form';
 
-export const NumberSpinner: React.FC<NumberSpinnerProps> = ({
-    className,
-    min,
-    max,
-    name,
-    ...inputProps
-}) => {
-    const { register } = useFormContext();
-    const [value, setValue] = React.useState(0);
-    const changeValueBy = (operation) => {
-        setValue(_.toInteger(value) + operation);
-    }
-    return (
-        <div>
-            <Button
-                onClick={() => changeValueBy(-1)}
-                type="button"
-                variant="plain"
-                isDisabled={!_.isNil(min) && value <= min}
-                aria-label="Decrement"
-                className="co-m-number-spinner__button"
-            >
-                <MinusSquareIcon className="co-m-number-spinner__down-icon" />
-            </Button>
-            <input
-                name={name}
-                type="number"
-                ref={register({ min: min, max: max })}
-                value={value}
-                onChange={(e: any) => setValue(e.target.value)}
-                className={classNames(className, 'co-m-number-spinner__input')}
-                {...inputProps}>
-            </input>
-            <Button
-                onClick={() => changeValueBy(1)}
-                type="button"
-                variant="plain"
-                isDisabled={!_.isNil(max) && value >= max}
-                aria-label="Increment"
-                className="co-m-number-spinner__button"
-            >
-                <PlusSquareIcon className="co-m-number-spinner__up-icon" />
-            </Button>
-        </div>
-    );
+export const NumberSpinner: React.FC<NumberSpinnerProps> = ({ className, initialValue, min, max, name, ...inputProps }) => {
+  const { register } = useFormContext();
+  const [value, setValue] = React.useState(initialValue);
+  const changeValueBy = operation => {
+    setValue(_.toInteger(value) + operation);
+  };
+  return (
+    <div>
+      <Button onClick={() => changeValueBy(-1)} type="button" variant="plain" isDisabled={!_.isNil(min) && value <= min} aria-label="Decrement" className="co-m-number-spinner__button">
+        <MinusSquareIcon className="co-m-number-spinner__down-icon" />
+      </Button>
+      <input name={name} type="number" ref={register({ min: min, max: max })} value={value} onChange={(e: any) => setValue(e.target.value)} className={classNames(className, 'co-m-number-spinner__input')} {...inputProps}></input>
+      <Button onClick={() => changeValueBy(1)} type="button" variant="plain" isDisabled={!_.isNil(max) && value >= max} aria-label="Increment" className="co-m-number-spinner__button">
+        <PlusSquareIcon className="co-m-number-spinner__up-icon" />
+      </Button>
+    </div>
+  );
 };
 
 type NumberSpinnerProps = {
-    className?: string;
-    min?: number;
-    max?: number;
-    name?: string;
+  className?: string;
+  initialValue?: number;
+  min?: number;
+  max?: number;
+  name?: string;
 } & React.HTMLProps<HTMLInputElement>;
 
 NumberSpinner.defaultProps = {
-    name: "numberSpinner"
+  name: 'numberSpinner',
+  initialValue: 0,
 };
