09ff4a34a80c8612745c12d84a37c5bc5154fbb8
Rebecca Alpert <ralpert@redhat.com>
2020-11-16 11:00:13 -0500

    CONSOLE-2475: Check in Japanese and Chinese translations
    Adding first human translations for Japanese and Chinese from Terry's team.
    
    Fixes https://issues.redhat.com/browse/CONSOLE-2475

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -207,7 +207,7 @@ const config: Configuration = {
     new CopyWebpackPlugin([{ from: './packages/container-security/locales', to: 'locales' }]),
     new CopyWebpackPlugin([{ from: './packages/pipelines-plugin/locales', to: 'locales' }]),
     new MomentLocalesPlugin({
-      localesToKeep: ['en', 'ja', 'ko'],
+      localesToKeep: ['en', 'ja', 'ko', 'zh-cn'],
     }),
     new webpack.IgnorePlugin(/prettier/),
     extractCSS,
