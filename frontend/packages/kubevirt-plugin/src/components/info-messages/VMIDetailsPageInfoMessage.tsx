76afc21935c525be1930243ee31688153ef7d1f7
metalice <mschatzm@redhat.com>
2020-11-24 20:15:20 +0200

    Show info message only when vmi not controlled
    New component and login to choose when to show
    
    Signed-off-by: metalice <mschatzm@redhat.com>

---
+++ frontend/packages/kubevirt-plugin/src/components/info-messages/VMIDetailsPageInfoMessage.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import { HintBlock, ExternalLink } from '@console/internal/components/utils';
+import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
+import { getOwnerReferences } from '@console/shared/src';
+import { VirtualMachineInstanceModel, VirtualMachineModel } from '../../models/index';
+import { VMIKind } from '../../types';
+
+const VMIDetailsPageInfoMessage: React.FC<InfoMessageHintBlockProps> = ({ name, namespace }) => {
+  const { t } = useTranslation();
+  const [vmi, isLoaded] = useK8sWatchResource<VMIKind>({
+    kind: VirtualMachineInstanceModel.kind,
+    name,
+    namespace,
+  });
+
+  const isOwnedByVM =
+    vmi && !!getOwnerReferences<VMIKind>(vmi).find(({ kind }) => kind === VirtualMachineModel.kind);
+
+  const showMessage = isLoaded && !isOwnedByVM && vmi !== null;
+
+  return (
+    showMessage && (
+      <HintBlock
+        className="kubevirt-vmi-details-page-info-message__hint-block"
+        title={t('kubevirt-plugin~`Virtual Machine Instance {{name}}`', { name })}
+      >
+        <p>
+          {t(
+            'kubevirt-plugin~Consider using a Virtual Machine that will provide additional management capabilities to a VirtualMachineInstance inside the cluster.',
+          )}
+        </p>
+        <ExternalLink
+          href="https://kubevirt.io/user-guide/#/architecture?id=virtualmachine"
+          text={t('kubevirt-plugin~Learn more')}
+        />
+      </HintBlock>
+    )
+  );
+};
+
+type InfoMessageHintBlockProps = {
+  name: string;
+  namespace: string;
+};
+
+export default VMIDetailsPageInfoMessage;
---
+++ frontend/packages/kubevirt-plugin/src/components/info-messages/__tests__/mock_data.ts
@@ -0,0 +1,86 @@
+import { VMIKind } from '@console/kubevirt-plugin/src/types';
+
+export const mockData: { [key: string]: [VMIKind, boolean] } = {
+  testWithoutVMOwner: [
+    {
+      apiVersion: 'kubevirt.io/v1alpha3',
+      kind: 'VirtualMachineInstance',
+      metadata: {
+        ownerReferences: [
+          {
+            apiVersion: 'kubevirt.io/v1alpha3',
+            blockOwnerDeletion: true,
+            controller: true,
+            kind: 'some-kind',
+            name: 'vm-example',
+            uid: '03aec711-6673-417a-be03-2d7580b524ed',
+          },
+        ],
+      },
+      spec: {
+        affinity: 'any',
+        dnsConfig: 'any',
+        dnsPolicy: 'string',
+        hostname: 'string',
+        livenessProbe: 'any',
+        nodeSelector: { test: 'test' },
+        readinessProbe: 'any',
+        subdomain: 'string',
+        terminationGracePeriodSeconds: 0,
+        tolerations: [],
+      },
+      status: {
+        conditions: [],
+        interfaces: [],
+        migrationMethod: 'string',
+        migrationState: 'any',
+        nodeName: 'string',
+        phase: 'string',
+        reason: 'string',
+      },
+    },
+    true,
+  ],
+  testWithVMOwner: [
+    {
+      apiVersion: 'kubevirt.io/v1alpha3',
+      kind: 'VirtualMachineInstance',
+      metadata: {
+        ownerReferences: [
+          {
+            apiVersion: 'kubevirt.io/v1alpha3',
+            blockOwnerDeletion: true,
+            controller: true,
+            kind: 'VirtualMachine',
+            name: 'vm-example',
+            uid: '03aec711-6673-417a-be03-2d7580b524ed',
+          },
+        ],
+      },
+      spec: {
+        affinity: 'any',
+        dnsConfig: 'any',
+        dnsPolicy: 'string',
+        hostname: 'string',
+        livenessProbe: 'any',
+        nodeSelector: { test: 'test' },
+        readinessProbe: 'any',
+        subdomain: 'string',
+        terminationGracePeriodSeconds: 0,
+        tolerations: [],
+      },
+      status: {
+        conditions: [],
+        interfaces: [],
+        migrationMethod: 'string',
+        migrationState: 'any',
+        nodeName: 'string',
+        phase: 'string',
+        reason: 'string',
+      },
+    },
+    true,
+  ],
+  testNoVMILoadedTrue: [null, true],
+  testLoadedFalse: [null, false],
+};
