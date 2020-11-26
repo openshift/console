1811e23c45cc1a6956fea2f0d4494c3f8c0d6c36
Phillip Bailey <phbailey@redhat.com>
2020-11-25 19:22:44 -0500

    Localize the Bare Metal Hosts side nav name

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -220,6 +220,7 @@ const config: Configuration = {
     new CopyWebpackPlugin([{ from: './packages/kubevirt-plugin/locales', to: 'locales' }]),
     new CopyWebpackPlugin([{ from: './packages/ceph-storage-plugin/locales', to: 'locales' }]),
     new CopyWebpackPlugin([{ from: './packages/noobaa-storage-plugin/locales', to: 'locales' }]),
+    new CopyWebpackPlugin([{ from: './packages/metal3-plugin/locales', to: 'locales' }]),
     new MomentLocalesPlugin({
       localesToKeep: ['en', 'ja', 'ko'],
     }),
