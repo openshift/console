9408a98e35f28a006a08d7e20a90ada706144a5d
Gilad Lekner <gilad215@gmail.com>
2020-12-02 19:15:38 +0200

    add sc detection to vm snapshots

--- frontend/packages/kubevirt-plugin/src/components/modals/snapshot-modal/snapshot-modal.tsx
+++ frontend/packages/kubevirt-plugin/src/components/modals/snapshot-modal/snapshot-modal.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
-import { Alert, AlertVariant, Form, TextArea, TextInput } from '@patternfly/react-core';
-import { buildOwnerReference, prefixedID } from '../../../utils';
-import { HandlePromiseProps, withHandlePromise } from '@console/internal/components/utils';
+import { useTranslation } from 'react-i18next';
+import {
+  Alert,
+  AlertVariant,
+  ExpandableSection,
+  Form,
+  Stack,
+  StackItem,
+  TextArea,
+  Checkbox,
+  TextInput,
+} from '@patternfly/react-core';
+import {
+  ExternalLink,
+  HandlePromiseProps,
+  withHandlePromise,
+} from '@console/internal/components/utils';
 import { getName, getNamespace } from '@console/shared';
 import {
   createModalLauncher,
@@ -10,11 +24,14 @@ import {
   ModalTitle,
 } from '@console/internal/components/factory';
 import { k8sCreate } from '@console/internal/module/k8s';
+import { buildOwnerReference, prefixedID } from '../../../utils';
 import { VMLikeEntityKind } from '../../../types/vmLike';
 import { FormRow } from '../../form/form-row';
-import { SAVE } from '../../../utils/strings';
 import { ModalFooter } from '../modal/modal-footer';
 import { VMSnapshotWrapper } from '../../../k8s/wrapper/vm/vm-snapshot-wrapper';
+import { asVM, getVolumeSnapshotStatuses } from '../../../selectors/vm';
+import { SNAPSHOT_SUPPORT_URL } from '../../../constants';
+import './snapshot-modal.scss';
 
 const getSnapshotName = (vmName: string) => {
   const date = new Date();
@@ -23,10 +40,17 @@ const getSnapshotName = (vmName: string) => {
 
 const SnapshotsModal = withHandlePromise((props: SnapshotsModalProps) => {
   const { vmLikeEntity, inProgress, errorMessage, handlePromise, close, cancel } = props;
+  const { t } = useTranslation();
   const vmName = getName(vmLikeEntity);
   const [name, setName] = React.useState(getSnapshotName(vmName));
   const [description, setDescription] = React.useState('');
+  const [approveUnsupported, setApproveUnsupported] = React.useState(false);
   const asId = prefixedID.bind(null, 'snapshot');
+
+  const volumeSnapshotStatuses = getVolumeSnapshotStatuses(asVM(vmLikeEntity)) || [];
+  const supportedVolumes = volumeSnapshotStatuses.filter((status) => status?.enabled);
+  const unsupportedVolumes = volumeSnapshotStatuses.filter((status) => !status?.enabled);
+  const hasUnsupportedVolumes = unsupportedVolumes.length > 0;
 
   const submit = async (e) => {
     e.preventDefault();
@@ -44,16 +68,18 @@ const SnapshotsModal = withHandlePromise((props: SnapshotsModalProps) => {
 
   return (
     <div className="modal-content">
-      <ModalTitle>{'Take Snapshot'}</ModalTitle>
+      <ModalTitle>{t('kubevirt-plugin~Take Snapshot')}</ModalTitle>
       <ModalBody>
         <Alert
-          title="Snapshot only includes disks backed by a snapshot supported storage class"
+          title={t(
+            'kubevirt-plugin~Snapshot only includes disks backed by a snapshot supported storage class',
+          )}
           isInline
           variant={AlertVariant.info}
           className="co-m-form-row"
         />
         <Form onSubmit={submit}>
-          <FormRow title="Snapshot Name" fieldId={asId('name')} isRequired>
+          <FormRow title={t('kubevirt-plugin~Snapshot Name')} fieldId={asId('name')} isRequired>
             <TextInput
               autoFocus
               isRequired
@@ -62,20 +88,87 @@ const SnapshotsModal = withHandlePromise((props: SnapshotsModalProps) => {
               onChange={(v) => setName(v)}
             />
           </FormRow>
-          <FormRow title="Description" fieldId={asId('desc')}>
+          <FormRow title={t('kubevirt-plugin~Description')} fieldId={asId('desc')}>
             <TextArea
               value={description}
               onChange={(d) => setDescription(d)}
-              aria-label="description text area"
+              aria-label={t('kubevirt-plugin~description text area')}
             />
           </FormRow>
+          <FormRow fieldId="supported-volumes">
+            <Stack hasGutter>
+              {supportedVolumes.length > 0 && (
+                <StackItem>
+                  <ExpandableSection
+                    toggleText={t('kubevirt-plugin~Disks included in this Snapshot ({{count}})', {
+                      count: supportedVolumes?.length,
+                    })}
+                  >
+                    <Stack>
+                      {supportedVolumes?.map((vol) => (
+                        <StackItem key={vol.name}>{vol.name}</StackItem>
+                      ))}
+                    </Stack>
+                  </ExpandableSection>
+                </StackItem>
+              )}
+              {hasUnsupportedVolumes && (
+                <StackItem>
+                  <Alert
+                    variant={AlertVariant.warning}
+                    isInline
+                    title={t(
+                      'kubevirt-plugin~The following disk will not be included in the snapshot',
+                      { count: unsupportedVolumes?.length },
+                    )}
+                  >
+                    <Stack hasGutter>
+                      <StackItem>
+                        <Stack>
+                          {unsupportedVolumes?.map((vol) => (
+                            <StackItem>
+                              <strong>{vol.name}</strong> - {vol.reason}
+                            </StackItem>
+                          ))}
+                        </Stack>
+                      </StackItem>
+                      <StackItem>
+                        {t(
+                          'kubevirt-plugin~Edit the disk or contact your cluster admin for further details.',
+                          { count: unsupportedVolumes?.length },
+                        )}
+                      </StackItem>
+                      <StackItem>
+                        <ExternalLink
+                          additionalClassName="kv-snapshot-modal__link"
+                          text={<div>{t('kubevirt-plugin~Learn more about snapshots')}</div>}
+                          href={SNAPSHOT_SUPPORT_URL}
+                        />
+                      </StackItem>
+                    </Stack>
+                  </Alert>
+                </StackItem>
+              )}
+            </Stack>
+          </FormRow>
+          {hasUnsupportedVolumes && (
+            <FormRow fieldId="unsupported-approve-checkbox">
+              <Checkbox
+                id="approve-checkbox"
+                isChecked={approveUnsupported}
+                aria-label={t('kubevirt-plugin~unsupported approve checkbox')}
+                description={t('kubevirt-plugin~I am aware of this warning and wish to proceed')}
+                onChange={setApproveUnsupported}
+              />
+            </FormRow>
+          )}
         </Form>
       </ModalBody>
       <ModalFooter
         id="snapshot"
-        submitButtonText={SAVE}
+        submitButtonText={t('kubevirt-plugin~Save')}
         errorMessage={errorMessage}
-        isDisabled={inProgress}
+        isDisabled={inProgress || (hasUnsupportedVolumes && !approveUnsupported)}
         inProgress={inProgress}
         onSubmit={submit}
         onCancel={(e) => {
