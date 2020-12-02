924498e08ea502727183b0fd581f3050d9877acc
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2020-12-02 13:36:07 +0900

    [feat] proxy 관련 header 부분 추가

--- pkg/backend/proxy.go
+++ pkg/backend/proxy.go
@@ -37,46 +37,46 @@ func newReverseProxy(target *url.URL) *httputil.ReverseProxy {
 
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	proxy.Director = func(outReq *http.Request) {
-		// 	u := outReq.URL
-		// 	if outReq.RequestURI != "" {
-		// 		parsedURL, err := url.ParseRequestURI(outReq.RequestURI)
-		// 		if err == nil {
-		// 			u = parsedURL
-		// 		}
-		// 	}
+		u := outReq.URL
+		if outReq.RequestURI != "" {
+			parsedURL, err := url.ParseRequestURI(outReq.RequestURI)
+			if err == nil {
+				u = parsedURL
+			}
+		}
 
-		// 	outReq.URL.Path = u.Path
-		// 	outReq.URL.RawPath = u.RawPath
-		// 	outReq.URL.RawQuery = u.RawQuery
-		// 	outReq.RequestURI = "" // Outgoing request should not have RequestURI
+		outReq.URL.Path = u.Path
+		outReq.URL.RawPath = u.RawPath
+		outReq.URL.RawQuery = u.RawQuery
+		outReq.RequestURI = "" // Outgoing request should not have RequestURI
 
-		// 	outReq.Proto = "HTTP/1.1"
-		// 	outReq.ProtoMajor = 1
-		// 	outReq.ProtoMinor = 1
+		outReq.Proto = "HTTP/1.1"
+		outReq.ProtoMajor = 1
+		outReq.ProtoMinor = 1
 
-		// 	if _, ok := outReq.Header["User-Agent"]; !ok {
-		// 		outReq.Header.Set("User-Agent", "")
-		// 	}
+		if _, ok := outReq.Header["User-Agent"]; !ok {
+			outReq.Header.Set("User-Agent", "")
+		}
 
-		// 	// Do not pass client Host header unless optsetter PassHostHeader is set.
-		// 	// if passHostHeader != nil && !*passHostHeader {
-		// 	// 	outReq.Host = outReq.URL.Host
-		// 	// }
+		// Do not pass client Host header unless optsetter PassHostHeader is set.
+		// if passHostHeader != nil && !*passHostHeader {
+		// 	outReq.Host = outReq.URL.Host
+		// }
 
-		// 	// Even if the websocket RFC says that headers should be case-insensitive,
-		// 	// some servers need Sec-WebSocket-Key, Sec-WebSocket-Extensions, Sec-WebSocket-Accept,
-		// 	// Sec-WebSocket-Protocol and Sec-WebSocket-Version to be case-sensitive.
-		// 	// https://tools.ietf.org/html/rfc6455#page-20
-		// 	outReq.Header["Sec-WebSocket-Key"] = outReq.Header["Sec-Websocket-Key"]
-		// 	outReq.Header["Sec-WebSocket-Extensions"] = outReq.Header["Sec-Websocket-Extensions"]
-		// 	outReq.Header["Sec-WebSocket-Accept"] = outReq.Header["Sec-Websocket-Accept"]
-		// 	outReq.Header["Sec-WebSocket-Protocol"] = outReq.Header["Sec-Websocket-Protocol"]
-		// 	outReq.Header["Sec-WebSocket-Version"] = outReq.Header["Sec-Websocket-Version"]
-		// 	delete(outReq.Header, "Sec-Websocket-Key")
-		// 	delete(outReq.Header, "Sec-Websocket-Extensions")
-		// 	delete(outReq.Header, "Sec-Websocket-Accept")
-		// 	delete(outReq.Header, "Sec-Websocket-Protocol")
-		// 	delete(outReq.Header, "Sec-Websocket-Version")
+		// Even if the websocket RFC says that headers should be case-insensitive,
+		// some servers need Sec-WebSocket-Key, Sec-WebSocket-Extensions, Sec-WebSocket-Accept,
+		// Sec-WebSocket-Protocol and Sec-WebSocket-Version to be case-sensitive.
+		// https://tools.ietf.org/html/rfc6455#page-20
+		outReq.Header["Sec-WebSocket-Key"] = outReq.Header["Sec-Websocket-Key"]
+		outReq.Header["Sec-WebSocket-Extensions"] = outReq.Header["Sec-Websocket-Extensions"]
+		outReq.Header["Sec-WebSocket-Accept"] = outReq.Header["Sec-Websocket-Accept"]
+		outReq.Header["Sec-WebSocket-Protocol"] = outReq.Header["Sec-Websocket-Protocol"]
+		outReq.Header["Sec-WebSocket-Version"] = outReq.Header["Sec-Websocket-Version"]
+		delete(outReq.Header, "Sec-Websocket-Key")
+		delete(outReq.Header, "Sec-Websocket-Extensions")
+		delete(outReq.Header, "Sec-Websocket-Accept")
+		delete(outReq.Header, "Sec-Websocket-Protocol")
+		delete(outReq.Header, "Sec-Websocket-Version")
 	}
 	proxy.Transport = &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
