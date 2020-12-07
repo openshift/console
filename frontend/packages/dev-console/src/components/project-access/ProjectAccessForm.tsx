5a7128d69bc3d5221544f62a0465f2d95f8cf223
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2020-12-07 19:48:23 +0530

    Do not show misleading alert message

--- frontend/packages/dev-console/src/components/project-access/ProjectAccessForm.tsx
+++ frontend/packages/dev-console/src/components/project-access/ProjectAccessForm.tsx
@@ -3,9 +3,9 @@ import * as _ from 'lodash';
 import { Form, TextInputTypes } from '@patternfly/react-core';
 import { FormikProps, FormikValues } from 'formik';
 import { MultiColumnField, InputField, DropdownField, FormFooter } from '@console/shared';
+import { useTranslation } from 'react-i18next';
 
 enum accessRoles {
-  '' = 'Select a role',
   admin = 'Admin',
   edit = 'Edit',
   view = 'View',
@@ -18,29 +18,40 @@ const ProjectAccessForm: React.FC<FormikProps<FormikValues>> = ({
   status,
   errors,
   dirty,
-}) => (
-  <Form onSubmit={handleSubmit}>
-    <div className="co-m-pane__form">
-      <MultiColumnField
-        name="projectAccess"
-        addLabel="Add Access"
-        headers={['Name', 'Role']}
-        emptyValues={{ user: '', role: '' }}
-      >
-        <InputField name="user" type={TextInputTypes.text} placeholder="Name" />
-        <DropdownField name="role" items={accessRoles} fullWidth />
-      </MultiColumnField>
-      <hr />
-      <FormFooter
-        handleReset={handleReset}
-        isSubmitting={isSubmitting}
-        errorMessage={status && status.submitError}
-        successMessage={status && !dirty && status.success}
-        disableSubmit={!dirty || !_.isEmpty(errors)}
-        showAlert={dirty}
-      />
-    </div>
-  </Form>
-);
+}) => {
+  const { t } = useTranslation();
+  const disableSubmit = !dirty || !_.isEmpty(errors);
+  return (
+    <Form onSubmit={handleSubmit}>
+      <div className="co-m-pane__form">
+        <MultiColumnField
+          name="projectAccess"
+          addLabel={t('devconsole~Add Access')}
+          headers={[t('devconsole~Name'), t('devconsole~Role')]}
+          emptyValues={{ user: '', role: '' }}
+        >
+          <InputField name="user" type={TextInputTypes.text} placeholder={t('devconsole~Name')} />
+          <DropdownField
+            name="role"
+            title={t('devconsole~Select a role')}
+            items={accessRoles}
+            fullWidth
+          />
+        </MultiColumnField>
+        <hr />
+        <FormFooter
+          handleReset={handleReset}
+          isSubmitting={isSubmitting}
+          errorMessage={status && status.submitError}
+          successMessage={status && !dirty && status.success}
+          disableSubmit={disableSubmit}
+          showAlert={!disableSubmit}
+          submitLabel={t('devconsole~Save')}
+          resetLabel={t('devconsole~Reload')}
+        />
+      </div>
+    </Form>
+  );
+};
 
 export default ProjectAccessForm;
