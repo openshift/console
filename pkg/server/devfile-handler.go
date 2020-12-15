7a9fd1a187426051d013865515da0318eb1d65e0
Maysun J Faisal <maysunaneek@gmail.com>
2020-12-15 17:20:27 -0500

    Update devfile handler for dockerfile loc and attr filter
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pkg/server/devfile-handler.go
+++ pkg/server/devfile-handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path"
 
 	buildv1 "github.com/openshift/api/build/v1"
 	imagev1 "github.com/openshift/api/image/v1"
@@ -18,12 +19,7 @@ import (
 	devfile "github.com/devfile/library/pkg/devfile"
 	"github.com/devfile/library/pkg/devfile/generator"
 	"github.com/devfile/library/pkg/devfile/parser"
-)
-
-const (
-	// TODO: Support from the UI a custom path
-	// dockerfilePath is the path to the Dockerfile (including the file name; ie "./my-path/Dockerfile")
-	dockerfilePath = "Dockerfile"
+	"github.com/devfile/library/pkg/devfile/parser/data/v2/common"
 )
 
 func (s *Server) devfileHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,15 +46,27 @@ func (s *Server) devfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	containerComponents := devfileObj.Data.GetDevfileContainerComponents() //TODO: filter thru Console attribute, right now if there is more than one component container err out
+	filterOptions := common.DevfileOptions{
+		Filter: map[string]interface{}{
+			"tool": "console-import",
+		},
+	}
+
+	containerComponents, err := devfileObj.Data.GetDevfileContainerComponents(filterOptions) //For Dev Preview, if there is more than one component container err out
+	if err != nil {
+		errMsg := fmt.Sprintf("Failed to get the container component from devfile with attribute 'tool: console-import': %v", err)
+		klog.Error(errMsg)
+		serverutils.SendResponse(w, http.StatusBadRequest, serverutils.ApiError{Err: errMsg})
+		return
+	}
 	if len(containerComponents) != 1 {
-		errMsg := "Console Devfile Import Dev Preview, supports only one component container"
+		errMsg := "Console Devfile Import Dev Preview, supports only one component container with attribute 'tool: console-import'"
 		klog.Error(errMsg)
 		serverutils.SendResponse(w, http.StatusBadRequest, serverutils.ApiError{Err: errMsg})
 		return
 	}
 
-	deploymentResource, err := getDeployResource(data, devfileObj)
+	deploymentResource, err := getDeployResource(data, devfileObj, filterOptions)
 	if err != nil {
 		errMsg := fmt.Sprintf("Failed to get deployment resource for the devfile: %v", err)
 		klog.Error(errMsg)
@@ -66,7 +74,7 @@ func (s *Server) devfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	service, err := getService(devfileObj)
+	service, err := getService(devfileObj, filterOptions)
 	if err != nil {
 		errMsg := fmt.Sprintf("Failed to get service for the devfile: %v", err)
 		klog.Error(errMsg)
@@ -74,9 +82,27 @@ func (s *Server) devfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	dockerfileRelativePath := devfileObj.Data.GetMetadata().Attributes.GetString("alpha.build-dockerfile", &err)
+	if err != nil {
+		errMsg := fmt.Sprintf("Failed to get the Dockerfile location from devfile metadata attribute 'alpha.build-dockerfile': %v", err)
+		klog.Error(errMsg)
+		serverutils.SendResponse(w, http.StatusBadRequest, serverutils.ApiError{Err: errMsg})
+		return
+	}
+
+	dockerRelativeSrcContext := devfileObj.Data.GetMetadata().Attributes.GetString("alpha.build-context", &err)
+	if err != nil {
+		errMsg := fmt.Sprintf("Failed to get the Dockerfile location from devfile metadata attribute 'alpha.build-context': %v", err)
+		klog.Error(errMsg)
+		serverutils.SendResponse(w, http.StatusBadRequest, serverutils.ApiError{Err: errMsg})
+		return
+	}
+
+	dockerContextDir := path.Join(data.Git.Dir, dockerRelativeSrcContext)
+
 	devfileResources := devfileResources{
 		ImageStream:    getImageStream(),
-		BuildResource:  getBuildResource(data),
+		BuildResource:  getBuildResource(data, dockerfileRelativePath, dockerContextDir),
 		DeployResource: deploymentResource,
 		Service:        service,
 		Route:          getRoutes(data, containerComponents),
@@ -101,7 +127,7 @@ func getImageStream() imagev1.ImageStream {
 	return imageStream
 }
 
-func getBuildResource(data devfileForm) buildv1.BuildConfig {
+func getBuildResource(data devfileForm, dockerfilePath, contextDir string) buildv1.BuildConfig {
 
 	buildConfigParams := generator.BuildConfigParams{
 		TypeMeta: generator.GetTypeMeta("BuildConfig", "build.openshift.io/v1"),
@@ -109,19 +135,19 @@ func getBuildResource(data devfileForm) buildv1.BuildConfig {
 			ImageStreamTagName: data.Name + ":latest", // TODO Post Dev Preview. Update as per proposal i.e.; use the image mentioned in the devfile and push build to it.
 			GitRef:             data.Git.Ref,
 			GitURL:             data.Git.URL,
+			ContextDir:         contextDir,
 			BuildStrategy:      generator.GetDockerBuildStrategy(dockerfilePath, nil), // TODO use the Dockerfile path from the devfile instead of assuming
 		},
 	}
 
 	buildConfig := generator.GetBuildConfig(buildConfigParams)
-	buildConfig.Spec.CommonSpec.Source.ContextDir = data.Git.Dir
 
 	return *buildConfig
 }
 
-func getDeployResource(data devfileForm, devfileObj parser.DevfileObj) (appsv1.Deployment, error) {
+func getDeployResource(data devfileForm, devfileObj parser.DevfileObj, filterOptions common.DevfileOptions) (appsv1.Deployment, error) {
 
-	containers, err := generator.GetContainers(devfileObj)
+	containers, err := generator.GetContainers(devfileObj, filterOptions)
 	if err != nil {
 		return appsv1.Deployment{}, err
 	}
@@ -137,13 +163,13 @@ func getDeployResource(data devfileForm, devfileObj parser.DevfileObj) (appsv1.D
 	return *deployment, nil
 }
 
-func getService(devfileObj parser.DevfileObj) (corev1.Service, error) {
+func getService(devfileObj parser.DevfileObj, filterOptions common.DevfileOptions) (corev1.Service, error) {
 
 	serviceParams := generator.ServiceParams{
 		TypeMeta: generator.GetTypeMeta("Service", "v1"),
 	}
 
-	service, err := generator.GetService(devfileObj, serviceParams)
+	service, err := generator.GetService(devfileObj, serviceParams, filterOptions)
 	if err != nil {
 		return corev1.Service{}, err
 	}
