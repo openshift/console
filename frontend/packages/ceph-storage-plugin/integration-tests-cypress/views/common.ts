da0848efca9b1485de2beb200f0291cfc15175c6
bipuladh <badhikar@redhat.com>
2020-12-17 15:32:54 +0530

    Cypress command updated to be a function from an object value

--- frontend/packages/ceph-storage-plugin/integration-tests-cypress/views/common.ts
+++ frontend/packages/ceph-storage-plugin/integration-tests-cypress/views/common.ts
@@ -4,5 +4,5 @@ export const commonFlows = {
     cy.byLegacyTestID('item-filter').type('ocs-operator');
     cy.byTestOperatorRow('OpenShift Container Storage').click();
   },
-  checkAll: cy.get('input[name=check-all]'),
+  checkAll: () => cy.get('input[name=check-all]'),
 };
