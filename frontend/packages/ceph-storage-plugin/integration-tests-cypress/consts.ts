65030dcc423aa7145a25a2b736adafdeeb25e1d4
Arun Kumar Mohan <amohan@redhat.com>
2020-12-22 08:36:28 +0530

    Migrating multiple Storageclass tests to cypress
    Signed-off-by: Arun Kumar Mohan <amohan@redhat.com>

---
+++ frontend/packages/ceph-storage-plugin/integration-tests-cypress/consts.ts
@@ -0,0 +1,110 @@
+export const OCS_OP = 'OpenShift Container Storage';
+export const NS = 'openshift-storage';
+
+export const SECOND = 1000;
+export const MINUTE = 60 * SECOND;
+
+export enum POD_NAME_PATTERNS {
+  OCS = 'ocs-operator-',
+  ROOK = 'rook-ceph-operator-',
+  NOOBA_OPERATOR = 'noobaa-operator-',
+  NOOBAA_CORE = 'noobaa-core-',
+  ROOK_CEPH_MON = 'rook-ceph-mon',
+  ROOK_CEPH_MGR = 'rook-ceph-mgr',
+  CSI_CEPHFS = 'csi-cephfsplugin-',
+  CSI_RBD = 'csi-rbdplugin-',
+  ROOK_CEPH_MDS = 'rook-ceph-mds-ocs-storagecluster-cephfilesystem',
+  ROOK_CEPH_OSD = 'rook-ceph-osd-',
+  ROOK_CEPH_OSD_PREPARE = 'rook-ceph-osd-prepare-',
+}
+
+export enum STORAGE_CLASS_PATTERNS {
+  RBD = 'ocs-storagecluster-ceph-rbd',
+  FS = 'ocs-storagecluster-cephfs',
+  NOOBAA = 'noobaa.io',
+}
+
+export enum CLUSTER_STATUS {
+  READY = 'Ready',
+  PROGRESSING = 'Progressing',
+  HEALTH_ERROR = 'HEALTH_ERR',
+}
+
+export const OCS_NODE_LABEL = 'cluster.ocs.openshift.io/openshift-storage';
+export const CATALOG_SRC = 'redhat-operators';
+
+export const KIND = 'storagecluster';
+export const EXPAND_WAIT = 15 * MINUTE;
+export const CAPACITY_UNIT = 'TiB';
+export const CAPACITY_VALUE = '2';
+export const OCS_OPERATOR_NAME = 'ocs-operatorv4';
+export const STORAGE_CLUSTER_NAME = 'ocs-storagecluster';
+export const HOST = 'host';
+export const ZONE = 'zone';
+export const OSD = 'osd';
+
+export const SUCCESS = 'Succeeded';
+export const READY_FOR_USE = 'ready for use';
+
+export const ocsTaint = Object.freeze({
+  key: 'node.ocs.openshift.io/storage',
+  value: 'true',
+  effect: 'NoSchedule',
+});
+
+export enum VOLUME_ACCESS_MODES {
+  RWO = 'ReadWriteOnce',
+  RWX = 'ReadWriteMany',
+  ROX = 'ReadOnlyMany',
+}
+
+export enum SIZE_UNITS {
+  MI = 'Mi',
+  GI = 'Gi',
+  TI = 'Ti',
+}
+
+export enum PVC_STATUS {
+  PENDING = 'Pending',
+  BOUND = 'Bound',
+}
+
+export enum OCP_TEXT_STATUS {
+  HEALTHY = 'healthy',
+  DEGRADED = 'health is degraded',
+  NOT_AVAILABLE = 'is not available',
+}
+
+export enum OCP_HEALTH_ICON_COLORS {
+  GREEN = '#486b00',
+  YELLOW = '#f0ab00',
+  RED = '#c9190b',
+  GREEN46 = '#3e8635',
+}
+
+export const EXAMPLE_PVC = {
+  name: 'expample-pvc',
+  namespace: NS,
+  size: '5',
+  sizeUnits: SIZE_UNITS.GI,
+  storageClass: STORAGE_CLASS_PATTERNS.RBD,
+  accessMode: VOLUME_ACCESS_MODES.RWO,
+};
+
+export const CHANNEL_43 = 'stable-4.3';
+export const CHANNEL_44 = 'stable-4.4';
+export const CHANNEL_45 = 'stable-4.5';
+
+export const LSO_INFO_MSG = 'Local Storage Operator Not Installed';
+export const SC_STEPS_NAME = {
+  DISCOVERY: 'Discover Disks',
+  STORAGECLASS: 'Create Storage Class',
+  STORAGECLUSTER: 'Create Storage Cluster',
+};
+export const CONFIRM_MODAL_TITLE = 'Create Storage Class';
+
+export const OSD_SIZES_MAP = {
+  '512Gi': 0.5,
+  '2Ti': 2,
+  '4Ti': 4,
+};
---
+++ frontend/packages/ceph-storage-plugin/integration-tests-cypress/tests/multiple-storageclass-selection-scenarios.ts
@@ -0,0 +1,124 @@
+import { testEbsSC, testNoProvisionerSC } from '../mocks/storageclass';
+import { commonFlows } from '../views/common';
+import { K8sResourceKind } from '@console/internal/module/k8s';
+import { DeviceSet } from '../../src/types';
+import { OCS_INTERNAL_CR_NAME } from '../../src/constants';
+import { NS } from '../consts';
+import { getCurrentDeviceSetIndex } from '../../src/utils/add-capacity';
+
+interface IndexAndDeviceSet {
+  index: number;
+  deviceSets: DeviceSet[];
+}
+
+const withJSONResult = (res: Cypress.Exec, scName: string, iAndD: IndexAndDeviceSet) => {
+  const jsonOut: K8sResourceKind = JSON.parse(res.stdout);
+  iAndD.deviceSets = jsonOut.spec.storageDeviceSets;
+  iAndD.index = getCurrentDeviceSetIndex(iAndD.deviceSets, scName);
+};
+
+const fetchStorageClusterJson = () =>
+  cy.exec(`kubectl get --ignore-not-found storagecluster ${OCS_INTERNAL_CR_NAME} -n ${NS} -o json`);
+
+const addCapacity = (uid: string, scName: string) => {
+  console.log(`UID: ${uid} == SCName: ${scName}`);
+  cy.byLegacyTestID('kebab-button').click(); // 'data-test-id'
+  cy.byTestActionID('Add Capacity').click(); // 'data-test-action'
+  cy.byTestID('add-cap-sc-dropdown').click(); // 'data-test'
+  cy.byTestID('dropdown-menu-item-link')
+    .contains(scName)
+    .click();
+  cy.byTestID('confirm-action').click();
+};
+
+describe('Add capacity using multiple storage classes', () => {
+  const beforeCapacityAddition = {
+    deviceSets: null,
+    portability: null,
+    devicesCount: null,
+  };
+
+  before(() => {
+    cy.login();
+    cy.visit('/');
+    cy.install();
+    cy.exec(`echo '${JSON.stringify(testEbsSC)}' | kubectl apply -f -`);
+    cy.exec(`echo '${JSON.stringify(testNoProvisionerSC)}' | kubectl apply -f -`);
+    commonFlows.navigateToOCS();
+    cy.byLegacyTestID('horizontal-link-Storage Cluster').click();
+  });
+  after(() => {
+    cy.exec(`echo '${JSON.stringify(testEbsSC)}' | kubectl delete -f -`);
+    cy.exec(`echo '${JSON.stringify(testNoProvisionerSC)}' | kubectl delete -f -`);
+    cy.logout();
+  });
+
+  describe('Add capacity with a new storage class having EBS as provisioner', () => {
+    const { name: scName } = testEbsSC.metadata;
+    const iAndD: IndexAndDeviceSet = { index: 0, deviceSets: [] };
+    before(() => {
+      fetchStorageClusterJson().then((res) => {
+        const json: K8sResourceKind = JSON.parse(res.stdout);
+        beforeCapacityAddition.deviceSets = json.spec.storageDeviceSets.length;
+        addCapacity(json.metadata.uid, scName);
+        fetchStorageClusterJson().then((newRes) => {
+          withJSONResult(newRes, scName, iAndD);
+        });
+      });
+    });
+    it('New device set is created', () =>
+      expect(iAndD.deviceSets.length).toBe(beforeCapacityAddition.deviceSets + 1));
+    it('Device count is 1 in the new device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].count).toBe(1));
+    it('Osd portability is enabled in the new device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].portable).toBe(true));
+  });
+
+  describe('Add capacity with an existing storage class having EBS as provisioner', () => {
+    const { name: scName } = testEbsSC.metadata;
+    const iAndD: IndexAndDeviceSet = { index: 0, deviceSets: [] };
+    before(() => {
+      fetchStorageClusterJson().then((res) => {
+        const json: K8sResourceKind = JSON.parse(res.stdout);
+        const deviceSets: DeviceSet[] = json.spec.storageDeviceSets;
+        const index = getCurrentDeviceSetIndex(deviceSets, scName);
+        beforeCapacityAddition.deviceSets = deviceSets.length;
+        beforeCapacityAddition.portability = deviceSets[index].portable;
+        beforeCapacityAddition.devicesCount = deviceSets[index].count;
+        addCapacity(json.metadata.uid, scName);
+        fetchStorageClusterJson().then((newRes) => {
+          withJSONResult(newRes, scName, iAndD);
+        });
+      });
+    });
+
+    it('New device set is not created', () =>
+      expect(iAndD.deviceSets.length).toBe(beforeCapacityAddition.deviceSets));
+    it('Devices count is incremented by 1 in the corresponding device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].count).toBe(beforeCapacityAddition.devicesCount + 1));
+    it('Osd portability is not modified in the corresponding device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].portable).toBe(beforeCapacityAddition.portability));
+  });
+
+  describe(`Add capacity with a new storage class having NO-PROVISIONER as provisioner`, () => {
+    const { name: scName } = testNoProvisionerSC.metadata;
+    const iAndD: IndexAndDeviceSet = { index: 0, deviceSets: [] };
+    before(() => {
+      fetchStorageClusterJson().then((res) => {
+        const json: K8sResourceKind = JSON.parse(res.stdout);
+        beforeCapacityAddition.deviceSets = json.spec.storageDeviceSets.length;
+        addCapacity(json.metadata.uid, scName);
+        fetchStorageClusterJson().then((newRes) => {
+          withJSONResult(newRes, scName, iAndD);
+        });
+      });
+    });
+
+    it('New device set is created', () =>
+      expect(iAndD.deviceSets.length).toBe(beforeCapacityAddition.deviceSets + 1));
+    it('Device count is 1 in the new device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].count).toBe(1));
+    it('Osd portability is disabled in the new device set', () =>
+      expect(iAndD.deviceSets[iAndD.index].portable).toBe(false));
+  });
+});
