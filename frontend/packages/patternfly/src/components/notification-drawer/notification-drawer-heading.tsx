4d0e8d4072c81d49e1f6a71e251f52789eea108f
Joseph Caiani <jcaiani@redhat.com>
2020-12-23 16:17:00 -0500

    Bug 1910409: Add externalized strings for i18n for notification drawer

--- frontend/packages/patternfly/src/components/notification-drawer/notification-drawer-heading.tsx
+++ frontend/packages/patternfly/src/components/notification-drawer/notification-drawer-heading.tsx
@@ -1,34 +1,42 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const NotificationDrawerHeading: React.FC<NotificationDrawerHeadingProps> = ({
   count,
   children,
   onClose,
-}) => (
-  <div className="pf-c-notification-drawer">
-    <div className="pf-c-notification-drawer__header">
-      <h1 className="pf-c-notification-drawer__header-title">Notifications</h1>
-      {count && (
-        <span className="pf-c-notification-drawer__header-status">{`${count} unread`}</span>
-      )}
-      <div className="pf-c-notification-drawer__header-action">
-        <div className="pf-c-notification-drawer__header-action-close">
-          <button
-            className="pf-c-button pf-m-plain"
-            type="button"
-            aria-label="Close"
-            onClick={onClose}
-          >
-            <i className="fas fa-times" aria-hidden="true"></i>
-          </button>
+}) => {
+  const { t } = useTranslation();
+  return (
+    <div className="pf-c-notification-drawer">
+      <div className="pf-c-notification-drawer__header">
+        <h1 className="pf-c-notification-drawer__header-title">
+          {t('notification-drawer~Notifications')}
+        </h1>
+        {count && (
+          <span className="pf-c-notification-drawer__header-status">
+            {t('notification-drawer~{{count}} unread', { count })}
+          </span>
+        )}
+        <div className="pf-c-notification-drawer__header-action">
+          <div className="pf-c-notification-drawer__header-action-close">
+            <button
+              className="pf-c-button pf-m-plain"
+              type="button"
+              aria-label={t('notification-drawer~Close')}
+              onClick={onClose}
+            >
+              <i className="fas fa-times" aria-hidden="true"></i>
+            </button>
+          </div>
         </div>
       </div>
+      <div className="pf-c-notification-drawer__body">
+        <div className="pf-c-notification-drawer__group-list">{children}</div>
+      </div>
     </div>
-    <div className="pf-c-notification-drawer__body">
-      <div className="pf-c-notification-drawer__group-list">{children}</div>
-    </div>
-  </div>
-);
+  );
+};
 
 type NotificationDrawerHeadingProps = {
   children: React.ReactNode;
