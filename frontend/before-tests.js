1096d544a9ebebd0b8d5521d3b345639b2bc31ec
zherman0 <zherman@redhat.com>
2021-01-06 11:00:53 -0700

    Bump react to v17

--- frontend/before-tests.js
+++ frontend/before-tests.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
 import { configure } from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import 'url-search-params-polyfill';
 
