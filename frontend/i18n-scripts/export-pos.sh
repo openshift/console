c83619aebde2fbe63fe2707282f46226793566f8
Rebecca Alpert <ralpert@redhat.com>
2021-01-06 10:41:36 -0500

    Export zh-cn and zh-tw
    Export po files for zh-cn and zh-tw instead of just zh.

--- frontend/i18n-scripts/export-pos.sh
+++ frontend/i18n-scripts/export-pos.sh
@@ -3,7 +3,8 @@
 set -exuo pipefail
 
 for f in public/locales/en/* ; do
-  yarn i18n-to-po -f $(basename "$f" .json) -l zh
+  yarn i18n-to-po -f $(basename "$f" .json) -l zh-cn
+  yarn i18n-to-po -f $(basename "$f" .json) -l zh-tw
   yarn i18n-to-po -f $(basename "$f" .json) -l ja
   yarn i18n-to-po -f $(basename "$f" .json) -l ko
 done
@@ -13,7 +14,8 @@ for d in */ ; do
   if [ -d "$d/locales/en" ]
   then
     for f in $d/locales/en/* ; do
-      yarn i18n-to-po -p "$d" -f $(basename "$f" .json) -l zh
+      yarn i18n-to-po -p "$d" -f $(basename "$f" .json) -l zh-cn
+      yarn i18n-to-po -p "$d" -f $(basename "$f" .json) -l zh-tw
       yarn i18n-to-po -p "$d" -f $(basename "$f" .json) -l ja
       yarn i18n-to-po -p "$d" -f $(basename "$f" .json) -l ko
     done
