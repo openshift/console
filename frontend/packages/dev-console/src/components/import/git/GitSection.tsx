ad47f2e7311883e8b50f2f94d621b31da1418375
Mohammed Saud <md.saud020@gmail.com>
2021-01-15 20:12:15 +0530

    avoid setting application name in edit flow

--- frontend/packages/dev-console/src/components/import/git/GitSection.tsx
+++ frontend/packages/dev-console/src/components/import/git/GitSection.tsx
@@ -91,6 +91,7 @@ const GitSection: React.FC<GitSectionProps> = ({
 
       gitRepoName && !values.name && setFieldValue('name', gitRepoName);
       gitRepoName &&
+        values.formType !== 'edit' &&
         !values.application.name &&
         values.application.selectedKey !== UNASSIGNED_KEY &&
         setFieldValue('application.name', `${gitRepoName}-app`);
@@ -118,6 +119,7 @@ const GitSection: React.FC<GitSectionProps> = ({
       setFieldValue,
       values.application.name,
       values.application.selectedKey,
+      values.formType,
       values.git.type,
       values.name,
     ],
@@ -165,6 +167,7 @@ const GitSection: React.FC<GitSectionProps> = ({
     const gitRepoName = detectGitRepoName(url);
     values.formType !== 'edit' && gitRepoName && setFieldValue('name', gitRepoName);
     gitRepoName &&
+      values.formType !== 'edit' &&
       !values.application.name &&
       values.application.selectedKey !== UNASSIGNED_KEY &&
       setFieldValue('application.name', `${gitRepoName}-app`);
