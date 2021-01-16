4160e425cd6f16ec041fa45850b38b5ce04ca23c
Wojciech Trocki <wtrocki@redhat.com>
2021-01-16 16:34:29 +0000

    fix: example usage of the k8 api

--- frontend/packages/rhoas-plugin/src/components/rhosak-page/ManagedKafkas.tsx
+++ frontend/packages/rhoas-plugin/src/components/rhosak-page/ManagedKafkas.tsx
@@ -7,6 +7,48 @@ import { Button, ActionList, ActionListItem } from '@patternfly/react-core';
 import { PageBody } from '@console/shared';
 
 const ManagedKafkas = () => {
+
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const existingSecret = await k8sGet(SecretModel.kind, "testsomesecret", "default", {})
+    console.log(existingSecret)
+
+    if (existingSecret) {
+      return;
+    }
+
+    const secret = {
+      apiVersion: SecretModel.apiVersion,
+      kind: SecretModel.kind,
+      metadata: {
+        name: 'testsomesecret',
+        namespace: "default",
+      },
+      stringData: {
+        accessToken: "hardcoded"
+      },
+      type: 'Opaque',
+    };
+
+    const mkRequest = {
+      apiVersion: ManagedKafkaRequestModel.apiVersion,
+      kind: ManagedKafkaRequestModel.kind,
+      metadata: {
+        name: 'KafkaRequest-' + new Date().getTime(),
+        // TODO - namespace based url
+        namespace: "default",
+      },
+      spec: {
+        accessTokenSecretName: "testsomesecret",
+      },
+    };
+
+    console.log(await k8sCreate(SecretModel, secret))
+    console.log(await k8sCreate(ManagedKafkaRequestModel, mkRequest));
+  }
+
+
   return (
     <>
       <NamespacedPage variant={NamespacedPageVariants.light} hideApplications>
