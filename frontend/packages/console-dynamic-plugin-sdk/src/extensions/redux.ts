63d0bdcfa8f62cae1a68817d5f66e94a582f0e57
Gilad Lekner <gilad215@gmail.com>
2021-01-18 18:44:12 +0200

    adding reducer extension types to dynamic-plugin-sdk

---
+++ frontend/packages/console-dynamic-plugin-sdk/src/extensions/redux.ts
@@ -0,0 +1,31 @@
+import { Reducer } from 'redux';
+import { Extension } from '@console/plugin-sdk/src/typings/base';
+import { CodeRef, EncodedCodeRef, UpdateExtensionProperties } from '../types';
+
+export namespace ExtensionProperties {
+  export type ReduxReducer = {
+    /** The key to represent the reducer-managed substate within the Redux state object. */
+    scope: string;
+    /** The reducer function, operating on the reducer-managed substate. */
+    reducer: EncodedCodeRef;
+  };
+  export type ReduxReducerCodeRefs = {
+    reducer: CodeRef<Reducer>;
+  };
+}
+
+// Extension types
+
+export type ReduxReducer = Extension<ExtensionProperties.ReduxReducer> & {
+  type: 'console.redux-reducer';
+};
+
+export type ResolvedReduxReducer = UpdateExtensionProperties<
+  ReduxReducer,
+  ExtensionProperties.ReduxReducerCodeRefs
+>;
+
+// Type guards
+
+export const isReduxReducer = (e: Extension): e is ResolvedReduxReducer =>
+  e.type === 'console.redux-reducer';
