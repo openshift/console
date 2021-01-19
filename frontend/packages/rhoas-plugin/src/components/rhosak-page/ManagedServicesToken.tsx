9552f1e81cf9dfc077d34a4ea010b63933b396df
Wojciech Trocki <wtrocki@redhat.com>
2021-01-19 11:34:58 +0000

    fix: create token page

---
+++ frontend/packages/rhoas-plugin/src/components/rhosak-page/ManagedServicesToken.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { ManagedKafkaRequestModel } from '../../models';
+import { SecretModel } from '@console/internal/models';
+import {
+  k8sGet,
+  k8sCreate,
+  k8sWatch
+} from '@console/internal/module/k8s';
+
+// To be clarified if we watch for resource on second page
+// import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
+import { useK8sGet } from '@console/internal/components/utils/k8s-get-hook';
+
+import {
+  FormFooter,
+  FlexForm,
+  FormHeader,
+} from '@console/shared';
+
+
+const ManagedServicesToken = () => {
+
+  // TODO change namespace from URL (args)
+  const namespace = "default";
+  const secretName = "MyLittleSecret"
+  const accessToken = "notrelevantyet"
+
+  const [data, loaded, loadError] = useK8sGet(SecretModel, secretName, namespace)
+
+  if (loaded) {
+    return "Loading"
+  }
+
+  console.log(data, loadError);
+
+  if (data) {
+    return "Token already exist"
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const existingSecret = await k8sGet(SecretModel.kind, secretName, namespace, {})
+    console.log(existingSecret)
+
+    if (existingSecret) {
+      return;
+    }
+
+    const secret = {
+      apiVersion: SecretModel.apiVersion,
+      kind: SecretModel.kind,
+      metadata: {
+        name: secretName,
+        namespace
+      },
+      stringData: {
+        accessToken
+      },
+      type: 'Opaque',
+    };
+
+    const mkRequest = {
+      apiVersion: ManagedKafkaRequestModel.apiVersion,
+      kind: ManagedKafkaRequestModel.kind,
+      metadata: {
+        // TODO better name generation
+        name: 'KafkaRequest-' + new Date().getTime(),
+        namespace
+      },
+      spec: {
+        accessTokenSecretName: secretName,
+      },
+    };
+
+    // TODO proper handling for create
+    console.log(await k8sCreate(SecretModel, secret))
+    console.log(await k8sCreate(ManagedKafkaRequestModel, mkRequest));
+    // TODO This is for tesing and should not be on this page
+    k8sWatch(ManagedKafkaRequestModel.kind, {}).onmessage((msg) => {
+      console.log("resource updated", msg)
+    })
+  }
+
+  return (
+    <>
+      Managed Managed Kafka Access token.
+      {/*  TODO Add environment abstraction */}
+      Go to https://qaprodauth.cloud.redhat.com/openshift/token to retrieve token
+
+      <FlexForm onSubmit={onSubmit}>
+        <FormHeader title="Access token" helpText="token help" marginBottom="lg" />
+        Provide accessTokenSecretName
+        <input id="token" type="text"></input>
+        <FormFooter
+          isSubmitting={false}
+          errorMessage=""
+          submitLabel="Next"
+          disableSubmit={false}
+          resetLabel="Reset"
+          sticky
+        />
+      </FlexForm>
+    </>
+  )
+
+}
+
+export default ManagedServicesToken;
