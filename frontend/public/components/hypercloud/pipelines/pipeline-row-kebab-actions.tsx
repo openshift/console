94d8d2f05c1bbc711a702c3a70b9fa749d7f22b3
hyejin_park <hyejinp128@naver.com>
2021-01-21 19:51:09 +0900

    [feat] 파이프라인 table에 마지막 실행된 파이프라인런 정보 추가

--- frontend/public/components/hypercloud/pipelines/pipeline-row-kebab-actions.tsx
+++ frontend/public/components/hypercloud/pipelines/pipeline-row-kebab-actions.tsx
@@ -3,17 +3,18 @@ import { referenceForModel } from '@console/internal/module/k8s';
 import { ResourceKebab } from '@console/internal/components/utils';
 //import { ResourceKebabWithUserLabel } from '../pipelineruns/triggered-by';
 import { getPipelineKebabActions } from '../utils/pipeline-actions';
-import { Pipeline } from '../utils/pipeline-augment';
+import { Pipeline, PipelineRun } from '../utils/pipeline-augment';
 import { PipelineModel } from '../../../models';
 import { usePipelineTriggerTemplateNames } from './utils/triggers';
 
 type PipelineRowKebabActionsProps = {
   pipeline: Pipeline;
+  pipelineRun?: PipelineRun;
 };
 
 const pipelineReference = referenceForModel(PipelineModel);
 
-const PipelineRowKebabActions: React.FC<PipelineRowKebabActionsProps> = ({ pipeline }) => {
+const PipelineRowKebabActions: React.FC<PipelineRowKebabActionsProps> = ({ pipeline, pipelineRun }) => {
   const {
     metadata: { name, namespace },
   } = pipeline;
@@ -21,7 +22,7 @@ const PipelineRowKebabActions: React.FC<PipelineRowKebabActionsProps> = ({ pipel
 
   return (
     <ResourceKebab
-      actions={getPipelineKebabActions(pipeline.latestRun, templateNames.length > 0)}
+      actions={getPipelineKebabActions(pipeline.latestRun ?? pipelineRun, templateNames.length > 0)}
       kind={pipelineReference}
       resource={pipeline}
     />
