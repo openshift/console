6754ae9b095b09b71edc1131ad49810fcd850968
jjpan <jjpan@cn.ibm.com>
2021-01-22 15:03:09 +0800

    fix UT
    remove global mock of i18n

--- frontend/__tests__/components/modals/configure-update-strategy-modal.spec.tsx
+++ frontend/__tests__/components/modals/configure-update-strategy-modal.spec.tsx
@@ -8,7 +8,13 @@ import {
 } from '@console/internal/components/modals/configure-update-strategy-modal';
 import { RadioInput } from '@console/internal/components/radio';
 
-jest.mock('i18next');
+jest.mock('react-i18next', () => {
+  const reactI18next = require.requireActual('react-i18next');
+  return {
+    ...reactI18next,
+    useTranslation: () => ({ t: (key) => key }),
+  };
+});
 
 describe(ConfigureUpdateStrategy.displayName, () => {
   let wrapper: ShallowWrapper<ConfigureUpdateStrategyProps>;
