2ebc0feefd8667fbefaeba6b40e868ab6b53c8dd
gyoungjun_lee <injxgj@gmail.com>
2021-01-25 14:36:00 +0900

    [feat]줄정리

--- frontend/public/components/ingress.jsx
+++ frontend/public/components/ingress.jsx
@@ -3,25 +3,13 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { sortable } from '@patternfly/react-table';
 import { DetailsPage, ListPage, Table, TableRow, TableData } from './factory';
-import {
-  Kebab,
-  SectionHeading,
-  LabelList,
-  ResourceKebab,
-  ResourceIcon,
-  detailsPage,
-  EmptyBox,
-  navFactory,
-  ResourceLink,
-  ResourceSummary,
-} from './utils';
+import { Kebab, SectionHeading, LabelList, ResourceKebab, ResourceIcon, detailsPage, EmptyBox, navFactory, ResourceLink, ResourceSummary } from './utils';
 
 const menuActions = Kebab.factory.common;
 
-export const ingressValidHosts = (ingress) =>
-  _.map(_.get(ingress, 'spec.rules', []), 'host').filter(_.isString);
+export const ingressValidHosts = ingress => _.map(_.get(ingress, 'spec.rules', []), 'host').filter(_.isString);
 
-const getHosts = (ingress) => {
+const getHosts = ingress => {
   const hosts = ingressValidHosts(ingress);
 
   if (hosts.length) {
@@ -36,7 +24,7 @@ const getHosts = (ingress) => {
   return <div className="text-muted">No hosts</div>;
 };
 
-const getTLSCert = (ingress) => {
+const getTLSCert = ingress => {
   if (!_.has(ingress.spec, 'tls')) {
     return (
       <div>
@@ -55,13 +43,7 @@ const getTLSCert = (ingress) => {
   );
 };
 
-const tableColumnClasses = [
-  classNames('col-md-3', 'col-sm-4', 'col-xs-6'),
-  classNames('col-md-3', 'col-sm-4', 'col-xs-6'),
-  classNames('col-md-3', 'col-sm-4', 'hidden-xs'),
-  classNames('col-md-3', 'hidden-sm', 'hidden-xs'),
-  Kebab.columnClass,
-];
+const tableColumnClasses = [classNames('col-md-3', 'col-sm-4', 'col-xs-6'), classNames('col-md-3', 'col-sm-4', 'col-xs-6'), classNames('col-md-3', 'col-sm-4', 'hidden-xs'), classNames('col-md-3', 'hidden-sm', 'hidden-xs'), Kebab.columnClass];
 
 const kind = 'Ingress';
 
@@ -103,19 +85,10 @@ const IngressTableRow = ({ obj: ingress, index, key, style }) => {
   return (
     <TableRow id={ingress.metadata.uid} index={index} trKey={key} style={style}>
       <TableData className={tableColumnClasses[0]}>
-        <ResourceLink
-          kind={kind}
-          name={ingress.metadata.name}
-          namespace={ingress.metadata.namespace}
-          title={ingress.metadata.uid}
-        />
+        <ResourceLink kind={kind} name={ingress.metadata.name} namespace={ingress.metadata.namespace} title={ingress.metadata.uid} />
       </TableData>
       <TableData className={classNames(tableColumnClasses[1], 'co-break-word')}>
-        <ResourceLink
-          kind="Namespace"
-          name={ingress.metadata.namespace}
-          title={ingress.metadata.namespace}
-        />
+        <ResourceLink kind="Namespace" name={ingress.metadata.namespace} title={ingress.metadata.namespace} />
       </TableData>
       <TableData className={tableColumnClasses[2]}>
         <LabelList kind={kind} labels={ingress.metadata.labels} />
@@ -146,13 +119,7 @@ const RulesRow = ({ rule, namespace }) => {
       <div className="col-xs-3 co-break-word">
         <div>{rule.path}</div>
       </div>
-      <div className="col-xs-3">
-        {rule.serviceName ? (
-          <ResourceLink kind="Service" name={rule.serviceName} namespace={namespace} />
-        ) : (
-          '-'
-        )}
-      </div>
+      <div className="col-xs-3">{rule.serviceName ? <ResourceLink kind="Service" name={rule.serviceName} namespace={namespace} /> : '-'}</div>
       <div className="col-xs-2">
         <div>{rule.servicePort || '-'}</div>
       </div>
@@ -160,11 +127,11 @@ const RulesRow = ({ rule, namespace }) => {
   );
 };
 
-const RulesRows = (props) => {
+const RulesRows = props => {
   const rules = [];
 
   if (_.has(props.spec, 'rules')) {
-    _.forEach(props.spec.rules, (rule) => {
+    _.forEach(props.spec.rules, rule => {
       const paths = _.get(rule, 'http.paths');
       if (_.isEmpty(paths)) {
         rules.push({
@@ -174,7 +141,7 @@ const RulesRows = (props) => {
           servicePort: _.get(props.spec, 'backend.servicePort'),
         });
       } else {
-        _.forEach(paths, (path) => {
+        _.forEach(paths, path => {
           rules.push({
             host: rule.host || '*',
             path: path.path || '*',
@@ -185,7 +152,7 @@ const RulesRows = (props) => {
       }
     });
 
-    const rows = _.map(rules, (rule) => {
+    const rows = _.map(rules, rule => {
       return <RulesRow rule={rule} key={rule.serviceName} namespace={props.namespace} />;
     });
 
@@ -206,11 +173,7 @@ const Details = ({ obj: ingress }) => (
     </div>
     <div className="co-m-pane__body">
       <SectionHeading text="Ingress Rules" />
-      <p className="co-m-pane__explanation">
-        These rules are handled by a routing layer (Ingress Controller) which is updated as the
-        rules are modified. The Ingress controller implementation defines how headers and other
-        metadata are forwarded or manipulated.
-      </p>
+      <p className="co-m-pane__explanation">These rules are handled by a routing layer (Ingress Controller) which is updated as the rules are modified. The Ingress controller implementation defines how headers and other metadata are forwarded or manipulated.</p>
       <div className="co-m-table-grid co-m-table-grid--bordered">
         <RulesHeader />
         <RulesRows spec={ingress.spec} namespace={ingress.metadata.namespace} />
@@ -219,25 +182,9 @@ const Details = ({ obj: ingress }) => (
   </>
 );
 
-const IngressesDetailsPage = (props) => (
-  <DetailsPage
-    {...props}
-    menuActions={menuActions}
-    pages={[navFactory.details(detailsPage(Details)), navFactory.editYaml()]}
-  />
-);
-const IngressesList = (props) => (
-  <Table
-    {...props}
-    aria-label="Ingresses"
-    Header={IngressTableHeader}
-    Row={IngressTableRow}
-    virtualize
-  />
-);
+const IngressesDetailsPage = props => <DetailsPage {...props} menuActions={menuActions} pages={[navFactory.details(detailsPage(Details)), navFactory.editYaml()]} />;
+const IngressesList = props => <Table {...props} aria-label="Ingresses" Header={IngressTableHeader} Row={IngressTableRow} virtualize />;
 
-const IngressesPage = (props) => (
-  <ListPage ListComponent={IngressesList} canCreate={true} {...props} />
-);
+const IngressesPage = props => <ListPage ListComponent={IngressesList} canCreate={true} {...props} />;
 
 export { IngressesList, IngressesPage, IngressesDetailsPage };
