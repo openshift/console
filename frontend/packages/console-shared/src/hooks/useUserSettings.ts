e599e50e61d3da6e5c867a7bbc930729fae08ddb
Mohammed Saud <md.saud020@gmail.com>
2021-01-25 22:41:08 +0530

    always use local fallback if CM creation fails

--- frontend/packages/console-shared/src/hooks/useUserSettings.ts
+++ frontend/packages/console-shared/src/hooks/useUserSettings.ts
@@ -84,8 +84,7 @@ export const useUserSettings = <T>(
           if (err?.response?.status === 403 || err?.response?.status === 404) {
             setFallbackLocalStorage(true);
           } else {
-            setSettings(defaultValueRef.current);
-            setLoaded(true);
+            throw err;
           }
         }
       })();
