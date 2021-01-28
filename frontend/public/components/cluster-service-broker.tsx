b285c0fe4d597cbd86afdde5ca1bca36562363cd
gyoungjun_lee <injxgj@gmail.com>
2021-01-28 15:37:38 +0900

    [feat] 줄정리

--- frontend/public/components/cluster-service-broker.tsx
+++ frontend/public/components/cluster-service-broker.tsx
@@ -2,35 +2,15 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { sortable } from '@patternfly/react-table';
 import { DetailsPage, ListPage, Table, TableRow, TableData, RowFunction } from './factory';
-import {
-  Kebab,
-  SectionHeading,
-  detailsPage,
-  navFactory,
-  ResourceLink,
-  ResourceKebab,
-  ResourceSummary,
-  StatusWithIcon,
-  Timestamp,
-  ExternalLink,
-} from './utils';
+import { Kebab, SectionHeading, detailsPage, navFactory, ResourceLink, ResourceKebab, ResourceSummary, StatusWithIcon, Timestamp, ExternalLink } from './utils';
 import { K8sResourceKind, referenceForModel } from '../module/k8s';
 import { ClusterServiceBrokerModel } from '../models';
 import { Conditions } from './conditions';
 import { ClusterServiceClassPage } from './cluster-service-class';
 
-const menuActions = [
-  ...Kebab.getExtensionsActionsForKind(ClusterServiceBrokerModel),
-  ...Kebab.factory.common,
-];
+const menuActions = [...Kebab.getExtensionsActionsForKind(ClusterServiceBrokerModel), ...Kebab.factory.common];
 
-const tableColumnClasses = [
-  classNames('col-sm-3', 'col-xs-6'),
-  classNames('col-sm-3', 'col-xs-6'),
-  classNames('col-sm-3', 'hidden-xs'),
-  classNames('col-sm-3', 'hidden-xs'),
-  Kebab.columnClass,
-];
+const tableColumnClasses = [classNames('col-sm-3', 'col-xs-6'), classNames('col-sm-3', 'col-xs-6'), classNames('col-sm-3', 'hidden-xs'), classNames('col-sm-3', 'hidden-xs'), Kebab.columnClass];
 
 const ClusterServiceBrokerTableHeader = () => {
   return [
@@ -66,19 +46,11 @@ const ClusterServiceBrokerTableHeader = () => {
 };
 ClusterServiceBrokerTableHeader.displayName = 'ClusterServiceBrokerTableHeader';
 
-const ClusterServiceBrokerTableRow: RowFunction<K8sResourceKind> = ({
-  obj: serviceBroker,
-  index,
-  key,
-  style,
-}) => {
+const ClusterServiceBrokerTableRow: RowFunction<K8sResourceKind> = ({ obj: serviceBroker, index, key, style }) => {
   return (
     <TableRow id={serviceBroker.metadata.uid} index={index} trKey={key} style={style}>
       <TableData className={tableColumnClasses[0]}>
-        <ResourceLink
-          kind={referenceForModel(ClusterServiceBrokerModel)}
-          name={serviceBroker.metadata.name}
-        />
+        <ResourceLink kind={referenceForModel(ClusterServiceBrokerModel)} name={serviceBroker.metadata.name} />
       </TableData>
       <TableData className={classNames(tableColumnClasses[1], 'co-break-word')}>
         <StatusWithIcon obj={serviceBroker} />
@@ -88,19 +60,13 @@ const ClusterServiceBrokerTableRow: RowFunction<K8sResourceKind> = ({
         <Timestamp timestamp={serviceBroker.status.lastCatalogRetrievalTime} />
       </TableData>
       <TableData className={tableColumnClasses[4]}>
-        <ResourceKebab
-          actions={menuActions}
-          kind={referenceForModel(ClusterServiceBrokerModel)}
-          resource={serviceBroker}
-        />
+        <ResourceKebab actions={menuActions} kind={referenceForModel(ClusterServiceBrokerModel)} resource={serviceBroker} />
       </TableData>
     </TableRow>
   );
 };
 
-const ClusterServiceBrokerDetails: React.SFC<ClusterServiceBrokerDetailsProps> = ({
-  obj: serviceBroker,
-}) => {
+const ClusterServiceBrokerDetails: React.SFC<ClusterServiceBrokerDetailsProps> = ({ obj: serviceBroker }) => {
   return (
     <>
       <div className="co-m-pane__body">
@@ -113,9 +79,7 @@ const ClusterServiceBrokerDetails: React.SFC<ClusterServiceBrokerDetailsProps> =
               <Timestamp timestamp={serviceBroker.status.lastCatalogRetrievalTime} />
             </dd>
             {serviceBroker.spec.insecureSkipTLSVerify && <dt>Insecure Skip TLS Verify</dt>}
-            {serviceBroker.spec.insecureSkipTLSVerify && (
-              <dd>{serviceBroker.spec.insecureSkipTLSVerify}</dd>
-            )}
+            {serviceBroker.spec.insecureSkipTLSVerify && <dd>{serviceBroker.spec.insecureSkipTLSVerify}</dd>}
           </div>
           <div className="col-md-6">
             <dl className="co-m-pane__details">
@@ -147,45 +111,11 @@ const ClusterServiceBrokerDetails: React.SFC<ClusterServiceBrokerDetailsProps> =
   );
 };
 
-const ServiceClassTabPage = ({ obj }) => (
-  <ClusterServiceClassPage
-    showTitle={false}
-    fieldSelector={`spec.clusterServiceBrokerName=${obj.metadata.name}`}
-  />
-);
-export const ClusterServiceBrokerDetailsPage: React.SFC<ClusterServiceBrokerDetailsPageProps> = (
-  props,
-) => (
-  <DetailsPage
-    {...props}
-    kind={referenceForModel(ClusterServiceBrokerModel)}
-    menuActions={menuActions}
-    pages={[
-      navFactory.details(detailsPage(ClusterServiceBrokerDetails)),
-      navFactory.editYaml(),
-      navFactory.clusterServiceClasses(ServiceClassTabPage),
-    ]}
-  />
-);
-export const ClusterServiceBrokerList: React.SFC = (props) => (
-  <Table
-    {...props}
-    aria-label="Cluster Service Brokers"
-    Header={ClusterServiceBrokerTableHeader}
-    Row={ClusterServiceBrokerTableRow}
-    virtualize
-  />
-);
+const ServiceClassTabPage = ({ obj }) => <ClusterServiceClassPage showTitle={false} fieldSelector={`spec.clusterServiceBrokerName=${obj.metadata.name}`} />;
+export const ClusterServiceBrokerDetailsPage: React.SFC<ClusterServiceBrokerDetailsPageProps> = props => <DetailsPage {...props} kind={referenceForModel(ClusterServiceBrokerModel)} menuActions={menuActions} pages={[navFactory.details(detailsPage(ClusterServiceBrokerDetails)), navFactory.editYaml(), navFactory.clusterServiceClasses(ServiceClassTabPage)]} />;
+export const ClusterServiceBrokerList: React.SFC = props => <Table {...props} aria-label="Cluster Service Brokers" Header={ClusterServiceBrokerTableHeader} Row={ClusterServiceBrokerTableRow} virtualize />;
 
-export const ClusterServiceBrokerPage: React.SFC<ClusterServiceBrokerPageProps> = (props) => (
-  <ListPage
-    {...props}
-    ListComponent={ClusterServiceBrokerList}
-    kind={referenceForModel(ClusterServiceBrokerModel)}
-    canCreate={true}
-    showTitle={false}
-  />
-);
+export const ClusterServiceBrokerPage: React.SFC<ClusterServiceBrokerPageProps> = props => <ListPage {...props} ListComponent={ClusterServiceBrokerList} kind={referenceForModel(ClusterServiceBrokerModel)} canCreate={true} showTitle={false} />;
 
 export type ClusterServiceBrokerPageProps = {
   obj: K8sResourceKind;
