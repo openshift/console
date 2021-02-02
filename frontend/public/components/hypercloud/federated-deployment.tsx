d32a8577ebc5a4b4af4ca790cdba2b219b916378
gyoungjun_lee <injxgj@gmail.com>
2021-02-02 17:33:25 +0900

    [feat] federated deployment 기획 수정

--- frontend/public/components/hypercloud/federated-deployment.tsx
+++ frontend/public/components/hypercloud/federated-deployment.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import * as classNames from 'classnames';
 import { sortable } from '@patternfly/react-table';
 
 import { AddHealthChecks, EditHealthChecks } from '@console/app/src/actions/modify-health-checks';
 import { K8sResourceKind } from '../../module/k8s';
 import { DetailsPage, ListPage, Table, TableRow, TableData, RowFunction } from '../factory';
-import { DetailsItem, Kebab, KebabAction, detailsPage, LabelList, navFactory, ResourceKebab, ResourceLink, ResourceSummary, SectionHeading, Selector } from '../utils';
+import { Kebab, KebabAction, detailsPage, LabelList, navFactory, ResourceKebab, ResourceLink, ResourceSummary, SectionHeading, Selector } from '../utils';
 import { ResourceEventStream } from '../events';
 import { FederatedDeploymentModel } from '../../models';
 
@@ -14,7 +13,7 @@ export const menuActions: KebabAction[] = [AddHealthChecks, Kebab.factory.AddSto
 
 const kind = FederatedDeploymentModel.kind;
 
-const tableColumnClasses = ['', '', classNames('pf-m-hidden', 'pf-m-visible-on-sm', 'pf-u-w-16-on-lg'), classNames('pf-m-hidden', 'pf-m-visible-on-lg'), classNames('pf-m-hidden', 'pf-m-visible-on-lg'), Kebab.columnClass];
+const tableColumnClasses = ['', '', classNames('pf-m-hidden', 'pf-m-visible-on-lg'), classNames('pf-m-hidden', 'pf-m-visible-on-lg'), Kebab.columnClass];
 
 const FederatedDeploymentTableHeader = () => {
   return [
@@ -31,26 +30,20 @@ const FederatedDeploymentTableHeader = () => {
       props: { className: tableColumnClasses[1] },
     },
     {
-      title: 'Status',
-      sortFunc: 'deploymentNumScheduled',
-      transforms: [sortable],
-      props: { className: tableColumnClasses[2] },
-    },
-    {
       title: 'Labels',
       sortField: 'metadata.labels',
       transforms: [sortable],
-      props: { className: tableColumnClasses[3] },
+      props: { className: tableColumnClasses[2] },
     },
     {
       title: 'Pod Selector',
       sortField: 'spec.selector',
       transforms: [sortable],
-      props: { className: tableColumnClasses[4] },
+      props: { className: tableColumnClasses[3] },
     },
     {
       title: '',
-      props: { className: tableColumnClasses[5] },
+      props: { className: tableColumnClasses[4] },
     },
   ];
 };
@@ -66,29 +59,17 @@ const FederatedDeploymentTableRow: RowFunction<K8sResourceKind> = ({ obj: deploy
         <ResourceLink kind="Namespace" name={deployment.metadata.namespace} title={deployment.metadata.namespace} />
       </TableData>
       <TableData className={tableColumnClasses[2]}>
-        <Link to={`/k8s/ns/${deployment.metadata.namespace}/deployments/${deployment.metadata.name}/pods`} title="pods">
-          {deployment.status.currentNumberScheduled} of {deployment.status.desiredNumberScheduled} pods
-        </Link>
-      </TableData>
-      <TableData className={tableColumnClasses[3]}>
         <LabelList kind={kind} labels={deployment.metadata.labels} />
       </TableData>
-      <TableData className={tableColumnClasses[4]}>
+      <TableData className={tableColumnClasses[3]}>
         <Selector selector={deployment.spec.selector} namespace={deployment.metadata.namespace} />
       </TableData>
-      <TableData className={tableColumnClasses[5]}>
+      <TableData className={tableColumnClasses[4]}>
         <ResourceKebab actions={menuActions} kind={kind} resource={deployment} />
       </TableData>
     </TableRow>
   );
 };
-
-export const FederatedDeploymentDetailsList: React.FC<FederatedDeploymentDetailsListProps> = ({ ds }) => (
-  <dl className="co-m-pane__details">
-    <DetailsItem label="Current Count" obj={ds} path="status.currentNumberScheduled" />
-    <DetailsItem label="Desired Count" obj={ds} path="status.desiredNumberScheduled" />
-  </dl>
-);
 
 const FederatedDeploymentDetails: React.FC<FederatedDeploymentDetailsProps> = ({ obj: deployment }) => (
   <>
@@ -98,13 +79,7 @@ const FederatedDeploymentDetails: React.FC<FederatedDeploymentDetailsProps> = ({
         <div className="col-lg-6">
           <ResourceSummary resource={deployment} showPodSelector showNodeSelector showTolerations />
         </div>
-        <div className="col-lg-6">
-          <FederatedDeploymentDetailsList ds={deployment} />
-        </div>
       </div>
-    </div>
-    <div className="co-m-pane__body">
-      <SectionHeading text="Containers" />
     </div>
   </>
 );
@@ -115,10 +90,6 @@ export const FederatedDeployments: React.FC = props => <Table {...props} aria-la
 export const FederatedDeploymentsPage: React.FC<FederatedDeploymentsPageProps> = props => <ListPage canCreate={true} ListComponent={FederatedDeployments} kind={kind} {...props} />;
 
 export const FederatedDeploymentsDetailsPage: React.FC<FederatedDeploymentsDetailsPageProps> = props => <DetailsPage {...props} kind={kind} menuActions={menuActions} pages={[details(detailsPage(FederatedDeploymentDetails)), editYaml(), events(ResourceEventStream)]} />;
-
-type FederatedDeploymentDetailsListProps = {
-  ds: K8sResourceKind;
-};
 
 type FederatedDeploymentDetailsProps = {
   obj: K8sResourceKind;
