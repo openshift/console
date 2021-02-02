31523fa2cc2dee3af90e2ac04241e4a8bb98da85
Christie Molloy <cmolloy@redhat.com>
2021-02-02 09:14:03 -0500

    get delete to work

--- frontend/packages/rhoas-plugin/src/actions/modify-kafka-connection.tsx
+++ frontend/packages/rhoas-plugin/src/actions/modify-kafka-connection.tsx
@@ -1,42 +1,12 @@
-import { deleteResourceModal } from '@console/shared';
-import { coFetchJSON } from '@console/internal/co-fetch';
-import { history } from '@console/internal/components/utils';
 import { k8sKillByName } from '@console/internal/module/k8s';
 import { NamespaceModel } from '@console/internal/models';
 
-export const deleteManagedKafkaConnection = (name: string, namespace: string, redirect?: string) => {
-  console.log('does it make it here' + name + namespace )
-
-  const deleteNamespace = async () => {
-    try {
-      await k8sKillByName(NamespaceModel, namespace);
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.warn('Could not delete node terminal debug namespace.', e);
-    }
-  };
-
+export const deleteManagedKafkaConnection = (name: string, namespace: string) => {
+  console.log('dont think we will need namespace' + namespace);
   return {
     labelKey: 'Delete Kafka Connection',
-    deleteNamespace();
-    // callback: () => {
-    //   deleteResourceModal({
-    //     blocking: true,
-    //     resourceName: 'test',
-    //     resourceType: 'Helm Release',
-    //     // t('helm-plugin~Uninstall')
-    //     actionLabelKey: 'helm-plugin~Uninstall',
-    //     redirect,
-    //     onSubmit: () => {
-    //       deleteNamespace();
-    //       // return coFetchJSON.delete(
-    //       //   `/api/helm/release?name=${releaseName}&ns=${namespace}`,
-    //       //   null,
-    //       //   null,
-    //       //   -1,
-    //       // );
-    //     },
-    //   });
-    // },
+    callback: () => {
+      k8sKillByName(NamespaceModel, name);
+    }
   };
 };
