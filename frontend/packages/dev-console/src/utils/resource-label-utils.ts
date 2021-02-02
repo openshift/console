8216ab6a3cbf20dbbf1446fd1015ec9ac353f9c6
Mohammed Saud <md.saud020@gmail.com>
2021-02-02 22:18:28 +0530

    craft git url according to git provider

--- frontend/packages/dev-console/src/utils/resource-label-utils.ts
+++ frontend/packages/dev-console/src/utils/resource-label-utils.ts
@@ -49,10 +49,9 @@ export const getAppLabels = ({
 };
 
 export const getGitAnnotations = (gitURL: string, gitRef?: string) => {
-  const ref = gitRef || 'master';
   return {
     'app.openshift.io/vcs-uri': gitURL,
-    'app.openshift.io/vcs-ref': ref,
+    'app.openshift.io/vcs-ref': gitRef || '',
   };
 };
 
