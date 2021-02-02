4db1dd1348da4c2c37cb7e42f2019f44ca7b0410
Christie Molloy <cmolloy@redhat.com>
2021-02-02 09:32:02 -0500

    update

--- frontend/packages/rhoas-plugin/src/topology/components/rhoasComponentFactory.ts
+++ frontend/packages/rhoas-plugin/src/topology/components/rhoasComponentFactory.ts
@@ -3,7 +3,6 @@ import {
   GraphElement,
   ComponentFactory,
 } from '@patternfly/react-topology';
-
 import { withEditReviewAccess } from '@console/topology/src/utils';
 import {
   createConnectorCallback,
@@ -22,31 +21,13 @@ import {
 } from '@patternfly/react-topology';
 import { kebabOptionsToMenu } from '@console/internal/components/utils';
 import KafkaNode from './KafkaNode';
-// import { K8sResourceKind, modelFor, referenceFor } from '@console/internal/module/k8s';
-// import { KebabOption } from '@console/internal/components/utils';
 import {
   Node,
 } from '@patternfly/react-topology';
-// import { getResource } from '@console/topology/src/utils';
-// import { ModifyApplication } from '@console/topology/src/actions';
 import { MANAGED_KAFKA_TOPOLOGY_TYPE } from '../rhoas-topology-plugin';
 import { rhoasActions } from '../actions/rhoasActions';
 
-// export const rhoasActions = (
-//   contextMenuResource: K8sResourceKind
-// ): KebabOption[] => {
-//   if (!contextMenuResource) {
-//     return null;
-//   }
-
-//   const model = modelFor(referenceFor(contextMenuResource));
-//   return [
-//     ModifyApplication(model, contextMenuResource)
-//   ];
-// };
-
 export const rhoasContextMenu = (element: Node) => {
-  console.log('what is element' + element);
   return createMenuItems(kebabOptionsToMenu(rhoasActions(element)));
 };
 
