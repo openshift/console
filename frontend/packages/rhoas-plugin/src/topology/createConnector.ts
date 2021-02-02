3bcc65942d30cf4ba877a6e94a1226114880fa5e
Wojciech Trocki <wtrocki@redhat.com>
2021-02-02 17:34:09 +0000

    fix: hacking topology

---
+++ frontend/packages/rhoas-plugin/src/topology/createConnector.ts
@@ -0,0 +1,22 @@
+import { createServiceBinding } from "@console/topology/src/operators/actions/serviceBindings";
+import { Node } from '@patternfly/react-topology';
+
+export const getCreateConnector = (createHints: string[]) => {
+  if (
+    createHints
+    // THis should be there but..
+    // createHints.includes('createServiceBinding') &&
+    // !createHints.includes('operator-workload')
+  ) {
+    return createServiceBindingConnection;
+  }
+  return null;
+};
+
+
+const createServiceBindingConnection = (source: Node, target: Node) => {
+  const sourceResource = source.getData().resource || source.getData().resources?.obj;
+  const targetResource = target.getData().resource || target.getData().resources?.obj;
+
+  return createServiceBinding(sourceResource, targetResource).then(() => null);
+};
--- frontend/packages/rhoas-plugin/src/topology/rhoas-topology-plugin.tsx
+++ frontend/packages/rhoas-plugin/src/topology/rhoas-topology-plugin.tsx
@@ -1,16 +1,20 @@
 import { Plugin } from '@console/plugin-sdk';
 import {
   TopologyComponentFactory,
+  TopologyCreateConnector,
   TopologyDataModelFactory,
 } from '@console/topology/src/extensions/topology';
 import { getRhoasComponentFactory, getRhoasTopologyDataModel } from './index';
 import { WatchK8sResources } from 'public/components/utils/k8s-watch-hook';
 import { ManagedKafkaConnectionModel } from '../models';
 import { referenceForModel } from '@console/internal/module/k8s';
+import { ALLOW_SERVICE_BINDING_FLAG } from '@console/topology/src/const';
+import { getExecutableCodeRef } from '@console/dynamic-plugin-sdk/src/coderefs/coderef-utils';
 
 export type TopologyConsumedExtensions =
   | TopologyComponentFactory
   | TopologyDataModelFactory
+  | TopologyCreateConnector
 
 
 export const MANAGED_KAFKA_TOPOLOGY_TYPE = ManagedKafkaConnectionModel.kind
@@ -41,6 +45,18 @@ export const rhoasTopologyPlugin: Plugin<TopologyConsumedExtensions> = [
       getDataModel: getRhoasTopologyDataModel,
       resources: getRhoasWatchedResources,
       workloadKeys: ['kafkaConnections']
+    }
+  },
+  {
+    type: 'Topology/CreateConnector',
+    properties: {
+      getCreateConnector: getExecutableCodeRef(() =>
+        import('./createConnector' /* webpackChunkName: "operators-service-bindings" */).then(
+          (m) => m.getCreateConnector,
+        )),
     },
-  }
+    flags: {
+      required: [ALLOW_SERVICE_BINDING_FLAG],
+    }
+  },
 ];
