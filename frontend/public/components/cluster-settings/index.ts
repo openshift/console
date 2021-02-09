e6785c06dd02a23953df3c7a86c5e30e64841b5b
Rohit Rai <rohitkrai03@gmail.com>
2021-02-09 22:53:17 +0530

    Use dry run options directly to remove bad dependency

--- frontend/public/components/cluster-settings/index.ts
+++ frontend/public/components/cluster-settings/index.ts
@@ -3,7 +3,6 @@ import * as _ from 'lodash-es';
 import { OAuthModel } from '../../models';
 import { IdentityProvider, k8sGet, k8sPatch, OAuthKind } from '../../module/k8s';
 import { history, resourcePathFromModel } from '../utils';
-import { dryRunOpt } from '@console/dev-console/src/utils/shared-submit-utils';
 
 // The name of the cluster-scoped OAuth configuration resource.
 const OAUTH_RESOURCE_NAME = 'cluster';
@@ -18,7 +17,7 @@ export const addIDP = (
   const patch = _.isEmpty(oauth.spec.identityProviders)
     ? { op: 'add', path: '/spec/identityProviders', value: [idp] }
     : { op: 'add', path: '/spec/identityProviders/-', value: idp };
-  return k8sPatch(OAuthModel, oauth, [patch], dryRun ? dryRunOpt : {});
+  return k8sPatch(OAuthModel, oauth, [patch], dryRun ? { queryParams: { dryRun: 'All' } } : {});
 };
 
 export const redirectToOAuthPage = () => {
