bffb8f4f5574be5bae4c72793228b06de990fd59
hyejin_park <hyejinp128@naver.com>
2021-02-10 14:48:39 +0900

    [feat] invite member modal에 수정모드 추가

--- frontend/public/components/hypercloud/modals/invite-member-modal.tsx
+++ frontend/public/components/hypercloud/modals/invite-member-modal.tsx
@@ -14,14 +14,16 @@ import { RadioGroup } from '@console/internal/components/radio';
 import { coFetchJSON } from '../../../co-fetch';
 import { getId } from '../../../hypercloud/auth';
 
-const radioItems = [
+const radioItems = (disabled:boolean) => [
   {
     title: 'User',
     value: 'User',
+    disabled
   },
   {
     title: 'User Group',
     value: 'Group',
+    disabled
   },
 ];
 
@@ -32,10 +34,12 @@ const dropdownItems = {
 };
 
 export const InviteMemberModal = withHandlePromise((props: InviteMemberModalProps) => {
-  const [member, setMember] = React.useState('');
-  const [type, setType] = React.useState('User');
-  const [role, setRole] = React.useState('guest');
+  const [member, setMember] = React.useState(props.member ?? '');
+  const [type, setType] = React.useState(props.type ?? 'User');
+  const [role, setRole] = React.useState(props.role ?? 'guest');
   const [errorMsg, setError] = React.useState('')
+
+  const isUpdate = props.requestType === 'update';
 
   const onChangeText = (e: any) => {
     setMember(e.target.value);
@@ -44,12 +48,12 @@ export const InviteMemberModal = withHandlePromise((props: InviteMemberModalProp
   const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     // Append to an existing array, but handle the special case when the array is null.
-    coFetchJSON(`/api/multi-hypercloud/cluster/member?userId=${getId()}&cluster=${props.clusterName}&target${type}=${member}&remoteRole=${role}`, 'POST')
+    coFetchJSON(`/api/multi-hypercloud/cluster/member?userId=${getId()}&cluster=${props.clusterName}&target${type}=${member}&remoteRole=${role}`, isUpdate ? 'PUT' : 'POST')
       .then((res) => {
         props.close();
       })
       .catch((err) => {
-        setError("Fail to invite member(s). " + err);
+        setError("Fail to invite/update member(s). " + err);
       })
   };
 
@@ -62,11 +66,11 @@ export const InviteMemberModal = withHandlePromise((props: InviteMemberModalProp
             <RadioGroup
               id="type"
               currentValue={type}
-              items={radioItems}
+              items={radioItems(isUpdate)}
               onChange={({ currentTarget }) => setType(currentTarget.value)}
               inline
             />
-            <input className="pf-c-form-control" id="user" name="members" placeholder="Insert User / User Group Name" value={member} onChange={onChangeText} required />
+            <input className="pf-c-form-control" id="user" name="members" placeholder="Insert User / User Group Name" value={member} onChange={onChangeText} required disabled={isUpdate} />
           </div>
         </Section>
         <Section label="Role" id="role" isRequired={true}>
@@ -76,7 +80,7 @@ export const InviteMemberModal = withHandlePromise((props: InviteMemberModalProp
       <ModalSubmitFooter
         errorMessage={errorMsg}
         inProgress={props.inProgress}
-        submitText="Invite"
+        submitText={isUpdate ? "Update" : "Invite"}
         cancel={props.cancel}
       />
     </form>
@@ -87,5 +91,9 @@ export const inviteMemberModal = createModalLauncher(InviteMemberModal);
 
 export type InviteMemberModalProps = {
   clusterName: string;
+  type: string;
+  member: string;
+  role: string;
+  requestType: string;
 } & ModalComponentProps &
   HandlePromiseProps;
