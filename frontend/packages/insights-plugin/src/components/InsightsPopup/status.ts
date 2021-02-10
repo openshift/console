cba794302c611fed11efa65c3fd86470070ae612
Rebecca Alpert <ralpert@redhat.com>
2021-02-10 12:56:24 -0500

    Bug 1927465: i18n Dashboard content
    Some plugins contribute Dashboard content to the homepage. These values were not internationalized. I updated this info so the popovers and text can be translated.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1927465

--- frontend/packages/insights-plugin/src/components/InsightsPopup/status.ts
+++ frontend/packages/insights-plugin/src/components/InsightsPopup/status.ts
@@ -1,5 +1,7 @@
 import * as _ from 'lodash';
 import { PrometheusHealthHandler, SubsystemHealth } from '@console/plugin-sdk';
+import i18next from 'i18next';
+
 import { HealthState } from '@console/shared/src/components/dashboard/status-card/states';
 import { PrometheusResponse } from '@console/internal/components/graphs';
 import { mapMetrics, isError, isWaitingOrDisabled } from './mappers';
@@ -11,7 +13,7 @@ export const getClusterInsightsComponentStatus = (
   if (error) {
     return {
       state: HealthState.NOT_AVAILABLE,
-      message: 'Not available',
+      message: i18next.t('insights-plugin~Not available'),
     };
   }
   if (!response) {
@@ -20,16 +22,19 @@ export const getClusterInsightsComponentStatus = (
   const values = mapMetrics(response);
 
   if (isError(values)) {
-    return { state: HealthState.ERROR, message: 'Not available' };
+    return { state: HealthState.ERROR, message: i18next.t('insights-plugin~Not available') };
   }
 
   if (!isError(values) && isWaitingOrDisabled(values)) {
-    return { state: HealthState.UNKNOWN, message: 'Not available' };
+    return { state: HealthState.UNKNOWN, message: i18next.t('insights-plugin~Not available') };
   }
 
   // Insights Operator has sent rules results
   const issuesNumber = Object.values(values).reduce((acc, cur) => acc + cur, 0);
-  const issueStr = `${issuesNumber} ${issuesNumber === 1 ? 'issue' : 'issues'} found`;
+  const issueStr =
+    issuesNumber === 1
+      ? i18next.t('insights-plugin~{{issuesNumber}} issue found', { issuesNumber })
+      : i18next.t('insights-plugin~{{issuesNumber}} issues found', { issuesNumber });
   if (values.critical > 0) {
     return { state: HealthState.ERROR, message: issueStr };
   }
