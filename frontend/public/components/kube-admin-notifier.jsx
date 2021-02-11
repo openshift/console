27da44be6f797532190e73428ebc4428cd93b289
Rebecca Alpert <ralpert@redhat.com>
2021-02-11 14:03:55 -0500

    Bug 1927898: i18n Admin Notifier
    Internationalized notifier text.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1927898

--- frontend/public/components/kube-admin-notifier.jsx
+++ frontend/public/components/kube-admin-notifier.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as _ from 'lodash-es';
+import { useTranslation, Trans } from 'react-i18next';
 
 import { KUBE_ADMIN_USERNAME } from '@console/shared';
 import { OAuthModel } from '../models';
@@ -11,14 +12,17 @@ import { resourcePathFromModel } from './utils/resource-link';
 const oAuthResourcePath = resourcePathFromModel(OAuthModel, 'cluster');
 
 export const KubeAdminNotifier = connect(userStateToProps)(({ user }) => {
+  const { t } = useTranslation();
   const username = _.get(user, 'metadata.name');
   return username === KUBE_ADMIN_USERNAME ? (
     <div className="co-global-notification">
       <div className="co-global-notification__content">
         <p className="co-global-notification__text">
-          You are logged in as a temporary administrative user. Update the{' '}
-          <Link to={oAuthResourcePath}>cluster OAuth configuration</Link> to allow others to
-          log&nbsp;in.
+          <Trans t={t} ns="public">
+            You are logged in as a temporary administrative user. Update the{' '}
+            <Link to={oAuthResourcePath}>cluster OAuth configuration</Link> to allow others to log
+            in.
+          </Trans>
         </p>
       </div>
     </div>
