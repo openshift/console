5cc87a0f41d79bc7cf73dddffa52a08ed5fcf349
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2021-02-12 05:55:54 +0530

    Bug 1932272: Fix KMS validation error for 4.7 release
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- frontend/packages/ceph-storage-plugin/src/components/ocs-storage-class-form/ocs-storage-class-form.tsx
+++ frontend/packages/ceph-storage-plugin/src/components/ocs-storage-class-form/ocs-storage-class-form.tsx
@@ -46,7 +46,13 @@ import { POOL_STATE } from '../../constants/storage-pool-const';
 import { KMSConfigure } from '../kms-config/kms-config';
 import { scReducer, scInitialState, SCActionType } from '../../utils/storage-pool';
 import { KMSConfig, KMSConfigMap } from '../ocs-install/types';
-import { createKmsResources, setEncryptionDispatch, parseURL } from '../kms-config/utils';
+import {
+  createKmsResources,
+  setEncryptionDispatch,
+  parseURL,
+  scKmsConfigValidation,
+  getPort,
+} from '../kms-config/utils';
 import './ocs-storage-class-form.scss';
 import { GUARDED_FEATURES } from '../../features';
 
@@ -296,6 +302,7 @@ export const StorageClassEncryption: React.FC<ProvisionerProps> = ({ onParamChan
       const kmsData = JSON.parse(csiConfigMap?.data[serviceNames[serviceNames.length - 1]]);
       setCurrentKMS(kmsData);
       const url = parseURL(kmsData.VAULT_ADDR);
+      const port = getPort(url);
       const kmsObj: KMSConfig = {
         name: {
           value: kmsData.KMS_SERVICE_NAME,
@@ -306,7 +313,7 @@ export const StorageClassEncryption: React.FC<ProvisionerProps> = ({ onParamChan
           valid: true,
         },
         port: {
-          value: url.port,
+          value: port,
           valid: true,
         },
         backend: kmsData.VAULT_BACKEND_PATH,
@@ -328,14 +335,9 @@ export const StorageClassEncryption: React.FC<ProvisionerProps> = ({ onParamChan
   }, [csiConfigMap, editKMS, isKmsSupported]);
 
   React.useEffect(() => {
-    if (isKmsSupported && editKMS) {
-      if (state.kms.name.valid && state.kms.address.valid && state.kms.port.valid) {
-        setValidSave(true);
-      } else {
-        setValidSave(false);
-      }
-      setEncryptionDispatch(SCActionType.SET_KMS_ENCRYPTION, '', dispatch, state.kms);
-    }
+    if (isKmsSupported && (editKMS || !_.isEqual(state.kms, scInitialState.kms)))
+      scKmsConfigValidation(state.kms) ? setValidSave(true) : setValidSave(false);
+    else setValidSave(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     state.kms.name.value,
@@ -344,6 +346,8 @@ export const StorageClassEncryption: React.FC<ProvisionerProps> = ({ onParamChan
     state.kms.name.valid,
     state.kms.address.valid,
     state.kms.port.valid,
+    editKMS,
+    csiConfigMapLoaded,
     isKmsSupported,
   ]);
 
