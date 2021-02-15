2cb4e375f5ae08c07cc1c9edeb698a175897a37c
gyoungjun_lee <injxgj@gmail.com>
2021-02-15 18:38:09 +0900

    [bugfix] deployment form에서 resource 안보이던 버그 수정

--- frontend/public/models/hypercloud/structural-schema-template.ts
+++ frontend/public/models/hypercloud/structural-schema-template.ts
@@ -3087,29 +3087,25 @@ metadata:
                             limits: {
                               description: 'Limits describes the maximum amount of compute resources allowed. More info: https://kubernetes.io/docs/concepts/configuration/manage-compute-resources-container/',
                               type: 'object',
-                              additionalProperties: {
-                                oneOf: [
-                                  {
-                                    type: ['string', 'null'],
-                                  },
-                                  {
-                                    type: 'integer',
-                                  },
-                                ],
+                              properties: {
+                                cpu: {
+                                  type: ['string', 'null'],
+                                },
+                                memory: {
+                                  type: ['string', 'null'],
+                                },
                               },
                             },
                             requests: {
                               description: 'Requests describes the minimum amount of compute resources required. If Requests is omitted for a container, it defaults to Limits if that is explicitly specified, otherwise to an implementation-defined value. More info: https://kubernetes.io/docs/concepts/configuration/manage-compute-resources-container/',
                               type: 'object',
-                              additionalProperties: {
-                                oneOf: [
-                                  {
-                                    type: ['string', 'null'],
-                                  },
-                                  {
-                                    type: 'integer',
-                                  },
-                                ],
+                              properties: {
+                                cpu: {
+                                  type: ['string', 'null'],
+                                },
+                                memory: {
+                                  type: ['string', 'null'],
+                                },
                               },
                             },
                           },
