0074ec79cde94859ff10ffebc3de6767cef1e72e
Rebecca Alpert <ralpert@redhat.com>
2021-02-15 10:15:32 -0500

    Add Korean to language selector

--- frontend/public/components/modals/language-preferences-modal.tsx
+++ frontend/public/components/modals/language-preferences-modal.tsx
@@ -16,6 +16,7 @@ const LanguagePreferencesModal = (props: LanguagePreferencesModalProps) => {
   const supportedLocales = {
     en: 'English',
     zh: '中文',
+    ko: '한국어',
     ja: '日本語',
   };
   const langOptions = Object.keys(supportedLocales).map((lang) => ({
