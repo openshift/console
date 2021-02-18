dc1247f4c3807e59833dde4ee50dffbdd6224032
Vikram Raj <vikrmrj@gmail.com>
2021-02-18 17:25:08 +0530

    move route section after 'create a route to the application'  option

--- frontend/packages/dev-console/src/components/import/route/RouteCheckbox.tsx
+++ frontend/packages/dev-console/src/components/import/route/RouteCheckbox.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { CheckboxField } from '@console/shared';
+import { Resources, RouteData } from '../import-types';
+import AdvancedRouteOptions from './AdvancedRouteOptions';
 
 interface RouteCheckboxProps {
-  isDisabled?: boolean;
+  route: RouteData;
+  resources: Resources;
 }
 
-const RouteCheckbox: React.FC<RouteCheckboxProps> = ({ isDisabled }) => {
+const RouteCheckbox: React.FC<RouteCheckboxProps> = ({ route, resources }) => {
   const { t } = useTranslation();
+  const { disable, create } = route;
   return (
-    <CheckboxField
-      name="route.create"
-      label={t('devconsole~Create a Route to the Application')}
-      helpText={t('devconsole~Exposes your Application at a public URL')}
-      isDisabled={isDisabled}
-    />
+    <>
+      <CheckboxField
+        name="route.create"
+        label={t('devconsole~Create a Route to the Application')}
+        helpText={t('devconsole~Exposes your Application at a public URL')}
+        isDisabled={disable}
+      />
+      <AdvancedRouteOptions canCreateRoute={create} resources={resources} />
+    </>
   );
 };
 
