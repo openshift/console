d6df541158ae26a20926ce389ab41b20b9e475e7
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2021-02-22 15:26:34 +0530

    Fix trigger annotation

--- frontend/packages/knative-plugin/src/utils/create-knative-utils.ts
+++ frontend/packages/knative-plugin/src/utils/create-knative-utils.ts
@@ -93,6 +93,7 @@ export const getKnativeServiceDepResource = (
           ...(concurrencylimit && { containerConcurrency: concurrencylimit }),
           containers: [
             {
+              name,
               image: `${imageStreamUrl}`,
               ...(contTargetPort && {
                 ports: [
