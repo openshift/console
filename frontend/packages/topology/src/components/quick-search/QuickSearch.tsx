9ce578556ec30b109d6061541f3231093763b753
Christian Vogt <cvogt@redhat.com>
2021-03-04 13:55:11 -0500

    display full quick start prereqs in quick search

--- frontend/packages/topology/src/components/quick-search/QuickSearch.tsx
+++ frontend/packages/topology/src/components/quick-search/QuickSearch.tsx
@@ -3,12 +3,9 @@ import CatalogServiceProvider, {
   CatalogService,
 } from '@console/dev-console/src/components/catalog/service/CatalogServiceProvider';
 import QuickStartsLoader from '@console/app/src/components/quick-starts/loader/QuickStartsLoader';
+import { QuickStart } from '@console/app/src/components/quick-starts/utils/quick-start-types';
 import QuickSearchController from './QuickSearchController';
-import {
-  QuickStartContext,
-  QuickStartContextValues,
-} from '@console/app/src/components/quick-starts/utils/quick-start-context';
-import { getTransformedQuickStarts } from './utils/quick-search-utils';
+import { useTransformedQuickStarts } from './utils/quick-search-utils';
 import { QuickSearchProviders } from './utils/quick-search-types';
 
 interface QuickSearchProps {
@@ -16,45 +13,56 @@ interface QuickSearchProps {
   viewContainer?: HTMLElement;
 }
 
+const Contents: React.FC<{
+  quickStarts: QuickStart[];
+  quickStartsLoaded: boolean;
+  catalogService: CatalogService;
+} & QuickSearchProps> = ({
+  quickStarts,
+  quickStartsLoaded,
+  catalogService,
+  namespace,
+  viewContainer,
+}) => {
+  const quickStartItems = useTransformedQuickStarts(quickStarts);
+  const quickSearchProviders: QuickSearchProviders = [
+    {
+      catalogType: 'devCatalog',
+      items: catalogService.items,
+      loaded: catalogService.loaded,
+      getCatalogURL: (searchTerm: string, ns: string) => `/catalog/ns/${ns}?keyword=${searchTerm}`,
+      // t('topology~View all developer catalog items ({{itemCount, number}})')
+      catalogLinkLabel: 'topology~View all developer catalog items ({{itemCount, number}})',
+    },
+    {
+      catalogType: 'quickStarts',
+      items: quickStartItems,
+      loaded: quickStartsLoaded,
+      getCatalogURL: (searchTerm: string) => `/quickstart?keyword=${searchTerm}`,
+      // t('topology~View all quick starts ({{itemCount, number}})'
+      catalogLinkLabel: 'topology~View all quick starts ({{itemCount, number}})',
+    },
+  ];
+  return (
+    <QuickSearchController
+      quickSearchProviders={quickSearchProviders}
+      allItemsLoaded={catalogService.loaded && quickStartsLoaded}
+      namespace={namespace}
+      viewContainer={viewContainer}
+    />
+  );
+};
+
 const QuickSearch: React.FC<QuickSearchProps> = ({ namespace, viewContainer }) => {
-  const { setActiveQuickStart } = React.useContext<QuickStartContextValues>(QuickStartContext);
   return (
     <CatalogServiceProvider namespace={namespace}>
       {(catalogService: CatalogService) => (
         <QuickStartsLoader>
-          {(quickStarts, qsLoaded) => {
-            const quickStartItems = qsLoaded
-              ? getTransformedQuickStarts(quickStarts, setActiveQuickStart)
-              : [];
-            const quickSearchProviders: QuickSearchProviders = [
-              {
-                catalogType: 'devCatalog',
-                items: catalogService.items,
-                loaded: catalogService.loaded,
-                getCatalogURL: (searchTerm: string, ns: string) =>
-                  `/catalog/ns/${ns}?keyword=${searchTerm}`,
-                // t('topology~View all developer catalog items ({{itemCount, number}})')
-                catalogLinkLabel:
-                  'topology~View all developer catalog items ({{itemCount, number}})',
-              },
-              {
-                catalogType: 'quickStarts',
-                items: quickStartItems,
-                loaded: qsLoaded,
-                getCatalogURL: (searchTerm: string) => `/quickstart?keyword=${searchTerm}`,
-                // t('topology~View all quick starts ({{itemCount, number}})'
-                catalogLinkLabel: 'topology~View all quick starts ({{itemCount, number}})',
-              },
-            ];
-            return (
-              <QuickSearchController
-                quickSearchProviders={quickSearchProviders}
-                allItemsLoaded={catalogService.loaded && qsLoaded}
-                namespace={namespace}
-                viewContainer={viewContainer}
-              />
-            );
-          }}
+          {(quickStarts, quickStartsLoaded) => (
+            <Contents
+              {...{ namespace, viewContainer, catalogService, quickStarts, quickStartsLoaded }}
+            />
+          )}
         </QuickStartsLoader>
       )}
     </CatalogServiceProvider>
--- frontend/packages/topology/src/components/quick-search/utils/quick-search-utils.tsx
+++ frontend/packages/topology/src/components/quick-search/utils/quick-search-utils.tsx
@@ -1,36 +1,55 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import { TextList, TextListItem } from '@patternfly/react-core';
 import { CatalogItem } from '@console/plugin-sdk';
 import { keywordCompare } from '@console/dev-console/src/components/catalog/utils/catalog-utils';
 import { QuickStart } from '@console/app/src/components/quick-starts/utils/quick-start-types';
-import QuickStartTileDescription from '@console/app/src/components/quick-starts/catalog/QuickStartTileDescription';
+import {
+  QuickStartContext,
+  QuickStartContextValues,
+} from '@console/app/src/components/quick-starts/utils/quick-start-context';
 
 export const quickSearch = (items: CatalogItem[], query: string) => {
   return items.filter((item) => keywordCompare(query, item));
 };
 
-export const getTransformedQuickStarts = (
-  quickStarts: QuickStart[],
-  setActiveQuickStart: (quickStartId: string, totalTasks?: number) => void,
-): CatalogItem[] => {
-  return quickStarts.map((qs: QuickStart) => {
-    const description = (
-      <QuickStartTileDescription
-        description={qs.spec.description}
-        prerequisites={qs.spec.prerequisites}
-      />
-    );
-    return {
-      name: qs.spec.displayName,
-      type: 'Quick Start',
-      uid: qs.metadata.uid,
-      cta: {
-        callback: () => setActiveQuickStart(qs.metadata.name, qs.spec.tasks?.length),
-        label: 'Start',
-      },
-      icon: {
-        url: qs.spec.icon,
-      },
-      description,
-    };
-  });
+export const useTransformedQuickStarts = (quickStarts: QuickStart[]): CatalogItem[] => {
+  const { setActiveQuickStart } = React.useContext<QuickStartContextValues>(QuickStartContext);
+  const { t } = useTranslation();
+  return React.useMemo(
+    () =>
+      quickStarts.map((qs: QuickStart) => {
+        const prerequisites = qs.spec.prerequisites?.filter((p) => p);
+        const description = (
+          <>
+            <p>{qs.spec.description}</p>
+            {prerequisites?.length > 0 && (
+              <>
+                <h5>{t('topology~Prerequisites')}</h5>
+                <TextList>
+                  {prerequisites.map((prerequisite, index) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <TextListItem key={index}>{prerequisite}</TextListItem>
+                  ))}
+                </TextList>
+              </>
+            )}
+          </>
+        );
+        return {
+          name: qs.spec.displayName,
+          type: 'Quick Start',
+          uid: qs.metadata.uid,
+          cta: {
+            callback: () => setActiveQuickStart(qs.metadata.name, qs.spec.tasks?.length),
+            label: t('topology~Start'),
+          },
+          icon: {
+            url: qs.spec.icon,
+          },
+          description,
+        };
+      }),
+    [t, quickStarts, setActiveQuickStart],
+  );
 };
