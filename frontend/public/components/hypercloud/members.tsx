037a55e03eb41946b2be58a4cf299aa703b1e947
hyejin_park <hyejinp128@naver.com>
2021-03-19 18:07:13 +0900

    [feat] 엑세스 권한 부분 임시 커밋

--- frontend/public/components/hypercloud/members.tsx
+++ frontend/public/components/hypercloud/members.tsx
@@ -1,65 +1,90 @@
 import * as _ from "lodash";
 import * as React from "react";
 import * as classNames from 'classnames';
-import { Button } from '@patternfly/react-core';
-import { EmptyBox, PageHeading, SectionHeading, } from '../utils';
-import { Table, TableHeader, TableBody, sortable, SortByDirection } from '@patternfly/react-table';
-import { getId } from '../../hypercloud/auth';
+import { Button, Dropdown, DropdownToggle, DropdownItem, TextInput } from '@patternfly/react-core';
+import { EmptyBox, SectionHeading, /*Kebab,*/ } from '../utils';
+import { Table, TableHeader, TableBody, sortable, SortByDirection, IRow } from '@patternfly/react-table';
+import { CaretDownIcon, UsersIcon } from '@patternfly/react-icons';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from "i18next";
+//import { getId } from '../../hypercloud/auth';
 
-const getRowUserData = (users): RowUserData[] => {
-  const data: RowUserData[] = [];
-
-  _.forEach(users, (role, name) => {
-    data.push([name, role]);
-  });
-  return data;
-};
-
-const UsersTableHeader = [
+const testData: ITableRow[] = [
   {
-    title: 'Name',
-    transforms: [sortable],
+    cells: ['qwer', 'qwer@tmax.co.kr', 'admin'],
+    obj: { name: 'qwer', email: 'qwer@tmax.co.kr', role: 'admin', type: 'user' }
   },
   {
-    title: 'Role',
-    transforms: [sortable],
-  },
-];
-
-const getRowUserGroupData = (userGroups): RowUserGroupData[] => {
-  const data: RowUserGroupData[] = [];
-
-  _.forEach(userGroups, (role, name) => {
-    data.push([name, role]);
-  });
-  return data;
-};
-
-const UserGroupsTableHeader = [
-  {
-    title: 'Name',
-    transforms: [sortable],
+    cells: ['as as as', 'asdf@tmax.co.kr', 'developer'],
+    obj: { name: 'as as as', email: 'asdf@tmax.co.kr', role: 'developer', type: 'user' }
   },
   {
-    title: 'Role',
-    transforms: [sortable],
+    cells: [<><UsersIcon className='hc-member__group-icon' />a123 346jso</>, '', 'guest'],
+    obj: { name: 'a123 346jso', email: '', role: 'guest', type: 'group' }
+  },
+  {
+    cells: [<><UsersIcon className='hc-member__group-icon' />pojgjdrk82347598 fhsuih103 fahsjk</>, '', 'developer'],
+    obj: { name: 'pojgjdrk82347598 fhsuih103 fahsjk', email: '', role: 'developer', type: 'group' }
   },
 ];
+
+const tableColumnClasses = ['', '', classNames('pf-m-hidden', 'pf-m-visible-on-sm', 'pf-u-w-16-on-lg'), /*Kebab.columnClass*/];
+
+const MemberTableRows = (members, isUser): ITableRow[] => {
+  const data = [];
+  _.forEach(members, (role, name) => {
+    const member = { name: name, role: role, type: isUser ? 'user' : 'group', email: isUser ? name : '' };
+    data.push({
+      cells: [member.name, member.email, member.role],
+      obj: member
+    });
+  });
+  return data;
+}
+
+const MemberTableHeader = (t?: TFunction) => {
+  return [
+    {
+      title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_TABLEHEADER_2'),
+      transforms: [sortable],
+      props: { className: tableColumnClasses[0] },
+      data: 'name'
+    },
+    {
+      title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_TABLEHEADER_1'),
+      transforms: [sortable],
+      props: { className: tableColumnClasses[1] },
+      data: 'email'
+    },
+    {
+      title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_TABLEHEADER_3'),
+      transforms: [sortable],
+      props: { className: tableColumnClasses[2] },
+      data: 'role'
+    },
+    /* {
+      title: '',
+      props: { className: tableColumnClasses[3] },
+    }, */
+  ];
+};
+MemberTableHeader.displayName = 'UserTableHeader';
 
 export const UsersTable = (props) => {
-  const { clusterName, isOwner, users, heading } = props;
+  const { clusterName, isOwner, users, groups, heading } = props;
 
-  const [rows, setRows] = React.useState(getRowUserData(users));
+  const [rows, setRows] = React.useState(_.concat(testData, MemberTableRows(users, true), MemberTableRows(groups, false)));
   const [sortBy, setSortBy] = React.useState({ index: 0, direction: SortByDirection.asc });
 
   React.useEffect(() => {
-    setRows(getRowUserData(users));
-  }, [users]);
+    setRows(_.concat(testData, MemberTableRows(users, true), MemberTableRows(groups, false)));
+  }, [users, groups]);
 
-  const onSort = (_event, index, direction) => {
+  const onSort = (_event, index, direction, extraData) => {
+    const sortField = extraData.column.data;
     const sortedRows = rows.sort((a, b) => {
-      const compA = typeof a[index] === 'string' ? (a[index] as string).toLowerCase() : a[index],
-        compB = typeof b[index] === 'string' ? (b[index] as string).toLowerCase() : b[index];
+      const compA = typeof a.obj[sortField] === 'string' ? (a.obj[sortField] as string).toLowerCase() : a.obj[sortField],
+        compB = typeof b.obj[sortField] === 'string' ? (b.obj[sortField] as string).toLowerCase() : b.obj[sortField];
       return compA < compB ? -1 : compA > compB ? 1 : 0;
     });
 
@@ -71,89 +96,33 @@ export const UsersTable = (props) => {
 
   };
 
-  const userActions = [
+  const userActions = (t?: TFunction) => [
     {
-      title: 'Update User',
+      title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_ACTIONBUTTON_1'),
       onClick: (event, rowId, rowData, extra) => {
-        inviteMemberModal({ clusterName, modalClassName: 'modal-lg', type: 'User', member: rowData[0], role: rowData[1], requestType: 'update' })
+        modifyMemberModal({ clusterName, modalClassName: 'modal-lg', member: rowData.obj })
       }
     },
     {
-      title: 'Delete User',
+      title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_ACTIONBUTTON_2'),
       onClick: (event, rowId, rowData, extra) => {
-        console.log('clicked on Some action, on row: ', rowId)
-        removeMemberModal({ clusterName, modalClassName: 'modal-lg', type: 'User', member: rowData[0] })
+        removeMemberModal({ clusterName, modalClassName: 'modal-lg', member: rowData.obj })
       }
     }
-  ]
+  ];
 
+  const { t } = useTranslation();
   return (
     <div className="hc-members__users">
       {heading && <SectionHeading text={heading} />}
       {_.isEmpty(rows) ? (
         <EmptyBox label="Users" />
       ) : (
-          <Table aria-label="Users" sortBy={sortBy} onSort={onSort} cells={UsersTableHeader} rows={rows} actions={isOwner && userActions}>
-            <TableHeader />
-            <TableBody />
-          </Table>
-        )}
-    </div>
-  );
-};
-
-export const UserGroupsTable = (props) => {
-  const { clusterName, isOwner, groups, heading } = props;
-  const data: RowUserGroupData[] = getRowUserGroupData(groups);
-
-  const [rows, setRows] = React.useState(data);
-  const [sortBy, setSortBy] = React.useState({ index: 0, direction: SortByDirection.asc });
-
-  React.useEffect(() => {
-    setRows(getRowUserGroupData(groups));
-  }, [groups]);
-
-  const onSort = (_event, index, direction) => {
-    const sortedRows = rows.sort((a, b) => {
-      const compA = typeof a[index] === 'string' ? (a[index] as string).toLowerCase() : a[index],
-        compB = typeof b[index] === 'string' ? (b[index] as string).toLowerCase() : b[index];
-      return compA < compB ? -1 : compA > compB ? 1 : 0;
-    });
-
-    setSortBy({
-      index,
-      direction
-    });
-    setRows(direction === SortByDirection.asc ? sortedRows : sortedRows.reverse());
-
-  };
-
-  const userGroupActions = [
-    {
-      title: 'Update User Group',
-      onClick: (event, rowId, rowData, extra) => {
-        inviteMemberModal({ clusterName, modalClassName: 'modal-lg', type: 'Group', member: rowData[0], role: rowData[1], requestType: 'update' })
-      }
-    },
-    {
-      title: 'Delete User Group',
-      onClick: (event, rowId, rowData, extra) => {
-        removeMemberModal({ clusterName, modalClassName: 'modal-lg', type: 'Group', member: rowData[0] })
-      }
-    }
-  ]
-
-  return (
-    <div className="hc-members__user-groups">
-      {heading && <SectionHeading text={heading} />}
-      {_.isEmpty(data) ? (
-        <EmptyBox label="UserGroups" />
-      ) : (
-          <Table aria-label="UserGroups" sortBy={sortBy} onSort={onSort} cells={UserGroupsTableHeader} rows={rows} actions={isOwner && userGroupActions}>
-            <TableHeader />
-            <TableBody />
-          </Table>
-        )}
+        <Table aria-label="Users" sortBy={sortBy} onSort={onSort} cells={MemberTableHeader.bind(null, t)()} rows={rows} actions={isOwner && userActions.bind(null, t)()}>
+          <TableHeader />
+          <TableBody />
+        </Table>
+      )}
     </div>
   );
 };
@@ -161,29 +130,68 @@ export const UserGroupsTable = (props) => {
 export const inviteMemberModal = (props) =>
   import('./modals/invite-member-modal' /* webpackChunkName: "members-modal" */).then((m) => m.inviteMemberModal(props));
 
+export const modifyMemberModal = (props) =>
+  import('./modals/modify-member-modal' /* webpackChunkName: "modify-member-modal" */).then((m) => m.modifyMemberModal(props));
+
 export const removeMemberModal = (props) =>
   import('./modals/remove-member-modal' /* webpackChunkName: "remove-member-modal" */).then((m) => m.removeMemberModal(props));
 
 export const MembersPage = (props) => {
-  const isOwner = Object.keys(props.resource.status.owner)[0] === getId();
+  const [searchType, setSearchType] = React.useState('Email');
+  const [isOpen, setOpen] = React.useState(false);
+
+  const onToggle = (open: boolean) => setOpen(open);
+  const onSelect = event => {
+    const selectedName = event.target.name;
+    setSearchType(selectedName);
+    setOpen(!isOpen);
+  };
+
+  const dropdownItems = [
+    <DropdownItem key="label-action" name='Email' component="button">
+      Search By Email
+    </DropdownItem>,
+    <DropdownItem key="name-action" name='Name' component="button">
+      Search By Name
+    </DropdownItem>,
+  ];
+
+  const isOwner = true; //Object.keys(props.resource.status.owner)[0] === getId();
+  const { t } = useTranslation();
   return (
     <>
-      <PageHeading title={props.title} className={classNames('co-m-nav-title--row')}>
+      <div className='hc-members__header'>
+        <Dropdown
+          onSelect={onSelect}
+          toggle={
+            <DropdownToggle id="toggle-id" onToggle={onToggle} iconComponent={CaretDownIcon}>
+              {searchType}
+            </DropdownToggle>}
+          isOpen={isOpen}
+          dropdownItems={dropdownItems}
+        />
+        <TextInput className='hc-members__search'></TextInput>
         {isOwner &&
           <div className="co-m-primary-action">
-            <Button variant="primary" id="yaml-create" onClick={() => inviteMemberModal({ clusterName: props.resource.metadata.name, modalClassName: 'modal-lg' })}>
-              Invite Member
-          </Button>
+            <Button variant="primary" id="yaml-create" onClick={() => inviteMemberModal({ clusterName: props.resource.metadata.name, modalClassName: 'modal-lg', existMembers: props.resource.status.members, existGroups: props.resource.status.groups })}>
+              {t('MULTI:MSG_MULTI_CLUSTERS_INVITEPEOPLEPOPUP_BUTTON_1')}
+            </Button>
           </div>}
-      </PageHeading>
+      </div>
       <div className="hc-members__body">
-        <UsersTable clusterName={props.resource.metadata.name} isOwner={isOwner} users={props.resource.status.members} heading={props.userHeading} />
-        <UserGroupsTable clusterName={props.resource.metadata.name} isOwner={isOwner} groups={props.resource.status.groups} heading={props.userGroupHeading} />
+        <UsersTable clusterName={props.resource.metadata.name} isOwner={isOwner} users={props.resource.status.members} groups={props.resource.status.groups} />
       </div>
     </>
   );
 }
 
-export type RowUserData = [string, string];
+export type RowMemberData = {
+  name: string,
+  role: string,
+  type: 'user' | 'group',
+  email: string
+};
 
-export type RowUserGroupData = [string, string];
+export interface ITableRow extends IRow {
+  obj: RowMemberData
+}
---
+++ frontend/public/components/hypercloud/modals/modify-member-modal.tsx
@@ -0,0 +1,82 @@
+import * as _ from 'lodash-es';
+import * as React from 'react';
+
+import {
+  ModalBody,
+  ModalComponentProps,
+  ModalSubmitFooter,
+  ModalTitle,
+  createModalLauncher,
+} from '../../factory/modal';
+import { HandlePromiseProps, withHandlePromise } from '../../utils';
+import { Section } from '../utils/section';
+import { RadioGroup } from '@console/internal/components/radio';
+import { coFetchJSON } from '../../../co-fetch';
+import { getId } from '../../../hypercloud/auth';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
+
+const roleItems = (t?: TFunction) => [
+  {
+    title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_1'),
+    value: 'admin',
+  },
+  {
+    title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_2'),
+    value: 'developer',
+  },
+  {
+    title: t('COMMON:MSG_DETAILS_TABACCESSPERMISSIONS_RADIOBUTTON_3'),
+    value: 'guest'
+  },
+];
+
+
+export const ModifyMemberModal = withHandlePromise((props: ModifyMemberModalProps) => {
+  const [role, setRole] = React.useState(props.member.role);
+  const [errorMsg, setError] = React.useState('')
+
+  const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    coFetchJSON(`/api/multi-hypercloud/cluster/member?userId=${getId()}&cluster=${props.clusterName}&target${props.member.type}=${props.member.type === 'user' ? props.member.email : props.member.name}&remoteRole=${role}`, 'PUT')
+      .then((res) => {
+        props.close();
+      })
+      .catch((err) => {
+        setError(err);
+      })
+  };
+
+  const { t } = useTranslation();
+  return (
+    <form onSubmit={submit} name="form" className="modal-content ">
+      <ModalTitle>
+        {t('MULTI:MSG_MULTI_CLUSTERS_CHANGEPERMISSIONSPOPUP_TITLE_1')}
+      </ModalTitle>
+      <ModalBody className="modal-body">
+        <Section id='role'>
+          <RadioGroup
+            id='role'
+            currentValue={role}
+            items={roleItems.bind(null, t)()}
+            onChange={({ currentTarget }) => setRole(currentTarget.value)}
+          />
+        </Section>
+      </ModalBody>
+      <ModalSubmitFooter errorMessage={errorMsg} inProgress={props.inProgress} submitText='Save' cancelText={t('MULTI:MSG_MULTI_CLUSTERS_INVITEPEOPLEPOPUP_BUTTON_2')} cancel={props.cancel} />
+    </form>
+  );
+});
+
+export const modifyMemberModal = createModalLauncher(ModifyMemberModal);
+
+export type ModifyMemberModalProps = {
+  clusterName: string;
+  member: {
+    name: string,
+    role: string,
+    type: 'user' | 'group',
+    email: string
+  };
+} & ModalComponentProps &
+  HandlePromiseProps;
