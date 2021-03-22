7f2597616e1cd2590a5cbd2f44f0d743e21caeb4
Rebecca Alpert <ralpert@redhat.com>
2021-03-22 13:45:48 -0400

    Bug 1939753: Track and show error messages in modals
    Error messages weren't being passed to the footer in a few modals. I refactored them to catch and show the error messages.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1939753

--- frontend/__tests__/components/utils/promise-component.spec.tsx
+++ frontend/__tests__/components/utils/promise-component.spec.tsx
@@ -8,6 +8,16 @@ import {
   HandlePromiseProps,
 } from '../../../public/components/utils/promise-component';
 
+jest.mock('react-i18next', () => {
+  const reactI18next = require.requireActual('react-i18next');
+  return {
+    ...reactI18next,
+    useTranslation: () => ({ t: (key) => key }),
+  };
+});
+
+const i18nNS = 'public';
+
 describe('withHandlePromise', () => {
   type TestProps = {
     promise: Promise<any>;
@@ -49,7 +59,7 @@ describe('withHandlePromise', () => {
           .dive()
           .find('h1')
           .text(),
-      ).toEqual('An error occurred. Please try again.');
+      ).toEqual(`${i18nNS}~An error occurred. Please try again.`);
       done();
     }, 10);
   });
