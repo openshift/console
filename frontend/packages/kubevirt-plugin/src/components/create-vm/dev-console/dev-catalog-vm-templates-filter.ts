3d6fd7a2443e5c536ac087d05b3db92dd8e8b9f9
Rohit Rai <rohitkrai03@gmail.com>
2021-03-23 00:42:06 +0530

    Update the catalog item filter for vm templates

--- frontend/packages/kubevirt-plugin/src/components/create-vm/dev-console/dev-catalog-vm-templates-filter.ts
+++ frontend/packages/kubevirt-plugin/src/components/create-vm/dev-console/dev-catalog-vm-templates-filter.ts
@@ -3,10 +3,9 @@ import { CatalogItem } from '@console/dynamic-plugin-sdk';
 import { TEMPLATE_TYPE_BASE, TEMPLATE_TYPE_VM, TEMPLATE_TYPE_LABEL } from '../../../constants';
 
 // removes all Templates identified as VM templates
-const filter = (items: CatalogItem<PartialObjectMetadata>[]) =>
-  items.filter((item) => {
-    const vmTemplateLabel = item.data?.metadata?.labels?.[TEMPLATE_TYPE_LABEL];
-    return vmTemplateLabel !== TEMPLATE_TYPE_VM && vmTemplateLabel !== TEMPLATE_TYPE_BASE;
-  });
+const filter = (item: CatalogItem<PartialObjectMetadata>): boolean => {
+  const vmTemplateLabel = item.data?.metadata?.labels?.[TEMPLATE_TYPE_LABEL];
+  return vmTemplateLabel !== TEMPLATE_TYPE_VM && vmTemplateLabel !== TEMPLATE_TYPE_BASE;
+};
 
 export default filter;
