40c197990ac0e41c325cf448faddb976df963d6c
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2021-03-23 16:52:49 +0530

    fixes i18n miss in PeojectAccess

--- frontend/packages/dev-console/src/components/project-access/__tests__/ProjectAccess.spec.tsx
+++ frontend/packages/dev-console/src/components/project-access/__tests__/ProjectAccess.spec.tsx
@@ -7,6 +7,14 @@ import ProjectAccess from '../ProjectAccess';
 type ProjectAccessProps = React.ComponentProps<typeof ProjectAccess>;
 let projectAccessProps: ProjectAccessProps;
 
+jest.mock('react-i18next', () => {
+  const reactI18next = require.requireActual('react-i18next');
+  return {
+    ...reactI18next,
+    useTranslation: () => ({ t: (key) => key.split('~')[1] }),
+  };
+});
+
 describe('Project Access', () => {
   beforeEach(() => {
     projectAccessProps = {
