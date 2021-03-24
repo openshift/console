e60fe14487f02c29dafe3bedbb09397890948559
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2021-03-24 15:33:27 +0900

    [feat] ADD script for getting resource-schema json

--- build-backend.sh
+++ build-backend.sh
@@ -12,3 +12,12 @@ GIT_TAG=${SOURCE_GIT_TAG:-$(git describe --always --tags HEAD)}
 LD_FLAGS="-w -X github.com/openshift/console/pkg/version.Version=${GIT_TAG}"
 
 go build -ldflags "${LD_FLAGS}" -o bin/bridge github.com/openshift/console/cmd/bridge
+
+# get json file of k8s api resource 
+mkdir tmp
+echo "clone from git reposit of resource-schema"
+git clone https://github.com/tmax-cloud/resource-schema.git ./tmp
+echo "move json file from tmp folder"
+cp -r ./tmp/management ./tmp/network ./tmp/storage ./tmp/workload ./api
+echo "delete tmp folder"
+rm -rf ./tmp 
