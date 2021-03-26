c8b1b4a0373880975db7ce09e91e77b279240f27
hyejin_park <hyejinp128@naver.com>
2021-03-26 18:17:30 +0900

    [feat] 파이프라인 생성화면 추가

--- frontend/packages/dev-console/src/components/pipelines/pipeline-builder/PipelineBuilderHeader.tsx
+++ frontend/packages/dev-console/src/components/pipelines/pipeline-builder/PipelineBuilderHeader.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Button, Flex, FlexItem, FlexItemModifiers } from '@patternfly/react-core';
 import { warnYAML } from './modals';
-import { TechPreviewBadge } from '@console/shared';
 import { Pipeline } from '../../../utils/pipeline-augment';
 import { goToYAML } from './utils';
+import { useTranslation } from 'react-i18next';
 
 import './PipelineBuilderHeader.scss';
 
@@ -14,12 +14,13 @@ type PipelineBuilderHeaderProps = {
 
 const PipelineBuilderHeader: React.FC<PipelineBuilderHeaderProps> = (props) => {
   const { existingPipeline, namespace } = props;
-
+  const { t } = useTranslation();
   return (
     <div className="odc-pipeline-builder-header">
-      <Flex className="odc-pipeline-builder-header__content">
+      <Flex className="odc-pipeline-builder-header__content pf-m-column">
+      <Flex>
         <FlexItem breakpointMods={[{ modifier: FlexItemModifiers.grow }]}>
-          <h1 className="odc-pipeline-builder-header__title">Pipeline Builder</h1>
+          <h1 className="odc-pipeline-builder-header__title">Create Pipeline</h1>
         </FlexItem>
         <FlexItem>
           <Button
@@ -31,9 +32,10 @@ const PipelineBuilderHeader: React.FC<PipelineBuilderHeaderProps> = (props) => {
             Edit YAML
           </Button>
         </FlexItem>
-        <FlexItem>
-          <TechPreviewBadge />
-        </FlexItem>
+      </Flex>
+      <p className="help-block">
+        {t('Description')}
+      </p>
       </Flex>
       <hr />
     </div>
---
+++ frontend/packages/dev-console/src/components/pipelines/pipeline-builder/index.ts
@@ -0,0 +1,1 @@
+export { default as PipelineBuilderPage } from './PipelineBuilderPage';
