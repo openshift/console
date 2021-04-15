90af0b30fe9771aeaf7d0831e4f81e2d61687d72
karthik <karthik.jk11@gmail.com>
2021-04-15 17:42:41 +0530

    Fix start pipeline action  access review

--- frontend/packages/pipelines-plugin/src/utils/pipeline-actions.tsx
+++ frontend/packages/pipelines-plugin/src/utils/pipeline-actions.tsx
@@ -99,9 +99,8 @@ export const startPipeline: KebabAction = (
     }
   },
   accessReview: {
-    group: kind.apiGroup,
-    resource: kind.plural,
-    name: pipeline.metadata.name,
+    group: PipelineRunModel.apiGroup,
+    resource: PipelineRunModel.plural,
     namespace: pipeline.metadata.namespace,
     verb: 'create',
   },
