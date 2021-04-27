71b84067a20dbaeec0589de63c13f34899da425c
Cyril <cyril.ajieh@gmail.com>
2021-04-27 10:43:25 -0400

    user-details-displays-unrelated-subjects-rolebinding

--- frontend/public/components/factory/table-filters.ts
+++ frontend/public/components/factory/table-filters.ts
@@ -100,20 +100,10 @@ export const tableFilters: TableFilterMap = {
   'role-binding-roleRef-kind': (kind: string, binding) => binding.roleRef.kind === kind,
 
   // Filter role bindings by user name
-  'role-binding-user': (userName, binding) =>
-    _.some(binding.subjects, {
-      kind: 'User',
-      apiGroup: 'rbac.authorization.k8s.io',
-      name: userName,
-    }),
+  'role-binding-user': (userName, { subject }) => subject.name === userName,
 
   // Filter role bindings by group name
-  'role-binding-group': (groupName, binding) =>
-    _.some(binding.subjects, {
-      kind: 'Group',
-      apiGroup: 'rbac.authorization.k8s.io',
-      name: groupName,
-    }),
+  'role-binding-group': (groupName, { subject }) => subject.name === groupName,
 
   selector: (selector, obj) => {
     if (!selector || !selector.values || !selector.values.size) {
