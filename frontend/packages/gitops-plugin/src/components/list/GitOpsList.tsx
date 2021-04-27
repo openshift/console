387c8f2633d73855e2c06334dd6949c220b21cee
Regina Scott <rescott@redhat.com>
2021-04-27 17:48:01 -0400

    Convert card view to table view with filter

--- frontend/packages/gitops-plugin/src/components/list/GitOpsList.tsx
+++ frontend/packages/gitops-plugin/src/components/list/GitOpsList.tsx
@@ -1,41 +1,49 @@
 import * as React from 'react';
-import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
-import GitOpsListItem from './GitOpsListItem';
-import { Stack, StackItem, Split, SplitItem } from '@patternfly/react-core';
-import { K8sResourceKind } from '@console/internal/module/k8s';
 import { GitOpsAppGroupData } from '../utils/gitops-types';
+import GitOpsTableRow from './GitOpsTableRow';
+import GitOpsTableHeader from './GitOpsTableHeader';
 import GitOpsEmptyState from '../GitOpsEmptyState';
+import { Table, TextFilter } from '@console/internal/components/factory';
+import { fuzzyCaseInsensitive } from '@console/internal/components/factory/table-filters';
 import './GitOpsList.scss';
 
 interface GitOpsListProps {
   appGroups: GitOpsAppGroupData[];
   emptyStateMsg: string;
-  argocdLink?: K8sResourceKind;
 }
 
-const GitOpsList: React.FC<GitOpsListProps> = ({ appGroups, emptyStateMsg, argocdLink }) => {
+const GitOpsList: React.FC<GitOpsListProps> = ({ appGroups, emptyStateMsg }) => {
   const { t } = useTranslation();
+  const [textFilter, setTextFilter] = React.useState('');
+
+  const visibleItems = appGroups.filter(({ name }) => {
+    return (
+      fuzzyCaseInsensitive(textFilter, name) ||
+      name.toLowerCase().indexOf(textFilter.toLowerCase()) !== -1
+    );
+  });
+
   return (
     <div className="odc-gitops-list">
       {!emptyStateMsg ? (
-        <Stack hasGutter>
-          <StackItem>
-            <Split>
-              <SplitItem isFilled />
-              <SplitItem>
-                {t('gitops-plugin~{{count, number}} item', {
-                  count: _.size(appGroups),
-                })}
-              </SplitItem>
-            </Split>
-          </StackItem>
-          {_.map(appGroups, (appGroup) => (
-            <StackItem key={`${appGroup.name}-${appGroup.repo_url}`}>
-              <GitOpsListItem appGroup={appGroup} argocdLink={argocdLink} />
-            </StackItem>
-          ))}
-        </Stack>
+        <>
+          <div className="co-m-pane__filter-row">
+            <TextFilter
+              value={textFilter}
+              label={t('gitops-plugin~by name')}
+              onChange={(val) => setTextFilter(val)}
+            />
+          </div>
+          <Table
+            data={visibleItems}
+            aria-label={t('gitops-plugin~Environments table')}
+            Header={GitOpsTableHeader}
+            Row={GitOpsTableRow}
+            loaded={!emptyStateMsg}
+            virtualize
+          />
+        </>
       ) : (
         <GitOpsEmptyState emptyStateMsg={emptyStateMsg} />
       )}
