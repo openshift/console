d442121e48b4ccb3c8d2e2dc3b39ed485a558672
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2021-04-28 15:59:57 +0530

    adds devfile provider for catalog

--- frontend/packages/dev-console/src/components/catalog/providers/index.ts
+++ frontend/packages/dev-console/src/components/catalog/providers/index.ts
@@ -5,3 +5,5 @@ export { default as templateProvider } from './useTemplates';
 export { default as builderImageSamplesProvider } from './useBuilderImageSamples';
 
 export { default as devfileSamplesProvider } from './useDevfileSamples';
+
+export { default as devfileProvider } from './useDevfile';
---
+++ frontend/packages/dev-console/src/components/catalog/providers/useDevfile.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { TFunction } from 'i18next';
+import { useTranslation } from 'react-i18next';
+import { APIError } from '@console/shared';
+import { ExtensionHook, CatalogItem } from '@console/dynamic-plugin-sdk';
+import { coFetchJSON } from '@console/internal/co-fetch';
+import { ExternalLink } from '@console/internal/components/utils';
+import { DevfileSample } from '../../import/devfile/devfile-types';
+
+const normalizeDevfile = (devfileSamples: DevfileSample[], t: TFunction): CatalogItem[] => {
+  const normalizedDevfileSamples = devfileSamples?.map((sample) => {
+    const { name: uid, displayName, description, tags, git, icon } = sample;
+    const gitRepoUrl = Object.values(git.remotes)[0];
+    const iconUrl = icon ? `data:image/png;base64,${icon}` : '';
+    const href = `/import?importType=devfile&devfileName=${uid}&gitRepo=${gitRepoUrl}`;
+    const createLabel = t('devconsole~Create Application');
+    const type = 'Devfile';
+
+    const detailsProperties = [
+      {
+        label: t('devconsole~Sample repository'),
+        value: (
+          <ExternalLink href={gitRepoUrl} additionalClassName="co-break-all" text={gitRepoUrl} />
+        ),
+      },
+    ];
+
+    const detailsDescriptions = [
+      {
+        value: <p>{description}</p>,
+      },
+    ];
+
+    const item: CatalogItem = {
+      uid: `${type}-${uid}`,
+      type,
+      name: displayName,
+      description,
+      tags,
+      cta: {
+        label: createLabel,
+        href,
+      },
+      icon: { url: iconUrl },
+      details: {
+        properties: detailsProperties,
+        descriptions: detailsDescriptions,
+      },
+    };
+
+    return item;
+  });
+
+  return normalizedDevfileSamples;
+};
+
+const useDevfile: ExtensionHook<CatalogItem[]> = (): [CatalogItem[], boolean, any] => {
+  const [devfileSamples, setDevfileSamples] = React.useState<DevfileSample[]>([]);
+  const [loadedError, setLoadedError] = React.useState<APIError>();
+  const { t } = useTranslation();
+
+  React.useEffect(() => {
+    let mounted = true;
+    const payload = {
+      registry: 'sample-placeholder',
+    };
+    coFetchJSON
+      .put('/api/devfile/samples', payload)
+      .then((resp) => {
+        if (mounted) setDevfileSamples(resp);
+      })
+      .catch((err) => {
+        if (mounted) setLoadedError(err);
+      });
+    return () => (mounted = false);
+  }, []);
+
+  const normalizedDevfileSamples = React.useMemo(() => normalizeDevfile(devfileSamples, t), [
+    devfileSamples,
+    t,
+  ]);
+
+  const loaded = !!devfileSamples;
+
+  return [normalizedDevfileSamples, loaded, loadedError];
+};
+
+export default useDevfile;
