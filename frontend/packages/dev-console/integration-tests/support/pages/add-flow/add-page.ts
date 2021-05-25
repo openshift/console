a7f0635d8eed75e6f485c663f6dae2d5bea19681
Gajanan More <gamore@redhat.com>
2021-05-25 12:12:08 +0530

    Gherkin Script Design-(ODC-5013-Add Page Modification)

--- frontend/packages/dev-console/integration-tests/support/pages/add-flow/add-page.ts
+++ frontend/packages/dev-console/integration-tests/support/pages/add-flow/add-page.ts
@@ -10,21 +10,21 @@ export const addPage = {
     switch (card) {
       case 'Git':
       case addOptions.Git:
-        cy.byLegacyTestID('import-from-git').click();
+        cy.byLegacyTestID('odc-add-card-item-import-from-git').click();
         // Bug: 1890678 is created related to Accessibility violation - Until bug fix, below line is commented to execute the scripts in CI
         // cy.testA11y('Import from Git Page');
         detailsPage.titleShouldContain(pageTitle.Git);
         break;
       case 'Deploy Image':
       case addOptions.ContainerImage:
-        cy.byLegacyTestID('deploy-image').click();
+        cy.byLegacyTestID('odc-add-card-item-deploy-image').click();
         // Bug: 1890678 is created related to Accessibility violation - Until bug fix, below line is commented to execute the scripts in CI
         // cy.testA11y('Deploy Page');
         detailsPage.titleShouldContain(pageTitle.ContainerImage);
         break;
       case 'Import from Dockerfile':
       case addOptions.DockerFile:
-        cy.byLegacyTestID('import-from-dockerfile').click();
+        cy.byLegacyTestID('odc-add-card-item-import-from-dockerfile').click();
         // Bug: 1890678 is created related to Accessibility violation - Until bug fix, below line is commented to execute the scripts in CI
         // cy.testA11y('Import from Docker file');
         detailsPage.titleShouldContain(pageTitle.DockerFile);
@@ -32,39 +32,39 @@ export const addPage = {
       case 'Developer Catalog':
       case 'From Catalog':
       case addOptions.DeveloperCatalog:
-        cy.byLegacyTestID('dev-catalog').click();
+        cy.byLegacyTestID('odc-add-card-item-dev-catalog').click();
         app.waitForDocumentLoad();
         detailsPage.titleShouldContain(pageTitle.DeveloperCatalog);
         cy.testA11y(pageTitle.DeveloperCatalog);
         break;
       case 'Database':
       case addOptions.Database:
-        cy.byLegacyTestID('dev-catalog-databases').click();
+        cy.byLegacyTestID('odc-add-card-item-dev-catalog-databases').click();
         detailsPage.titleShouldContain(pageTitle.DeveloperCatalog);
         cy.testA11y(pageTitle.DeveloperCatalog);
         break;
       case 'Event Source':
       case addOptions.EventSource:
-        cy.byLegacyTestID('knative-event-source').click();
+        cy.byLegacyTestID('odc-add-card-item-knative-event-source').click();
         detailsPage.titleShouldContain(pageTitle.EventSource);
         // Bug: ODC 5719 is created related to Accessibility violation - Until bug fix, below line is commented to execute the scripts in CI
         // cy.testA11y(pageTitle.EventSource);
         break;
       case 'Helm Chart':
       case addOptions.HelmChart:
-        cy.byLegacyTestID('helm').click({ force: true });
+        cy.byLegacyTestID('odc-add-card-item-helm').click({ force: true });
         detailsPage.titleShouldContain(pageTitle.HelmCharts);
         cy.testA11y(pageTitle.HelmCharts);
         break;
       case 'Operator Backed':
       case addOptions.OperatorBacked:
-        cy.byLegacyTestID('operator-backed').click();
+        cy.byLegacyTestID('odc-add-card-item-operator-backed').click();
         detailsPage.titleShouldContain(pageTitle.OperatorBacked);
         cy.testA11y(pageTitle.OperatorBacked);
         break;
       case 'Pipeline':
       case addOptions.Pipeline:
-        cy.byLegacyTestID('pipeline').click();
+        cy.byLegacyTestID('odc-add-card-item-pipeline').click();
         cy.get('.odc-pipeline-builder-header__title').should(
           'have.text',
           pageTitle.PipelineBuilder,
@@ -73,24 +73,24 @@ export const addPage = {
         break;
       case 'Yaml':
       case addOptions.YAML:
-        cy.byLegacyTestID('import-yaml').click();
+        cy.byLegacyTestID('odc-add-card-item-import-yaml').click();
         cy.get('[data-mode-id="yaml"]').should('be.visible');
         cy.testA11y(pageTitle.YAML);
         break;
       case 'Channel':
       case addOptions.Channel:
-        cy.byLegacyTestID('knative-eventing-channel').click();
+        cy.byLegacyTestID('odc-add-card-item-knative-eventing-channel').click();
         detailsPage.titleShouldContain(pageTitle.Channel);
         cy.testA11y(pageTitle.Channel);
         break;
       case addOptions.DevFile:
-        cy.byLegacyTestID('import-from-devfile').click();
+        cy.byLegacyTestID('odc-add-card-item-import-from-devfile').click();
         detailsPage.titleShouldContain(pageTitle.DevFile);
         // Below line is commented due to Bug: ODC-5832
         // cy.testA11y(pageTitle.DevFile);
         break;
       case addOptions.UploadJARFile:
-        cy.byLegacyTestID('upload-jar').click();
+        cy.byLegacyTestID('odc-add-card-item-upload-jar').click();
         detailsPage.titleShouldContain(pageTitle.UploadJarFile);
         // Below line is commented due to Bug: ODC-5832
         // cy.testA11y(pageTitle.UploadJarFile);
