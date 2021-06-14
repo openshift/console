7092dea7ced11a00498fddf7e1cc21a96614b577
Allen Bai <abai@redhat.com>
2021-06-14 15:15:32 -0400

    Bug 1934604: Add RetryOnConflict wrapper around helm chart install
    Adds a retry wrapper around console helm chart installtion to workaround
    the situation where resourcequota has been modified and causes
    installation failure.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1934604
    Signed-off-by: Allen Bai <abai@redhat.com>

--- pkg/helm/actions/install_chart.go
+++ pkg/helm/actions/install_chart.go
@@ -5,38 +5,50 @@ import (
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/release"
+	"k8s.io/client-go/util/retry"
 )
 
 func InstallChart(ns, name, url string, vals map[string]interface{}, conf *action.Configuration) (*release.Release, error) {
-	cmd := action.NewInstall(conf)
+	var release *release.Release
 
-	releaseName, chartName, err := cmd.NameAndChart([]string{name, url})
-	if err != nil {
-		return nil, err
-	}
-	cmd.ReleaseName = releaseName
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		cmd := action.NewInstall(conf)
 
-	cp, err := cmd.ChartPathOptions.LocateChart(chartName, settings)
-	if err != nil {
-		return nil, err
-	}
+		releaseName, chartName, err := cmd.NameAndChart([]string{name, url})
+		if err != nil {
+			return err
+		}
+		cmd.ReleaseName = releaseName
 
-	ch, err := loader.Load(cp)
-	if err != nil {
-		return nil, err
-	}
+		cp, err := cmd.ChartPathOptions.LocateChart(chartName, settings)
+		if err != nil {
+			return err
+		}
 
-	// Add chart URL as an annotation before installation
-	if ch.Metadata == nil {
-		ch.Metadata = new(chart.Metadata)
-	}
-	if ch.Metadata.Annotations == nil {
-		ch.Metadata.Annotations = make(map[string]string)
-	}
-	ch.Metadata.Annotations["chart_url"] = url
+		ch, err := loader.Load(cp)
+		if err != nil {
+			return err
+		}
 
-	cmd.Namespace = ns
-	release, err := cmd.Run(ch, vals)
+		// Add chart URL as an annotation before installation
+		if ch.Metadata == nil {
+			ch.Metadata = new(chart.Metadata)
+		}
+		if ch.Metadata.Annotations == nil {
+			ch.Metadata.Annotations = make(map[string]string)
+		}
+		ch.Metadata.Annotations["chart_url"] = url
+
+		cmd.Namespace = ns
+		release, err = cmd.Run(ch, vals)
+
+		// Cleanup failed release before re-Run
+		if err != nil {
+			UninstallRelease(name, conf)
+		}
+		return err
+	})
+
 	if err != nil {
 		return nil, err
 	}
