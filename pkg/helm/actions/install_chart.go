ceb00dcbf660e3422daf3732490503b871dd860a
Allen Bai <abai@redhat.com>
2021-06-15 11:29:40 -0400

    Bug 1934604: Wrap around cmd.Run only and set retry steps to 2
    Signed-off-by: Allen Bai <abai@redhat.com>

--- pkg/helm/actions/install_chart.go
+++ pkg/helm/actions/install_chart.go
@@ -9,36 +9,37 @@ import (
 )
 
 func InstallChart(ns, name, url string, vals map[string]interface{}, conf *action.Configuration) (*release.Release, error) {
+	cmd := action.NewInstall(conf)
+
+	releaseName, chartName, err := cmd.NameAndChart([]string{name, url})
+	if err != nil {
+		return nil, err
+	}
+	cmd.ReleaseName = releaseName
+
+	cp, err := cmd.ChartPathOptions.LocateChart(chartName, settings)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := loader.Load(cp)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add chart URL as an annotation before installation
+	if ch.Metadata == nil {
+		ch.Metadata = new(chart.Metadata)
+	}
+	if ch.Metadata.Annotations == nil {
+		ch.Metadata.Annotations = make(map[string]string)
+	}
+	ch.Metadata.Annotations["chart_url"] = url
+
+	r := retry.DefaultRetry
+	r.Steps = 2
 	var release *release.Release
-
-	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		cmd := action.NewInstall(conf)
-
-		releaseName, chartName, err := cmd.NameAndChart([]string{name, url})
-		if err != nil {
-			return err
-		}
-		cmd.ReleaseName = releaseName
-
-		cp, err := cmd.ChartPathOptions.LocateChart(chartName, settings)
-		if err != nil {
-			return err
-		}
-
-		ch, err := loader.Load(cp)
-		if err != nil {
-			return err
-		}
-
-		// Add chart URL as an annotation before installation
-		if ch.Metadata == nil {
-			ch.Metadata = new(chart.Metadata)
-		}
-		if ch.Metadata.Annotations == nil {
-			ch.Metadata.Annotations = make(map[string]string)
-		}
-		ch.Metadata.Annotations["chart_url"] = url
-
+	err = retry.RetryOnConflict(r, func() error {
 		cmd.Namespace = ns
 		release, err = cmd.Run(ch, vals)
 
