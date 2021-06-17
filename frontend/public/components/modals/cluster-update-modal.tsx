477f9f9c2c920bca0a6139972a2a808359d84c27
Robb Hamilton <rhamilto@redhat.com>
2021-06-17 16:59:16 -0400

    CONSOLE-2152: Improve upgrade messaging when ClusterVersion Upgradeable=False

--- frontend/public/components/modals/cluster-update-modal.tsx
+++ frontend/public/components/modals/cluster-update-modal.tsx
@@ -1,14 +1,17 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Select, SelectOption } from '@patternfly/react-core';
 
 import { ClusterVersionModel } from '../../models';
-import { Dropdown, HandlePromiseProps, withHandlePromise } from '../utils';
+import { HandlePromiseProps, withHandlePromise } from '../utils';
 import {
   ClusterVersionKind,
   getAvailableClusterUpdates,
+  getConditionUpgradeableFalse,
   getDesiredClusterVersion,
   getSortedUpdates,
+  isMinorVersionNewer,
   k8sPatch,
 } from '../../module/k8s';
 import {
@@ -18,28 +21,31 @@ import {
   ModalSubmitFooter,
   ModalTitle,
 } from '../factory/modal';
+import { ClusterNotUpgradeableAlert } from '../cluster-settings/cluster-settings';
 
 const ClusterUpdateModal = withHandlePromise((props: ClusterUpdateModalProps) => {
   const { cancel, close, cv, errorMessage, handlePromise, inProgress } = props;
+  const clusterUpgradeableFalse = !!getConditionUpgradeableFalse(cv);
+  const availableSortedUpdates = getSortedUpdates(cv);
+  const currentVersion = getDesiredClusterVersion(cv);
+  const currentMinorVersionPatchUpdate = availableSortedUpdates?.find(
+    (update) => !isMinorVersionNewer(currentVersion, update.version),
+  );
   const [desiredVersion, setDesiredVersion] = React.useState(
-    getSortedUpdates(cv)[0]?.version || '',
+    (clusterUpgradeableFalse
+      ? currentMinorVersionPatchUpdate?.version
+      : availableSortedUpdates[0]?.version) || '',
   );
   const [error, setError] = React.useState(errorMessage);
-  const currentVersion = getDesiredClusterVersion(cv);
-  const availableSortedUpdates = getSortedUpdates(cv);
-  const dropdownItems = _.reduce(
-    availableSortedUpdates,
-    (acc, { version }) => {
-      acc[version] = version;
-      return acc;
-    },
-    {},
-  );
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onToggle = () => setIsOpen(!isOpen);
+  const onSelect = (event, selection) => {
+    event.preventDefault();
+    setDesiredVersion(selection);
+    setIsOpen(!isOpen);
+  };
   const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    if (!desiredVersion) {
-      return;
-    }
     const available = getAvailableClusterUpdates(cv);
     const desired = _.find(available, { version: desiredVersion });
     if (!desired) {
@@ -54,26 +60,38 @@ const ClusterUpdateModal = withHandlePromise((props: ClusterUpdateModalProps) =>
     const patch = [{ op: 'add', path: '/spec/desiredUpdate', value: desired }];
     return handlePromise(k8sPatch(ClusterVersionModel, cv, patch), close);
   };
+  const options = availableSortedUpdates.map(({ version }) => {
+    return (
+      <SelectOption
+        key={version}
+        value={version}
+        isDisabled={clusterUpgradeableFalse && isMinorVersionNewer(currentVersion, version)}
+      />
+    );
+  });
   const { t } = useTranslation();
 
   return (
     <form onSubmit={submit} name="form" className="modal-content modal-content--no-inner-scroll">
       <ModalTitle>{t('public~Update cluster')}</ModalTitle>
       <ModalBody>
+        {clusterUpgradeableFalse && <ClusterNotUpgradeableAlert cv={cv} />}
         <div className="form-group">
           <label>{t('public~Current version')}</label>
           <p>{currentVersion}</p>
         </div>
         <div className="form-group">
-          <label htmlFor="version_dropdown">{t('public~Select new version')}</label>
-          <Dropdown
-            className="cluster-update-modal__dropdown"
-            id="version_dropdown"
-            items={dropdownItems}
-            onChange={(newDesiredVersion: string) => setDesiredVersion(newDesiredVersion)}
-            selectedKey={desiredVersion}
-            title={t('public~Select version')}
-          />
+          <label id="version-label">{t('public~Select new version')}</label>
+          <Select
+            aria-labelledby="version-label"
+            onToggle={onToggle}
+            onSelect={onSelect}
+            selections={desiredVersion}
+            isOpen={isOpen}
+            isDisabled={clusterUpgradeableFalse && !currentMinorVersionPatchUpdate}
+          >
+            {options}
+          </Select>
         </div>
       </ModalBody>
       <ModalSubmitFooter
@@ -82,6 +100,7 @@ const ClusterUpdateModal = withHandlePromise((props: ClusterUpdateModalProps) =>
         submitText={t('public~Update')}
         cancelText={t('public~Cancel')}
         cancel={cancel}
+        submitDisabled={!desiredVersion}
       />
     </form>
   );
