a87a7b1bd3a0a195e50e774bca8e89fda0e24e75
Jon Jackson <jonjacks@redhat.com>
2021-06-22 15:27:55 -0400

    Fix 1973524: Fix edge case for "managed by" links
    In the 'managed by' link components and helpers, account for the edge case where a resource has a CSV as an ownerReference.

--- frontend/public/module/k8s/managed-by.ts
+++ frontend/public/module/k8s/managed-by.ts
@@ -1,23 +1,32 @@
-import { groupVersionFor, K8sResourceCommon, OwnerReference } from './';
+import { K8sResourceCommon, OwnerReference } from './';
 import { ClusterServiceVersionKind } from '@console/operator-lifecycle-manager';
+import { referenceForGroupVersionKind, referenceForOwnerRef, referenceFor } from './k8s';
 
-const isOwnedByOperator = (csv: ClusterServiceVersionKind, owner: OwnerReference) => {
-  const { group } = groupVersionFor(owner.apiVersion);
-  return csv.spec?.customresourcedefinitions?.owned?.some((owned) => {
-    const ownedGroup = owned.name.substring(owned.name.indexOf('.') + 1);
-    return owned.kind === owner.kind && ownedGroup === group;
+// Check if owner is an Operand under csv
+const isOwnedByOperator = (csv: ClusterServiceVersionKind, owner: OwnerReference) =>
+  csv.spec?.customresourcedefinitions?.owned?.some((crd) => {
+    const apiGroup = crd.name.substring(crd.name.indexOf('.') + 1);
+    const crdGroupVersionKind = referenceForGroupVersionKind(apiGroup)(crd.version)(crd.kind);
+    const ownerGroupVersionKind = referenceForOwnerRef(owner);
+    return crdGroupVersionKind === ownerGroupVersionKind;
   });
+
+// Check if csv === owner (there is no need to check namespace because ownerReferences must
+// be in the same namespace as the owned resource, or be cluster-scoped).
+const isOwnedByCSV = (csv: ClusterServiceVersionKind, owner: OwnerReference) => {
+  const csvGroupVersionKind = referenceFor(csv);
+  const ownerGroupVersionKind = referenceForOwnerRef(owner);
+  return csvGroupVersionKind === ownerGroupVersionKind && csv.metadata.name === owner.name;
 };
 
-const isOwnedByCSV = (csv: ClusterServiceVersionKind, owner: OwnerReference) =>
-  csv.kind === owner.kind && csv.apiVersion === owner.apiVersion;
-
+// Find an Operator CSV that either is the owner or owns the owner
 export const matchOwnerAndCSV = (owner: OwnerReference, csvs: ClusterServiceVersionKind[] = []) => {
   return csvs.find((csv) => isOwnedByOperator(csv, owner) || isOwnedByCSV(csv, owner));
 };
 
-export const findOwner = (obj: K8sResourceCommon, data: ClusterServiceVersionKind[]) => {
+// Find onwerReference that is either a CSV or is an Operand managed by a CSV.
+export const findOwner = (obj: K8sResourceCommon, csvs: ClusterServiceVersionKind[]) => {
   return obj?.metadata?.ownerReferences?.find((o) =>
-    data?.some((csv) => isOwnedByOperator(csv, o) || isOwnedByCSV(csv, o)),
+    csvs?.some((csv) => isOwnedByOperator(csv, o) || isOwnedByCSV(csv, o)),
   );
 };
