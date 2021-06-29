1b0083f0a6211d237ca3ecd8eae57eafa3d2e6ba
sanjalkatiyar <sanjaldhir@gmail.com>
2021-06-29 15:20:16 +0530

    add update ocs option on details card

--- frontend/packages/ceph-storage-plugin/src/components/dashboards/persistent-internal/details-card.tsx
+++ frontend/packages/ceph-storage-plugin/src/components/dashboards/persistent-internal/details-card.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { getInfrastructurePlatform, useFlag } from '@console/shared';
+import {
+  BlueArrowCircleUpIcon,
+  getInfrastructurePlatform,
+  useFlag,
+  useDeepCompareMemoize,
+} from '@console/shared';
 import DashboardCard from '@console/shared/src/components/dashboard/dashboard-card/DashboardCard';
 import DashboardCardBody from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardBody';
 import DashboardCardHeader from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardHeader';
@@ -17,16 +23,28 @@ import { InfrastructureModel } from '@console/internal/models/index';
 import {
   SubscriptionModel,
   ClusterServiceVersionModel,
+  PackageManifestModel,
+  InstallPlanModel,
 } from '@console/operator-lifecycle-manager/src/models';
-import { K8sResourceKind } from '@console/internal/module/k8s/index';
+import { K8sKind, K8sResourceKind, k8sUpdate } from '@console/internal/module/k8s/index';
 import { getName } from '@console/shared/src/selectors/common';
 import { referenceForModel } from '@console/internal/module/k8s/k8s';
 import { useK8sGet } from '@console/internal/components/utils/k8s-get-hook';
 import { resourcePathFromModel } from '@console/internal/components/utils/resource-link';
+import {
+  PackageManifestKind,
+  SubscriptionKind,
+  InstallPlanKind,
+} from '@console/operator-lifecycle-manager/src/types';
+import { createSubscriptionChannelModal } from '@console/operator-lifecycle-manager/src/components/modals/subscription-channel-modal';
+import { installPlanForSubscription } from '@console/operator-lifecycle-manager/src/components/subscription';
+import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
+import { Button } from '@patternfly/react-core';
 import { OCSServiceModel } from '../../../models';
 import { getOCSVersion, getODFVersion } from '../../../selectors';
 import { CEPH_STORAGE_NAMESPACE, ODF_MODEL_FLAG } from '../../../constants';
 import { StorageClusterKind } from '../../../types';
+import './details-card.scss';
 
 const ocsResource: FirehoseResource = {
   kind: referenceForModel(OCSServiceModel),
@@ -43,6 +61,29 @@ const SubscriptionResource: FirehoseResource = {
   isList: true,
 };
 
+const ocsSubscriptionResource = {
+  kind: referenceForModel(SubscriptionModel),
+  namespaced: true,
+  namespace: 'openshift-storage',
+  isList: false,
+  name: 'ocs-operator',
+};
+
+const InstallPlanResource = {
+  kind: referenceForModel(InstallPlanModel),
+  namespaced: true,
+  isList: true,
+  namespace: 'openshift-storage',
+};
+
+const PackageManifestResource = {
+  kind: referenceForModel(PackageManifestModel),
+  namespaced: true,
+  isList: false,
+  namespace: 'openshift-storage',
+  name: 'ocs-operator',
+};
+
 const DetailsCard: React.FC<DashboardItemProps> = ({
   watchK8sResource,
   stopWatchK8sResource,
@@ -50,10 +91,62 @@ const DetailsCard: React.FC<DashboardItemProps> = ({
 }) => {
   const { t } = useTranslation();
   const isODF = useFlag(ODF_MODEL_FLAG);
+
   const [infrastructure, infrastructureLoaded, infrastructureError] = useK8sGet<K8sResourceKind>(
     InfrastructureModel,
     'cluster',
   );
+  const [ocsSubscription, ocsSubscriptionLoaded, ocsSubscriptionError] = useK8sWatchResource<
+    SubscriptionKind
+  >(ocsSubscriptionResource);
+  const [installPlans, installPlansLoaded, installPlansError] = useK8sWatchResource<
+    InstallPlanKind[]
+  >(InstallPlanResource);
+  const [packageManifest, packageManifestLoaded, packageManifestError] = useK8sWatchResource<
+    PackageManifestKind
+  >(PackageManifestResource);
+
+  const isSafe =
+    !ocsSubscriptionError &&
+    ocsSubscriptionLoaded &&
+    !packageManifestError &&
+    packageManifestLoaded &&
+    !installPlansError &&
+    installPlansLoaded;
+  const memoizedPackageManifest = useDeepCompareMemoize(packageManifest, true);
+  const memoizedOcsSubscription = useDeepCompareMemoize(ocsSubscription, true);
+  const installPlan = installPlanForSubscription(installPlans, memoizedOcsSubscription);
+  const plan = installPlan?.status?.plan || [];
+  const previousInstallPlan = React.useRef<string>('');
+  let currentChannel: string;
+  let currentChannelVersion: number;
+
+  if (ocsSubscriptionLoaded && packageManifestLoaded) {
+    currentChannel =
+      memoizedOcsSubscription?.spec?.channel ??
+      memoizedPackageManifest?.status?.channels?.[0]?.name;
+    currentChannelVersion = parseFloat(
+      currentChannel.substring(currentChannel.lastIndexOf('-') + 1),
+    );
+  }
+
+  const filteredVersions = memoizedPackageManifest?.status?.channels?.filter(
+    ({ name }) => parseFloat(name.substring(name.lastIndexOf('-') + 1)) > currentChannelVersion,
+  );
+  _.set(memoizedPackageManifest, 'status.channels', filteredVersions);
+  const ocsChannelsList = memoizedPackageManifest?.status?.channels;
+
+  // plan.length <= 0 means InstallPlan has not been fully resolved yet.
+  if (isSafe && plan.length > 0 && previousInstallPlan.current !== installPlan?.metadata?.name) {
+    if (previousInstallPlan.current && !installPlan?.spec?.approved) {
+      k8sUpdate(InstallPlanModel, {
+        ...installPlan,
+        spec: { ...installPlan.spec, approved: true },
+      });
+    }
+    previousInstallPlan.current = installPlan.metadata.name;
+  }
+
   React.useEffect(() => {
     watchK8sResource(SubscriptionResource);
     watchK8sResource(ocsResource);
@@ -79,9 +172,22 @@ const DetailsCard: React.FC<DashboardItemProps> = ({
     serviceVersion,
     CEPH_STORAGE_NAMESPACE,
   )}`;
+
   const serviceName = isODF
     ? t('ceph-storage-plugin~OpenShift Data Foundation')
     : t('ceph-storage-plugin~OpenShift Container Storage');
+
+  const launchModal = () => {
+    if (isSafe) {
+      return createSubscriptionChannelModal({
+        subscription: memoizedOcsSubscription,
+        pkg: memoizedPackageManifest,
+        k8sUpdate: (kind: K8sKind, newObj: K8sResourceKind) => k8sUpdate(kind, newObj),
+      });
+    }
+    return null;
+  };
+
   return (
     <DashboardCard>
       <DashboardCardHeader>
@@ -124,7 +230,29 @@ const DetailsCard: React.FC<DashboardItemProps> = ({
             isLoading={!subscriptionLoaded}
             error={subscriptionLoaded && !serviceVersion}
           >
-            {serviceVersion}
+            <div className="ceph-details-card__version-details">
+              <div>{serviceVersion}</div>
+              <div className="ceph-details-card__update-version">
+                {!_.isEmpty(filteredVersions) &&
+                  memoizedOcsSubscription?.spec?.installPlanApproval === 'Manual' && (
+                    <>
+                      {ocsSubscriptionLoaded && packageManifestLoaded && installPlansLoaded && (
+                        <Button
+                          type="button"
+                          isInline
+                          onClick={launchModal}
+                          variant="link"
+                          className="pf-u-ml-xl"
+                        >
+                          <BlueArrowCircleUpIcon className="co-icon-space-r" />
+                          {t('ceph-storage-plugin~Update to ')}
+                          {ocsChannelsList[ocsChannelsList.length - 1].name}
+                        </Button>
+                      )}
+                    </>
+                  )}
+              </div>
+            </div>
           </DetailItem>
         </DetailsBody>
       </DashboardCardBody>
