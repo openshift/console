781420987bc2a8ec9133aeec013fab265c63f2fc
Rohit Rai <rohitkrai03@gmail.com>
2021-06-29 21:49:15 +0530

    Migrate helm details page actions to use extensions

--- frontend/packages/console-shared/src/components/actions/index.ts
+++ frontend/packages/console-shared/src/components/actions/index.ts
@@ -1,2 +1,3 @@
 export { default as ActionsLoader } from './loader/ActionsLoader';
 export { default as ActionMenu } from './menu/ActionMenu';
+export * from './menu/menu-types';
--- frontend/packages/console-shared/src/components/actions/menu/ActionMenu.tsx
+++ frontend/packages/console-shared/src/components/actions/menu/ActionMenu.tsx
@@ -4,6 +4,7 @@ import { EllipsisVIcon } from '@patternfly/react-icons';
 import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { Action } from '@console/dynamic-plugin-sdk';
+import { useSafetyFirst } from '@console/internal/components/safety-first';
 import { checkAccess } from '@console/internal/components/utils';
 import { Popper } from '../../popper';
 import ActionMenuContent from './ActionMenuContent';
@@ -25,12 +26,15 @@ const ActionMenu: React.FC<ActionMenuProps> = ({
   label,
 }) => {
   const { t } = useTranslation();
+  const isKebabVariant = variant === ActionMenuVariant.KEBAB;
+  const [isVisible, setVisible] = useSafetyFirst(isKebabVariant);
   const [active, setActive] = React.useState<boolean>(false);
   const toggleRef = React.useRef<HTMLButtonElement>();
   const toggleRefCb = React.useCallback(() => toggleRef.current, []);
   const menuRef = React.useRef<HTMLDivElement>();
   const menuRefCb = React.useCallback(() => menuRef.current, []);
   const toggleLabel = label || t('console-shared~Actions');
+  const menuOptions = options || actions;
 
   const toggleMenu = () => setActive((value) => !value);
 
@@ -55,50 +59,75 @@ const ActionMenu: React.FC<ActionMenuProps> = ({
     });
   }, [actions]);
 
-  const menuOptions = options || actions;
+  // Check if any actions are visible when actions have access reviews.
+  React.useEffect(() => {
+    if (!actions.length) {
+      setVisible(false);
+      return;
+    }
+    // Do nothing if variant is kebab. The action menu should be visible and acces review happens on hover.
+    if (isKebabVariant) return;
+
+    const promises = actions.reduce((acc, action) => {
+      if (action.accessReview) {
+        acc.push(checkAccess(action.accessReview));
+      }
+      return acc;
+    }, []);
+
+    // Only need to resolve if all actions require access review
+    if (promises.length !== actions.length) {
+      setVisible(true);
+      return;
+    }
+    Promise.all(promises)
+      .then((results) => setVisible(_.some(results, 'status.allowed')))
+      .catch(() => setVisible(true));
+  }, [actions, isKebabVariant, setVisible]);
 
   return (
-    <div>
-      <MenuToggle
-        variant={variant}
-        innerRef={toggleRef}
-        isExpanded={active}
-        isDisabled={isDisabled}
-        aria-expanded={active}
-        aria-label={toggleLabel}
-        aria-haspopup="true"
-        data-test-id="menu-toggle-button"
-        onClick={toggleMenu}
-        onFocus={handleHover}
-        onMouseEnter={handleHover}
-      >
-        {variant === ActionMenuVariant.KEBAB ? <EllipsisVIcon /> : toggleLabel}
-      </MenuToggle>
-      <Popper
-        open={!isDisabled && active}
-        placement="bottom-end"
-        onRequestClose={handleRequestClose}
-        reference={toggleRefCb}
-        closeOnEsc
-        closeOnOutsideClick
-      >
-        <FocusTrap
-          focusTrapOptions={{
-            clickOutsideDeactivates: true,
-            returnFocusOnDeactivate: false,
-            fallbackFocus: menuRefCb,
-          }}
+    isVisible && (
+      <div>
+        <MenuToggle
+          variant={variant}
+          innerRef={toggleRef}
+          isExpanded={active}
+          isDisabled={isDisabled}
+          aria-expanded={active}
+          aria-label={toggleLabel}
+          aria-haspopup="true"
+          data-test-id="menu-toggle-button"
+          onClick={toggleMenu}
+          {...(isKebabVariant ? { onFocus: handleHover, onMouseEnter: handleHover } : {})}
         >
-          <div ref={menuRef} className="pf-c-menu pf-m-flyout">
-            <ActionMenuContent
-              options={menuOptions}
-              onClick={hideMenu}
-              focusItem={menuOptions[0]}
-            />
-          </div>
-        </FocusTrap>
-      </Popper>
-    </div>
+          {isKebabVariant ? <EllipsisVIcon /> : toggleLabel}
+        </MenuToggle>
+        <Popper
+          open={!isDisabled && active}
+          placement="bottom-end"
+          onRequestClose={handleRequestClose}
+          reference={toggleRefCb}
+          closeOnEsc
+          closeOnOutsideClick
+        >
+          <FocusTrap
+            focusTrapOptions={{
+              clickOutsideDeactivates: true,
+              returnFocusOnDeactivate: false,
+              fallbackFocus: menuRefCb,
+            }}
+          >
+            <div ref={menuRef} className="pf-c-menu pf-m-flyout">
+              <ActionMenuContent
+                options={menuOptions}
+                onClick={hideMenu}
+                focusItem={menuOptions[0]}
+              />
+            </div>
+          </FocusTrap>
+        </Popper>
+      </div>
+    )
   );
 };
 
