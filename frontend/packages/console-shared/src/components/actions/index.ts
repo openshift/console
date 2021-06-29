41e9007ba171e60d8aab8fd30f9e89f53ec30939
Rohit Rai <rohitkrai03@gmail.com>
2021-06-29 21:39:19 +0530

    Created new action menu components using PF menu

--- frontend/packages/console-shared/src/components/actions/index.ts
+++ frontend/packages/console-shared/src/components/actions/index.ts
@@ -1,1 +1,2 @@
-export { default as ActionsLoader } from './ActionsLoader';
+export { default as ActionsLoader } from './loader/ActionsLoader';
+export { default as ActionMenu } from './menu/ActionMenu';
--- frontend/packages/console-shared/src/components/kebab/kebab-types.ts
+++
@@ -1,9 +0,0 @@
-import { Action } from '@console/dynamic-plugin-sdk';
-
-export type KebabMenuOption = KebabSubMenuOption | Action;
-
-export type KebabSubMenuOption = {
-  id: string;
-  label?: string;
-  children: KebabMenuOption[];
-};
