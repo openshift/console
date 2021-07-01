12f118c2dd58ec5031e61b97167110a874d886f0
Joseph Caiani <jcaiani@redhat.com>
2021-06-30 22:01:18 -0400

    Add cypress tests for name/value secrets with binary, ascii, and unicode values

--- frontend/packages/integration-tests-cypress/views/form.ts
+++ frontend/packages/integration-tests-cypress/views/form.ts
@@ -1,3 +1,4 @@
 export const submitButton = 'button[type=submit]';
 export const errorMessage = '.pf-c-alert.pf-m-inline.pf-m-danger';
+export const infoMessage = '.pf-c-alert.pf-m-inline.pf-m-info';
 export const successMessage = '.pf-c-alert.pf-m-inline.pf-m-success';
