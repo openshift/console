a1618834d286a03bf52187aaff4ffff53d1fac82
Rastislav Wagner <rawagner@redhat.com>
2021-07-07 15:07:14 +0200

    Override dynamic plugin sdk module in runtime

--- frontend/packages/console-dynamic-plugin-sdk/src/api/api.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/api/api.ts
@@ -1,10 +1,8 @@
-import { UseK8sWatchResource, UseK8sWatchResources } from './api-types';
+/* eslint-disable */
+import { UseK8sWatchResource, UseK8sWatchResources } from '../extensions/console-types';
 
-export * from './api-types';
+export const useK8sWatchResource: UseK8sWatchResource = require('@console/internal/components/utils/k8s-watch-hook')
+  .useK8sWatchResource;
 
-const MockImpl = () => {
-  throw new Error('You need to configure webpack externals to use this function at runtime.');
-};
-
-export const useK8sWatchResource: UseK8sWatchResource = MockImpl;
-export const useK8sWatchResources: UseK8sWatchResources = MockImpl;
+export const useK8sWatchResources: UseK8sWatchResources = require('@console/internal/components/utils/k8s-watch-hook')
+  .useK8sWatchResources;
