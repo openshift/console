43fec267a6c4adcad176f708081810a7a77761fa
Vikram Raj <vikrmrj@gmail.com>
2021-07-07 22:14:01 +0530

    Add RBAC check on view shortcuts actions

--- frontend/packages/topology/src/components/graph-view/TopologyShortcuts.tsx
+++ frontend/packages/topology/src/components/graph-view/TopologyShortcuts.tsx
@@ -7,39 +7,47 @@ export type Options = {
   supportedFileTypes: string[];
   isEmptyModel: boolean;
   viewType: TopologyViewType;
+  allImportAccess: boolean;
 };
-export const getTopologyShortcuts = (t: TFunction, options: Options): React.ReactElement => (
-  <ShortcutTable>
-    {!options.isEmptyModel && options.viewType === TopologyViewType.graph && (
-      <>
-        <Shortcut data-test-id="move" drag>
-          {t('topology~Move')}
+export const getTopologyShortcuts = (t: TFunction, options: Options): React.ReactElement => {
+  const { supportedFileTypes, isEmptyModel, viewType, allImportAccess } = options;
+  return (
+    <ShortcutTable>
+      {!isEmptyModel && viewType === TopologyViewType.graph && (
+        <>
+          <Shortcut data-test-id="move" drag>
+            {t('topology~Move')}
+          </Shortcut>
+          {allImportAccess && (
+            <>
+              <Shortcut data-test-id="edit-application-grouping" shift drag>
+                {t('topology~Edit Application grouping')}
+              </Shortcut>
+              <Shortcut data-test-id="context-menu" rightClick>
+                {t('topology~Access context menu')}
+              </Shortcut>
+              <Shortcut data-test-id="create-connector-handle" hover>
+                {t('topology~Access create connector handle')}
+              </Shortcut>
+            </>
+          )}
+        </>
+      )}
+      {!isEmptyModel && (
+        <Shortcut data-test-id="view-details" click>
+          {t('topology~View details in side panel')}
         </Shortcut>
-        <Shortcut data-test-id="edit-application-grouping" shift drag>
-          {t('topology~Edit Application grouping')}
-        </Shortcut>
-        <Shortcut data-test-id="context-menu" rightClick>
-          {t('topology~Access context menu')}
-        </Shortcut>
-        <Shortcut data-test-id="create-connector-handle" hover>
-          {t('topology~Access create connector handle')}
-        </Shortcut>
-      </>
-    )}
-    {!options.isEmptyModel && (
-      <Shortcut data-test-id="view-details" click>
-        {t('topology~View details in side panel')}
+      )}
+      <Shortcut data-test-id="open-quick-search" ctrl keyName="Spacebar">
+        {t('topology~Open quick search modal')}
       </Shortcut>
-    )}
-    <Shortcut data-test-id="open-quick-search" ctrl keyName="Spacebar">
-      {t('topology~Open quick search modal')}
-    </Shortcut>
-    {options.supportedFileTypes?.length > 0 && (
-      <Shortcut data-test-id="upload-file" dragNdrop>
-        {t('topology~Upload file ({{fileTypes}}) to project', {
-          fileTypes: options.supportedFileTypes.map((ex) => `.${ex}`).toString(),
-        })}
-      </Shortcut>
-    )}
-  </ShortcutTable>
-);
+      {supportedFileTypes?.length > 0 && allImportAccess && (
+        <Shortcut data-test-id="upload-file" dragNdrop>
+          {t('topology~Upload file ({{fileTypes}}) to project', {
+            fileTypes: supportedFileTypes.map((ex) => `.${ex}`).toString(),
+          })}
+        </Shortcut>
+      )}
+    </ShortcutTable>
+  );
+};
