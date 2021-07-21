d1821ad4436a7187d794828f3ea03096933f010b
Christoph Jerolimov <jerolimov+github@redhat.com>
2021-07-21 18:38:25 +0200

    Fix missing build status translation in topology sidebar

--- frontend/public/components/overview/build-overview.tsx
+++ frontend/public/components/overview/build-overview.tsx
@@ -14,54 +14,53 @@ import { ResourceLink, SidebarSectionHeading, useAccessReview } from '../utils';
 
 const StatusTitle = ({ build }: { build: K8sResourceKind }) => {
   const { t } = useTranslation();
-  const link = <BuildNumberLink build={build} />;
   switch (build.status.phase) {
     case BuildPhase.Cancelled:
       return (
         <Trans t={t} ns="public">
-          Build {link} was cancelled
+          Build <BuildNumberLink build={build} /> was cancelled
         </Trans>
       );
     case BuildPhase.Complete:
       return (
         <Trans t={t} ns="public">
-          Build {link} was complete
+          Build <BuildNumberLink build={build} /> was complete
         </Trans>
       );
     case BuildPhase.Error:
       return (
         <Trans t={t} ns="public">
-          Build {link} encountered an error
+          Build <BuildNumberLink build={build} /> encountered an error
         </Trans>
       );
     case BuildPhase.Failed:
       return (
         <Trans t={t} ns="public">
-          Build {link} failed
+          Build <BuildNumberLink build={build} /> failed
         </Trans>
       );
     case BuildPhase.New:
       return (
         <Trans t={t} ns="public">
-          Build {link} is new
+          Build <BuildNumberLink build={build} /> is new
         </Trans>
       );
     case BuildPhase.Pending:
       return (
         <Trans t={t} ns="public">
-          Build {link} is pending
+          Build <BuildNumberLink build={build} /> is pending
         </Trans>
       );
     case BuildPhase.Running:
       return (
         <Trans t={t} ns="public">
-          Build {link} is running
+          Build <BuildNumberLink build={build} /> is running
         </Trans>
       );
     default:
       return (
         <Trans t={t} ns="public">
-          Build {link} is {_.toLower(build.status.phase)}
+          Build <BuildNumberLink build={build} /> is {_.toLower(build.status.phase)}
         </Trans>
       );
   }
