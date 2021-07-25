6941ae415b5c8d3e6904efbd67f1ac54b15adeea
Matan Schatzman <mschatzm@redhat.com>
2021-07-25 13:14:15 +0300

    Fix pencil button
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

---
+++ frontend/packages/kubevirt-plugin/src/components/vms/VMEditWithPencil.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Button } from '@patternfly/react-core';
+import { PencilAltIcon } from '@patternfly/react-icons';
+
+type VMEditWithPencilProps = {
+  onEditClick: React.MouseEventHandler<HTMLButtonElement>;
+  isEdit: boolean;
+};
+
+const VMEditWithPencil: React.FC<VMEditWithPencilProps> = ({ children, isEdit, onEditClick }) => {
+  return (
+    <Button
+      data-test="edit-annotations"
+      type="button"
+      isInline
+      isDisabled={!isEdit}
+      onClick={onEditClick}
+      variant="link"
+    >
+      {children}
+      {isEdit && <PencilAltIcon className="co-icon-space-l pf-c-button-icon--plain" />}
+    </Button>
+  );
+};
+
+export default VMEditWithPencil;
