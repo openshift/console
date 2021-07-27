b904a6bc6d22722e3fa6fb0fbb611434f9f667e1
Mohammed Saud <md.saud020@gmail.com>
2021-07-27 18:16:14 +0530

    add inspect url to devconsole monitoring chart

--- frontend/packages/dev-console/src/components/monitoring/dashboard/MonitoringDashboardGraph.tsx
+++ frontend/packages/dev-console/src/components/monitoring/dashboard/MonitoringDashboardGraph.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import DashboardCard from '@console/shared/src/components/dashboard/dashboard-card/DashboardCard';
 import DashboardCardHeader from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardHeader';
+import DashboardCardLink from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardLink';
 import DashboardCardTitle from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardTitle';
 import DashboardCardBody from '@console/shared/src/components/dashboard/dashboard-card/DashboardCardBody';
 import { QueryBrowser } from '@console/internal/components/monitoring/query-browser';
 import { Humanize } from '@console/internal/components/utils';
 import { ByteDataTypes } from '@console/shared/src/graph-helper/data-utils';
-import { PrometheusGraphLink } from '@console/internal/components/graphs/prometheus-graph';
 import './MonitoringDashboardGraph.scss';
 
 export enum GraphTypes {
   area = 'Area',
   line = 'Line',
 }
+
+const PrometheusGraphLink = ({ query, namespace, ariaChartLinkLabel }) => {
+  const { t } = useTranslation();
+  const queries = _.compact(_.castArray(query));
+  if (!queries.length) {
+    return null;
+  }
+  const params = new URLSearchParams();
+  queries.forEach((q, index) => params.set(`query${index}`, q));
+  return (
+    <DashboardCardLink
+      aria-label={ariaChartLinkLabel}
+      to={`/dev-monitoring/ns/${namespace}/metrics?${params.toString()}`}
+    >
+      {t('devconsole~Inspect')}
+    </DashboardCardLink>
+  );
+};
 
 type MonitoringDashboardGraphProps = {
   title: string;
@@ -44,28 +63,28 @@ export const MonitoringDashboardGraph: React.FC<MonitoringDashboardGraphProps> =
     <DashboardCard className="monitoring-dashboards__card odc-monitoring-dashboard-graph">
       <DashboardCardHeader>
         <DashboardCardTitle>{title}</DashboardCardTitle>
-      </DashboardCardHeader>
-      <DashboardCardBody>
         <PrometheusGraphLink
+          namespace={namespace}
           query={query}
           ariaChartLinkLabel={t('devconsole~View metrics for {{title}}', {
             title,
           })}
-        >
-          <QueryBrowser
-            hideControls
-            defaultTimespan={DEFAULT_TIME_SPAN}
-            defaultSamples={DEFAULT_SAMPLES}
-            namespace={namespace}
-            queries={[query]}
-            isStack={graphType === GraphTypes.area}
-            timespan={timespan}
-            pollInterval={pollInterval}
-            fixedEndTime={endTime}
-            formatSeriesTitle={(labels) => labels.pod}
-            showLegend
-          />
-        </PrometheusGraphLink>
+        />
+      </DashboardCardHeader>
+      <DashboardCardBody>
+        <QueryBrowser
+          hideControls
+          defaultTimespan={DEFAULT_TIME_SPAN}
+          defaultSamples={DEFAULT_SAMPLES}
+          namespace={namespace}
+          queries={[query]}
+          isStack={graphType === GraphTypes.area}
+          timespan={timespan}
+          pollInterval={pollInterval}
+          fixedEndTime={endTime}
+          formatSeriesTitle={(labels) => labels.pod}
+          showLegend
+        />
       </DashboardCardBody>
     </DashboardCard>
   );
