d942c3cc3acaf6e5a5976b6136acfcc222de965e
Mohammed Saud <md.saud020@gmail.com>
2021-07-27 18:16:14 +0530

    add inspect url to devconsole monitoring chart

--- frontend/packages/dev-console/src/components/monitoring/dashboard/__tests__/MonitoringDashboardGraph.spec.tsx
+++ frontend/packages/dev-console/src/components/monitoring/dashboard/__tests__/MonitoringDashboardGraph.spec.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import { TFunction } from 'i18next';
 import * as redux from 'react-redux';
-import { PrometheusGraphLink } from '@console/internal/components/graphs/prometheus-graph';
 import { QueryBrowser } from '@console/internal/components/monitoring/query-browser';
 import { monitoringDashboardQueries } from '../../queries';
 import { MonitoringDashboardGraph, GraphTypes } from '../MonitoringDashboardGraph';
@@ -53,8 +52,8 @@ describe('Monitoring Dashboard graph', () => {
   it('should add link to line graph', () => {
     monitoringDashboardGraphProps.graphType = GraphTypes.line;
     const wrapper = shallow(<MonitoringDashboardGraph {...monitoringDashboardGraphProps} />);
-    expect(wrapper.find(PrometheusGraphLink).exists()).toBe(true);
-    expect(wrapper.find(PrometheusGraphLink).props().query).toEqual(
+    expect(wrapper.find('PrometheusGraphLink').exists()).toBe(true);
+    expect(wrapper.find('PrometheusGraphLink').prop('query')).toEqual(
       monitoringDashboardGraphProps.query,
     );
   });
