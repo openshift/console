ff21dcf22677dae13d939b08691f446b92ccc642
apeksha-khandelwal <akhandel@redhat.com>
2021-07-27 21:45:55 -0400

    Final conflict resolution

--- frontend/public/components/utils/horizontal-nav.tsx
+++ frontend/public/components/utils/horizontal-nav.tsx
@@ -333,6 +333,7 @@ export type HorizontalNavProps = {
   EmptyMsg?: React.ComponentType<any>;
   noStatusBox?: boolean;
   customData?: any;
+  alertURL?: string;
 };
 
 export type PageComponentProps<R extends K8sResourceCommon = K8sResourceKind> = {
