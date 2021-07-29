abe00ed0c00c39676cfe705353b9025caa865a85
James Talton <jtalton@redhat.com>
2021-07-29 13:32:38 -0400

    Update dynamic plugin demo to add navigation and pages
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/dynamic-demo-plugin/src/utils/bar.ts
+++ frontend/dynamic-demo-plugin/src/utils/bar.ts
@@ -1,7 +1,5 @@
 import { SetFeatureFlag } from '@openshift-console/dynamic-plugin-sdk';
 
-export default (label: string) => `Hello ${label} Function!`;
-
 export const testHandler = (callback: SetFeatureFlag) => {
   // eslint-disable-next-line no-console
   console.log('testHandler called', callback);
---
+++ frontend/dynamic-demo-plugin/src/utils/example-navs.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import './fix-ocp-pf.css';
+import { ExamplePage } from '../components/ExamplePage'
+
+export const DynamicPage1 = () => <ExamplePage title="Dynamic Page 1"/>;
+export const DynamicPage2 = () => <ExamplePage title="Dynamic Page 2"/>;
