ddccd97ca05b6a1cd17938e0043500d10a3c706c
yaacov <kobi.zamir@gmail.com>
2021-07-29 07:56:53 +0300

    user supported templates will not show supported flag

--- frontend/packages/kubevirt-plugin/integration-tests-cypress/tests/vm-template/template-support.spec.ts
+++ frontend/packages/kubevirt-plugin/integration-tests-cypress/tests/vm-template/template-support.spec.ts
@@ -37,7 +37,6 @@ describe('test VM template support', () => {
     wizard.template.createTemplate(TEMPLATE_NAME, 'bar', true);
     virtualization.templates.testProvider(TEMPLATE_NAME, 'bar');
     virtualization.templates.testSource(TEMPLATE_NAME, 'bar');
-    virtualization.templates.testSupport(TEMPLATE_NAME, 'bar');
 
     wizard.template.open();
     wizard.template.createTemplate(TEMPLATE_NO_SUPPORT_NAME, 'bar', false);
