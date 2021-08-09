a3913a80cdea828d3bb019c7d4f14ecc527936d7
Vojtech Szocs <vszocs@redhat.com>
2021-08-09 21:22:01 +0200

    Address review comments and add unit test

--- frontend/packages/console-plugin-sdk/src/utils/useTranslationExt.ts
+++ frontend/packages/console-plugin-sdk/src/utils/useTranslationExt.ts
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { TFunction } from 'i18next';
 import { Namespace, useTranslation, UseTranslationOptions } from 'react-i18next';
-import { isTranslatableString } from './extension-i18n';
+import { isTranslatableString, getTranslationKey } from './extension-i18n';
 
-// translates strings if the key matches the pattern `%...%`
-
-// extend react-i18next useTranslation and override the `t` function
+/**
+ * Extends i18next `useTranslation` hook and overrides the `t` function.
+ *
+ * Translatable strings in Console application must use the `%key%` pattern.
+ */
 const useTranslationExt = (ns?: Namespace, options?: UseTranslationOptions) => {
   const result = useTranslation(ns, options);
   const { t } = result;
   const cb: TFunction = React.useCallback(
-    (key: string) => (isTranslatableString(key) ? t(key.substr(1, key.length - 2)) : key),
+    (value: string) => (isTranslatableString(value) ? t(getTranslationKey(value)) : value),
     [t],
   );
   return { ...result, t: cb };
