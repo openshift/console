bb346a6210a370ab4c299ca24feaa1e0462cfcb0
debsmita1 <debsmita.santra@gmail.com>
2021-08-09 20:08:30 +0530

    reload entered data if any when switching strategy

--- frontend/packages/dev-console/src/components/edit-deployment/deployment-strategy/DeploymentStrategySection.tsx
+++ frontend/packages/dev-console/src/components/edit-deployment/deployment-strategy/DeploymentStrategySection.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { FormikValues, useFormikContext } from 'formik';
-import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { K8sResourceKind } from '@console/internal/module/k8s';
 import { DropdownField } from '@console/shared/src';
@@ -31,7 +30,7 @@ const DeploymentStrategySection: React.FC<DeploymentStrategySectionProps> = ({
       formData: {
         name: resName,
         project: { name: resNamespace },
-        deploymentStrategy: { type },
+        deploymentStrategy,
       },
     },
     initialValues,
@@ -39,7 +38,7 @@ const DeploymentStrategySection: React.FC<DeploymentStrategySectionProps> = ({
   } = useFormikContext<FormikValues>();
 
   const deploymentStrategyFields = React.useMemo(() => {
-    switch (type) {
+    switch (deploymentStrategy.type) {
       case DeploymentStrategyType.recreateParams:
         return <RecreateStrategy resourceType={resourceType} resourceObj={resourceObj} />;
       case DeploymentStrategyType.rollingParams:
@@ -50,20 +49,15 @@ const DeploymentStrategySection: React.FC<DeploymentStrategySectionProps> = ({
       default:
         return null;
     }
-  }, [type, resourceObj, resourceType]);
+  }, [deploymentStrategy.type, resourceObj, resourceType]);
 
   const onChange = React.useCallback(
     (value: DeploymentStrategyType) => {
       const strategyDefaultValues = getStrategyData(value, {}, resName, resNamespace, resourceType);
       const strategyData = {
-        ..._.omit(resourceObj.spec?.strategy, [
-          'rollingParams',
-          'recreateParams',
-          'customParams',
-          'rollingUpdate',
-        ]),
-        type: value,
         ...strategyDefaultValues,
+        ...deploymentStrategy,
+        type: value,
       };
       initialValues.formData.deploymentStrategy = strategyData;
       setFieldValue('formData.deploymentStrategy', strategyData);
@@ -72,9 +66,9 @@ const DeploymentStrategySection: React.FC<DeploymentStrategySectionProps> = ({
       initialValues.formData.deploymentStrategy,
       resName,
       resNamespace,
-      resourceObj.spec,
       resourceType,
       setFieldValue,
+      deploymentStrategy,
     ],
   );
 
@@ -84,7 +78,7 @@ const DeploymentStrategySection: React.FC<DeploymentStrategySectionProps> = ({
         name="formData.deploymentStrategy.type"
         label={t('devconsole~Strategy type')}
         items={getDeploymentStrategyItems(resourceType, t)}
-        helpText={getDeploymentStrategyHelpText(resourceType, type, t)}
+        helpText={getDeploymentStrategyHelpText(resourceType, deploymentStrategy.type, t)}
         onChange={onChange}
         fullWidth
       />
