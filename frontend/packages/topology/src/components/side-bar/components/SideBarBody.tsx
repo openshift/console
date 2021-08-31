73ffdb544403d2af3d6903caae713a211e1462d3
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-08-31 18:35:16 +0530

    persist last viewed tab on topology sidepanel

--- frontend/packages/topology/src/components/side-bar/components/SideBarBody.tsx
+++ frontend/packages/topology/src/components/side-bar/components/SideBarBody.tsx
@@ -1,20 +1,41 @@
 import * as React from 'react';
 import { GraphElement } from '@patternfly/react-topology';
-import { SimpleTabNav } from '@console/internal/components/utils';
+import { useTranslation } from 'react-i18next';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore: FIXME missing exports due to out-of-sync @types/react-redux version
+import { useSelector, useDispatch } from 'react-redux';
+import * as UIActions from '@console/internal/actions/ui';
+import { SimpleTabNav, Tab } from '@console/internal/components/utils';
+import { useQueryParams } from '@console/shared/src';
 import SideBarTabLoader from '../providers/SideBarTabLoader';
+
+const SimpleTabNavWrapper: React.FC<{ tabs: Tab[] }> = ({ tabs }) => {
+  const { t } = useTranslation();
+  const selectedTab = useSelector(({ UI }) => UI.getIn(['overview', 'selectedDetailsTab']));
+  const dispatch = useDispatch();
+  const queryParams = useQueryParams();
+  const selectTabParam = queryParams.get('selectTab');
+  const handleClickTab = React.useCallback(
+    (name) => {
+      dispatch(UIActions.selectOverviewDetailsTab(name));
+    },
+    [dispatch],
+  );
+  return (
+    <SimpleTabNav
+      selectedTab={selectTabParam || selectedTab || t('topology~Details')}
+      tabs={tabs}
+      tabProps={null}
+      onClickTab={handleClickTab}
+      additionalClassNames="co-m-horizontal-nav__menu--within-sidebar co-m-horizontal-nav__menu--within-overview-sidebar"
+    />
+  );
+};
 
 const SideBarBody: React.FC<{ element: GraphElement }> = ({ element }) => {
   return (
     <SideBarTabLoader element={element}>
-      {(tabs, loaded) =>
-        loaded ? (
-          <SimpleTabNav
-            tabs={tabs}
-            tabProps={null}
-            additionalClassNames="co-m-horizontal-nav__menu--within-sidebar co-m-horizontal-nav__menu--within-overview-sidebar"
-          />
-        ) : null
-      }
+      {(tabs, loaded) => (loaded ? <SimpleTabNavWrapper tabs={tabs} /> : null)}
     </SideBarTabLoader>
   );
 };
