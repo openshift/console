fcf5bddd9ad2c67f6ed87a5d0048b6a1a1aa0c67
yzamir <yzamir@redhat.com>
2021-09-05 10:51:56 +0300

    Kubevirt, use currect API when checking healthz

--- frontend/packages/kubevirt-plugin/src/flags/detect-kubevirt-virtual-machines.ts
+++ frontend/packages/kubevirt-plugin/src/flags/detect-kubevirt-virtual-machines.ts
@@ -4,6 +4,7 @@ import {
   FLAG_KUBEVIRT,
   FLAG_KUBEVIRT_HAS_PRINTABLESTATUS,
   FLAG_KUBEVIRT_HAS_V1_API,
+  FLAG_KUBEVIRT_HAS_V1ALPHA3_API,
 } from './const';
 
 export const detectKubevirtVirtualMachines = async (setFeatureFlag: SetFeatureFlag) => {
@@ -28,6 +29,7 @@ export const detectKubevirtVirtualMachines = async (setFeatureFlag: SetFeatureFl
 
         setFeatureFlag(FLAG_KUBEVIRT, hasV1APIVersion || hasV1Alpha3APIVersion);
         setFeatureFlag(FLAG_KUBEVIRT_HAS_V1_API, hasV1APIVersion);
+        setFeatureFlag(FLAG_KUBEVIRT_HAS_V1ALPHA3_API, hasV1Alpha3APIVersion && !hasV1APIVersion);
         setFeatureFlag(FLAG_KUBEVIRT_HAS_PRINTABLESTATUS, hasPrintableStatus);
       })
       .catch(() => {
