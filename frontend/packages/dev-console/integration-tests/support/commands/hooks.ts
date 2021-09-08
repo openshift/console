845ea1fbe133f0eb8282909368ffeb86d425b1c2
Balaji Makam <bmakam@redhat.com>
2021-09-08 21:52:02 +0530

    update cypress hooks for modifying user settings storage to local

--- frontend/packages/dev-console/integration-tests/support/commands/hooks.ts
+++ frontend/packages/dev-console/integration-tests/support/commands/hooks.ts
@@ -3,6 +3,10 @@ before(() => {
   cy.document()
     .its('readyState')
     .should('eq', 'complete');
+  // set the user settings location to local storage, so that no need of deleting config map from openshift-console-user-settings namespace
+  cy.window().then((win: any) => {
+    win.SERVER_FLAGS.userSettingsLocation = 'localstorage';
+  });
 });
 
 after(() => {
