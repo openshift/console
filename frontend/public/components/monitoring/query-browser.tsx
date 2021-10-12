f3a31a8b425c0bd116ea199d024b22130e14f6a0
Andy Pickering <anpicker@redhat.com>
2021-10-12 18:03:00 +0900

    Monitoring dashboards: Improve series labels ordering in tooltips
    The number of labels is often too great to fit in the tooltip and
    sometimes the first one or two labels have the same value for all
    series, making the text in the tooltip useless. This tries to order the
    labels intelligently so that those with different values are visible.

--- frontend/public/components/monitoring/query-browser.tsx
+++ frontend/public/components/monitoring/query-browser.tsx
@@ -157,6 +157,14 @@ const TOOLTIP_MAX_ENTRIES = 20;
 const TOOLTIP_MAX_WIDTH = 300;
 const TOOLTIP_MAX_HEIGHT = 400;
 
+type TooltipSeries = {
+  color: string;
+  labels: PrometheusLabels;
+  name: string;
+  total: number;
+  value: string;
+};
+
 // For performance, use this instead of PatternFly's ChartTooltip or Victory VictoryTooltip
 const Tooltip_: React.FC<TooltipProps> = ({ activePoints, center, height, style, width, x }) => {
   const time = activePoints?.[0]?.x;
@@ -172,14 +180,14 @@ const Tooltip_: React.FC<TooltipProps> = ({ activePoints, center, height, style,
   }
 
   // Pick tooltip width and location (left or right of the cursor) to maximize its available space
-  const tooltipMaxWidth = Math.min(width / 2 + 60, TOOLTIP_MAX_WIDTH);
-  const isOnLeft = x > (width - 40) / 2;
+  const tooltipMaxWidth: number = Math.min(width / 2 + 60, TOOLTIP_MAX_WIDTH);
+  const isOnLeft: boolean = x > (width - 40) / 2;
 
   // Sort the entries in the tooltip from largest to smallest (to match the position of points in
   // the graph) and limit to the maximum number we can display. There could be a large number of
   // points, so we use a slightly less succinct approach to avoid sorting the whole list of points
   // and to avoid processing points that won't fit in the tooltip.
-  const largestPoints = [];
+  const largestPoints: TooltipSeries[] = [];
   activePoints.forEach(({ _y1, y }, i) => {
     const total = _y1 ?? y;
     if (
@@ -188,6 +196,7 @@ const Tooltip_: React.FC<TooltipProps> = ({ activePoints, center, height, style,
     ) {
       const point = {
         color: style[i]?.fill,
+        labels: style[i]?.labels,
         name: style[i]?.name,
         total,
         value: valueFormatter(style[i]?.units)(y),
@@ -199,7 +208,28 @@ const Tooltip_: React.FC<TooltipProps> = ({ activePoints, center, height, style,
       );
     }
   });
-  const allSeries = largestPoints.slice(0, TOOLTIP_MAX_ENTRIES);
+  const allSeries: TooltipSeries[] = largestPoints.slice(0, TOOLTIP_MAX_ENTRIES);
+
+  // For each series we are displaying in the tooltip, create a name based on its labels. We have
+  // limited space, so sort the labels to try to show the most useful first since later labels will
+  // likely be cut off. Sort first by the number of unique values for the label (prefer to show
+  // labels with more values because they are more helpful in identifying the series), then by the
+  // length of the label (prefer to show sorter labels because space is limited).
+  const allSeriesSorted: string[] = _.sortBy(
+    _.without(
+      _.uniq(_.flatMap(allSeries, (s) => (s.labels ? Object.keys(s.labels) : []))),
+      '__name__',
+    ),
+    [(k) => -_.uniq(allSeries.map((s) => s.labels[k])).length, (k) => k.length],
+  );
+  const getSeriesName = (series: TooltipSeries): string => {
+    if (series.name) {
+      return series.name;
+    }
+    const name = series.labels?.__name__ ?? '';
+    const otherLabels = _.intersection(allSeriesSorted, Object.keys(series.labels));
+    return `${name}{${_.map(otherLabels, (l) => `${l}=${series.labels[l]}`).join(',')}}`;
+  };
 
   return (
     <>
@@ -223,7 +253,7 @@ const Tooltip_: React.FC<TooltipProps> = ({ activePoints, center, height, style,
               {allSeries.map((s, i) => (
                 <div className="query-browser__tooltip-series" key={i}>
                   <div className="query-browser__series-btn" style={{ backgroundColor: s.color }} />
-                  <div className="co-nowrap co-truncate">{s.name}</div>
+                  <div className="co-nowrap co-truncate">{getSeriesName(s)}</div>
                   <div className="query-browser__tooltip-value">{s.value}</div>
                 </div>
               ))}
@@ -265,12 +295,6 @@ const LegendContainer = ({ children }: { children?: React.ReactNode }) => {
 const Null = () => null;
 const nullComponent = <Null />;
 
-const formatLabels = (labels: PrometheusLabels) => {
-  const name = labels?.__name__ ?? '';
-  const otherLabels = _.omit(labels, '__name__');
-  return `${name}{${_.map(otherLabels, (v, k) => `${k}=${v}`).join(',')}}`;
-};
-
 type GraphSeries = GraphDataPoint[] | null;
 
 const getXDomain = (endTime: number, span: number): AxisDomain => [endTime - span, endTime];
@@ -289,6 +313,7 @@ const Graph: React.FC<GraphProps> = React.memo(
   }) => {
     const data: GraphSeries[] = [];
     const tooltipSeriesNames: string[] = [];
+    const tooltipSeriesLabels: PrometheusLabels[] = [];
     const legendData: { name: string }[] = [];
     const { t } = useTranslation();
 
@@ -310,7 +335,7 @@ const Graph: React.FC<GraphProps> = React.memo(
           legendData.push({ name });
           tooltipSeriesNames.push(name);
         } else {
-          tooltipSeriesNames.push(formatLabels(metric));
+          tooltipSeriesLabels.push(metric);
         }
       });
     });
@@ -394,7 +419,12 @@ const Graph: React.FC<GraphProps> = React.memo(
             const color = colors[i % colors.length];
             const style = {
               data: { [isStack ? 'fill' : 'stroke']: color },
-              labels: { fill: color, name: tooltipSeriesNames[i], units },
+              labels: {
+                fill: color,
+                labels: tooltipSeriesLabels[i],
+                name: tooltipSeriesNames[i],
+                units,
+              },
             };
             return (
               // We need to use the `name` prop to prevent an error in VictorySharedEvents when
@@ -982,7 +1012,7 @@ type TooltipProps = {
   activePoints?: { x: number; y: number; _y1?: number }[];
   center?: { x: number; y: number };
   height?: number;
-  style?: { fill: string; name: string; units: string };
+  style?: { fill: string; labels: PrometheusLabels; name: string; units: string };
   width?: number;
   x?: number;
 };
