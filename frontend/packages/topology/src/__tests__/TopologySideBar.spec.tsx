9c07e4635f4d8a4f8c36f816c3d30280ed126ddc
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-10-28 19:54:32 +0530

    fix unit tests for resizable topology side bar

--- frontend/packages/topology/src/__tests__/TopologySideBar.spec.tsx
+++ frontend/packages/topology/src/__tests__/TopologySideBar.spec.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Button } from '@patternfly/react-core';
 import { shallow } from 'enzyme';
+import { useUserSettings } from '@console/shared';
 import CloseButton from '@console/shared/src/components/close-button';
 import TopologySideBar from '../components/side-bar/TopologySideBar';
+
+jest.mock('@console/shared/src/hooks/useUserSettings', () => ({
+  useUserSettings: jest.fn(),
+}));
+
+const mockUserSettings = useUserSettings as jest.Mock;
 
 describe('TopologySideBar:', () => {
   const props = {
@@ -11,11 +18,13 @@ describe('TopologySideBar:', () => {
   };
 
   it('renders a SideBar', () => {
+    mockUserSettings.mockReturnValue([100, () => {}, true]);
     const wrapper = shallow(<TopologySideBar {...props} />);
     expect(wrapper.exists()).toBeTruthy();
   });
 
   it('clicking on close button should call the onClose callback function', () => {
+    mockUserSettings.mockReturnValue([100, () => {}, true]);
     const onClose = jest.fn();
     const wrapper = shallow(<TopologySideBar onClose={onClose} />);
     wrapper
