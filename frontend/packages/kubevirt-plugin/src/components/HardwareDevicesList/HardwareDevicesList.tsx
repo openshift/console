f543b7fb2b9b2deaf373bebdd4148a8e97dcd085
Aviv Turgeman <aturgema@redhat.com>
2021-11-02 13:27:54 +0200

    Bug 2015115: [feature] PCI passthrough - attach/detach HW modal to VM
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- frontend/packages/kubevirt-plugin/src/components/HardwareDevicesList/HardwareDevicesList.tsx
+++ frontend/packages/kubevirt-plugin/src/components/HardwareDevicesList/HardwareDevicesList.tsx
@@ -1,45 +1,101 @@
 import * as React from 'react';
-import { V1GPU, V1HostDevice } from '../../types/api';
+import { EmptyState, GridItem, Split, SplitItem, Text, TextVariants } from '@patternfly/react-core';
+import { useTranslation } from 'react-i18next';
+import AddButton from '../AddButton/AddButton';
 import { HardwareDevicesListRow, HardwareDevicesListRowProps } from './HardwareDevicesListRow';
+import {
+  HardwareDevicesListRowAddDevice,
+  HardwareDevicesListRowAddDeviceProps,
+} from './HardwareDevicesListRowAddDevice';
+
+export type HardwareDevice = {
+  // Common denominator of V1GPU and V1HostDevice
+  deviceName: string; // 'deviceName' is the resource name of the host device exposed by a device plugin
+  name: string; // Name of the host device as exposed by a device plugin
+};
 
 export type HardwareDevicesListProps = {
-  devices: V1GPU[] | V1HostDevice[];
-  onDetachHandler?: (id?: any) => void;
-  onCancelAttachHandler?: () => void;
-} & HardwareDevicesListRowProps;
+  devices: HardwareDevice[];
+  noDevicesFoundText?: string;
+  addDeviceText?: string;
+  onAttachHandler?: () => void;
+  showAddDeviceRow?: boolean;
+} & HardwareDevicesListRowProps &
+  HardwareDevicesListRowAddDeviceProps;
 
 const HardwareDevicesList: React.FC<HardwareDevicesListProps> = ({
   devices,
   onDetachHandler,
   onCancelAttachHandler,
-  isAttachDevice,
-  isNameEmpty,
-  isNameUsed,
-  textProps,
-  selectProps,
-  isBlur,
-}) => (
-  <>
-    {devices?.map((device) => (
-      <HardwareDevicesListRow
-        textProps={{ isReadOnly: true }}
-        deviceName={device?.deviceName}
-        name={device?.name}
-        onClick={() => onDetachHandler(device?.name)}
-      />
-    ))}
-    {isAttachDevice && (
-      <HardwareDevicesListRow
-        isBlur={isBlur}
-        isNameUsed={isNameUsed}
-        isNameEmpty={isNameEmpty}
-        isAttachDevice={isAttachDevice}
-        onClick={onCancelAttachHandler}
-        textProps={{ ...textProps, isReadOnly: false }}
-        selectProps={{ ...selectProps }}
-      />
-    )}
-  </>
-);
+  showAddDeviceRow,
+  name,
+  onNameChange,
+  onValidateName,
+  onResetValidateName,
+  deviceName,
+  noDevicesFoundText = 'No devices found',
+  addDeviceText,
+  onAttachHandler,
+  onDeviceNameChange,
+  isUserForbidden,
+}) => {
+  const { t } = useTranslation();
+  const showEmptyState = !(devices?.length > 0 || showAddDeviceRow);
+
+  const headers = (
+    <>
+      <GridItem span={5}>
+        <Text component={TextVariants.h4}>{t('kubevirt-plugin~Name')}</Text>
+      </GridItem>
+      <GridItem span={6}>
+        <Text component={TextVariants.h4}>{t('kubevirt-plugin~Device name')}</Text>
+      </GridItem>
+    </>
+  );
+
+  const addButton = (
+    <Split>
+      <SplitItem>
+        <AddButton
+          isDisabled={showAddDeviceRow || isUserForbidden}
+          onClick={onAttachHandler}
+          btnText={addDeviceText}
+        />
+      </SplitItem>
+    </Split>
+  );
+
+  return (
+    <>
+      {showEmptyState ? (
+        <EmptyState>{noDevicesFoundText}</EmptyState>
+      ) : (
+        <>
+          {headers}
+          {devices?.map((device) => (
+            <HardwareDevicesListRow
+              name={device?.name}
+              deviceName={device?.deviceName}
+              onDetachHandler={() => onDetachHandler(device?.name)}
+              isUserForbidden={isUserForbidden}
+            />
+          ))}
+          {showAddDeviceRow && (
+            <HardwareDevicesListRowAddDevice
+              onCancelAttachHandler={onCancelAttachHandler}
+              name={name}
+              onNameChange={onNameChange}
+              onValidateName={onValidateName}
+              onResetValidateName={onResetValidateName}
+              deviceName={deviceName}
+              onDeviceNameChange={onDeviceNameChange}
+            />
+          )}
+        </>
+      )}
+      {addButton}
+    </>
+  );
+};
 
 export default HardwareDevicesList;
