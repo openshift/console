811689aaabe955a557c5895c74e7d3c1185a61b9
karthik <karthik.jk11@gmail.com>
2021-11-16 15:12:20 +0530

    Remove Tech preview badge for the triggers component for triggers GA operator (1.6)

--- frontend/packages/pipelines-plugin/src/utils/hooks.ts
+++ frontend/packages/pipelines-plugin/src/utils/hooks.ts
@@ -1,12 +1,21 @@
 import { BadgeType, getBadgeFromType } from '@console/shared';
 import {
   isGAVersionInstalled,
+  isTriggersGAVersion,
   usePipelineOperatorVersion,
 } from '../components/pipelines/utils/pipeline-operator';
+
+const getOSPTechPreviewBadge = (installedGAVersion: boolean) =>
+  installedGAVersion ? null : getBadgeFromType(BadgeType.TECH);
 
 export const usePipelineTechPreviewBadge = (namespace: string) => {
   const operator = usePipelineOperatorVersion(namespace);
   if (!operator) return null;
-  const installedGA = isGAVersionInstalled(operator);
-  return installedGA ? null : getBadgeFromType(BadgeType.TECH);
+  return getOSPTechPreviewBadge(isGAVersionInstalled(operator));
+};
+
+export const useTriggersTechPreviewBadge = (namespace: string) => {
+  const operator = usePipelineOperatorVersion(namespace);
+  if (!operator) return null;
+  return getOSPTechPreviewBadge(isTriggersGAVersion(operator));
 };
