d5ae3ac7a814fa9db392bf375545d930635beea1
Jakub Hadvig <jhadvig@redhat.com>
2021-11-17 12:20:14 +0100

    Bug 2024115: Remove logging from proxy package

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"k8s.io/klog"
 )
 
 var websocketPingInterval = 30 * time.Second
@@ -100,9 +99,6 @@ func CopyRequestHeaders(originalRequest, newRequest *http.Request) {
 }
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	klog.Infof("PROXY: %#q\n", r.URL)
-
 	// Block scripts from running in proxied content for browsers that support Content-Security-Policy.
 	w.Header().Set("Content-Security-Policy", "sandbox;")
 	// Add `X-Content-Security-Policy` for IE11 and older browsers.
