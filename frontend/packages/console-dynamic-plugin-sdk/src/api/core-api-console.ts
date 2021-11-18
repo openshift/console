6b8581c02bd50698cbf407f1477c7ef0d7b8ad2a
Bryan Florkiewicz <bflorkie@redhat.com>
2021-11-17 20:12:04 -0500

    Code reviw comments - create new files for APIs that will migrate

--- frontend/packages/console-dynamic-plugin-sdk/src/api/core-api-console.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import {
-  UseActivePerspective,
-} from '../extensions/console-types';
-
-export const useActivePerspective: UseActivePerspective = require('@console/dynamic-plugin-sdk/src/perspective/useActivePerspective')
-  .default;
