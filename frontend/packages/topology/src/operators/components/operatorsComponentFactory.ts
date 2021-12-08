c598f36a421f2527b51274dc34c03933d0f6a0c4
Yohaan Vakil <yvakil@redhat.com>
2021-12-08 14:43:55 +0530

    use dynamic context menu servicebinding and VMNode

--- frontend/packages/topology/src/operators/components/operatorsComponentFactory.ts
+++ frontend/packages/topology/src/operators/components/operatorsComponentFactory.ts
@@ -1,29 +1,14 @@
 import * as React from 'react';
 import { GraphElement, withSelection } from '@patternfly/react-topology';
-import { Kebab, kebabOptionsToMenu } from '@console/internal/components/utils';
-import { modelFor, referenceFor } from '@console/internal/module/k8s';
 import { contextMenuActions } from '../../actions';
 import {
   withContextMenu,
   withNoDrop,
-  createMenuItems,
   ServiceBinding,
 } from '../../components/graph-view/components';
 import { TYPE_SERVICE_BINDING } from '../../const';
-import { OdcBaseEdge } from '../../elements';
 import { TYPE_OPERATOR_BACKED_SERVICE } from './const';
 import OperatorBackedService from './OperatorBackedService';
-
-const serviceBindingActions = (edge: OdcBaseEdge) => {
-  const sbr = edge.getResource();
-  const { common } = Kebab.factory;
-  const menuActions = [
-    ...Kebab.getExtensionsActionsForKind(modelFor(referenceFor(sbr))),
-    ...common,
-  ];
-  const actions = menuActions.map((a) => a(modelFor(referenceFor(sbr)), sbr));
-  return createMenuItems(kebabOptionsToMenu(actions));
-};
 
 export const getOperatorsComponentFactory = (
   kind,
@@ -35,7 +20,7 @@ export const getOperatorsComponentFactory = (
         withContextMenu(contextMenuActions)(withNoDrop()(OperatorBackedService)),
       );
     case TYPE_SERVICE_BINDING:
-      return withContextMenu(serviceBindingActions)(ServiceBinding);
+      return withContextMenu(contextMenuActions)(ServiceBinding);
     default:
       return undefined;
   }
