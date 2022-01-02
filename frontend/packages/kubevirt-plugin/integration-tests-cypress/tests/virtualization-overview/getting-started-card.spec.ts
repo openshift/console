557851c4a61aa0a56ecb9a32fd046ed3db6d0a85
Leon Kladnitsky <lkladnit@redhat.com>
2022-01-02 03:10:03 +0200

    Bug 2035602 - [e2e][automation] add tests for Virtualization Overview page cards

--- frontend/packages/kubevirt-plugin/integration-tests-cypress/tests/virtualization-overview/getting-started-card.spec.ts
+++ frontend/packages/kubevirt-plugin/integration-tests-cypress/tests/virtualization-overview/getting-started-card.spec.ts
@@ -1,41 +1,37 @@
+import { adminOnlyDescribe } from '../../utils/const';
 import * as virtOverview from '../../views/virtualization-overview';
 
 const EXIST = 'exist';
 const NOT_EXIST = 'not.exist';
 
-describe('Test Getting started card of the Virtualization Overview page', () => {
+adminOnlyDescribe('Test Getting started card of the Virtualization Overview page', () => {
   before(() => {
     cy.Login();
     cy.visit('/virtualization/overview');
   });
 
-  it('Getting started card is displayed', () => {
+  it('ID(CNV-7940) Getting started card is displayed and can be toggled', () => {
     cy.get(virtOverview.gettingStartedCard).should(EXIST);
-  });
-
-  it('Getting started card can be hidden', () => {
     cy.get(virtOverview.hideGettingStartedCardKebab).click();
     cy.get(virtOverview.hideGettingStartedCardTooltip).should(EXIST);
     cy.get(virtOverview.hideGettingStartedCardTooltip).click();
 
     cy.get(virtOverview.gettingStartedCard).should(NOT_EXIST);
-  });
 
-  it('Getting started card can be restored', () => {
     cy.get(virtOverview.restoreGettingStartedBtn).should(EXIST);
     cy.get(virtOverview.restoreGettingStartedBtn).click();
     cy.get(virtOverview.gettingStartedCard).should(EXIST);
   });
 
-  it('Quick starts card is displayed', () => {
+  it('ID(CNV-7927) Quick starts card is displayed', () => {
     cy.get(virtOverview.quickStartCard).should(EXIST);
   });
 
-  it('Feature highlights card is displayed', () => {
+  it('ID(CNV-7924) Feature highlights card is displayed', () => {
     cy.get(virtOverview.featureHighlightsCard).should(EXIST);
   });
 
-  it('Recommended operators card is displayed', () => {
+  it('ID(CNV-7925) Recommended operators card is displayed', () => {
     cy.get(virtOverview.recommendedOperatorsCard).should(EXIST);
   });
 });
