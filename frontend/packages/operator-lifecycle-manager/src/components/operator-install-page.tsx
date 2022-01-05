fc3160920d00c68df5db1a3e746a1bcedfe42e80
Rebecca Alpert <ralpert@redhat.com>
2022-01-05 12:00:04 -0500

    Bug 2035910: Show manual approval options
    It looks like the linter moved some parentheses around and broke the logic that shows the manual approval options.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2035910

--- frontend/packages/operator-lifecycle-manager/src/components/operator-install-page.tsx
+++ frontend/packages/operator-lifecycle-manager/src/components/operator-install-page.tsx
@@ -125,25 +125,24 @@ const InstallNeedsApprovalMessage: React.FC<InstallNeedsApprovalMessageProps> = 
       </h2>
       <ActionGroup className="pf-c-form pf-c-form__group--no-top-margin">
         <InstallPlanReview installPlan={installObj} />
-        {(installObjIsInstallPlan && canPatchInstallPlans) ||
-          (!installObjIsInstallPlan && (
-            <>
-              <Button variant="primary" onClick={approve}>
-                {t('olm~Approve')}
+        {((installObjIsInstallPlan && canPatchInstallPlans) || !installObjIsInstallPlan) && (
+          <>
+            <Button variant="primary" onClick={approve}>
+              {t('olm~Approve')}
+            </Button>
+            <Link
+              to={`${resourcePathFromModel(
+                SubscriptionModel,
+                subscriptionObj?.metadata?.name,
+                namespace,
+              )}?showDelete=true`}
+            >
+              <Button className="co-clusterserviceversion__button" variant="secondary">
+                {t('olm~Deny')}
               </Button>
-              <Link
-                to={`${resourcePathFromModel(
-                  SubscriptionModel,
-                  subscriptionObj?.metadata?.name,
-                  namespace,
-                )}?showDelete=true`}
-              >
-                <Button className="co-clusterserviceversion__button" variant="secondary">
-                  {t('olm~Deny')}
-                </Button>
-              </Link>
-            </>
-          ))}
+            </Link>
+          </>
+        )}
         {!canPatchInstallPlans && installObjIsInstallPlan && (
           <NeedInstallPlanPermissions installPlan={installObj as InstallPlanKind} />
         )}
