5d7715032559ae45ca84fd5f72dd0ae0364cfe1b
Aviv Turgeman <aturgema@redhat.com>
2022-01-31 19:16:32 +0200

    Bug 2048631: missing volumes list in snapshot modal
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- frontend/packages/kubevirt-plugin/src/components/modals/snapshot-modal/SupportedSnapshotVolumesList.tsx
+++ frontend/packages/kubevirt-plugin/src/components/modals/snapshot-modal/SupportedSnapshotVolumesList.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import { ExpandableSection, Stack, StackItem } from '@patternfly/react-core';
+import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 
 const SupportedSnapshotVolumesList = ({ supportedVolumes }: SupportedSnapshotVolumesListProps) => {
   const { t } = useTranslation();
-
-  if (supportedVolumes) {
+  const [isExpanded, setIsExpanded] = React.useState<boolean>(true);
+  if (_.isEmpty(supportedVolumes)) {
     return null;
   }
 
   return (
     <StackItem>
       <ExpandableSection
+        isExpanded={isExpanded}
+        onClick={() => setIsExpanded((prev) => !prev)}
         toggleText={t('kubevirt-plugin~Disks included in this snapshot ({{count}})', {
           count: supportedVolumes?.length,
         })}
