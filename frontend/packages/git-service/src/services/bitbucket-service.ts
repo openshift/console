e3a0b1414d35e05a0d084777b7cef0c7f507a01e
Rebecca Alpert <ralpert@redhat.com>
2022-02-07 17:37:52 -0500

    Update parsebitbucketurl import

--- frontend/packages/git-service/src/services/bitbucket-service.ts
+++ frontend/packages/git-service/src/services/bitbucket-service.ts
@@ -1,5 +1,5 @@
 import { Base64 } from 'js-base64';
-import * as ParseBitbucketUrl from 'parse-bitbucket-url';
+import ParseBitbucketUrl from 'parse-bitbucket-url';
 import 'whatwg-fetch';
 import {
   GitSource,
