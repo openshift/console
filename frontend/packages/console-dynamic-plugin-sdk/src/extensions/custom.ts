ad667aeb1f49c14d783eabf0f11699ae428735ec
Vojtech Szocs <vszocs@redhat.com>
2022-02-14 20:01:07 +0100

    Improve CustomExtension type doc

--- frontend/packages/console-dynamic-plugin-sdk/src/extensions/custom.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/extensions/custom.ts
@@ -3,6 +3,8 @@ import { ExtensionDeclaration } from '../types';
 /**
  * Custom Console extension with arbitrary `type` and `properties`.
  *
- * This type represents all non-standard Console extension declarations.
+ * This is a special type representing all non-standard Console extension declarations.
+ *
+ * This allows dynamic plugins to consume extensions which are specific to other plugins.
  */
 export type CustomExtension = ExtensionDeclaration<string, { [key: string]: any }>;
