2011f869169f0db81a072b8d2da6e8b198ad4fcd
Bipul Adhikari <badhikar@redhat.com>
2022-02-16 13:47:41 +0545

    Validate core extensions only

--- frontend/packages/console-dynamic-plugin-sdk/src/validation/SchemaValidator.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/validation/SchemaValidator.ts
@@ -7,13 +7,21 @@ export class SchemaValidator {
 
   readonly assert: ValidationAssertions;
 
+  knownExtensions: string[];
+
   constructor(description: string, private readonly ajv = new Ajv({ allErrors: true })) {
     this.result = new ValidationResult(description);
     this.assert = new ValidationAssertions(this.result);
   }
 
-  validate(schema: {}, data: any, dataVar: string = 'obj') {
-    if (!this.ajv.validate(schema, data)) {
+  validate(schema: any, data: any, dataVar: string = 'obj') {
+    if (!this.knownExtensions) {
+      this.knownExtensions = Object.entries(schema.definitions).reduce((acc, [, v]: any) => {
+        return v?.properties?.type?.const ? [v.properties.type.const, ...acc] : acc;
+      }, []);
+    }
+    const refinedData = data.filter((datum) => this.knownExtensions.includes(datum.type));
+    if (!this.ajv.validate(schema, refinedData)) {
       this.ajv.errors.forEach((error) => {
         // This format is consistent with ajv.errorsText() implementation
         this.result.addError(`${dataVar}${error.dataPath} ${error.message}`);
