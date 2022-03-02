d1750f5d94d92ed9764ddbba2c9310a356b7591f
Bryan Florkiewicz <bflorkie@redhat.com>
2022-03-02 14:32:00 -0500

    Fix linter errors

--- frontend/packages/console-shared/src/components/actions/types.ts
+++ frontend/packages/console-shared/src/components/actions/types.ts
@@ -1,12 +1,9 @@
 import { Action, ActionGroup } from '@console/dynamic-plugin-sdk';
+import { ActionContext as ActionContextType } from '@console/dynamic-plugin-sdk/src/api/internal-types';
 
-export type {
-  ActionContext
-} from '@console/dynamic-plugin-sdk/src/api/internal-types';
+export { ActionMenuVariant } from '@console/dynamic-plugin-sdk/src/api/internal-types';
 
-export {
-  ActionMenuVariant
-} from '@console/dynamic-plugin-sdk/src/api/internal-types';
+export type ActionContext = ActionContextType;
 
 export type MenuOption = Action | GroupedMenuOption;
 
