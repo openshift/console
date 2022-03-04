a59453aededf1db53997fec99ff6d1b563d88f12
Vikram Raj <vikrmrj@gmail.com>
2022-03-04 14:04:02 +0530

    fix unit tests

--- frontend/packages/helm-plugin/src/components/details-page/__tests__/HelmReleaseNotes.spec.tsx
+++ frontend/packages/helm-plugin/src/components/details-page/__tests__/HelmReleaseNotes.spec.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { SyncMarkdownView } from '@console/internal/components/markdown-view';
+import { useUserSettings } from '@console/shared';
 import { mockHelmReleases } from '../../__tests__/helm-release-mock-data';
 import HelmReleaseNotes from '../notes/HelmReleaseNotes';
 import HelmReleaseNotesEmptyState from '../notes/HelmReleaseNotesEmptyState';
@@ -9,13 +10,21 @@ jest.mock('@console/shared/src/hooks/useResizeObserver', () => ({
   useResizeObserver: jest.fn(),
 }));
 
+jest.mock('@console/shared/src/hooks/useUserSettings', () => ({
+  useUserSettings: jest.fn(),
+}));
+
+const mockUserSettings = useUserSettings as jest.Mock;
+
 describe('HelmReleaseNotes', () => {
   it('should render the SyncMarkdownView component when notes are available', () => {
+    mockUserSettings.mockReturnValue(['light', jest.fn(), true]);
     const helmReleaseResources = mount(<HelmReleaseNotes customData={mockHelmReleases[0]} />);
     expect(helmReleaseResources.find(SyncMarkdownView).exists()).toBe(true);
   });
 
   it('should render empty state when release notes are not given', () => {
+    mockUserSettings.mockReturnValue(['light', jest.fn(), true]);
     const component = mount(<HelmReleaseNotes customData={mockHelmReleases[1]} />);
     expect(component.find(HelmReleaseNotesEmptyState)).toHaveLength(1);
   });
--- frontend/packages/helm-plugin/src/topology/__tests__/TopologyHelmReleaseNotesPanel.spec.tsx
+++ frontend/packages/helm-plugin/src/topology/__tests__/TopologyHelmReleaseNotesPanel.spec.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { shallow, mount } from 'enzyme';
 import { SyncMarkdownView } from '@console/internal/components/markdown-view';
 import { SidebarSectionHeading } from '@console/internal/components/utils';
+import { useUserSettings } from '@console/shared';
 import TopologyGroupResourcesPanel from '@console/topology/src/components/side-bar/TopologyGroupResourcesPanel';
 import HelmReleaseNotesEmptyState from '../../components/details-page/notes/HelmReleaseNotesEmptyState';
 import TopologyHelmReleaseNotesPanel from '../TopologyHelmReleaseNotesPanel';
@@ -10,6 +11,12 @@ import { mockManifest, mockReleaseNotes } from './mockData';
 jest.mock('@console/shared/src/hooks/useResizeObserver', () => ({
   useResizeObserver: jest.fn(),
 }));
+
+jest.mock('@console/shared/src/hooks/useUserSettings', () => ({
+  useUserSettings: jest.fn(),
+}));
+
+const mockUserSettings = useUserSettings as jest.Mock;
 
 describe('TopologyHelmReleaseResourcesPanel', () => {
   const manifestResources = mockManifest;
@@ -29,11 +36,13 @@ describe('TopologyHelmReleaseNotesPanel', () => {
   const releaseNotes = mockReleaseNotes;
 
   it('should render markdown when release notes are given', () => {
+    mockUserSettings.mockReturnValue(['light', jest.fn(), true]);
     const component = mount(<TopologyHelmReleaseNotesPanel releaseNotes={releaseNotes} />);
     expect(component.find(SyncMarkdownView)).toHaveLength(1);
   });
 
   it('should render empty state when release notes are not given', () => {
+    mockUserSettings.mockReturnValue(['light', jest.fn(), true]);
     const component = mount(<TopologyHelmReleaseNotesPanel releaseNotes="" />);
     expect(component.find(HelmReleaseNotesEmptyState)).toHaveLength(1);
   });
