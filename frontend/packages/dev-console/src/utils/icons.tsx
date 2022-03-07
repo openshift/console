26c02f12f3cd0a5c24659dd6509cba0455a75d6b
Vikram Raj <vikrmrj@gmail.com>
2022-03-07 15:18:22 +0530

    [Dark theme]Fix namespaced pages, add page and catalogs page

--- frontend/packages/dev-console/src/utils/icons.tsx
+++ frontend/packages/dev-console/src/utils/icons.tsx
@@ -6,19 +6,19 @@ import {
   LaptopCodeIcon,
   OsImageIcon,
   FileUploadIcon,
+  GitAltIcon,
+  OutlinedFileCodeIcon,
 } from '@patternfly/react-icons';
 import * as devfileIcon from '../images/devfile.svg';
 import * as dockerfileIcon from '../images/dockerfile.svg';
-import * as gitIcon from '../images/from-git.svg';
-import * as yamlIcon from '../images/yaml.svg';
 
-export const gitIconSVG = gitIcon;
+export const gitIconElement = <GitAltIcon />;
 
 export const devfileIconSVG = devfileIcon;
 
 export const dockerfileIconSVG = dockerfileIcon;
 
-export const yamlIconSVG = yamlIcon;
+export const yamlIconElement = <OutlinedFileCodeIcon />;
 
 export const deployIconElement = <OsImageIcon />;
 
