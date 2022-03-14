39539fe151401c4272b0d8bbebfa0735f2d4b91f
karthik <karthik.jk11@gmail.com>
2022-03-14 21:46:01 +0530

    Change the tekton hub api endpoint to use v1 api

--- frontend/packages/pipelines-plugin/src/components/catalog/const.ts
+++ frontend/packages/pipelines-plugin/src/components/catalog/const.ts
@@ -1,3 +1,3 @@
-export const TEKTON_HUB_API_ENDPOINT = 'https://api.hub.tekton.dev';
+export const TEKTON_HUB_API_ENDPOINT = 'https://api.hub.tekton.dev/v1';
 export const TEKTON_HUB_ENDPOINT = `https://hub.tekton.dev`;
 export const TEKTON_HUB_INTEGRATION_KEY = 'enable-devconsole-integration';
