dcff32f046736e8e302ff033e9cf61d8dcac179d
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-03-18 16:08:00 +0100

    Add a workaround to show the PF validation status also when the input field is autofilled.

--- frontend/packages/console-shared/src/components/formik-fields/InputField.tsx
+++ frontend/packages/console-shared/src/components/formik-fields/InputField.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
-import { TextInput, TextInputTypes } from '@patternfly/react-core';
+import { TextInput, TextInputTypes, ValidatedOptions } from '@patternfly/react-core';
 import BaseInputField from './BaseInputField';
 import { BaseInputFieldProps } from './field-types';
+
+import './InputField.scss';
 
 const InputField: React.FC<BaseInputFieldProps> = (
   { type = TextInputTypes.text, ...baseProps },
   ref,
 ) => (
   <BaseInputField type={type} {...baseProps}>
-    {(props) => <TextInput ref={ref} {...props} />}
+    {(props) => (
+      <div className="oc-inputfield">
+        <TextInput ref={ref} {...props} />
+        {props.validated && props.validated !== ValidatedOptions.default ? (
+          <div
+            // pf-c-form-control is needed to load the right PatternFly variables.
+            className={`pf-c-form-control oc-inputfield__validation-icon ${props.validated}`}
+            // The BaseInputField will show an description (helper-text) below
+            // the input field that describes the validation error.
+            aria-hidden="true"
+          />
+        ) : null}
+      </div>
+    )}
   </BaseInputField>
 );
 
