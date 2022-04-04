a065059cdda6222a7c38087e51e3ee92597bd9a9
Andrew Ballantyne <aballant@redhat.com>
2022-04-04 12:05:06 -0400

    Translate Extensions On Each Language Change

--- frontend/packages/console-plugin-sdk/src/utils/extension-i18n.ts
+++ frontend/packages/console-plugin-sdk/src/utils/extension-i18n.ts
@@ -1,5 +1,9 @@
 import { TFunction } from 'i18next';
-import { deepForOwn } from '@console/dynamic-plugin-sdk/src/utils/object';
+import {
+  deepForOwn,
+  PredicateCheck,
+  ValueCallback,
+} from '@console/dynamic-plugin-sdk/src/utils/object';
 import { Extension } from '../typings';
 
 export const isTranslatableString = (value): value is string => {
@@ -11,12 +15,20 @@ export const isTranslatableString = (value): value is string => {
 export const getTranslationKey = (value: string) =>
   isTranslatableString(value) ? value.substr(1, value.length - 2) : undefined;
 
+export const translateExtensionDeep = <E extends Extension>(
+  extension: E,
+  translationStringPredicate: PredicateCheck<string>,
+  cb: ValueCallback<string>,
+): void => {
+  deepForOwn(extension.properties, translationStringPredicate, cb);
+};
+
 /**
  * Recursively updates the extension's properties, replacing all translatable string values
  * via the provided `t` function.
  */
 export const translateExtension = <E extends Extension>(extension: E, t: TFunction): E => {
-  deepForOwn(extension.properties, isTranslatableString, (value, key, obj) => {
+  translateExtensionDeep(extension, isTranslatableString, (value, key, obj) => {
     obj[key] = t(value);
   });
 
