664711e82bb4b946917f139929359906891df99a
Andrew Ballantyne <aballant@redhat.com>
2022-04-04 12:05:06 -0400

    Translate Extensions On Each Language Change

--- frontend/packages/console-dynamic-plugin-sdk/src/utils/object.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/utils/object.ts
@@ -2,29 +2,37 @@ import * as _ from 'lodash';
 
 const isPlainNonReactObject = (obj: any) => _.isPlainObject(obj) && !obj.$$typeof;
 
+export type ValueCallback<T> = (value: T, key: string, container: {}, path: string) => void;
+export type PredicateCheck<T> = (value: unknown, path: string) => value is T;
+
 /**
  * Recursive equivalent of `_.forOwn` function that traverses plain objects and arrays.
  */
 export const deepForOwn = <T = any>(
   obj: {},
-  predicate: (value: any) => value is T,
-  valueCallback: (value: T, key: string, container: {}) => void,
+  predicate: PredicateCheck<T>,
+  valueCallback: ValueCallback<T>,
+  pathParts: string[] = [],
 ) => {
-  const visitValue = (value: any, key: string, container: {}) => {
-    if (predicate(value)) {
-      valueCallback(value, key, container);
+  const visitValue = (value: any, key: string, container: {}, newPathParts: string[]) => {
+    const path = newPathParts.join('.');
+    if (predicate(value, path)) {
+      valueCallback(value, key, container, path);
     } else if (isPlainNonReactObject(value)) {
-      deepForOwn(value, predicate, valueCallback);
+      deepForOwn(value, predicate, valueCallback, newPathParts);
     }
   };
 
-  _.forOwn<any>(obj, (value, key, container) => {
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+    const newPathParts = [...pathParts, key];
     if (Array.isArray(value)) {
       value.forEach((arrayElement, index) => {
-        visitValue(arrayElement, index.toString(), value);
+        const indexKey = index.toString();
+        visitValue(arrayElement, indexKey, value, [...newPathParts, indexKey]);
       });
     } else {
-      visitValue(value, key, container);
+      visitValue(value, key, obj, newPathParts);
     }
   });
 };
