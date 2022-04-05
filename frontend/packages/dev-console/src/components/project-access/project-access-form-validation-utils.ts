5fd05de37efaa8a371949226fc9c2f63a68a3cb2
debsmita1 <debsmita.santra@gmail.com>
2022-04-06 01:18:04 +0530

    fix rolebinding in DevConsole dropping all subjects when updating

--- frontend/packages/dev-console/src/components/project-access/project-access-form-validation-utils.ts
+++ frontend/packages/dev-console/src/components/project-access/project-access-form-validation-utils.ts
@@ -1,10 +1,11 @@
 import * as yup from 'yup';
+import i18n from '@console/internal/i18n';
 
 export const validationSchema = yup.object().shape({
   projectAccess: yup.array().of(
     yup.object().shape({
-      user: yup.string().required('Required'),
-      role: yup.string().required('Required'),
+      subject: yup.object().shape({ name: yup.string().required(i18n.t('devconsole~Required')) }),
+      role: yup.string().required(i18n.t('devconsole~Required')),
     }),
   ),
 });
