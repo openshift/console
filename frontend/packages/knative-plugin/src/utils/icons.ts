7842935b0cf9da8115e0a0f6e083272d3e87190f
Vikram Raj <vikrmrj@gmail.com>
2022-04-25 19:27:36 +0530

    change Event Sink and Event Source icons

--- frontend/packages/knative-plugin/src/utils/icons.ts
+++
@@ -1,6 +0,0 @@
-import * as channelIcon from '../imgs/channel.svg';
-import * as eventSourceIcon from '../imgs/event-source.svg';
-
-export const eventSourceIconSVG = eventSourceIcon;
-
-export const channelIconSVG = channelIcon;
---
+++ frontend/packages/knative-plugin/src/utils/icons.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { SignInAltIcon, SignOutAltIcon } from '@patternfly/react-icons';
+import * as channelIcon from '../imgs/channel.svg';
+
+export const eventSourceIcon = <SignOutAltIcon />;
+
+export const eventSinkIcon = <SignInAltIcon />;
+
+export const channelIconSVG = channelIcon;
