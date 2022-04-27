04eab50c3161277b6ec1f2b69385db9082ea8c65
Pranshu Srivastava <rexagod@gmail.com>
2022-04-27 21:23:30 +0530

    Update MON_DISK_LOW whitelisted alert documentation URL

--- frontend/packages/ceph-storage-plugin/src/components/dashboards/persistent-internal/status-card/whitelisted-health-checks.ts
+++ frontend/packages/ceph-storage-plugin/src/components/dashboards/persistent-internal/status-card/whitelisted-health-checks.ts
@@ -1,4 +1,4 @@
 export const whitelistedHealthChecksRef = {
   MON_DISK_LOW:
-    'https://access.redhat.com/documentation/en-us/red_hat_openshift_data_foundation/4.9/html-single/troubleshooting_openshift_data_foundation#resolving-cluster-health-issues_rhodf',
+    'https://access.redhat.com/documentation/en-us/red_hat_openshift_data_foundation/4.10/html-single/troubleshooting_openshift_data_foundation#resolving-cluster-health-issues_rhodf',
 };
