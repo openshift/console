595449c7b02a802fd2d17db00716ecbafb17446a
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-05-03 12:18:24 +0200

    Fix crash when stored topology layout isn't supported anymore

--- frontend/packages/topology/src/components/graph-view/layouts/layoutFactory.ts
+++ frontend/packages/topology/src/components/graph-view/layouts/layoutFactory.ts
@@ -3,8 +3,12 @@ import TopologyColaLayout from './TopologyColaLayout';
 
 const COLA_LAYOUT = 'Cola';
 
+const DEFAULT_LAYOUT = COLA_LAYOUT;
+
+const SUPPORTED_LAYOUTS = [COLA_LAYOUT];
+
 const layoutFactory: LayoutFactory = (type: string, graph: Graph): Layout | undefined => {
   return type === COLA_LAYOUT ? new TopologyColaLayout(graph, { layoutOnDrag: false }) : undefined;
 };
 
-export { layoutFactory, COLA_LAYOUT };
+export { COLA_LAYOUT, DEFAULT_LAYOUT, SUPPORTED_LAYOUTS, layoutFactory };
