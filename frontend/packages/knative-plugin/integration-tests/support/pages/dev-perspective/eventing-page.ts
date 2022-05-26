0fe0ed803294db53862273ad976f2ce00e927de8
sparsh kesari <sparshkesari98@gmail.com>
2022-05-26 08:46:27 +0530

    automation of eventing-camelk

--- frontend/packages/knative-plugin/integration-tests/support/pages/dev-perspective/eventing-page.ts
+++ frontend/packages/knative-plugin/integration-tests/support/pages/dev-perspective/eventing-page.ts
@@ -15,3 +15,67 @@ export const createEventPage = {
     });
   },
 };
+
+export const createCamelKSourceEvent = (optionName: string) => {
+  switch (optionName) {
+    case 'AWS SQS Source':
+      cy.get(eventingPO.awsSqsSource.accessKey).type('accessKey');
+      cy.get(eventingPO.awsSqsSource.queueName).type('queueNameOrArn');
+      cy.get(eventingPO.awsSqsSource.region).type('awsRegion');
+      cy.get(eventingPO.awsSqsSource.secretKey).type('secretKey');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    case 'Salesforce Source':
+      cy.get(eventingPO.saleforceSource.clientId).type('clientID');
+      cy.get(eventingPO.saleforceSource.clientSecret).type('clientSecret');
+      cy.get(eventingPO.saleforceSource.password).type('password');
+      cy.get(eventingPO.saleforceSource.query).type('query');
+      cy.get(eventingPO.saleforceSource.topicName).type('topicName');
+      cy.get(eventingPO.saleforceSource.userName).type('userName');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    case 'AWS Kinesis Source':
+      cy.get(eventingPO.awsKinesisSource.accessKey).type('accesskey');
+      cy.get(eventingPO.awsKinesisSource.region).type('awsregion');
+      cy.get(eventingPO.awsKinesisSource.secretKey).type('secretkey');
+      cy.get(eventingPO.awsKinesisSource.stream).type('streamName');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    case 'Jira Source':
+      cy.get(eventingPO.jiraSource.jiraURL).type('jiraUrl');
+      cy.get(eventingPO.jiraSource.password).type('password');
+      cy.get(eventingPO.jiraSource.userName).type('username');
+      cy.get(eventingPO.jiraSource.jql).type('jql');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    case 'Slack Source':
+      cy.get(eventingPO.slackSource.channel).type('channel');
+      cy.get(eventingPO.slackSource.token).type('token');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    case 'Telegram Source':
+      cy.get(eventingPO.telegramSource.authToken).type('authorizationToken');
+      cy.get(eventingPO.radioButtonresourceURI).click();
+      cy.get(eventingPO.resourceURI).type('http://cluster.example.com/svc');
+      cy.get(eventingPO.createConnector).click();
+      break;
+
+    default:
+      break;
+  }
+};
---
+++ frontend/packages/knative-plugin/integration-tests/support/step-definitions/eventing/eventing-camelk.ts
@@ -0,0 +1,44 @@
+import { Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { app } from '@console/dev-console/integration-tests/support/pages';
+import { topologyPO } from '@console/topology/integration-tests/support/page-objects/topology-po';
+import {
+  topologyHelper,
+  topologyPage,
+} from '@console/topology/integration-tests/support/pages/topology';
+import { eventingPO } from '../../pageObjects';
+import { createCamelKSourceEvent } from '../../pages/dev-perspective/eventing-page';
+
+Then('user is able to see multiple sources of the kind kamelets', () => {
+  cy.get(eventingPO.catlogTiles).should('exist');
+});
+
+When('user clicks on Event Sources card', () => {
+  cy.get(eventingPO.eventSourceCard).click();
+});
+
+Then('user will see the {string} card', (cardName: string) => {
+  cy.byTestID(`EventSource-${cardName}`).should('exist');
+});
+
+When('user clicks on the {string} card', (cardName: string) => {
+  cy.byTestID(`EventSource-${cardName}`).click();
+  cy.get(eventingPO.createCatalog)
+    .contains('Create Event Source')
+    .click({ force: true });
+});
+
+When('user creates {string}', (eventName: string) => {
+  createCamelKSourceEvent(eventName);
+});
+
+Then('user will be redirected to Topology page', () => {
+  topologyPage.verifyTopologyPage();
+});
+
+Then('user will see {string} connector', (connectorName: string) => {
+  topologyHelper.search(`kamelet-${connectorName}-source`);
+  cy.get(topologyPO.graph.fitToScreen).click();
+  cy.get(topologyPO.highlightNode).should('be.visible');
+  cy.get(topologyPO.graph.reset).click();
+  app.waitForDocumentLoad();
+});
