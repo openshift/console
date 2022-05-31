f7de34973f493e178daaf18dc2e685419130b146
Abhi <abhinandan13jan@gmail.com>
2022-05-31 22:13:30 +0530

    align with pfext-quickstart styling

--- frontend/packages/console-shared/src/components/markdown-extensions/multiline-execute-extension.ts
+++ frontend/packages/console-shared/src/components/markdown-extensions/multiline-execute-extension.ts
@@ -45,7 +45,7 @@ const useMultilineExecuteCommandShowdownExtension = () => {
                 </div>
               </div>
               <div class="pf-c-code-block__content">
-                <pre class="pf-c-code-block__pre ocs-code-block__pre">
+                <pre class="pf-c-code-block__pre pfext-code-block__pre">
                   <code class="pf-c-code-block__code" 
                     ${MARKDOWN_SNIPPET_ID}="${groupType}">${group}</code>
                 </pre>
