bb61e2702b69fb30ccd65c9b59f27d1b0e152694
gvudinh <gvudinh@redhat.com>
2022-06-03 15:31:53 -0400

    Bug 2092319: [Firefox] multi-line node status formatting issue
    Displays the 'Name' column text correctly when node is unschedulable in Firefox browser.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2092319

--- frontend/packages/console-dynamic-plugin-sdk/src/app/components/status/PopoverStatus.tsx
+++ frontend/packages/console-dynamic-plugin-sdk/src/app/components/status/PopoverStatus.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button, Popover, PopoverPosition } from '@patternfly/react-core';
+import './PopoverStatus.scss';
 
 type PopoverStatusProps = {
   statusBody: React.ReactNode;
@@ -32,7 +33,7 @@ const PopoverStatus: React.FC<PopoverStatusProps> = ({
       isVisible={isVisible}
       shouldClose={shouldClose}
     >
-      <Button variant="link" isInline>
+      <Button variant="link" isInline className="co-popover-status-button">
         {statusBody}
       </Button>
     </Popover>
