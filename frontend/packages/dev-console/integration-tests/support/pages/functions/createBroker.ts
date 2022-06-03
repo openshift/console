23d3070d5aada60f678e847cd80d72b92fffabe4
sparsh kesari <sparshkesari98@gmail.com>
2022-06-03 23:37:58 +0530

    automation of eventing-create-sink-broker

--- frontend/packages/dev-console/integration-tests/support/pages/functions/createBroker.ts
+++ frontend/packages/dev-console/integration-tests/support/pages/functions/createBroker.ts
@@ -1,5 +1,7 @@
+import { topologyPage } from '@console/topology/integration-tests/support/pages/topology/topology-page';
 import { addOptions } from '../../constants/add';
 import { devNavigationMenu } from '../../constants/global';
+import { topologyPO } from '../../pageObjects';
 import { addPage, gitPage } from '../add-flow';
 import { app, createForm, navigateTo } from '../app';
 
@@ -10,5 +12,28 @@ export const createBroker = (brokerName: string = 'broker-one') => {
   createForm.clickCreate().then(() => {
     app.waitForLoad();
     cy.log(`Broker "${brokerName}" is created`);
+  });
+};
+
+export const createBrokerIfNotExistsOnTopologyPage = (brokerName: string) => {
+  navigateTo(devNavigationMenu.Topology);
+  topologyPage.waitForLoad();
+  cy.get('body').then(($body) => {
+    if ($body.find(topologyPO.emptyStateIcon).length) {
+      navigateTo(devNavigationMenu.Add);
+      createBroker(brokerName);
+      topologyPage.verifyWorkloadInTopologyPage(brokerName);
+    } else {
+      topologyPage.search(brokerName);
+      cy.get('body').then(($node) => {
+        if ($node.find(topologyPO.highlightNode).length) {
+          cy.log(`Broker Name: "${brokerName}" is already available`);
+        } else {
+          navigateTo(devNavigationMenu.Add);
+          createBroker(brokerName);
+          topologyPage.verifyWorkloadInTopologyPage(brokerName);
+        }
+      });
+    }
   });
 };
