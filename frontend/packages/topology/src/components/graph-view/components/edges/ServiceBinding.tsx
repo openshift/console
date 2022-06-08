d0815afd4616b4ad965f3d2dcb0cfb4933217ea1
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-06-08 20:58:33 +0200

    Showing a red cross in the middle of the edge instead of the start

--- frontend/packages/topology/src/components/graph-view/components/edges/ServiceBinding.tsx
+++ frontend/packages/topology/src/components/graph-view/components/edges/ServiceBinding.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/react-styles/css/components/Topology/topology-components';
 import {
   Edge,
   EdgeTerminalType,
   NodeStatus,
+  StatusModifier,
   observer,
   WithSourceDragProps,
   WithTargetDragProps,
@@ -13,6 +16,9 @@ import { getComputedServiceBindingStatus } from '@console/service-binding-plugin
 import BaseEdge from './BaseEdge';
 
 import './ServiceBinding.scss';
+
+const ERROR_CROSS_SIZE = 8;
+const ERROR_CROSS_STROKE_WIDTH = 2;
 
 type ServiceBindingProps = {
   element: Edge;
@@ -26,15 +32,52 @@ const ServiceBinding: React.FC<ServiceBindingProps> = (props) => {
 
   const hasError = getComputedServiceBindingStatus(sbr) === ComputedServiceBindingStatus.ERROR;
 
+  let errorCross: JSX.Element;
+  if (hasError) {
+    const startPoint = props.element.getStartPoint();
+    const endPoint = props.element.getEndPoint();
+    const centerX = (startPoint.x + endPoint.x) / 2;
+    const centerY = (startPoint.y + endPoint.y) / 2;
+    const angleDeg =
+      Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x) * (180 / Math.PI);
+
+    const classNames = css(
+      // required to make it red
+      styles.topologyEdge,
+      StatusModifier[NodeStatus.danger],
+      // required to make it blue when the edge is selected
+      styles.topologyConnectorArrow,
+    );
+
+    errorCross = (
+      <g transform={`translate(${centerX}, ${centerY}) rotate(${angleDeg})`} className={classNames}>
+        <line
+          x1={-ERROR_CROSS_SIZE}
+          y1={-ERROR_CROSS_SIZE}
+          x2={ERROR_CROSS_SIZE}
+          y2={ERROR_CROSS_SIZE}
+          strokeWidth={ERROR_CROSS_STROKE_WIDTH}
+        />
+        <line
+          x1={-ERROR_CROSS_SIZE}
+          y1={ERROR_CROSS_SIZE}
+          x2={ERROR_CROSS_SIZE}
+          y2={-ERROR_CROSS_SIZE}
+          strokeWidth={ERROR_CROSS_STROKE_WIDTH}
+        />
+      </g>
+    );
+  }
+
   return (
     <BaseEdge
       className="odc-service-binding"
-      startTerminalStatus={hasError ? NodeStatus.danger : undefined}
-      startTerminalType={hasError ? EdgeTerminalType.cross : undefined}
       endTerminalStatus={hasError ? NodeStatus.danger : undefined}
       endTerminalType={EdgeTerminalType.directional}
       {...props}
-    />
+    >
+      {errorCross}
+    </BaseEdge>
   );
 };
 
