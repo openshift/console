aaac554a58d7479a8e6cb4b397c08b32d49617f9
Andrew Ballantyne <aballant@redhat.com>
2022-06-17 15:03:51 -0400

    Fix circular references in tests

--- frontend/packages/console-shared/src/components/error/fallbacks/ErrorBoundaryFallbackPage.tsx
+++ frontend/packages/console-shared/src/components/error/fallbacks/ErrorBoundaryFallbackPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
+import { Text, TextVariants } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import { ExpandCollapse } from '@console/internal/components/utils/expand-collapse';
-import { PageHeading } from '@console/internal/components/utils/headings';
 import { ErrorBoundaryFallbackProps } from '../types';
 import ErrorDetailsBlock from './ErrorDetailsBlock';
 
@@ -12,7 +12,9 @@ const ErrorBoundaryFallbackPage: React.FC<ErrorBoundaryFallbackProps> = (props) 
   const { t } = useTranslation();
   return (
     <div className="co-m-pane__body">
-      <PageHeading title={t('console-shared~Oh no! Something went wrong.')} centerText />
+      <Text component={TextVariants.h1} className="co-m-pane__heading co-m-pane__heading--center">
+        {t('console-shared~Oh no! Something went wrong.')}
+      </Text>
       <ExpandCollapse
         textCollapsed={t('console-shared~Show details')}
         textExpanded={t('console-shared~Hide details')}
