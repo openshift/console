07f59a5da3141cfcbcf2f243a978d69086c26284
lprabhu <lprabhu@redhat.com>
2022-06-21 15:38:17 +0530

    Modified advance section text and link separation issue

--- frontend/packages/dev-console/src/components/progressive-list/__tests__/ProgressiveListFooter.spec.tsx
+++ frontend/packages/dev-console/src/components/progressive-list/__tests__/ProgressiveListFooter.spec.tsx
@@ -1,29 +1,60 @@
 import * as React from 'react';
 import { Button } from '@patternfly/react-core';
 import { shallow } from 'enzyme';
+import i18n from 'i18next';
+import { setI18n, Trans, useTranslation } from 'react-i18next';
 import ProgressiveListFooter from '../ProgressiveListFooter';
+
+const Footer = ({ children }) => {
+  const { t } = useTranslation();
+  return (
+    <Trans
+      t={t}
+      ns="devconsole"
+      defaults="Click on the names to access advanced options for <0></0>."
+      components={[children]}
+    />
+  );
+};
+
+beforeEach(() => {
+  i18n.services.interpolator = {
+    init: () => undefined,
+    reset: () => undefined,
+    resetRegExp: () => undefined,
+    interpolate: (str: string) => str,
+    nest: (str: string) => str,
+  };
+  setI18n(i18n);
+});
 
 describe(ProgressiveListFooter.name, () => {
   it('should return JSX element if items array is not empty', () => {
-    const wrapper = shallow(<ProgressiveListFooter items={['Foo']} onShowItem={() => {}} />);
+    const wrapper = shallow(
+      <ProgressiveListFooter Footer={Footer} items={['Foo']} onShowItem={() => {}} />,
+    );
     expect(wrapper.find(Button).length).toEqual(1);
   });
 
   it('should return null if items array is empty', () => {
-    const wrapper = shallow(<ProgressiveListFooter items={[]} onShowItem={() => {}} />);
+    const wrapper = shallow(
+      <ProgressiveListFooter Footer={Footer} items={[]} onShowItem={() => {}} />,
+    );
     expect(wrapper.find(Button).length).toEqual(0);
   });
 
   it('should generate correct text', () => {
     const wrapper = shallow(
-      <ProgressiveListFooter items={['Foo', 'Bar', 'One']} onShowItem={() => {}} />,
+      <ProgressiveListFooter Footer={Footer} items={['Foo', 'Bar', 'One']} onShowItem={() => {}} />,
     );
-    expect(wrapper.render().text()).toEqual('Foo, Bar, and One');
+    expect(wrapper.render().text()).toEqual(
+      'Click on the names to access advanced options for Foo, Bar, and One.',
+    );
   });
 
   it('should have number of button equals to items in array', () => {
     const wrapper = shallow(
-      <ProgressiveListFooter items={['Foo', 'Bar', 'One']} onShowItem={() => {}} />,
+      <ProgressiveListFooter Footer={Footer} items={['Foo', 'Bar', 'One']} onShowItem={() => {}} />,
     );
     expect(wrapper.find(Button).length).toEqual(3);
   });
