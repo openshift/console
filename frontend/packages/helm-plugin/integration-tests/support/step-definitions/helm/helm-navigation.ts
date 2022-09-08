ec9bb105aa6b7070a267e0b966e0ab0b47af532e
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-09-09 00:40:03 +0200

    Improve e2e tests

--- frontend/packages/helm-plugin/integration-tests/support/step-definitions/helm/helm-navigation.ts
+++ frontend/packages/helm-plugin/integration-tests/support/step-definitions/helm/helm-navigation.ts
@@ -5,6 +5,7 @@ import {
   addOptions,
   pageTitle,
   switchPerspective,
+  catalogTypes,
 } from '@console/dev-console/integration-tests/support/constants';
 import {
   catalogPO,
@@ -170,7 +171,7 @@ Then('user will see the helm charts listed', () => {
 });
 
 When('user selects {string} option from Type section', (catalogType: string) => {
-  catalogPage.selectCatalogType(catalogType);
+  catalogPage.selectCatalogType(catalogType as catalogTypes);
 });
 
 Then('user can see {string} card on the Add page', (cardName: string) => {
