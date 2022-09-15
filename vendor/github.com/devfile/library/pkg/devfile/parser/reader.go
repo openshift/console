5aa8be0c4af832051a5be8258e8bc1e8ae016ccd
Maysun J Faisal <maysunaneek@gmail.com>
2022-09-15 16:16:52 -0400

    Update vendor pkg
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- vendor/github.com/devfile/library/pkg/devfile/parser/reader.go
+++ vendor/github.com/devfile/library/pkg/devfile/parser/reader.go
@@ -1,12 +1,27 @@
+//
+// Copyright 2022 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package parser
 
 import (
 	"bytes"
 	"io"
 
-	"github.com/devfile/library/pkg/testingutil/filesystem"
 	"github.com/devfile/library/pkg/util"
 	"github.com/pkg/errors"
+	"github.com/spf13/afero"
 	"gopkg.in/yaml.v3"
 	k8yaml "sigs.k8s.io/yaml"
 
@@ -39,7 +54,7 @@ type KubernetesResources struct {
 // It returns all the parsed Kubernetes objects as an array of interface.
 // Consumers interested in the Kubernetes resources are expected to Unmarshal
 // it to the struct of the respective Kubernetes resource.
-func ReadKubernetesYaml(src YamlSrc, fs filesystem.Filesystem) ([]interface{}, error) {
+func ReadKubernetesYaml(src YamlSrc, fs afero.Afero) ([]interface{}, error) {
 
 	var data []byte
 	var err error
