0eccce151d3749dda8fc6ca3a170045587bc4d64
Cyril <cyril.ajieh@gmail.com>
2022-09-16 10:10:31 -0400

    show-create-btn-users-with-sufff-permsn

--- frontend/packages/console-app/src/components/pdb/PDBPage.tsx
+++ frontend/packages/console-app/src/components/pdb/PDBPage.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ListPageBody } from '@console/dynamic-plugin-sdk';
 import { useListPageFilter } from '@console/internal/components/factory/ListPage/filter-hook';
-import { ListPageCreateLink } from '@console/internal/components/factory/ListPage/ListPageCreate';
+import ListPageCreate from '@console/internal/components/factory/ListPage/ListPageCreate';
 import ListPageFilter from '@console/internal/components/factory/ListPage/ListPageFilter';
 import ListPageHeader from '@console/internal/components/factory/ListPage/ListPageHeader';
 import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
+import { referenceForModel } from '@console/internal/module/k8s';
 import { PodDisruptionBudgetModel } from '../../models';
 import PodDisruptionBudgetList from './PDBList';
 import { PodDisruptionBudgetKind } from './types';
@@ -27,14 +28,17 @@ export const PodDisruptionBudgetsPage: React.FC<PodDisruptionBudgetsPageProps> =
   });
 
   const [data, filteredData, onFilterChange] = useListPageFilter(resources);
-  const createProps = `/k8s/ns/${namespace || 'default'}/poddisruptionbudget/form`;
-
+  const resourceKind = referenceForModel(PodDisruptionBudgetModel);
+  const accessReview = {
+    groupVersionKind: resourceKind,
+    namespace: namespace || 'default',
+  };
   return (
     <>
       <ListPageHeader title={showTitle ? t(PodDisruptionBudgetModel.labelPluralKey) : undefined}>
-        <ListPageCreateLink to={createProps}>
+        <ListPageCreate groupVersionKind={resourceKind} createAccessReview={accessReview}>
           {t('console-app~Create PodDiscruptionBudget')}
-        </ListPageCreateLink>
+        </ListPageCreate>
       </ListPageHeader>
       <ListPageBody>
         <ListPageFilter data={data} loaded={loaded} onFilterChange={onFilterChange} />
