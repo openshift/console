fce05d4f13dd2373b0aeca516712e265e3ff60c4
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-11-23 11:48:00 +0100

    Fix endless rerender loop when bindable-kinds is found but has no status

--- frontend/packages/dev-console/src/components/catalog/providers/useBindableItemMetadataProvider.ts
+++ frontend/packages/dev-console/src/components/catalog/providers/useBindableItemMetadataProvider.ts
@@ -8,19 +8,17 @@ import {
 import { getGroupVersionKindForModel } from '@console/dynamic-plugin-sdk/src/lib-core';
 import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
 import { BindableServicesModel } from '../../topology/bindable-services/models';
-import { BindableServiceGVK, BindableServicesKind } from '../../topology/bindable-services/types';
+import { BindableServicesKind } from '../../topology/bindable-services/types';
 
 const useBindableItemMetadataProvider: ExtensionHook<CatalogItemMetadataProviderFunction> = () => {
-  const [bindableKindsRes, loaded, error] = useK8sWatchResource<BindableServicesKind>({
+  const [bindableKinds, loaded, error] = useK8sWatchResource<BindableServicesKind>({
     groupVersionKind: getGroupVersionKindForModel(BindableServicesModel),
     name: 'bindable-kinds',
   });
 
-  const bindableServices: BindableServiceGVK[] = bindableKindsRes?.status ?? [];
-
   const [t] = useTranslation();
 
-  const bindableMtadata = React.useRef<ReturnType<CatalogItemMetadataProviderFunction>>({
+  const bindableMetadata = React.useRef<ReturnType<CatalogItemMetadataProviderFunction>>({
     tags: ['bindable'],
     badges: [{ text: t('devconsole~Bindable') }],
     attributes: {
@@ -30,9 +28,11 @@ const useBindableItemMetadataProvider: ExtensionHook<CatalogItemMetadataProvider
 
   const provider = React.useCallback<CatalogItemMetadataProviderFunction>(
     (item: CatalogItem) => {
+      if (!loaded || !Array.isArray(bindableKinds?.status)) {
+        return null;
+      }
       if (
-        loaded &&
-        bindableServices.some(
+        bindableKinds.status.some(
           (value) =>
             value.kind === item.data.kind &&
             value.version === item.data.version &&
@@ -43,11 +43,11 @@ const useBindableItemMetadataProvider: ExtensionHook<CatalogItemMetadataProvider
                 .join('.'),
         )
       ) {
-        return bindableMtadata.current;
+        return bindableMetadata.current;
       }
       return null;
     },
-    [bindableServices, loaded],
+    [bindableKinds, loaded],
   );
   return [provider, loaded, error];
 };
