8f1920e6a338741d95b68f04d0d6d43134bc6dfc
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-12-13 11:08:45 +0100

    Use mobx observer to update actions automatically

--- frontend/packages/topology/src/actions/TopologyActions.tsx
+++ frontend/packages/topology/src/actions/TopologyActions.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import { GraphElement } from '@patternfly/react-topology';
+import { observer, GraphElement } from '@patternfly/react-topology';
 import { referenceFor } from '@console/internal/module/k8s';
-import {
-  ActionMenu,
-  ActionMenuVariant,
-  ActionServiceProvider,
-  useDeepCompareMemoize,
-} from '@console/shared';
+import { ActionMenu, ActionMenuVariant, ActionServiceProvider } from '@console/shared';
 import { getResource } from '../utils';
 
 type TopologyActionsProps = {
@@ -15,16 +10,15 @@ type TopologyActionsProps = {
 
 const TopologyActions: React.FC<TopologyActionsProps> = ({ element }) => {
   const resource = getResource(element);
-  const memoizedRes = useDeepCompareMemoize(resource);
   const context = React.useMemo(() => {
     const { csvName } = element.getData()?.data ?? {};
     return {
       'topology-actions': element,
       'topology-context-actions': { element },
-      ...(memoizedRes ? { [referenceFor(memoizedRes)]: memoizedRes } : {}),
-      ...(csvName ? { 'csv-actions': { csvName, memoizedRes } } : {}),
+      ...(resource ? { [referenceFor(resource)]: resource } : {}),
+      ...(csvName ? { 'csv-actions': { csvName, resource } } : {}),
     };
-  }, [element, memoizedRes]);
+  }, [element, resource]);
   return (
     <ActionServiceProvider key={element.getId()} context={context}>
       {({ actions, options, loaded }) => {
@@ -38,4 +32,4 @@ const TopologyActions: React.FC<TopologyActionsProps> = ({ element }) => {
   );
 };
 
-export default TopologyActions;
+export default observer(TopologyActions);
