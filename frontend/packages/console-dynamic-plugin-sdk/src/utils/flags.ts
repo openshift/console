02f6261e86eb070ffe29451078316c57602cc177
Jon Jackson <jonjacks@redhat.com>
2022-12-19 13:55:33 -0500

    OCPBUGS-3033: Make all feature flags available in perspective extension 'getLandingPageUrl' callback
    Signed-off-by: Jon Jackson <jonjacks@redhat.com>

--- frontend/packages/console-dynamic-plugin-sdk/src/utils/flags.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/utils/flags.ts
@@ -1,9 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import { useSelector } from 'react-redux';
-import { FeatureSubStore } from '../app/features';
+import { FeatureSubStore, FlagsObject } from '../app/features';
 
 export type UseFlag = (flag: string) => boolean;
+export type UseAllFlags = () => FlagsObject;
 
 /**
  * Hook that returns the given feature flag from FLAGS redux state.
@@ -12,3 +13,10 @@ export type UseFlag = (flag: string) => boolean;
  */
 export const useFlag: UseFlag = (flag) =>
   useSelector<FeatureSubStore, boolean>(({ FLAGS }) => FLAGS.get(flag));
+
+/**
+ * Hook that returns the entire feature flags as a JS object.
+ * @returns all feature flags state as a JS object
+ */
+export const useAllFlags: UseAllFlags = () =>
+  useSelector<FeatureSubStore, FlagsObject>((store: FeatureSubStore) => store.FLAGS.toJS());
