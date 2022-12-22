9c4ee54179b27dd2049ed20171643cca5bde1e99
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-12-22 20:50:26 +0100

    Add DevSandbox specific configuration (to enforce cluster type and opt-out)

--- frontend/packages/console-shared/src/hooks/useTelemetry.ts
+++ frontend/packages/console-shared/src/hooks/useTelemetry.ts
@@ -6,6 +6,26 @@ import {
   TelemetryEventListener,
 } from '@console/dynamic-plugin-sdk';
 
+export const getConsoleVersion = () => window.SERVER_FLAGS?.consoleVersion;
+
+export const getClusterType = () => {
+  if (
+    window.SERVER_FLAGS?.telemetry?.CLUSTER_TYPE === 'OSD' &&
+    window.SERVER_FLAGS?.telemetry?.DEVSANDBOX === 'true'
+  ) {
+    return 'DEVSANDBOX';
+  }
+  return window.SERVER_FLAGS?.telemetry?.CLUSTER_TYPE;
+};
+
+let consoleVersion = getConsoleVersion();
+let clusterType = getClusterType();
+
+export const updateServerFlagsFromTests = () => {
+  consoleVersion = getConsoleVersion();
+  clusterType = getClusterType();
+};
+
 export const useTelemetry = () => {
   // TODO use useDynamicPluginInfo() hook to tell whether all dynamic plugins have been processed
   // to avoid firing telemetry events multiple times whenever a dynamic plugin loads asynchronously
@@ -15,8 +35,8 @@ export const useTelemetry = () => {
     (eventType, properties) => {
       extensions.forEach((e) =>
         e.properties.listener(eventType, {
-          consoleVersion: window.SERVER_FLAGS?.consoleVersion,
-          clusterType: window.SERVER_FLAGS?.telemetry?.CLUSTER_TYPE,
+          consoleVersion,
+          clusterType,
           ...properties,
         }),
       );
