8da8f7490500ab7f6148bda2eaaa5ebf1ac73823
Christoph Jerolimov <jerolimov+github@redhat.com>
2022-12-22 20:50:26 +0100

    Add DevSandbox specific configuration (to enforce cluster type and opt-out)

--- frontend/packages/console-telemetry-plugin/src/listeners/const.ts
+++ frontend/packages/console-telemetry-plugin/src/listeners/const.ts
@@ -1,3 +1,10 @@
-export const SEGMENT_API_KEY = window.SERVER_FLAGS?.telemetry?.SEGMENT_API_KEY ?? '';
-export const TELEMETRY_DISABLED = window.SERVER_FLAGS?.telemetry?.DISABLED === 'true';
+export const SEGMENT_API_KEY =
+  window.SERVER_FLAGS?.telemetry?.DEVSANDBOX_SEGMENT_API_KEY ||
+  window.SERVER_FLAGS?.telemetry?.SEGMENT_API_KEY ||
+  '';
+
+export const TELEMETRY_DISABLED =
+  window.SERVER_FLAGS?.telemetry?.DISABLED === 'true' ||
+  window.SERVER_FLAGS?.telemetry?.DEVSANDBOX_DISABLED === 'true';
+
 export const TELEMETRY_DEBUG = window.SERVER_FLAGS?.telemetry?.DEBUG === 'true';
