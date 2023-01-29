3d2ab0d1cda72113779ec2efdc6f4ab55e78e65d
Vikram Raj <vikrmrj@gmail.com>
2023-01-29 08:40:34 +0530

    Add RBAC check on Create a Project link in all-namespaces pages

--- frontend/packages/dev-console/src/components/add/AddPage.tsx
+++ frontend/packages/dev-console/src/components/add/AddPage.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { Button } from '@patternfly/react-core';
 import { Helmet } from 'react-helmet';
-import { useTranslation, Trans } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { match as RMatch } from 'react-router';
 import { withStartGuide } from '../../../../../public/components/start-guide';
 import NamespacedPage, { NamespacedPageVariants } from '../NamespacedPage';
-import CreateProjectListPage from '../projects/CreateProjectListPage';
+import CreateProjectListPage, { CreateAProjectButton } from '../projects/CreateProjectListPage';
 import AddPageLayout from './AddPageLayout';
 
 export interface AddPageProps {
@@ -25,11 +24,8 @@ export const PageContents: React.FC<AddPageProps> = ({ match }) => {
     <CreateProjectListPage title={t('devconsole~Add')}>
       {(openProjectModal) => (
         <Trans t={t} ns="devconsole">
-          Select a Project to start adding to it or{' '}
-          <Button isInline variant="link" onClick={openProjectModal}>
-            create a Project
-          </Button>
-          .
+          Select a Project to start adding to it
+          <CreateAProjectButton openProjectModal={openProjectModal} />.
         </Trans>
       )}
     </CreateProjectListPage>
