e2e9dfe20f9cabc43407b42fa6ddb5414b339648
Robb Hamilton <rhamilto@redhat.com>
2023-02-07 08:59:06 -0500

    OCPBUGS-6994: when ACM is installed and no previous perspective is set, default perspective to All Clusters

--- frontend/packages/console-app/src/components/detect-perspective/__tests__/useValuesForPerspectiveContext.spec.ts
+++ frontend/packages/console-app/src/components/detect-perspective/__tests__/useValuesForPerspectiveContext.spec.ts
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import { mockPerspectiveExtensions } from '@console/dynamic-plugin-sdk/src/perspective/__tests__/perspective.data';
-import { usePerspectives } from '@console/shared/src';
+import {
+  acmPerspectiveExtension,
+  mockPerspectiveExtensions,
+} from '@console/dynamic-plugin-sdk/src/perspective/__tests__/perspective.data';
+import { usePerspectiveExtension, usePerspectives } from '@console/shared/src';
 import { ACM_PERSPECTIVE_ID } from '../../../consts';
 import { usePreferredPerspective } from '../../user-preferences';
 import { useLastPerspective } from '../useLastPerspective';
 import { useValuesForPerspectiveContext } from '../useValuesForPerspectiveContext';
 
 jest.mock('@console/shared/src', () => ({
+  usePerspectiveExtension: jest.fn(),
   usePerspectives: jest.fn(),
   useTelemetry: jest.fn(),
 }));
@@ -28,6 +32,7 @@ jest.mock('react', () => {
 });
 
 const useStateMock = React.useState as jest.Mock;
+const usePerspectiveExtensionMock = usePerspectiveExtension as jest.Mock;
 const usePerspectivesMock = usePerspectives as jest.Mock;
 const useLastPerspectiveMock = useLastPerspective as jest.Mock;
 const usePreferredPerspectiveMock = usePreferredPerspective as jest.Mock;
@@ -39,8 +44,9 @@ describe('useValuesForPerspectiveContext', () => {
 
   it('should return undefined for PerspectiveType if the perspective from user preference does not match any of the perspectives received from extensions', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
-    useLastPerspectiveMock.mockReturnValue([ACM_PERSPECTIVE_ID, jest.fn(), true]);
+    useLastPerspectiveMock.mockReturnValue(['foo', jest.fn(), true]);
     usePreferredPerspectiveMock.mockReturnValue([undefined, jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['', jest.fn()]);
     const [perspective, ,] = useValuesForPerspectiveContext();
     expect(perspective).toBeUndefined();
@@ -50,6 +56,7 @@ describe('useValuesForPerspectiveContext', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
     useLastPerspectiveMock.mockReturnValue(['dev', jest.fn(), false]);
     usePreferredPerspectiveMock.mockReturnValue(['admin', jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['', jest.fn()]);
     let [perspective, , loaded] = useValuesForPerspectiveContext();
     expect(perspective).toBeUndefined();
@@ -60,6 +67,7 @@ describe('useValuesForPerspectiveContext', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
     useLastPerspectiveMock.mockReturnValue(['dev', jest.fn(), true]);
     usePreferredPerspectiveMock.mockReturnValue(['admin', jest.fn(), false]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['', jest.fn()]);
     [perspective, , loaded] = useValuesForPerspectiveContext();
     expect(perspective).toBeUndefined();
@@ -70,14 +78,17 @@ describe('useValuesForPerspectiveContext', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
     useLastPerspectiveMock.mockReturnValue(['dev', jest.fn(), true]);
     usePreferredPerspectiveMock.mockReturnValue(['admin', jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['', jest.fn()]);
     const [perspective, ,] = useValuesForPerspectiveContext();
     expect(perspective).toEqual('admin');
   });
+
   it('should return last viewed perspective if it matches one of the perspectives received from extensions and preferred perspective does not exist', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
     useLastPerspectiveMock.mockReturnValue(['dev', jest.fn(), true]);
     usePreferredPerspectiveMock.mockReturnValue([undefined, jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['', jest.fn()]);
     const [perspective, ,] = useValuesForPerspectiveContext();
     expect(perspective).toEqual('dev');
@@ -87,8 +98,19 @@ describe('useValuesForPerspectiveContext', () => {
     usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
     useLastPerspectiveMock.mockReturnValue(['dev', jest.fn(), true]);
     usePreferredPerspectiveMock.mockReturnValue(['dev', jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
     useStateMock.mockReturnValue(['admin', jest.fn()]);
     const [perspective, ,] = useValuesForPerspectiveContext();
     expect(perspective).toEqual('admin');
+  });
+
+  it(`should return ${ACM_PERSPECTIVE_ID} perspective if it exists and last viewed or preferred perspectives do not exist`, () => {
+    usePerspectivesMock.mockReturnValue(mockPerspectiveExtensions);
+    useLastPerspectiveMock.mockReturnValue([undefined, jest.fn(), true]);
+    usePreferredPerspectiveMock.mockReturnValue([undefined, jest.fn(), true]);
+    usePerspectiveExtensionMock.mockReturnValue(acmPerspectiveExtension);
+    useStateMock.mockReturnValue([ACM_PERSPECTIVE_ID, jest.fn()]);
+    const [perspective, ,] = useValuesForPerspectiveContext();
+    expect(perspective).toEqual(ACM_PERSPECTIVE_ID);
   });
 });
--- frontend/packages/console-dynamic-plugin-sdk/src/perspective/__tests__/perspective.data.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/perspective/__tests__/perspective.data.ts
@@ -1,5 +1,20 @@
+import { ACM_PERSPECTIVE_ID } from '@console/app/src/consts';
 import { Perspective } from '../../extensions/perspectives';
 import { LoadedExtension } from '../../types';
+
+export const acmPerspectiveExtension: LoadedExtension<Perspective> = {
+  type: 'console.perspective',
+  pluginID: '',
+  pluginName: '',
+  uid: '',
+  properties: {
+    id: ACM_PERSPECTIVE_ID,
+    name: 'All Clusters',
+    icon: null,
+    landingPageURL: async () => () => '',
+    importRedirectURL: async () => () => '',
+  },
+};
 
 export const mockPerspectiveExtensions: LoadedExtension<Perspective>[] = [
   {
@@ -28,4 +43,5 @@ export const mockPerspectiveExtensions: LoadedExtension<Perspective>[] = [
       importRedirectURL: async () => () => '',
     },
   },
+  acmPerspectiveExtension,
 ];
