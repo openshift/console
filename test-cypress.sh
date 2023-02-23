612f653982f3d97ef3f6ec99ebd84545fb1220fc
CYRIL AJIEH <cyril.ajieh@gmail.com>
2023-02-23 02:05:37 -0500

    poc improve e2e tests using cypress-parallel

--- test-cypress.sh
+++ test-cypress.sh
@@ -32,47 +32,49 @@ if [ $# -eq 0 ]; then
     echo "  '-h true' runs Cypress in headless mode. When omitted, launches Cypress Test Runner"
     echo "  '-n true' runs the 'nightly' suite, all specs from selected packages in headless mode"
     echo "Examples:"
-    echo "  test-cypress.sh                                       // displays this help text"
-    echo "  test-cypress.sh -p console                            // opens Cypress Test Runner for console tests"
-    echo "  test-cypress.sh -p olm                                // opens Cypress Test Runner for OLM tests"
+    # echo "  test-cypress.sh                                       // displays this help text"
+    # echo "  test-cypress.sh -p console                            // opens Cypress Test Runner for console tests"
+    # echo "  test-cypress.sh -p olm                                // opens Cypress Test Runner for OLM tests"
     echo "  test-cypress.sh -p ceph                               // opens Cypress Test Runner for Ceph tests"
-    echo "  test-cypress.sh -p dev-console                        // opens Cypress Test Runner for Dev-Console tests"
-    echo "  test-cypress.sh -p gitops                             // opens Cypress Test Runner for gitops tests"
-    echo "  test-cypress.sh -p knative                            // opens Cypress Test Runner for knative tests"
-    echo "  test-cypress.sh -p pipelines                          // opens Cypress Test Runner for pipelines tests"
-    echo "  test-cypress.sh -p shipwright                         // opens Cypress Test Runner for shipwright tests"
-    echo "  test-cypress.sh -h true                               // runs all packages in headless mode"
-    echo "  test-cypress.sh -p olm -h true                        // runs OLM tests in headless mode"
-    echo "  test-cypress.sh -p console -s 'tests/crud/*' -h true  // runs console CRUD tests in headless mode"
-    echo "  test-cypress.sh -n true                               // runs the whole nightly suite"
+    # echo "  test-cypress.sh -p dev-console                        // opens Cypress Test Runner for Dev-Console tests"
+    # echo "  test-cypress.sh -p gitops                             // opens Cypress Test Runner for gitops tests"
+    # echo "  test-cypress.sh -p knative                            // opens Cypress Test Runner for knative tests"
+    # echo "  test-cypress.sh -p pipelines                          // opens Cypress Test Runner for pipelines tests"
+    # echo "  test-cypress.sh -p shipwright                         // opens Cypress Test Runner for shipwright tests"
+    # echo "  test-cypress.sh -h true                               // runs all packages in headless mode"
+    # echo "  test-cypress.sh -p olm -h true                        // runs OLM tests in headless mode"
+    # echo "  test-cypress.sh -p console -s 'tests/crud/*' -h true  // runs console CRUD tests in headless mode"
+    # echo "  test-cypress.sh -n true                               // runs the whole nightly suite"
     trap EXIT
     exit;
 fi
 
-if [ -n "${nightly-}" ] && [ -z "${pkg-}" ]; then
-  # do not fail fast, let all suites run
-  set +e
-  err=0
-  trap 'err=1' ERR
+# if [ -n "${nightly-}" ] && [ -z "${pkg-}" ]; then
+#   # do not fail fast, let all suites run
+#   set +e
+#   err=0
+#   trap 'err=1' ERR
 
-  yarn run test-cypress-dev-console-nightly
-  yarn run test-cypress-helm-nightly
-  yarn run test-cypress-pipelines-nightly
-  yarn run test-cypress-shipwright-nightly
-  yarn run test-cypress-topology-nightly
-  yarn run test-cypress-knative-nightly
+#   yarn run test-cypress-dev-console-nightly
+#   yarn run test-cypress-helm-nightly
+#   yarn run test-cypress-pipelines-nightly
+#   yarn run test-cypress-shipwright-nightly
+#   yarn run test-cypress-topology-nightly
+#   yarn run test-cypress-knative-nightly
 
-  exit $err;
-fi
+#   exit $err;
+# fi
 
 if [ -n "${headless-}" ] && [ -z "${pkg-}" ]; then
-  yarn run test-cypress-console-headless
-  yarn run test-cypress-dev-console-headless
-  yarn run test-cypress-olm-headless
-  yarn run test-cypress-helm-headless
-  yarn run test-cypress-knative-headless
-  yarn run test-cypress-topology-headless
-  yarn run test-cypress-pipelines-headless
+  # yarn run test-cypress-console-headless
+  # cmd for parallel testing
+  yarn run cy:test-ceph-storage-plugin-parallel-headless
+  # yarn run test-cypress-dev-console-headless
+  # yarn run test-cypress-olm-headless
+  # yarn run test-cypress-helm-headless
+  # yarn run test-cypress-knative-headless
+  # yarn run test-cypress-topology-headless
+  # yarn run test-cypress-pipelines-headless
 #  yarn run test-cypress-shipwright-headless
   exit;
 fi
--- test-prow-e2e.sh
+++ test-prow-e2e.sh
@@ -80,22 +80,22 @@ if [ "$SCENARIO" == "nightly-cypress" ]; then
     PACKAGE="-p $2"
   fi
   ./test-cypress.sh -n true $PACKAGE
-elif [ "$SCENARIO" == "e2e" ] || [ "$SCENARIO" == "release" ]; then
-  ./test-cypress.sh -h true
-elif [ "$SCENARIO" == "login" ]; then
-  ./test-cypress.sh -p console -s 'tests/app/auth-multiuser-login.spec.ts' -h true
-elif [ "$SCENARIO" == "olmFull" ]; then
-  ./test-cypress.sh -p olm -h true
+# elif [ "$SCENARIO" == "e2e" ] || [ "$SCENARIO" == "release" ]; then
+#   ./test-cypress.sh -h true
+# elif [ "$SCENARIO" == "login" ]; then
+#   ./test-cypress.sh -p console -s 'tests/app/auth-multiuser-login.spec.ts' -h true
+# elif [ "$SCENARIO" == "olmFull" ]; then
+#   ./test-cypress.sh -p olm -h true
 elif [ "$SCENARIO" == "ceph" ]; then
   ./test-cypress.sh -p ceph -h true
-elif [ "$SCENARIO" == "kubevirt-gating" ]; then
-  ./test-cypress.sh -p kubevirt -h true
-elif [ "$SCENARIO" == "dev-console" ]; then
- ./test-cypress.sh -p dev-console -h true
-elif [ "$SCENARIO" == "pipelines" ]; then
- ./test-cypress.sh -p pipelines -h true
-elif [ "$SCENARIO" == "gitops" ]; then
- ./test-cypress.sh -p gitops -h true
- elif [ "$SCENARIO" == "knative" ]; then
- ./test-cypress.sh -p knative -h true
+# elif [ "$SCENARIO" == "kubevirt-gating" ]; then
+#   ./test-cypress.sh -p kubevirt -h true
+# elif [ "$SCENARIO" == "dev-console" ]; then
+#  ./test-cypress.sh -p dev-console -h true
+# elif [ "$SCENARIO" == "pipelines" ]; then
+#  ./test-cypress.sh -p pipelines -h true
+# elif [ "$SCENARIO" == "gitops" ]; then
+#  ./test-cypress.sh -p gitops -h true
+#  elif [ "$SCENARIO" == "knative" ]; then
+#  ./test-cypress.sh -p knative -h true
 fi
