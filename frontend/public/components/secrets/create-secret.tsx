90632eb59e5faac8d040148054072720c09c9375
Mylanos <marekziska99@gmail.com>
2023-02-27 12:27:01 +0100

    Removed the boolean constant in code and declared a state variable instead

--- frontend/public/components/secrets/create-secret.tsx
+++ frontend/public/components/secrets/create-secret.tsx
@@ -351,13 +351,15 @@ class ImageSecretFormWithTranslation extends React.Component<
       dataKey: getImageSecretKey(this.props.secretType),
       stringData: parsedData,
       authType: 'credentials',
+      disable: false,
     };
     this.onDataChanged = this.onDataChanged.bind(this);
     this.changeFormType = this.changeFormType.bind(this);
     this.onFormDisable = this.onFormDisable.bind(this);
   }
   onDataChanged(secretData) {
-    secretData ? this.onFormDisable(false) : null;
+    // console.log(secretData);
+    _.isError(secretData) ? null : this.onFormDisable(this.state.disable);
     const dataKey = secretData[AUTHS_KEY] ? '.dockerconfigjson' : '.dockercfg';
     this.setState(
       {
@@ -1422,6 +1424,7 @@ type ImageSecretFormState = {
   };
   authType: string;
   dataKey: string;
+  disable: boolean;
 };
 
 type ImageSecretFormProps = {
