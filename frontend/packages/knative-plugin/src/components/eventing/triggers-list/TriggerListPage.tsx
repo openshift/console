a219f3786f06cc592ebe1dab53ce9ad1ae9234cd
Vikram Raj <vikrmrj@gmail.com>
2023-03-03 08:24:56 +0530

    add page title for Knative resources, Pipelines list and fix User preferences title

--- frontend/packages/knative-plugin/src/components/eventing/triggers-list/TriggerListPage.tsx
+++ frontend/packages/knative-plugin/src/components/eventing/triggers-list/TriggerListPage.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
+import Helmet from 'react-helmet';
+import { useTranslation } from 'react-i18next';
 import { ListPage } from '@console/internal/components/factory';
 import { referenceForModel } from '@console/internal/module/k8s';
 import { EventingTriggerModel } from '../../../models';
 import TriggerList from './TriggerList';
 
-const TriggerListPage: React.FC<React.ComponentProps<typeof ListPage>> = (props) => (
-  <ListPage
-    canCreate={false}
-    {...props}
-    kind={referenceForModel(EventingTriggerModel)}
-    ListComponent={TriggerList}
-  />
-);
+const TriggerListPage: React.FC<React.ComponentProps<typeof ListPage>> = (props) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Helmet>
+        <title>{t('knative-plugin~Triggers')}</title>
+      </Helmet>
+      <ListPage
+        canCreate={false}
+        {...props}
+        kind={referenceForModel(EventingTriggerModel)}
+        ListComponent={TriggerList}
+      />
+    </>
+  );
+};
 
 export default TriggerListPage;
--- frontend/packages/knative-plugin/src/components/services/ServicesPage.tsx
+++ frontend/packages/knative-plugin/src/components/services/ServicesPage.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Helmet from 'react-helmet';
+import { useTranslation } from 'react-i18next';
 import { ListPage } from '@console/internal/components/factory';
 import { referenceForModel } from '@console/internal/module/k8s';
 import { ServiceModel } from '../../models';
@@ -8,13 +10,21 @@ export interface ServicesPageProps {
   namespace: string;
 }
 
-const ServicesPage: React.FC<React.ComponentProps<typeof ListPage>> = (props) => (
-  <ListPage
-    canCreate
-    {...props}
-    kind={referenceForModel(ServiceModel)}
-    ListComponent={ServiceList}
-  />
-);
+const ServicesPage: React.FC<React.ComponentProps<typeof ListPage>> = (props) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Helmet>
+        <title>{t('knative-plugin~Services')}</title>
+      </Helmet>
+      <ListPage
+        canCreate
+        {...props}
+        kind={referenceForModel(ServiceModel)}
+        ListComponent={ServiceList}
+      />
+    </>
+  );
+};
 
 export default ServicesPage;
