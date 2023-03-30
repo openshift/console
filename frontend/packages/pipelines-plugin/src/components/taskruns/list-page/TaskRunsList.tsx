34e6ab2c3776f9da8c2635bf820c02fd4c7dbf2c
AneeshMBhat <anebhat@redhat.com>
2023-03-30 14:56:44 +0530

    Added Manage Columns Option to TaskRun Page

--- frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsList.tsx
+++ frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsList.tsx
@@ -1,27 +1,46 @@
 import * as React from 'react';
 import { SortByDirection } from '@patternfly/react-table';
 import { useTranslation } from 'react-i18next';
+import { ColumnLayout } from '@console/dynamic-plugin-sdk';
 import { Table } from '@console/internal/components/factory';
+import { useActiveColumns } from '@console/internal/components/factory/Table/active-columns-hook';
 import { TaskRunModel } from '../../../models';
 import TaskRunsHeader from './TaskRunsHeader';
 import TaskRunsRow from './TaskRunsRow';
 
 interface TaskRunsListProps {
   customData?: { [key: string]: any };
+  columnLayout: ColumnLayout;
 }
 
 const TaskRunsList: React.FC<TaskRunsListProps> = (props) => {
   const { t } = useTranslation();
+  const {
+    columnLayout: { columns, id },
+    customData,
+  } = props;
+  const [activeColumns, userSettingsLoaded] = useActiveColumns({
+    columns,
+    columnManagementID: id,
+  });
+  const selectedColumns = React.useMemo(() => new Set(activeColumns.map((col) => col.id)), [
+    activeColumns,
+  ]);
+  const newCustomData = { ...customData, selectedColumns };
   return (
-    <Table
-      {...props}
-      aria-label={t(TaskRunModel.labelPluralKey)}
-      defaultSortField="status.startTime"
-      defaultSortOrder={SortByDirection.desc}
-      Header={TaskRunsHeader(props.customData?.showPipelineColumn)}
-      Row={TaskRunsRow}
-      virtualize
-    />
+    userSettingsLoaded && (
+      <Table
+        {...props}
+        aria-label={t(TaskRunModel.labelPluralKey)}
+        defaultSortField="status.startTime"
+        defaultSortOrder={SortByDirection.desc}
+        Header={TaskRunsHeader(props.customData?.showPipelineColumn)}
+        Row={TaskRunsRow}
+        virtualize
+        activeColumns={selectedColumns}
+        customData={newCustomData}
+      />
+    )
   );
 };
 
--- frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsListPage.tsx
+++ frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsListPage.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import Helmet from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { ListPage } from '@console/internal/components/factory';
 import { getURLSearchParams } from '@console/internal/components/utils';
 import { referenceForModel } from '@console/internal/module/k8s';
+import {
+  COLUMN_MANAGEMENT_CONFIGMAP_KEY,
+  COLUMN_MANAGEMENT_LOCAL_STORAGE_KEY,
+  TableColumnsType,
+  useUserSettingsCompatibility,
+} from '@console/shared';
 import { TaskRunModel } from '../../../models';
 import { usePipelineTechPreviewBadge } from '../../../utils/hooks';
 import { runFilters as taskRunFilters } from '../../pipelines/detail-page-tabs/PipelineRuns';
+import TaskRunsHeader from './TaskRunsHeader';
 import TaskRunsList from './TaskRunsList';
 
 interface TaskRunsListPageProps {
@@ -29,21 +37,41 @@ const TaskRunsListPage: React.FC<Omit<
     }),
     [showPipelineColumn],
   );
+  const columnManagementID = referenceForModel(TaskRunModel);
+  const [tableColumns, , userSettingsLoaded] = useUserSettingsCompatibility<TableColumnsType>(
+    COLUMN_MANAGEMENT_CONFIGMAP_KEY,
+    COLUMN_MANAGEMENT_LOCAL_STORAGE_KEY,
+    undefined,
+    true,
+  );
   return (
     <>
       <Helmet>
         <title>{t('pipelines-plugin~TaskRuns')}</title>
       </Helmet>
-      <ListPage
-        {...props}
-        customData={customData}
-        canCreate={kind?.includes(referenceForModel(TaskRunModel)) ?? false}
-        kind={referenceForModel(TaskRunModel)}
-        ListComponent={TaskRunsList}
-        rowFilters={taskRunFilters(t)}
-        badge={hideBadge ? null : badge}
-        namespace={namespace}
-      />
+      {userSettingsLoaded && (
+        <ListPage
+          {...props}
+          customData={customData}
+          canCreate={kind?.includes(referenceForModel(TaskRunModel)) ?? false}
+          kind={referenceForModel(TaskRunModel)}
+          ListComponent={TaskRunsList}
+          rowFilters={taskRunFilters(t)}
+          badge={hideBadge ? null : badge}
+          namespace={namespace}
+          columnLayout={{
+            columns: TaskRunsHeader()().map((column) =>
+              _.pick(column, ['title', 'additional', 'id']),
+            ),
+            id: columnManagementID,
+            selectedColumns:
+              tableColumns?.[columnManagementID]?.length > 0
+                ? new Set(tableColumns[columnManagementID])
+                : null,
+            type: t('pipelines-plugin~TaskRun'),
+          }}
+        />
+      )}
     </>
   );
 };
