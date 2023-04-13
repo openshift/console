209c0bf9ad800d427fb4a5ee6d36e277fc3cc710
Jon Jackson <jonjacks@redhat.com>
2023-04-13 13:24:52 -0400

    OCPBUGS-3036: Do not list subscriptions in all namespaces on CSV details page.

--- frontend/packages/operator-lifecycle-manager/src/const.ts
+++ frontend/packages/operator-lifecycle-manager/src/const.ts
@@ -19,14 +19,16 @@ export enum DefaultCatalogSourceDisplayName {
   Custom = 'Custom',
 }
 
-export const OPERATOR_UNINSTALL_MESSAGE_ANNOTATION = 'operator.openshift.io/uninstall-message';
-export const OPERATOR_TYPE_ANNOTATION = 'operators.operatorframework.io/operator-type';
-export const NON_STANDALONE_ANNOTATION_VALUE = 'non-standalone';
-export const INTERNAL_OBJECTS_ANNOTATION = 'operators.operatorframework.io/internal-objects';
-export const DEFAULT_SOURCE_NAMESPACE = 'openshift-marketplace';
-export const OPERATOR_PLUGINS_ANNOTATION = 'console.openshift.io/plugins';
 export const DEFAULT_GLOBAL_OPERATOR_INSTALLATION_NAMESPACE = 'openshift-operators';
+export const DEFAULT_SOURCE_NAMESPACE = 'openshift-marketplace';
 export const GLOBAL_COPIED_CSV_NAMESPACE = 'openshift';
+export const INTERNAL_OBJECTS_ANNOTATION = 'operators.operatorframework.io/internal-objects';
+export const NON_STANDALONE_ANNOTATION_VALUE = 'non-standalone';
+export const OPERATOR_NAMESPACE_ANNOTATION = 'olm.operatorNamespace';
+export const OPERATOR_PLUGINS_ANNOTATION = 'console.openshift.io/plugins';
+export const OPERATOR_TYPE_ANNOTATION = 'operators.operatorframework.io/operator-type';
+export const OPERATOR_UNINSTALL_MESSAGE_ANNOTATION = 'operator.openshift.io/uninstall-message';
+
 export const GLOBAL_OPERATOR_NAMESPACES = [
   DEFAULT_GLOBAL_OPERATOR_INSTALLATION_NAMESPACE,
   GLOBAL_COPIED_CSV_NAMESPACE,
