6c789d01d2a7e513981a7b3199a9d1aa34fa65cc
Robb Hamilton <rhamilto@redhat.com>
2023-04-17 08:28:02 -0400

    OCPBUGS-10562: disable operator-uninstall.ts as the uninstall test is still flaking

--- frontend/packages/operator-lifecycle-manager/integration-tests-cypress/tests/operator-uninstall.spec.ts
+++ frontend/packages/operator-lifecycle-manager/integration-tests-cypress/tests/operator-uninstall.spec.ts
@@ -39,7 +39,7 @@ const uninstallAndVerify = () => {
   cy.resourceShouldBeDeleted(testName, testOperand.kind, testOperand.exampleName);
 };
 
-describe(`Testing uninstall of ${testOperator.name} Operator`, () => {
+xdescribe(`Testing uninstall of ${testOperator.name} Operator`, () => {
   before(() => {
     cy.login();
     cy.visit('/');
