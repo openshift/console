078a139ed9f38409fbb4b9f46516ccc50aded50c
rawagner <rawagner@redhat.com>
2023-05-10 15:31:27 +0200

    Add vSphere cluster field. Fix some additional minor issues & simplify code

--- frontend/packages/vsphere-plugin/src/hooks/use-connection-form.ts
+++ frontend/packages/vsphere-plugin/src/hooks/use-connection-form.ts
@@ -1,12 +1,17 @@
+import * as React from 'react';
 import { K8sModel, k8sGet } from '@console/dynamic-plugin-sdk/src/api/core-api';
-import { ConfigMap, Secret } from '../resources';
-import { ConnectionFormContextSetters } from './types';
-import { decodeBase64, parseKeyValue } from './utils';
+import { useConnectionFormContext } from '../components/ConnectionFormContext';
+import { ConnectionFormContextSetters } from '../components/types';
+import { decodeBase64, parseKeyValue } from '../components/utils';
+import { FAILURE_DOMAIN_NAME } from '../constants';
+import { ConfigMap, Infrastructure, Secret } from '../resources';
+import { useConnectionModels } from './use-connection-models';
 
 // TODO: Adopt npm/ini package to do following parsing (see persist.ts)
 export const initialLoad = async (
   setters: ConnectionFormContextSetters,
   secretModel: K8sModel,
+  infrastructureModel: K8sModel,
   cloudProviderConfig: ConfigMap,
 ): Promise<void> => {
   const config = cloudProviderConfig.data?.config;
@@ -51,12 +56,55 @@ export const initialLoad = async (
     }
   }
 
+  let vCenterCluster = '';
+  try {
+    const infrastructure = await k8sGet<Infrastructure>({
+      model: infrastructureModel,
+      name: 'cluster',
+    });
+
+    const domain = infrastructure?.spec?.platformSpec?.vsphere?.failureDomains?.find(
+      (d) => d.name === FAILURE_DOMAIN_NAME,
+    );
+    vCenterCluster = domain?.topology?.networks?.[0] || '';
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to fetch infrastructure resource', e);
+  }
+
   setters.setVcenter(server);
   setters.setDatacenter(dc);
   setters.setDefaultDatastore(ds);
   setters.setFolder(folder);
   setters.setUsername(username);
   setters.setPassword(pwd);
+  setters.setVCenterCluster(vCenterCluster);
+};
 
-  setters.setDirty(false);
+export const useConnectionForm = (cloudProviderConfig?: ConfigMap) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const { secretModel, infrastructureModel } = useConnectionModels();
+  const { setDirty, setters, values } = useConnectionFormContext();
+
+  React.useEffect(() => {
+    const doItAsync = async () => {
+      if (isLoaded) {
+        return;
+      }
+      if (!cloudProviderConfig) {
+        return;
+      }
+      await initialLoad(setters, secretModel, infrastructureModel, cloudProviderConfig);
+      setIsLoaded(true);
+      setDirty(false);
+    };
+
+    doItAsync();
+  }, [cloudProviderConfig, infrastructureModel, isLoaded, secretModel, setDirty, setters]);
+
+  return {
+    setters,
+    values,
+    isLoaded,
+  };
 };
