2e58a526e19c31e91b63039828e01c7b78353e1f
Jakub Hadvig <jhadvig@redhat.com>
2023-05-15 12:41:54 +0200

    OCPBUGS-11286: Check if filtered object contains name property

--- frontend/packages/operator-lifecycle-manager/src/components/clusterserviceversion.tsx
+++ frontend/packages/operator-lifecycle-manager/src/components/clusterserviceversion.tsx
@@ -754,7 +754,7 @@ export const ClusterServiceVersionsPage: React.FC<ClusterServiceVersionsPageProp
         isCSV(obj) ||
         _.isUndefined(
           all.find(({ metadata }) =>
-            [obj?.status?.currentCSV, obj?.spec?.startingCSV].includes(metadata.name),
+            [obj?.status?.currentCSV, obj?.spec?.startingCSV].includes(metadata?.name),
           ),
         ),
     );
