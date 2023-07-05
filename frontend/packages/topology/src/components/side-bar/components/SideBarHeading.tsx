b284167b3b87b3ca1ff2bbb85f9fb8c757905bcc
Aviv Turgeman <aturgema@redhat.com>
2023-07-05 09:45:59 +0300

    Add deprecation alert for DeploymentConfig
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- frontend/packages/topology/src/components/side-bar/components/SideBarHeading.tsx
+++ frontend/packages/topology/src/components/side-bar/components/SideBarHeading.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { GraphElement } from '@patternfly/react-topology';
+import { DeploymentConfigDeprecationAlert } from '@console/internal/components/deployment-config';
+import { DeploymentConfigModel } from '@console/internal/models';
 import TopologyActions from '../../../actions/TopologyActions';
 import { useDetailsResourceLink } from '../providers/useDetailsResourceLink';
 import SideBarAlerts from './SideBarAlerts';
@@ -8,6 +10,7 @@ import './SideBarHeading.scss';
 const SideBarHeading: React.FC<{ element: GraphElement }> = ({ element }) => {
   const resourceLabel = element.getLabel();
   const resourceLink = useDetailsResourceLink(element);
+  const resourceKind = element.getData()?.resource?.kind;
   return (
     <div className="overview__sidebar-pane-head resource-overview__heading">
       <h1 className="co-m-pane__heading">
@@ -16,6 +19,11 @@ const SideBarHeading: React.FC<{ element: GraphElement }> = ({ element }) => {
           <TopologyActions element={element} />
         </div>
       </h1>
+      {resourceKind === DeploymentConfigModel.kind && (
+        <div className="dc-deprecation-sidebar-alert">
+          <DeploymentConfigDeprecationAlert />
+        </div>
+      )}
       <div className="odc-topology-sidebar-alert">
         <SideBarAlerts element={element} />
       </div>
