dfebdffc444f66289f57ec8f98b55b127895e5e2
Vikram Raj <vikrmrj@gmail.com>
2023-07-17 15:51:28 +0530

    Fix stop PLR option

--- frontend/packages/pipelines-plugin/src/components/repository/RepositoryPipelineRunList.tsx
+++ frontend/packages/pipelines-plugin/src/components/repository/RepositoryPipelineRunList.tsx
@@ -3,12 +3,17 @@ import { SortByDirection } from '@patternfly/react-table';
 import { useTranslation } from 'react-i18next';
 import { Table } from '@console/internal/components/factory';
 import { PipelineRunModel } from '../../models';
+import { usePipelineOperatorVersion } from '../pipelines/utils/pipeline-operator';
 import RepositoryPipelineRunHeader from './RepositoryPipelineRunHeader';
 import RepositoryPipelineRunRow from './RepositoryPipelineRunRow';
 
-export const RepositoryPipelineRunList: React.FC = (props) => {
-  const { t } = useTranslation();
+type RepositoryPipelineRunListProps = {
+  namespace: string;
+};
 
+export const RepositoryPipelineRunList: React.FC<RepositoryPipelineRunListProps> = (props) => {
+  const { t } = useTranslation();
+  const operatorVersion = usePipelineOperatorVersion(props.namespace);
   return (
     <Table
       {...props}
@@ -17,6 +22,7 @@ export const RepositoryPipelineRunList: React.FC = (props) => {
       defaultSortOrder={SortByDirection.desc}
       Header={RepositoryPipelineRunHeader}
       Row={RepositoryPipelineRunRow}
+      customData={operatorVersion}
       virtualize
     />
   );
