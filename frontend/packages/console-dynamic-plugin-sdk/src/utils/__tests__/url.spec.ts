43e8c672198a1283c1c071b579484149c6643046
Vojtech Szocs <vszocs@redhat.com>
2023-07-21 17:21:32 +0200

    Add cacheBuster query string when requesting plugin entry scripts

--- frontend/packages/console-dynamic-plugin-sdk/src/utils/__tests__/url.spec.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/utils/__tests__/url.spec.ts
@@ -1,18 +1,56 @@
+import * as _ from 'lodash';
 import { resolveURL } from '../url';
 
 describe('resolveURL', () => {
-  const getDocumentOrigin = () => 'https://example:1234';
+  const getDocumentOrigin = jest.fn(() => 'https://example:1234');
 
   it('uses the base URL as-is if it has the protocol', () => {
-    expect(resolveURL('http://test', 'foo', getDocumentOrigin)).toBe('http://test/foo');
-    expect(resolveURL('http://test/', 'foo', getDocumentOrigin)).toBe('http://test/foo');
-    expect(resolveURL('http://test/foo', 'bar', getDocumentOrigin)).toBe('http://test/bar');
-    expect(resolveURL('http://test/foo/', 'bar', getDocumentOrigin)).toBe('http://test/foo/bar');
+    expect(resolveURL('http://test', 'foobar', _.identity, getDocumentOrigin)).toBe(
+      'http://test/foobar',
+    );
+    expect(resolveURL('http://test/', 'foobar', _.identity, getDocumentOrigin)).toBe(
+      'http://test/foobar',
+    );
+    expect(resolveURL('http://test/foo', 'bar', _.identity, getDocumentOrigin)).toBe(
+      'http://test/bar',
+    );
+    expect(resolveURL('http://test/foo/', 'bar', _.identity, getDocumentOrigin)).toBe(
+      'http://test/foo/bar',
+    );
+
+    expect(getDocumentOrigin).not.toHaveBeenCalled();
   });
 
   it("makes the base URL relative to document origin if it's missing the protocol", () => {
-    expect(resolveURL('/', 'foo', getDocumentOrigin)).toBe('https://example:1234/foo');
-    expect(resolveURL('/foo', 'bar', getDocumentOrigin)).toBe('https://example:1234/bar');
-    expect(resolveURL('/foo/', 'bar', getDocumentOrigin)).toBe('https://example:1234/foo/bar');
+    expect(resolveURL('/', 'foobar', _.identity, getDocumentOrigin)).toBe(
+      'https://example:1234/foobar',
+    );
+    expect(resolveURL('/foo', 'bar', _.identity, getDocumentOrigin)).toBe(
+      'https://example:1234/bar',
+    );
+    expect(resolveURL('/foo/', 'bar', _.identity, getDocumentOrigin)).toBe(
+      'https://example:1234/foo/bar',
+    );
+
+    expect(getDocumentOrigin).toHaveBeenCalledTimes(3);
+  });
+
+  it('calls the URL processing callback before returning the URL string', () => {
+    const processURL = jest.fn((url: URL) => {
+      const newURL = new URL(url);
+
+      newURL.protocol = 'http';
+      newURL.hostname = 'custom-host';
+      newURL.port = '8080';
+      newURL.search = '?test=true';
+
+      return newURL;
+    });
+
+    expect(resolveURL('/', 'foobar', processURL, getDocumentOrigin)).toBe(
+      'http://custom-host:8080/foobar?test=true',
+    );
+
+    expect(processURL).toHaveBeenCalledWith(new URL('https://example:1234/foobar'));
   });
 });
