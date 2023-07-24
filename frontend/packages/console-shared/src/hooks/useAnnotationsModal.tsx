bafd5860d972f5d6c629d6595c4dae22661fcecb
Vojtech Szocs <vszocs@redhat.com>
2023-07-24 17:50:24 +0200

    Expose useAnnotationsModal hook via SDK core package

--- frontend/packages/console-shared/src/hooks/useAnnotationsModal.tsx
+++ frontend/packages/console-shared/src/hooks/useAnnotationsModal.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { ModalComponent } from '@console/dynamic-plugin-sdk/src/app/modal-support/ModalProvider';
 import { useModal } from '@console/dynamic-plugin-sdk/src/app/modal-support/useModal';
-import { K8sResourceCommon } from '@console/dynamic-plugin-sdk/src/extensions/console-types';
+import { UseAnnotationsModal } from '@console/dynamic-plugin-sdk/src/extensions/console-types';
 import { useK8sModel } from '@console/dynamic-plugin-sdk/src/utils/k8s/hooks/useK8sModel';
 import { getGroupVersionKindForResource } from '@console/dynamic-plugin-sdk/src/utils/k8s/k8s-ref';
 import { ModalWrapper } from '@console/internal/components/factory/modal';
 import { AnnotationsModal, AnnotationsModalProps } from '@console/internal/components/modals/tags';
-
-type UseAnnotationsModal = (resource: K8sResourceCommon) => () => void;
 
 const AnnotationsModalComponent: ModalComponent<AnnotationsModalProps> = ({
   closeModal,
@@ -21,19 +19,6 @@ const AnnotationsModalComponent: ModalComponent<AnnotationsModalProps> = ({
   );
 };
 
-/**
- * A hook for launching a modal for editing a resource's annotations.
- *
- * @hook useAnnotationsModal
- * @argument resource - The resource to edit annotations for.
- * @returns a function which will launch a modal for editing a resource's annotations.
- * @example
- * const PodAnnotationsButton = ({ pod }) => {
- *   const { t } = useTranslation();
- *   const launchAnnotationsModal = useAnnotationsModal<PodKind>(pod);
- *   return <button onClick={launchAnnotationsModal}>{t('Edit Pod Annotations')}</button>
- * }
- */
 export const useAnnotationsModal: UseAnnotationsModal = (resource) => {
   const launcher = useModal();
   const groupVersionKind = getGroupVersionKindForResource(resource);
