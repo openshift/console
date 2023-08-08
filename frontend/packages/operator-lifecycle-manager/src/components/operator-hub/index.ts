6eea20e3991b6a0ae5d59c40ad3ea38a268926f5
Lance Galletti <lgallett@redhat.com>
2023-08-08 11:28:47 -0400

    Add support for Azure Workload Identity / Federated Identity based installs in OperatorHub

--- frontend/packages/operator-lifecycle-manager/src/components/operator-hub/index.ts
+++ frontend/packages/operator-lifecycle-manager/src/components/operator-hub/index.ts
@@ -32,7 +32,7 @@ export enum InfraFeatures {
   csi = 'Container Storage Interface',
   sno = 'Single Node Clusters',
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  'Short-lived token authentication' = 'Short-lived token authentication',
+  TokenAuth = 'Short-lived token authentication',
 }
 
 export enum ValidSubscriptionValue {
