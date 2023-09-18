8246ac3f7cb36a4a83b1d3d98e68ea569b3914de
Robb Hamilton <rhamilto@redhat.com>
2023-09-18 11:49:22 -0400

    OCPBUGS-18832: change resource icon for FenceAgentRemediationTemplate to FAR

--- frontend/public/module/k8s/get-resources.ts
+++ frontend/public/module/k8s/get-resources.ts
@@ -26,7 +26,7 @@ const ADMIN_RESOURCES = new Set([
   'secrets',
 ]);
 
-const abbrBlacklist = ['ASS'];
+const abbrBlacklist = ['ASS', 'FART'];
 export const kindToAbbr = (kind) => {
   const abbrKind = (kind.replace(/[^A-Z]/g, '') || kind.toUpperCase()).slice(0, 4);
   return abbrBlacklist.includes(abbrKind) ? abbrKind.slice(0, -1) : abbrKind;
