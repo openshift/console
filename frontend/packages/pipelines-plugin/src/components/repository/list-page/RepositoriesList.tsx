09f4422ffdf075dbf05c58f4106ba4b18d7509f1
Vikram Raj <vikrmrj@gmail.com>
2023-11-30 11:06:30 +0530

    Remove tech preview badge from Pipeline repository pages

--- frontend/packages/pipelines-plugin/src/components/repository/list-page/RepositoriesList.tsx
+++ frontend/packages/pipelines-plugin/src/components/repository/list-page/RepositoriesList.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ListPage } from '@console/internal/components/factory';
 import { referenceForModel } from '@console/internal/module/k8s';
-import { getBadgeFromType } from '@console/shared/src';
 import { RepositoryModel } from '../../../models';
 import RepositoryList from './ReppositoryList';
 
@@ -14,7 +13,6 @@ const RepositoriesList: React.FC<React.ComponentProps<typeof ListPage>> = (props
     canCreate={props.canCreate ?? true}
     kind={referenceForModel(RepositoryModel)}
     ListComponent={RepositoryList}
-    badge={getBadgeFromType(RepositoryModel.badge)}
   />
 );
 
