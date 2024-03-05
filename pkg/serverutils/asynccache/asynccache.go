f8dafbd8709667a968d95e992605fedd9454faa0
Stanislav Laznicka <slaznick@redhat.com>
2024-03-05 11:10:34 +0100

    fix asynccache never returning an error

--- pkg/serverutils/asynccache/asynccache.go
+++ pkg/serverutils/asynccache/asynccache.go
@@ -10,10 +10,9 @@ import (
 	"k8s.io/klog"
 )
 
-const (
-	initializationRetryInterval = 10 * time.Second
+var ( // make these variable so that we can change them in unit tests
+	initializationRetryInterval = 5 * time.Second
 	initializationTimeout       = 5 * time.Minute
-	initializeImmediately       = true
 )
 
 type cachingFuncType[T any] func(ctx context.Context) (T, error)
@@ -33,12 +32,11 @@ func NewAsyncCache[T any](ctx context.Context, reloadPeriod time.Duration, cachi
 		cachingFunc:  cachingFunc,
 	}
 
-	var err error
-	wait.PollUntilContextTimeout(
+	err := wait.PollUntilContextTimeout(
 		ctx,
 		initializationRetryInterval,
 		initializationTimeout,
-		initializeImmediately,
+		true,
 		func(ctx context.Context) (bool, error) {
 			item, err := cachingFunc(ctx)
 			if err != nil {
