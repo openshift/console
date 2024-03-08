c3d89c5798025981b647269b36ae150ee9d9c23f
CYRIL AJIEH <cyril.ajieh@gmail.com>
2024-03-08 11:59:39 -0500

    add-locales-support-fr-es

--- frontend/packages/console-app/src/components/user-preferences/language/const.ts
+++ frontend/packages/console-app/src/components/user-preferences/language/const.ts
@@ -1,8 +1,10 @@
 export const supportedLocales = {
   en: 'English',
-  'zh-CN': '中文',
+  es: 'Español',
+  fr: 'Français',
   ko: '한국어',
   ja: '日本語',
+  'zh-CN': '中文',
 };
 
 export const LAST_LANGUAGE_LOCAL_STORAGE_KEY = 'bridge/last-language';
