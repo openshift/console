71cee397656dd7d66db381a413857a13eaa6bc7f
Robb Hamilton <rhamilto@redhat.com>
2024-04-19 17:54:33 -0400

    CONSOLE-3998: Update 'Explore new admin features' section (#13734)
    * CONSOLE-3998: Update 'Explore new admin features' section
    
    * Make LightSpeed link conditional on LightSpeed PackageManifest being present

--- frontend/public/components/dashboard/dashboards-page/cluster-dashboard/getting-started/explore-admin-features-getting-started-card.tsx
+++ frontend/public/components/dashboard/dashboards-page/cluster-dashboard/getting-started/explore-admin-features-getting-started-card.tsx
@@ -9,25 +9,59 @@ import {
   GettingStartedLink,
 } from '@console/shared/src/components/getting-started';
 import { DOC_URL_OPENSHIFT_WHATS_NEW } from '../../../../utils';
+import { k8sGetResource } from '@console/dynamic-plugin-sdk/src/utils/k8s';
+import { PackageManifestModel } from '@console/operator-lifecycle-manager/src';
+
+const getLightSpeedAvailability = async () => {
+  try {
+    await k8sGetResource({
+      model: PackageManifestModel,
+      name: 'lightspeed-operator',
+      ns: 'openshift-marketplace',
+    });
+    return true;
+  } catch (err) {
+    if (err.response.status !== 404) {
+      // eslint-disable-next-line no-console
+      console.log(err.message);
+    }
+    return false;
+  }
+};
 
 export const ExploreAdminFeaturesGettingStartedCard: React.FC = () => {
   const { t } = useTranslation();
   const parsed = semver.parse(useOpenShiftVersion());
   // Show only major and minor version.
   const version = parsed ? `${parsed.major}.${parsed.minor}` : '';
+  const links: GettingStartedLink[] = React.useMemo(
+    () => [
+      {
+        id: 'openshift-ai',
+        title: t('public~OpenShift AI'),
+        description: t('public~Build, deploy, and manage AI-enabled applications.'),
+        href:
+          '/operatorhub/all-namespaces?keyword=openshift+ai&details-item=rhods-operator-redhat-operators-openshift-marketplace',
+      },
+    ],
+    [t],
+  );
 
-  const links: GettingStartedLink[] = [
-    {
-      id: 'api-explorer',
-      title: t('public~API Explorer'),
-      href: '/api-explorer',
-    },
-    {
-      id: 'operatorhub',
-      title: t('public~OperatorHub'),
-      href: '/operatorhub',
-    },
-  ];
+  React.useEffect(() => {
+    const addLinkIfLightSpeedIsAvailable = async () => {
+      if (await getLightSpeedAvailability()) {
+        links.push({
+          id: 'lightspeed',
+          title: t('public~OpenShift LightSpeed'),
+          description: t('public~Your personal AI helper.'),
+          href:
+            '/operatorhub/all-namespaces?keyword=lightspeed&details-item=lightspeed-operator-lightspeed-operator-catalog-openshift-marketplace',
+        });
+      }
+    };
+
+    addLinkIfLightSpeedIsAvailable();
+  }, [links, t]);
 
   const moreLink: GettingStartedLink = {
     id: 'whats-new',
@@ -40,9 +74,8 @@ export const ExploreAdminFeaturesGettingStartedCard: React.FC = () => {
     <GettingStartedCard
       id="admin-features"
       icon={<FlagIcon color="var(--co-global--palette--orange-400)" aria-hidden="true" />}
-      title={t('public~Explore new admin features')}
+      title={t('public~Explore new features and capabilities')}
       titleColor={'var(--co-global--palette--orange-400)'}
-      description={t('public~Explore new features and resources within the admin perspective.')}
       links={links}
       moreLink={moreLink}
     />
