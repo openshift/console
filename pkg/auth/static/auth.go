577e582e6277d7c3c32de2526ff193baba643c70
Stanislav Laznicka <slaznick@redhat.com>
2024-04-29 12:55:36 +0200

    handle static user as a special-case authenticator

---
+++ pkg/auth/static/auth.go
@@ -0,0 +1,34 @@
+package static
+
+import (
+	"net/http"
+
+	"github.com/openshift/console/pkg/auth"
+	"github.com/openshift/console/pkg/auth/sessions"
+)
+
+type StaticAuthenticator struct {
+	user *auth.User
+}
+
+func NewStaticAuthenticator(user auth.User) *StaticAuthenticator {
+	return &StaticAuthenticator{
+		user: &user,
+	}
+}
+
+func (s *StaticAuthenticator) Authenticate(w http.ResponseWriter, req *http.Request) (*auth.User, error) {
+	userCopy := *s.user
+	return &userCopy, nil
+}
+
+func (s *StaticAuthenticator) LoginFunc(w http.ResponseWriter, req *http.Request)  {}
+func (s *StaticAuthenticator) LogoutFunc(w http.ResponseWriter, req *http.Request) {}
+
+func (s *StaticAuthenticator) CallbackFunc(fn func(loginInfo sessions.LoginJSON, successURL string, w http.ResponseWriter)) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {}
+}
+
+func (s *StaticAuthenticator) GetOCLoginCommand() string            { return "" }
+func (s *StaticAuthenticator) LogoutRedirectURL() string            { return "" }
+func (s *StaticAuthenticator) GetSpecialURLs() auth.SpecialAuthURLs { return auth.SpecialAuthURLs{} }
