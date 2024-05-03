c00082ab9c06075415ca3a3fc37fe6361b3fae52
Kartikey-star <kmamgain@redhat.com>
2024-05-03 16:11:18 +0530

    bump helm
    Signed-off-by: Kartikey-star <kmamgain@redhat.com>

--- vendor/github.com/devfile/api/v2/pkg/validation/endpoints.go
+++ vendor/github.com/devfile/api/v2/pkg/validation/endpoints.go
@@ -19,10 +19,10 @@ package validation
 import "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 
 // validateEndpoints checks if
-// 1. all the endpoint names are unique across components
-// 2. endpoint port are unique across component containers
-//   ie; two component containers cannot have the same target port but two endpoints
-//   in a single component container can have the same target port
+//  1. all the endpoint names are unique across components
+//  2. endpoint port are unique across component containers
+//     ie; two component containers cannot have the same target port but two endpoints
+//     in a single component container can have the same target port
 func validateEndpoints(endpoints []v1alpha2.Endpoint, processedEndPointPort map[int]bool, processedEndPointName map[string]bool) (errList []error) {
 	currentComponentEndPointPort := make(map[int]bool)
 
