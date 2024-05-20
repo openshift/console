96788f2b87493ec15c2ebe7709a3a0a89d698425
saumeya <saumeyakatyal@gmail.com>
2024-05-20 15:25:02 +0530

    review changes
    Signed-off-by: saumeya <saumeyakatyal@gmail.com>

--- frontend/packages/gitops-plugin/integration-tests/support/page-objects/environments-po.ts
+++ frontend/packages/gitops-plugin/integration-tests/support/page-objects/environments-po.ts
@@ -1,5 +1,5 @@
 export const environmentsPO = {
-  noGitOpsUrlsFound: '.loading-box.loading-box__loaded',
+  noGitOpsUrlsFound: '.gitops-plugin__application-list-page',
   argoCDLink:
     '[href="https://argocd-cluster-server-openshift-gitops.apps.rhamilto.devcluster.openshift.com"]',
 };
