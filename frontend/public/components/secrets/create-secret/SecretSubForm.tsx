3d74d9c0bcb7b0b1f48f69d7b62ca830fae2b9c4
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2024-06-21 11:45:51 -0400

    Revert "refactored imageSecretForm component"
    This reverts commit 548d7323e9ce9c4bfb4137d38e0a9342829b8fd3.

--- frontend/public/components/secrets/create-secret/SecretSubForm.tsx
+++ frontend/public/components/secrets/create-secret/SecretSubForm.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {
   SourceSecretForm,
   SecretTypeAbstraction,
-  PullSecretForm,
+  ImageSecretForm,
   WebHookSecretForm,
   GenericSecretForm,
   SecretType,
@@ -16,7 +16,7 @@ export const SecretSubForm: React.FC<WithTypeAbstraction<SecretSubFormProps>> = 
     case SecretTypeAbstraction.source:
       return <SourceSecretForm {...props} />;
     case SecretTypeAbstraction.image:
-      return <PullSecretForm {...props} />;
+      return <ImageSecretForm {...props} />;
     case SecretTypeAbstraction.webhook:
       return <WebHookSecretForm {...props} />;
     default:
