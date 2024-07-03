640e94e69a23df072a8dbdcc47a03ed0cc428708
logonoff <git@logonoff.co>
2024-07-03 09:30:48 -0400

    ODC-7632: Add BS and CBS tabs to Shipwright page
    Tabs are persisted

--- frontend/packages/shipwright-plugin/src/components/shipwright-tablistpage/ShipwrightTabListPage.tsx
+++ frontend/packages/shipwright-plugin/src/components/shipwright-tablistpage/ShipwrightTabListPage.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import { Badge } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
+import { useParams, useNavigate } from 'react-router-dom-v5-compat';
 import { useFlag } from '@console/dynamic-plugin-sdk/src/lib-core';
+import { DefaultPage } from '@console/internal/components/default-resource';
 import { Page } from '@console/internal/components/utils';
-import { MultiTabListPage, MenuActions } from '@console/shared';
-import { BuildModel, BuildModelV1Alpha1, BuildRunModel, BuildRunModelV1Alpha1 } from '../../models';
+import { referenceForModel } from '@console/internal/module/k8s';
+import { MenuActions, MultiTabListPage, useUserSettings } from '@console/shared';
+import { LAST_SHIPWRIGHT_PAGE_TAB_STORAGE_KEY } from '../../const';
+import {
+  BuildModelV1Alpha1,
+  BuildRunModelV1Alpha1,
+  ClusterBuildStrategyModel,
+  BuildStrategyModel,
+  BuildModel,
+  BuildRunModel,
+} from '../../models';
 import BuildListPage from '../build-list/BuildListPage';
 import BuildRunListPage from '../buildrun-list/BuildRunListPage';
-// import { RowFilter } from '@console/internal/components/filter-toolbar';
-// import { referenceForModel } from '@console/internal/module/k8s';
-// import { BuildRunModel, BuildRunModelV1Alpha1 } from '../../models';
-// import { BuildRun, ComputedBuildRunStatus } from '../../types';
-// import { getBuildRunStatus } from '../buildrun-status/BuildRunStatus';
-
-const FillerComponent = (props) => {
-  return <>{JSON.stringify(props)}</>;
-};
 
 const buildListPage: Page = {
   href: 'build',
@@ -29,10 +31,58 @@ const buildRunListPage: Page = {
   nameKey: 'shipwright-plugin~Shipwright BuildRuns',
 };
 
+const clusterBuildStrategyPage: Page = {
+  href: 'clusterbuildstrategy',
+  component: DefaultPage,
+  nameKey: ClusterBuildStrategyModel.labelPluralKey,
+  pageData: {
+    kind: referenceForModel(ClusterBuildStrategyModel),
+    autoFocus: true,
+    showTitle: false,
+    canCreate: false,
+    hideBadge: true,
+  },
+};
+
+const BuildStrategyPage: Page = {
+  href: 'buildstrategy',
+  component: DefaultPage,
+  nameKey: BuildStrategyModel.labelPluralKey,
+  pageData: {
+    kind: referenceForModel(BuildStrategyModel),
+    autoFocus: true,
+    showTitle: false,
+    canCreate: false,
+    hideBadge: true,
+  },
+};
+
 const ShipwrightTabListPage: React.FC = () => {
   const { t } = useTranslation();
   const menuActions: MenuActions = {};
   const pages: Page[] = [];
+  const navigate = useNavigate();
+  const { '*': currentTab } = useParams();
+  const [preferredTab, setPreferredTab, preferredTabLoaded] = useUserSettings<string>(
+    LAST_SHIPWRIGHT_PAGE_TAB_STORAGE_KEY,
+    'build',
+  );
+
+  /* Redirect to last visited tab */
+  React.useEffect(() => {
+    if (preferredTabLoaded && currentTab === '') {
+      navigate(preferredTab);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [preferredTabLoaded]);
+
+  React.useEffect(() => {
+    // update the preferred tab
+    if (preferredTabLoaded && pages.some((page) => page.href === currentTab)) {
+      setPreferredTab(currentTab);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTab, preferredTabLoaded]);
 
   if (useFlag('SHIPWRIGHT_BUILD')) {
     menuActions.build = {
@@ -66,17 +116,19 @@ const ShipwrightTabListPage: React.FC = () => {
     pages.includes(buildRunListPage) || pages.push(buildRunListPage);
   }
 
-  pages.push({
-    href: 'buildstrategy',
-    component: FillerComponent,
-    nameKey: 'shipwright-plugin~BuildStrategy',
-  });
+  menuActions.buildstrategy = {
+    model: ClusterBuildStrategyModel,
+    label: t('shipwright-plugin~BuildStrategy'),
+  };
 
-  pages.push({
-    href: 'clusterbuildstrategy',
-    component: FillerComponent,
-    nameKey: 'shipwright-plugin~ClusterBuildStrategy',
-  });
+  pages.push(BuildStrategyPage);
+
+  menuActions.clusterbuildstrategy = {
+    model: ClusterBuildStrategyModel,
+    label: t('shipwright-plugin~ClusterBuildStrategy'),
+  };
+
+  pages.push(clusterBuildStrategyPage);
 
   return (
     <MultiTabListPage
