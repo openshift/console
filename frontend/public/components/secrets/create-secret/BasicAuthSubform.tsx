cbf2384d7b5ad48d3eb28cbed2688b27f5bd606f
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2024-07-14 13:01:40 -0400

    refactoring basic auth subform

--- frontend/public/components/secrets/create-secret/BasicAuthSubform.tsx
+++ frontend/public/components/secrets/create-secret/BasicAuthSubform.tsx
@@ -1,90 +1,64 @@
 import * as React from 'react';
-import { withTranslation } from 'react-i18next';
-import { WithT } from 'i18next';
+import { useTranslation } from 'react-i18next';
+import { SecretChangeData, SecretStringData } from '.';
 
-class BasicAuthSubformWithTranslation extends React.Component<
-  BasicAuthSubformProps & WithT,
-  BasicAuthSubformState
-> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: this.props.stringData.username || '',
-      password: this.props.stringData.password || '',
-    };
-    this.changeData = this.changeData.bind(this);
-  }
-  changeData(event) {
-    this.setState(
-      {
-        [event.target.name]: event.target.value,
-      } as BasicAuthSubformState,
-      () => this.props.onChange(this.state),
-    );
-  }
+export const BasicAuthSubform: React.FC<BasicAuthSubformProps> = ({ onChange, stringData }) => {
+  const { t } = useTranslation();
+  const changeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    onChange({ stringData: { ...stringData, [name]: value }, base64StringData: {} });
+  };
 
-  render() {
-    const { t } = this.props;
-    return (
-      <>
-        <div className="form-group">
-          <label className="control-label" htmlFor="username">
-            {t('public~Username')}
-          </label>
-          <div>
-            <input
-              className="pf-v5-c-form-control"
-              id="username"
-              data-test="secret-username"
-              aria-describedby="username-help"
-              type="text"
-              name="username"
-              onChange={this.changeData}
-              value={this.state.username}
-            />
-            <p className="help-block" id="username-help">
-              {t('public~Optional username for Git authentication.')}
-            </p>
-          </div>
+  return (
+    <>
+      <div className="form-group">
+        <label className="control-label" htmlFor="username">
+          {t('public~Username')}
+        </label>
+        <div>
+          <input
+            className="pf-v5-c-form-control"
+            id="username"
+            data-test="secret-username"
+            aria-describedby="username-help"
+            type="text"
+            name="username"
+            onChange={changeData}
+            value={stringData.username}
+          />
+          <p className="help-block" id="username-help">
+            {t('public~Optional username for Git authentication.')}
+          </p>
         </div>
-        <div className="form-group">
-          <label className="control-label co-required" htmlFor="password">
-            {t('public~Password or token')}
-          </label>
-          <div>
-            <input
-              className="pf-v5-c-form-control"
-              id="password"
-              data-test="secret-password"
-              aria-describedby="password-help"
-              type="password"
-              name="password"
-              onChange={this.changeData}
-              value={this.state.password}
-              required
-            />
-            <p className="help-block" id="password-help">
-              {t(
-                'public~Password or token for Git authentication. Required if a ca.crt or .gitconfig file is not specified.',
-              )}
-            </p>
-          </div>
+      </div>
+      <div className="form-group">
+        <label className="control-label co-required" htmlFor="password">
+          {t('public~Password or token')}
+        </label>
+        <div>
+          <input
+            className="pf-v5-c-form-control"
+            id="password"
+            data-test="secret-password"
+            aria-describedby="password-help"
+            type="password"
+            name="password"
+            onChange={changeData}
+            value={stringData.password}
+            required
+          />
+          <p className="help-block" id="password-help">
+            {t(
+              'public~Password or token for Git authentication. Required if a ca.crt or .gitconfig file is not specified.',
+            )}
+          </p>
         </div>
-      </>
-    );
-  }
-}
-
-export const BasicAuthSubform = withTranslation()(BasicAuthSubformWithTranslation);
-
-type BasicAuthSubformState = {
-  username: string;
-  password: string;
+      </div>
+    </>
+  );
 };
 
 type BasicAuthSubformProps = {
-  onChange: Function;
-  stringData: {
-    [key: string]: string;
-  };
+  onChange: (stringData: SecretChangeData) => void;
+  stringData: SecretStringData;
 };
