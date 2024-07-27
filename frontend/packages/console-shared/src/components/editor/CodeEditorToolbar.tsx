9e4591a4c9586a6157405ff4c3340ea2ca028de1
Lucifergene <47265560+Lucifergene@users.noreply.github.com>
2024-07-27 08:19:11 +0530

    Added Ask Lightspeed button on YAML Editors
    Signed-off-by: Lucifergene <47265560+Lucifergene@users.noreply.github.com>

--- frontend/packages/console-shared/src/components/editor/CodeEditorToolbar.tsx
+++ frontend/packages/console-shared/src/components/editor/CodeEditorToolbar.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { Divider } from '@patternfly/react-core';
+import { Button, Divider } from '@patternfly/react-core';
+import { MagicIcon } from '@patternfly/react-icons';
 import { useTranslation } from 'react-i18next';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: FIXME missing exports due to out-of-sync @types/react-redux version
+import { useDispatch } from 'react-redux';
+import { action } from 'typesafe-actions';
+import { useOLSConfig } from '../../hooks/ols-hook';
 import { isMac, ShortcutCommand } from '../shortcuts/Shortcut';
 import ShortcutsLink from './ShortcutsLink';
 
@@ -11,12 +17,38 @@ interface CodeEditorToolbarProps {
   toolbarLinks?: React.ReactNodeArray;
 }
 
+export enum ActionType {
+  OpenOLS = 'openOLS',
+}
+
 const CodeEditorToolbar: React.FC<CodeEditorToolbarProps> = ({ showShortcuts, toolbarLinks }) => {
   const { t } = useTranslation();
+  const openOLS = () => action(ActionType.OpenOLS);
+  const showLightspeedButton = useOLSConfig();
+  const dispatch = useDispatch();
   if (!showShortcuts && !toolbarLinks?.length) return null;
   return (
     <div className="co-toolbar__group co-toolbar__group--right">
       <div className="ocs-yaml-editor-toolbar pf-v5-u-pb-sm pf-v5-l-flex">
+        {showLightspeedButton && (
+          <div className="ocs-yaml-editor-toolbar__link pf-v5-l-flex">
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() => {
+                dispatch(openOLS());
+              }}
+              icon={<MagicIcon />}
+            >
+              {t('console-shared~Ask OpenShift Lightspeed')}
+            </Button>
+            <Divider
+              orientation={{
+                default: 'vertical',
+              }}
+            />
+          </div>
+        )}
         <div className="ocs-yaml-editor-toolbar__link">
           <div>
             <span className="ocs-yaml-editor-shortcut__command">
---
+++ frontend/packages/console-shared/src/hooks/ols-hook.ts
@@ -0,0 +1,26 @@
+import { K8sKind, K8sResourceKind } from '@console/dynamic-plugin-sdk/src';
+import { useK8sGet } from '@console/internal/components/utils/k8s-get-hook';
+
+const OLSConfigModel: K8sKind = {
+  apiGroup: 'ols.openshift.io',
+  apiVersion: 'v1alpha1',
+  label: 'OLSConfig',
+  // t('console-shared~OLSConfig')
+  labelKey: 'console-shared~OLSConfig',
+  // t('console-shared~OLSConfigs')
+  labelPluralKey: 'console-shared~OLSConfigs',
+  plural: 'olsconfigs',
+  abbr: 'OLSC',
+  namespaced: false,
+  kind: 'OLSConfig',
+  id: 'olsconfig',
+  labelPlural: 'OLSConfigs',
+  crd: true,
+};
+
+const OLSCONFIG_INSTANCE = 'cluster';
+
+export const useOLSConfig = (): boolean => {
+  const [, loaded, err] = useK8sGet<K8sResourceKind>(OLSConfigModel, OLSCONFIG_INSTANCE);
+  return loaded && !err;
+};
