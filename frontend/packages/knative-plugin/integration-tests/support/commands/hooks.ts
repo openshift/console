24fde518509fe57cb0b4ec6103793947f1fe4680
The-Anton <shesingh@redhat.com>
2024-08-05 14:39:58 +0530

    enable knative e2e tests with increased namespace delete timeout

--- frontend/packages/knative-plugin/integration-tests/support/commands/hooks.ts
+++ frontend/packages/knative-plugin/integration-tests/support/commands/hooks.ts
@@ -23,11 +23,52 @@ beforeEach(() => {
 
 after(() => {
   const namespaces: string[] = Cypress.env('NAMESPACES') || [];
-  cy.exec(`oc delete namespace ${namespaces.join(' ')}`, { failOnNonZeroExit: false });
+  let start = 0;
+  cy.then(() => {
+    start = performance.now();
+  });
+
+  cy.exec(`oc adm top pod -n ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+  });
+
+  cy.exec(`oc get events -n ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+  });
+
+  cy.exec(`oc adm top pod -n ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+  });
+
+  cy.exec(`oc get all -n ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+  });
+
+  cy.exec(`oc delete namespace ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+    cy.log(`duration: ${performance.now() - start}`);
+  });
   // cy.logout();
 });
 
 afterEach(() => {
+  const namespaces: string[] = Cypress.env('NAMESPACES') || [];
+
+  cy.exec(`oc adm top pod -n ${namespaces.join(' ')}`, {
+    failOnNonZeroExit: false,
+  }).then((result) => {
+    cy.log(result.stdout);
+  });
   // Below code helps to close the form, when there is any issue. so that other scenarios will be executed
   checkErrors();
 });
