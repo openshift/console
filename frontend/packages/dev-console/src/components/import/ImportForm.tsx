c520eabd4884975ea011af07564d1075bf78d35f
logonoff <git@logonoff.co>
2024-09-09 02:26:00 -0400

    OCPBUGS-34764: Ensure envs are initted upon first load (#13966)
    * OCPBUGS-34764: Ensure envs are initted upon first load
    
    * OCPBUGS-34764: Do not show enviroment variables until they have been loaded
    
    Fixes race condition in which, the env variable component loads first. This sets the initial value as blank since the serverless fetcher/parser util is still working. Then the parser is done, updates the prop, but is ignored because the env variable component is already loaded
    
    * OCPBUGS-34764: Also apply fix to DeploymentSection
    
    Also move funcLoaded to serverless, felt that made more sense
    
    * OCPBUGS-34764: Add e2e test
    
    * OCPBUGS-34764: fallback if funcloaded is not defined
    
    * OCPBUGS-34764: Fix test
    
    * OCPBUGS-34764: Rebase
    
    * OCPBUGS-34764: kNative -> knative
    
    * Update frontend/packages/dev-console/integration-tests/features/e2e/knative-service-workload.feature
    
    Co-Authored-By: Sanket Pathak <sanketpathak95@gmail.com>
    
    ---------
    
    Co-authored-by: Sanket Pathak <sanketpathak95@gmail.com>

--- frontend/packages/dev-console/src/components/import/ImportForm.tsx
+++ frontend/packages/dev-console/src/components/import/ImportForm.tsx
@@ -138,6 +138,7 @@ const ImportForm: React.FC<ImportFormProps & StateProps> = ({
     },
     import: {
       loaded: false,
+      knativeFuncLoaded: false,
       loadError: null,
       strategies: [],
       selectedStrategy: {
