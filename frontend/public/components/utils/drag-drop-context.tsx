be9595fdb40bb4df6035b965ae8e74dfc1892cd1
Lokananda Prabhu <lprabhu@redhat.com>
2024-09-12 10:57:23 +0530

    updated withDragDropContext to resolve 'Cannot have two HTML5 backends at the same time.' issue

--- frontend/public/components/utils/drag-drop-context.tsx
+++ frontend/public/components/utils/drag-drop-context.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import { DndProvider, createDndContext } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-
-const dndContext = createDndContext(HTML5Backend);
 
 const withDragDropContext = <TProps extends {}>(
   Component: React.ComponentClass<TProps> | React.FC<TProps>,
 ) => (props: TProps) => {
-  const manager = React.useRef(dndContext);
   return (
-    <DndProvider manager={manager.current.dragDropManager}>
+    <DndProvider backend={HTML5Backend} context={window}>
       <Component {...props} />
     </DndProvider>
   );
