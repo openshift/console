03de0115ce429788e1daf33236150ac6dfa110e1
Phillip Rhodes <pcbailey@users.noreply.github.com>
2024-09-26 10:16:10 -0400

    Restore kubevirt extensions for topology

---
+++ frontend/packages/kubevirt-plugin/src/kubevirt-dependencies/k8s/requests/free-owned-resources.ts
@@ -0,0 +1,28 @@
+import { k8sKill, k8sPatch, OwnerReference } from '@console/internal/module/k8s';
+import { getOwnerReferences } from '../../selectors/k8sCommon';
+import { K8sResourceWithModel } from '../../types/k8s-resource-with-model';
+import { compareOwnerReference } from '../../utils/utils';
+import { PatchBuilder } from '../helpers/patch';
+
+export const freeOwnedResources = async (
+  ownedResources: K8sResourceWithModel[],
+  owner: OwnerReference,
+  doDelete: boolean,
+) => {
+  const freePromises = (ownedResources || [])
+    .filter((res) => res)
+    .map(({ model, resource: ownedResource }) => {
+      if (doDelete) {
+        return k8sKill(model, ownedResource);
+      }
+      return k8sPatch(model, ownedResource, [
+        new PatchBuilder('/metadata/ownerReferences')
+          .setListRemove(getOwnerReferences(ownedResource), (ownerReference) =>
+            compareOwnerReference(ownerReference, owner),
+          )
+          .build(),
+      ]);
+    });
+
+  return Promise.all(freePromises);
+};
