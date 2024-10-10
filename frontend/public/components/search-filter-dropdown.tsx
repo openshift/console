de6d87081fb057759193d7298f788d4c7504fd63
Robb Hamilton <rhamilto@redhat.com>
2024-10-10 12:39:02 -0400

    CONSOLE-4182: replace DropdownDeprecated in SearchFilterDropdown

--- frontend/public/components/search-filter-dropdown.tsx
+++ frontend/public/components/search-filter-dropdown.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import {
-  Dropdown as DropdownDeprecated,
-  DropdownToggle as DropdownToggleDeprecated,
-  DropdownItem as DropdownItemDeprecated,
-} from '@patternfly/react-core/deprecated';
-import { CaretDownIcon } from '@patternfly/react-icons/dist/esm/icons/caret-down-icon';
+  InputGroup,
+  InputGroupItem,
+  MenuToggle,
+  MenuToggleElement,
+  Select,
+  SelectList,
+  SelectOption,
+} from '@patternfly/react-core';
+
 import { FilterIcon } from '@patternfly/react-icons/dist/esm/icons/filter-icon';
 import { TextFilter } from './factory';
 
@@ -16,82 +20,82 @@ export enum searchFilterValues {
   Name = 'Name',
 }
 
-export const SearchFilterDropdown: React.SFC<SearchFilterDropdownProps> = (props) => {
+export const SearchFilterDropdown: React.FC<SearchFilterDropdownProps> = ({
+  labelFilterInput,
+  nameFilterInput,
+  onChange,
+}) => {
   const [isOpen, setOpen] = React.useState(false);
   const [selected, setSelected] = React.useState(searchFilterValues.Label);
-
-  const { onChange, nameFilterInput, labelFilterInput } = props;
-
   const { t } = useTranslation();
 
-  const onToggle = (_event, open: boolean) => setOpen(open);
-  const onSelect = (event: React.SyntheticEvent) => {
-    setSelected((event.target as HTMLInputElement).name as searchFilterValues);
-    setOpen(!isOpen);
+  const onToggle = () => setOpen(!isOpen);
+  const onSelect = (event: React.SyntheticEvent, value: string) => {
+    setSelected(value as searchFilterValues);
+    setOpen(false);
   };
-  const dropdownItems = [
-    <DropdownItemDeprecated
-      key="label-action"
-      data-test="label-filter"
-      name={searchFilterValues.Label}
-      component="button"
-    >
-      {t(searchFilterValues.Label)}
-    </DropdownItemDeprecated>,
-    <DropdownItemDeprecated
-      key="name-action"
-      data-test="name-filter"
-      name={searchFilterValues.Name}
-      component="button"
-    >
-      {t(searchFilterValues.Name)}
-    </DropdownItemDeprecated>,
-  ];
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       const { value } = e.target as HTMLInputElement;
       onChange(selected, value, true);
     }
   };
-
   const handleInputValue = (_event, value: string) => {
     onChange(selected, value, false);
   };
 
+  const selectItems = [
+    <SelectOption key="label-action" data-test="label-filter" value={searchFilterValues.Label}>
+      {t(searchFilterValues.Label)}
+    </SelectOption>,
+    <SelectOption key="name-action" data-test="name-filter" value={searchFilterValues.Name}>
+      {t(searchFilterValues.Name)}
+    </SelectOption>,
+  ];
+
   return (
-    <div className="pf-v5-c-input-group">
-      <DropdownDeprecated
-        onSelect={onSelect}
-        toggle={
-          <DropdownToggleDeprecated
-            id="toggle-id"
-            onToggle={onToggle}
-            toggleIndicator={CaretDownIcon}
-          >
-            <>
-              <FilterIcon className="span--icon__right-margin" /> {t(selected)}
-            </>
-          </DropdownToggleDeprecated>
-        }
-        isOpen={isOpen}
-        dropdownItems={dropdownItems}
-      />
-      <TextFilter
-        parentClassName="co-search__filter-input"
-        onChange={handleInputValue}
-        placeholder={selected === searchFilterValues.Label ? 'app=frontend' : 'my-resource'}
-        name="search-filter-input"
-        id="search-filter-input"
-        value={selected === searchFilterValues.Label ? labelFilterInput : nameFilterInput}
-        onKeyDown={handleKeyDown}
-        aria-labelledby="toggle-id"
-      />
-    </div>
+    <InputGroup>
+      <InputGroupItem>
+        <Select
+          isOpen={isOpen}
+          selected={selected}
+          onSelect={onSelect}
+          onOpenChange={(lala) => setOpen(lala)}
+          toggle={(toggleRef: React.Ref<MenuToggleElement>) => {
+            return (
+              <MenuToggle
+                ref={toggleRef}
+                isExpanded={isOpen}
+                onClick={onToggle}
+                icon={<FilterIcon />}
+                id="search-filter-toggle"
+              >
+                {selected}
+              </MenuToggle>
+            );
+          }}
+          shouldFocusToggleOnSelect
+        >
+          <SelectList>{selectItems}</SelectList>
+        </Select>
+      </InputGroupItem>
+      <InputGroupItem>
+        <TextFilter
+          onChange={handleInputValue}
+          placeholder={selected === searchFilterValues.Label ? 'app=frontend' : 'my-resource'}
+          name="search-filter-input"
+          id="search-filter-input"
+          value={selected === searchFilterValues.Label ? labelFilterInput : nameFilterInput}
+          onKeyDown={handleKeyDown}
+          aria-labelledby="search-filter-toggle"
+        />
+      </InputGroupItem>
+    </InputGroup>
   );
 };
 
 export type SearchFilterDropdownProps = {
-  onChange: (type: string, value: string, endOfString: boolean) => void;
-  nameFilterInput: string;
   labelFilterInput: string;
+  nameFilterInput: string;
+  onChange: (type: string, value: string, endOfString: boolean) => void;
 };
