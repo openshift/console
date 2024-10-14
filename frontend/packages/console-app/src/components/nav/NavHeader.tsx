d04eadab27e0386f76a91bdf1b490b391d773f42
Joachim Schuler <jschuler@redhat.com>
2024-10-14 14:52:27 -0400

    POC to remove the dev perspective

--- frontend/packages/console-app/src/components/nav/NavHeader.tsx
+++ frontend/packages/console-app/src/components/nav/NavHeader.tsx
@@ -136,7 +136,7 @@ const NavHeader: React.FC<NavHeaderProps> = ({ onPerspectiveSelected }) => {
 
   return (
     <>
-      {activePerspective !== ACM_PERSPECTIVE_ID && (
+      {activePerspective !== ACM_PERSPECTIVE_ID && perspectiveDropdownItems.length > 1 && (
         <div
           className="oc-nav-header"
           data-tour-id="tour-perspective-dropdown"
