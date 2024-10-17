0b36539f986c4d4446851816f0f4a2d4fc242f56
The-Anton <shesingh@redhat.com>
2024-10-17 20:06:36 +0530

    fix mochawesome compatibility with cypress-parallel

--- frontend/packages/integration-tests-cypress/reporters/parallel-test-reporter.js
+++ frontend/packages/integration-tests-cypress/reporters/parallel-test-reporter.js
@@ -1,11 +1,50 @@
 const fs = require('fs');
 const path = require('path');
-const Mocha = require('mocha');
+const Mochawesome = require('mochawesome');
+
 // const resultsPath = path.join(process.cwd(), 'runner-results');
 
-class CustomReporter extends Mocha.reporters.Base {
-  constructor(runner) {
-    super(runner);
+// class CustomReporter extends Mocha.reporters.Base {
+//   constructor(runner) {
+//     super(runner);
+
+//     let suiteStarted = false;
+
+//     // Get the thread identifier
+//     const threadId = process.env.CYPRESS_THREAD || 'default';
+
+//     // Set up a log file for this thread
+//     this.logFile = path.join(
+//       __dirname,
+//       `../../../gui_test_screenshots/integration-tests-cypress-test-results-thread-${threadId}.txt`,
+//     );
+
+//     runner.on('suite', (suite) => {
+//       if (!suiteStarted) {
+//         this.log(`\n\t Running: ${suite.title || suite.file || 'N/A'}\n`);
+//         suiteStarted = true;
+//       }
+//     });
+
+//     runner.on('pass', (test) => {
+//       this.log(`\n\t\t \u2713 ${test.title}  (${test.duration}s)`);
+//     });
+
+//     runner.on('fail', (test, err) => {
+//       this.log(`\n\t\t \u2716 ${test.title} - \n\t\t Error: ${err.message}`);
+//     });
+//   }
+
+//   log(message) {
+//     fs.appendFileSync(this.logFile, message, 'utf8');
+//   }
+// }
+
+// module.exports = CustomReporter;
+
+class CustomMochawesomeReporter extends Mochawesome {
+  constructor(runner, options) {
+    super(runner, options);
 
     let suiteStarted = false;
 
@@ -39,4 +78,4 @@ class CustomReporter extends Mocha.reporters.Base {
   }
 }
 
-module.exports = CustomReporter;
+module.exports = CustomMochawesomeReporter;
