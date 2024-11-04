564e1170ac64fe06256b1f61d036c199c4fab036
Lokananda Prabhu <lprabhu@redhat.com>
2024-11-04 17:53:38 +0530

    Removing ClusterTask dependecny since it is deprecated from Pipelines 1.17

--- frontend/packages/pipelines-plugin/src/components/pipelines/utils/pipeline-operator.ts
+++ frontend/packages/pipelines-plugin/src/components/pipelines/utils/pipeline-operator.ts
@@ -16,6 +16,8 @@ import {
   PipelineMetricsLevel,
   PIPELINE_NAMESPACE,
   FLAG_TEKTON_V1_ENABLED,
+  FLAG_PIPELINES_OPERATOR_VERSION_1_16_OR_NEWER,
+  FLAG_PIPELINES_OPERATOR_VERSION_1_17_OR_NEWER,
 } from '../const';
 import { MetricsQueryPrefix } from '../pipeline-metrics/pipeline-metrics-utils';
 import { getPipelineMetricsLevel, usePipelineConfig } from './pipeline-config';
@@ -100,4 +102,27 @@ export const useIsTektonV1VersionPresent = (setFeatureFlag: SetFeatureFlag) => {
   const operatorVersion = usePipelineOperatorVersion(activeNamespace);
   const isTektonV1VersionPresent = operatorVersion?.major === 1 && operatorVersion?.minor >= 11;
   setFeatureFlag(FLAG_TEKTON_V1_ENABLED, isTektonV1VersionPresent);
+};
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const useIsPipelineOperatorVersion_1_16_or_newer = (setFeatureFlag: SetFeatureFlag) => {
+  const [activeNamespace] = useActiveNamespace();
+  const operatorVersion = usePipelineOperatorVersion(activeNamespace);
+  const IS_PIPELINE_OPERATOR_VERSION_1_16_OR_NEWER =
+    operatorVersion?.major === 1 && operatorVersion?.minor >= 16;
+  setFeatureFlag(
+    FLAG_PIPELINES_OPERATOR_VERSION_1_16_OR_NEWER,
+    IS_PIPELINE_OPERATOR_VERSION_1_16_OR_NEWER,
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const useIsPipelineOperatorVersion_1_17_or_newer = (setFeatureFlag: SetFeatureFlag) => {
+  const [activeNamespace] = useActiveNamespace();
+  const operatorVersion = usePipelineOperatorVersion(activeNamespace);
+  const IS_PIPELINE_OPERATOR_VERSION_1_17_OR_NEWER =
+    operatorVersion?.major === 1 && operatorVersion?.minor >= 17;
+  setFeatureFlag(
+    FLAG_PIPELINES_OPERATOR_VERSION_1_17_OR_NEWER,
+    IS_PIPELINE_OPERATOR_VERSION_1_17_OR_NEWER,
+  );
 };
