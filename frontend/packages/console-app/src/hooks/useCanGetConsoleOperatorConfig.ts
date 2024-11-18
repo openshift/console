16c6473bd04889d230ee2d25320a98ce66707129
Robb Hamilton <rhamilto@redhat.com>
2024-11-18 14:41:41 -0500

    Add Dynamic Plugins nav item

---
+++ frontend/packages/console-app/src/hooks/useCanGetConsoleOperatorConfig.ts
@@ -0,0 +1,17 @@
+import { SetFeatureFlag, useAccessReview } from '@console/dynamic-plugin-sdk';
+import { ConsoleOperatorConfigModel } from '@console/internal/models';
+import { CONSOLE_OPERATOR_CONFIG_NAME } from '@console/shared/src';
+import { FLAG_CAN_GET_CONSOLE_OPERATOR_CONFIG } from '../consts';
+
+const useCanGetConsoleOperatorConfig = (setFeatureFlag: SetFeatureFlag) => {
+  const canGetConsoleOperatorConfig = useAccessReview({
+    group: ConsoleOperatorConfigModel.apiGroup,
+    resource: ConsoleOperatorConfigModel.plural,
+    verb: 'get',
+    name: CONSOLE_OPERATOR_CONFIG_NAME,
+  });
+
+  setFeatureFlag(FLAG_CAN_GET_CONSOLE_OPERATOR_CONFIG, canGetConsoleOperatorConfig[0]);
+};
+
+export default useCanGetConsoleOperatorConfig;
