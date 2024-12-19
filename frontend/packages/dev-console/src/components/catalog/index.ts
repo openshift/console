d9dec7a346cbb2fa9c14e4724bf9a90b4fcc19f7
logonoff <git@logonoff.co>
2024-12-19 12:33:55 -0500

    OCPBUGS-45847: Remove barrel file references in dev-console

--- frontend/packages/dev-console/src/components/catalog/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './providers';
-export { default as CatalogTypesConfiguration } from './CatalogTypesConfiguration';
-export { default as PinnedResourcesConfiguration } from './PinnedResourcesConfiguration';
-export { default as CatalogPage } from './CatalogPage';
