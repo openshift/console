288206b560b79a5c08cbde625941e5bc48e32536
Jon Jackson <jonjacks@redhat.com>
2025-02-13 15:43:07 -0500

    finishing touches on Extension CatalogItems

--- frontend/packages/operator-lifecycle-manager-v1/src/fbc/catalog-item.tsx
+++ frontend/packages/operator-lifecycle-manager-v1/src/fbc/catalog-item.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { CatalogItem } from '@console/dynamic-plugin-sdk/src';
+import { SyncMarkdownView } from '@console/internal/components/markdown-view';
 import { Timestamp } from '@console/internal/components/utils';
 import { CapabilityLevel } from '@console/operator-lifecycle-manager/src/components/operator-hub/operator-hub-item-details';
+import { validSubscriptionReducer } from '@console/operator-lifecycle-manager/src/components/operator-hub/operator-hub-utils';
 import { ExternalLink } from '@console/shared/src/components/links/ExternalLink';
 import { PlainList } from '@console/shared/src/components/lists/PlainList';
 import { ExtensionCatalogItem } from './types';
@@ -27,40 +29,53 @@ export const normalizeExtensionCatalogItem: NormalizeExtensionCatalogItem = (pkg
     validSubscription,
     createdAt,
   } = pkg;
+  const [validSubscriptions, validSubscriptionFilters] = validSubscriptionReducer(
+    validSubscription,
+  );
   return {
     attributes: {
       keywords,
       source,
       provider,
       infrastructureFeatures,
-      validSubscription,
       capabilities,
+      validSubscription: validSubscriptionFilters,
     },
     creationTimestamp: createdAt,
     cta: {
       label: 'Install',
       href: `/ecosystem/catalog/install/${catalog}/${name}`,
     },
-    description,
+    description: description || longDescription,
     details: {
       properties: [
         {
           label: 'Capability level',
           value: <CapabilityLevel capability={capabilities} />,
         },
-        { label: 'Source', value: source },
-        { label: 'Provider', value: provider },
-        { label: 'Infrastructure features', value: <PlainList items={infrastructureFeatures} /> },
-        { label: 'Valid subscriptions', value: <PlainList items={validSubscription} /> },
+        { label: 'Source', value: source || '-' },
+        { label: 'Provider', value: provider || '-' },
+        {
+          label: 'Infrastructure features',
+          value: infrastructureFeatures?.length ? (
+            <PlainList items={infrastructureFeatures} />
+          ) : (
+            '-'
+          ),
+        },
+        {
+          label: 'Valid subscriptions',
+          value: validSubscriptions?.length ? <PlainList items={validSubscriptions} /> : '-',
+        },
         {
           label: 'Repository',
-          value: repository ? <ExternalLink href={repository} /> : null,
+          value: repository ? <ExternalLink href={repository} /> : '-',
         },
-        { label: 'Container image', value: image },
-        { label: 'Created at', value: <Timestamp timestamp={createdAt} /> },
-        { label: 'Support', value: support },
+        { label: 'Container image', value: image || '-' },
+        { label: 'Created at', value: createdAt ? <Timestamp timestamp={createdAt} /> : '-' },
+        { label: 'Support', value: support || '-' },
       ],
-      descriptions: [{ value: longDescription }],
+      descriptions: [{ value: <SyncMarkdownView content={longDescription} /> }],
     },
     displayName,
     icon: icon ? { url: `data:${icon.mediatype};base64,${icon.base64data}` } : null,
