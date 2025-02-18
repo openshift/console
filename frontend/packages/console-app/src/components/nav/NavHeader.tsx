6f7a2f6bfd1cc689fc5a347dc92436bf55c7413b
logonoff <git@logonoff.co>
2025-02-18 10:26:27 -0500

    Revert "ODC-7700: Filter out ACM perspective in check"
    This reverts commit 0dc0a133dc8835b74cda86dfa57751678882298f.

--- frontend/packages/console-app/src/components/nav/NavHeader.tsx
+++ frontend/packages/console-app/src/components/nav/NavHeader.tsx
@@ -133,8 +133,7 @@ const NavHeader: React.FC<NavHeaderProps> = ({ onPerspectiveSelected }) => {
       : []),
   ];
 
-  return activePerspective !== ACM_PERSPECTIVE_ID &&
-    perspectiveItems.filter((perspective) => perspective !== ACM_PERSPECTIVE_ID).length > 1 ? (
+  return activePerspective !== ACM_PERSPECTIVE_ID && perspectiveItems.length > 1 ? (
     <div
       className="oc-nav-header"
       data-tour-id="tour-perspective-dropdown"
