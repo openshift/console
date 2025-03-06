8e96b34de7ad70f56accd0b389624caa7eba287c
logonoff <git@logonoff.co>
2025-03-06 15:10:01 -0500

    CONSOLE-3247: Remove `react-measure` from `MasonryLayout`

--- frontend/packages/dev-console/src/components/add/__tests__/MasonryLayout.spec.tsx
+++ frontend/packages/dev-console/src/components/add/__tests__/MasonryLayout.spec.tsx
@@ -1,16 +1,21 @@
-import { shallow } from 'enzyme';
-import Measure, { ContentRect } from 'react-measure';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import AddCardSectionSkeleton from '../AddCardSectionSkeleton';
 import { Masonry } from '../layout/Masonry';
 import { MasonryLayout } from '../layout/MasonryLayout';
 
 describe('Masonry Layout', () => {
-  const getContentRect = (width: number, height: number): ContentRect => ({
-    bounds: { width, height, top: 0, left: 0, bottom: 0, right: 0 },
-  });
+  const setWidth = (width: number) => {
+    window.HTMLElement.prototype.getBoundingClientRect = () =>
+      ({
+        width,
+      } as DOMRect);
+  };
 
   it('should render loading component if loading is true and LoadingComponent is defined', () => {
-    const wrapper = shallow(
+    setWidth(1400);
+
+    const wrapper = mount(
       <MasonryLayout columnWidth={300} loading LoadingComponent={AddCardSectionSkeleton}>
         <div>Child 1</div>
         <div>Child 2</div>
@@ -19,34 +24,35 @@ describe('Masonry Layout', () => {
         <div>Child 5</div>
       </MasonryLayout>,
     );
-    wrapper.find(Measure).prop('onResize')(getContentRect(1400, 900));
 
     // Should show 4 columns (Math.floor(1400 / 300))
-    expect(wrapper.dive().dive().find(Masonry).prop('columnCount')).toBe(4);
+    expect(wrapper.find(Masonry).prop('columnCount')).toBe(4);
     // Should render 4 placeholders
-    expect(wrapper.dive().dive().find(Masonry).find(AddCardSectionSkeleton)).toHaveLength(4);
+    expect(wrapper.find(Masonry).find(AddCardSectionSkeleton)).toHaveLength(4);
   });
 
   it('should render children if loading is false', () => {
-    const wrapper = shallow(
+    setWidth(1400);
+
+    const wrapper = mount(
       <MasonryLayout columnWidth={300}>
-        <div>Child 1</div>
-        <div>Child 2</div>
-        <div>Child 3</div>
-        <div>Child 4</div>
-        <div>Child 5</div>
+        <div className="child">Child 1</div>
+        <div className="child">Child 2</div>
+        <div className="child">Child 3</div>
+        <div className="child">Child 4</div>
+        <div className="child">Child 5</div>
       </MasonryLayout>,
     );
-    wrapper.find(Measure).prop('onResize')(getContentRect(1400, 900));
-
     // Should show 4 columns (Math.floor(1400 / 300))
-    expect(wrapper.dive().dive().find(Masonry).prop('columnCount')).toBe(4);
+    expect(wrapper.find(Masonry).prop('columnCount')).toBe(4);
     // Should render all childrens
-    expect(wrapper.dive().dive().find(Masonry).find('div')).toHaveLength(5);
+    expect(wrapper.find(Masonry).find('div.child')).toHaveLength(5);
   });
 
   it('should change columns if a resize event exceeds threshold', () => {
-    const wrapper = shallow(
+    setWidth(1200);
+
+    const wrapper = mount(
       <MasonryLayout columnWidth={300}>
         <div>Child 1</div>
         <div>Child 2</div>
@@ -55,16 +61,25 @@ describe('Masonry Layout', () => {
         <div>Child 5</div>
       </MasonryLayout>,
     );
-    wrapper.find(Measure).prop('onResize')(getContentRect(1200, 800));
-    // Should show 4 columns and all childrens, see test above.
 
-    wrapper.find(Measure).prop('onResize')(getContentRect(900, 800));
+    // Should show 4 columns and all children, see test above.
+    expect(wrapper.find(Masonry).prop('columnCount')).toBe(4);
+
+    act(() => {
+      setWidth(900);
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    wrapper.update();
+
     // Should show 3 columns now (Math.floor(900 / 300))
-    expect(wrapper.dive().dive().find(Masonry).prop('columnCount')).toBe(3);
+    expect(wrapper.find(Masonry).prop('columnCount')).toBe(3);
   });
 
   it('should not change columns if a resize event does not exceed threshold', () => {
-    const wrapper = shallow(
+    // Should show 4 columns and all childrens, see test above.
+    setWidth(1200);
+    const wrapper = mount(
       <MasonryLayout columnWidth={300}>
         <div>Child 1</div>
         <div>Child 2</div>
@@ -73,11 +88,15 @@ describe('Masonry Layout', () => {
         <div>Child 5</div>
       </MasonryLayout>,
     );
-    wrapper.find(Measure).prop('onResize')(getContentRect(1200, 800));
-    // Should show 4 columns and all childrens, see test above.
 
-    wrapper.find(Measure).prop('onResize')(getContentRect(1190, 800));
     // Should still show 4 columns because new width does not exceed threshold
-    expect(wrapper.dive().dive().find(Masonry).prop('columnCount')).toBe(4);
+    act(() => {
+      setWidth(1190);
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    wrapper.update();
+
+    expect(wrapper.find(Masonry).prop('columnCount')).toBe(4);
   });
 });
