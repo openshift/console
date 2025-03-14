091ff0fbe38bd2718c13e5489c2c49f690d75d57
Jon Jackson <jonjacks@redhat.com>
2025-03-14 10:30:38 -0400

    CONSOLE-4201: Migrate ExtensionCatalog to use console-shared catalog system and dynamic plugin catalog extensions

--- frontend/packages/operator-lifecycle-manager-v1/src/const.ts
+++ frontend/packages/operator-lifecycle-manager-v1/src/const.ts
@@ -1,5 +1,5 @@
-export const CLUSTER_CATALOG_GROUP = 'catalogd.operatorframework.io';
-export const CLUSTER_CATALOG_VERSION = 'v1alpha1';
+export const CLUSTER_CATALOG_GROUP = 'olm.operatorframework.io';
+export const CLUSTER_CATALOG_VERSION = 'v1';
 export const CLUSTER_CATALOG_KIND = 'ClusterCatalog';
 export const CLUSTER_CATALOG_GROUP_VERSION_KIND = {
   group: CLUSTER_CATALOG_GROUP,
