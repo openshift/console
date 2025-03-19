fef99e423f3d4afdb0015203a8dd2d72b9d325f0
Lokananda Prabhu <lprabhu@redhat.com>
2025-03-19 11:34:18 +0530

    Updated PR to close guided tour

--- frontend/packages/integration-tests-cypress/tests/favorite/favorite-option.cy.ts
+++ frontend/packages/integration-tests-cypress/tests/favorite/favorite-option.cy.ts
@@ -1,4 +1,5 @@
 import { checkErrors } from '../../support';
+import { guidedTour } from '../../views/guided-tour';
 import { nav } from '../../views/nav';
 
 describe('Favorites', () => {
@@ -6,6 +7,7 @@ describe('Favorites', () => {
     // clear any existing sessions
     Cypress.session.clearAllSavedSessions();
     cy.login();
+    guidedTour.close();
   });
 
   afterEach(() => {
