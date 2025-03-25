85061a0a549122b2a71af39d6c9b34d8efd40d49
logonoff <git@logonoff.co>
2025-03-25 16:26:39 -0400

    CONSOLE-4521: Remove `TextEncoder` support check

--- frontend/public/actions/ui.ts
+++ frontend/public/actions/ui.ts
@@ -215,13 +215,7 @@ export const setActiveNamespace = (namespace: string = '') => {
 };
 
 export const startImpersonate = (kind: string, name: string) => async (dispatch, getState) => {
-  let textEncoder;
-  try {
-    textEncoder = new TextEncoder();
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error('Your browser does not support TextEncoder. Please upgrade your browser.', e);
-  }
+  const textEncoder = new TextEncoder();
 
   const imp = getImpersonate(getState());
   if ((imp?.name && imp.name !== name) || (imp?.kind && imp.kind !== kind)) {
@@ -234,9 +228,9 @@ export const startImpersonate = (kind: string, name: string) => async (dispatch,
    * Subprotocols are comma-separated, so commas aren't allowed. Also "="
    * and "/" aren't allowed, so base64 but replace illegal chars.
    */
-  let encodedName = textEncoder.encode(name);
-  encodedName = Base64.encode(String.fromCharCode.apply(String, encodedName));
-  encodedName = encodedName.replace(/=/g, '_').replace(/\//g, '-');
+  const encodedName = Base64.encode(String.fromCharCode.apply(String, textEncoder.encode(name)))
+    .replace(/=/g, '_')
+    .replace(/\//g, '-');
 
   let subprotocols;
   if (kind === 'User') {
