3ec82368bad9924647b65df5d498314c2e5f68c7
Jon Jackson <jonjacks@redhat.com>
2025-04-18 15:25:24 -0400

    OCPBUGS-43792: Follow-on fix. Add new "plugins-order" flag to bridge cli args since "plugins" is an undordered map. We need a way to persist plugin order from console operator config to the frontend server flag.

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -132,6 +132,7 @@ func main() {
 
 	consolePluginsFlags := serverconfig.MultiKeyValue{}
 	fs.Var(&consolePluginsFlags, "plugins", "List of plugin entries that are enabled for the console. Each entry consist of plugin-name as a key and plugin-endpoint as a value.")
+	fPluginsOrder := fs.String("plugins-order", "", "List of plugin names which determines the order in which plugin extensions will be resolved.")
 	fPluginProxy := fs.String("plugin-proxy", "", "Defines various service types to which will console proxy plugins requests. (JSON as string)")
 	fI18NamespacesFlags := fs.String("i18n-namespaces", "", "List of namespaces separated by comma. Example --i18n-namespaces=plugin__acm,plugin__kubevirt")
 	fContentSecurityPolicyEnabled := fs.Bool("content-security-policy-enabled", false, "Flag to indicate if Content Secrity Policy features should be enabled.")
@@ -233,6 +234,20 @@ func main() {
 		}
 	}
 
+	pluginsOrder := []string{}
+	if *fPluginsOrder != "" {
+		for _, str := range strings.Split(*fPluginsOrder, ",") {
+			str = strings.TrimSpace(str)
+			if str == "" {
+				flags.FatalIfFailed(flags.NewInvalidFlagError("plugins-order", "list must contain names of plugins separated by comma"))
+			}
+			if consolePluginsFlags[str] == "" {
+				flags.FatalIfFailed(flags.NewInvalidFlagError("plugins-order", "list must only contain currently enabled plugins"))
+			}
+			pluginsOrder = append(pluginsOrder, str)
+		}
+	}
+
 	nodeArchitectures := []string{}
 	if *fNodeArchitectures != "" {
 		for _, str := range strings.Split(*fNodeArchitectures, ",") {
@@ -299,6 +314,7 @@ func main() {
 		K8sMode:                      *fK8sMode,
 		CopiedCSVsDisabled:           *fCopiedCSVsDisabled,
 		Capabilities:                 capabilities,
+		PluginsOrder:                 pluginsOrder,
 	}
 
 	completedAuthnOptions, err := authOptions.Complete()
