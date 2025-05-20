e0d25472fc9c32f74450a0bb3a25ffb92c800c81
Mylanos <mziska@redhat.com>
2025-05-20 15:21:54 +0200

    CONSOLE-4580: convert PVCActionFactory to hook form, expose the PVCActions via hook provider.

--- frontend/packages/console-app/src/actions/creators/pvc-factory.ts
+++ frontend/packages/console-app/src/actions/creators/pvc-factory.ts
@@ -1,57 +1,72 @@
-import i18next from 'i18next';
-import {
-  clonePVCModal,
-  expandPVCModal,
-  restorePVCModal,
-} from '@console/internal/components/modals';
+import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import { clonePVCModal, expandPVCModal } from '@console/internal/components/modals';
+import deletePVCModal from '@console/internal/components/modals/delete-pvc-modal';
 import { asAccessReview } from '@console/internal/components/utils';
 import { VolumeSnapshotModel } from '@console/internal/models';
-import { VolumeSnapshotKind } from '@console/internal/module/k8s';
+import { PersistentVolumeClaimKind, K8sModel } from '@console/internal/module/k8s';
 import { ResourceActionFactory } from './types';
 
-export const PVCActionFactory: ResourceActionFactory = {
-  ExpandPVC: (kind, obj) => ({
-    id: 'expand-pvc',
-    label: i18next.t('console-app~Expand PVC'),
-    cta: () =>
-      expandPVCModal({
-        kind,
-        resource: obj,
+const usePVCActionFactory = (): ResourceActionFactory => {
+  const { t } = useTranslation();
+
+  const factory = React.useMemo(() => {
+    return {
+      ExpandPVC: (kind: K8sModel, obj: PersistentVolumeClaimKind) => ({
+        id: 'expand-pvc',
+        label: t('console-app~Expand PVC'),
+        cta: () =>
+          expandPVCModal({
+            kind,
+            resource: obj,
+          }),
+        accessReview: asAccessReview(kind, obj, 'patch'),
       }),
-    accessReview: asAccessReview(kind, obj, 'patch'),
-  }),
-  PVCSnapshot: (kind, obj) => ({
-    id: 'create-snapshot',
-    label: i18next.t('console-app~Create snapshot'),
-    disabled: obj?.status?.phase !== 'Bound',
-    tooltip: obj?.status?.phase !== 'Bound' ? i18next.t('console-app~PVC is not Bound') : '',
-    cta: {
-      href: `/k8s/ns/${obj.metadata.namespace}/${VolumeSnapshotModel.plural}/~new/form?pvc=${obj.metadata.name}`,
-    },
-    accessReview: asAccessReview(kind, obj, 'create'),
-  }),
-  ClonePVC: (kind, obj) => ({
-    id: 'clone-pvc',
-    label: i18next.t('console-app~Clone PVC'),
-    disabled: obj?.status?.phase !== 'Bound',
-    tooltip: obj?.status?.phase !== 'Bound' ? i18next.t('console-app~PVC is not Bound') : '',
-    cta: () =>
-      clonePVCModal({
-        kind,
-        resource: obj,
+      PVCSnapshot: (kind: K8sModel, obj: PersistentVolumeClaimKind) => ({
+        id: 'create-snapshot',
+        label: t('console-app~Create snapshot'),
+        disabled: obj?.status?.phase !== 'Bound',
+        tooltip: obj?.status?.phase !== 'Bound' ? t('console-app~PVC is not Bound') : '',
+        cta: {
+          href: `/k8s/ns/${obj.metadata.namespace}/${VolumeSnapshotModel.plural}/~new/form?pvc=${obj.metadata.name}`,
+        },
+        accessReview: asAccessReview(kind, obj, 'create'),
       }),
-    accessReview: asAccessReview(kind, obj, 'create'),
-  }),
-  RestorePVC: (kind, obj: VolumeSnapshotKind) => ({
-    id: 'clone-pvc',
-    label: i18next.t('console-app~Restore as new PVC'),
-    disabled: !obj?.status?.readyToUse,
-    tooltip: !obj?.status?.readyToUse ? i18next.t('console-app~Volume Snapshot is not Ready') : '',
-    cta: () =>
-      restorePVCModal({
-        kind,
-        resource: obj,
+      ClonePVC: (kind: K8sModel, obj: PersistentVolumeClaimKind) => ({
+        id: 'clone-pvc',
+        label: t('console-app~Clone PVC'),
+        disabled: obj?.status?.phase !== 'Bound',
+        tooltip: obj?.status?.phase !== 'Bound' ? t('console-app~PVC is not Bound') : '',
+        cta: () =>
+          clonePVCModal({
+            kind,
+            resource: obj,
+          }),
+        accessReview: asAccessReview(kind, obj, 'create'),
       }),
-    accessReview: asAccessReview(kind, obj, 'create'),
-  }),
+      DeletePVC: (kind: K8sModel, obj: PersistentVolumeClaimKind) => ({
+        id: 'delete-pvc',
+        label: t('public~Delete PersistentVolumeClaim'),
+        cta: () =>
+          deletePVCModal({
+            pvc: obj,
+          }),
+        accessReview: asAccessReview(kind, obj, 'delete'),
+      }),
+    };
+  }, [t]);
+  return factory;
+};
+
+export const usePVCActions = (kind: K8sModel, obj: PersistentVolumeClaimKind) => {
+  const actionFactory = usePVCActionFactory();
+  const actions = React.useMemo(() => {
+    return [
+      actionFactory.ExpandPVC(kind, obj),
+      actionFactory.PVCSnapshot(kind, obj),
+      actionFactory.ClonePVC(kind, obj),
+      actionFactory.DeletePVC(kind, obj),
+    ];
+  }, [kind, obj, actionFactory]);
+  return actions;
 };
