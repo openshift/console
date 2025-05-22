798337d19daab1a01f7dcce8923033fea002a7d8
Mylanos <mziska@redhat.com>
2025-05-22 11:25:06 +0200

    CONSOLE-4580: create volume snapshot action factory and its ActionResourceProvider, consume actions from ActionsResourceProvider inside VolumeSnapshots, update i18n, remove PVC and VolumeSnapshot actions from kebabFactory.

--- frontend/packages/console-app/src/actions/providers/volume-snaphot-provider.ts
+++ frontend/packages/console-app/src/actions/providers/volume-snaphot-provider.ts
@@ -1,32 +1,18 @@
 import * as React from 'react';
-import i18next from 'i18next';
-import { restorePVCModal } from '@console/internal/components/modals';
-import { asAccessReview } from '@console/internal/components/utils';
+import { useVolumeSnapshotActionFactory } from '@console/app/src/actions/creators/volume-snapshot-factory';
 import { K8sResourceKind, referenceFor } from '@console/internal/module/k8s';
 import { useK8sModel } from '@console/shared/src/hooks/useK8sModel';
+import { getCommonResourceActions } from '../creators/common-factory';
 
 export const useVolumeSnapshotActionsProvider = (resource: K8sResourceKind) => {
   const [kindObj, inFlight] = useK8sModel(referenceFor(resource));
-
-  const actions = React.useMemo(
-    () => [
-      {
-        id: 'clone-pvc',
-        label: i18next.t('console-app~Restore as new PVC'),
-        disabled: !resource?.status?.readyToUse,
-        tooltip: !resource?.status?.readyToUse
-          ? i18next.t('console-app~Volume Snapshot is not Ready')
-          : '',
-        cta: () =>
-          restorePVCModal({
-            kindObj,
-            resource,
-          }),
-        accessReview: asAccessReview(kindObj, resource, 'create'),
-      },
-    ],
-    [kindObj, resource],
-  );
+  const actionFactory = useVolumeSnapshotActionFactory();
+  const actions = React.useMemo(() => {
+    return [
+      actionFactory.RestorePVC(kindObj, resource),
+      ...getCommonResourceActions(kindObj, resource),
+    ];
+  }, [kindObj, actionFactory, resource]);
 
   return [actions, !inFlight, undefined];
 };
