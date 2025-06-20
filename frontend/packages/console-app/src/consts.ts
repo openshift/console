db3724657a25dce60582bc28b69308e9d2f2743b
Lokananda Prabhu <lprabhu@redhat.com>
2025-06-20 14:44:50 +0530

    Handled deleted resource issue for favorites

--- frontend/packages/console-app/src/consts.ts
+++ frontend/packages/console-app/src/consts.ts
@@ -1,3 +1,5 @@
+import { STORAGE_PREFIX } from '@console/shared/src/constants/common';
+
 export const LAST_PERSPECTIVE_USER_SETTINGS_KEY = 'console.lastPerspective';
 export const LAST_PERSPECTIVE_LOCAL_STORAGE_KEY = `bridge/last-perspective`;
 export const HIDE_USER_WORKLOAD_NOTIFICATIONS_USER_SETTINGS_KEY =
@@ -6,3 +8,6 @@ export const FLAG_DEVELOPER_PERSPECTIVE = 'DEVELOPER_PERSPECTIVE';
 export const ACM_PERSPECTIVE_ID = 'acm';
 export const ADMIN_PERSPECTIVE_ID = 'admin';
 export const FLAG_CAN_GET_CONSOLE_OPERATOR_CONFIG = 'CAN_GET_CONSOLE_OPERATOR_CONFIG';
+
+export const FAVORITES_CONFIG_MAP_KEY = 'console.favorites';
+export const FAVORITES_LOCAL_STORAGE_KEY = `${STORAGE_PREFIX}/favorites`;
