1124761b5935bc8769a67ad64cfc2daf6deda4c1
Mylanos <mziska@redhat.com>
2025-06-26 13:17:55 +0200

    CONSOLE-4634: Refactor DeploymentActionFactory to hook form. 	- wrap the existing factory into useDeploymentActions, that constructs object of requested actions. The returned object is type safe, meaning it won't return unitialized actions. 	- kept the existing structure of Actions. 	- adjust consumers of DeploymentActionFactory to use the new hook form. 	- removed dead service-binding-provider.ts code

--- frontend/packages/console-app/src/actions/hooks/types.ts
+++ frontend/packages/console-app/src/actions/hooks/types.ts
@@ -1,3 +1,17 @@
+import { Action } from '@console/dynamic-plugin-sdk';
+
+export enum DeploymentActionCreator {
+  EditDeployment = 'EditDeployment',
+  UpdateStrategy = 'UpdateStrategy',
+  PauseRollout = 'PauseRollout',
+  RestartRollout = 'RestartRollout',
+  StartDCRollout = 'StartDCRollout',
+  EditResourceLimits = 'EditResourceLimits',
+}
+
+export type ActionObject<T extends readonly PropertyKey[]> = {
+  [K in T[number]]: Action;
+};
 export enum PVCActionCreator {
   ExpandPVC = 'ExpandPVC',
   PVCSnapshot = 'PVCSnapshot',
--- frontend/packages/console-app/src/actions/providers/deployment-provider.ts
+++ frontend/packages/console-app/src/actions/providers/deployment-provider.ts
@@ -4,35 +4,43 @@ import { Action } from '@console/dynamic-plugin-sdk/src';
 import { DeploymentKind, referenceFor } from '@console/internal/module/k8s';
 import { useK8sModel } from '@console/shared/src/hooks/useK8sModel';
 import { CommonActionFactory } from '../creators/common-factory';
-import { DeploymentActionFactory } from '../creators/deployment-factory';
 import { getHealthChecksAction } from '../creators/health-checks-factory';
 import { useHPAActions } from '../creators/hpa-factory';
 import { usePDBActions } from '../creators/pdb-factory';
+import { DeploymentActionCreator } from '../hooks/types';
+import { useDeploymentActions } from '../hooks/useDeploymentActions';
 
 export const useDeploymentActionsProvider = (resource: DeploymentKind) => {
   const [kindObj, inFlight] = useK8sModel(referenceFor(resource));
   const [hpaActions, relatedHPAs] = useHPAActions(kindObj, resource);
   const [pdbActions] = usePDBActions(kindObj, resource);
+  const deploymentActionsObject = useDeploymentActions(kindObj, resource, [
+    DeploymentActionCreator.PauseRollout,
+    DeploymentActionCreator.RestartRollout,
+    DeploymentActionCreator.UpdateStrategy,
+    DeploymentActionCreator.EditResourceLimits,
+    DeploymentActionCreator.EditDeployment,
+  ] as const);
 
   const deploymentActions = React.useMemo<Action[]>(
     () => [
       ...(relatedHPAs?.length === 0 ? [CommonActionFactory.ModifyCount(kindObj, resource)] : []),
       ...hpaActions,
       ...pdbActions,
-      DeploymentActionFactory.PauseRollout(kindObj, resource),
-      DeploymentActionFactory.RestartRollout(kindObj, resource),
+      deploymentActionsObject.PauseRollout,
+      deploymentActionsObject.RestartRollout,
       getHealthChecksAction(kindObj, resource),
       CommonActionFactory.AddStorage(kindObj, resource),
-      DeploymentActionFactory.UpdateStrategy(kindObj, resource),
-      DeploymentActionFactory.EditResourceLimits(kindObj, resource),
+      deploymentActionsObject.UpdateStrategy,
+      deploymentActionsObject.EditResourceLimits,
       CommonActionFactory.ModifyLabels(kindObj, resource),
       CommonActionFactory.ModifyAnnotations(kindObj, resource),
-      DeploymentActionFactory.EditDeployment(kindObj, resource),
+      deploymentActionsObject.EditDeployment,
       ...(resource.metadata.annotations?.['openshift.io/generated-by'] === 'OpenShiftWebConsole'
         ? [DeleteResourceAction(kindObj, resource)]
         : [CommonActionFactory.Delete(kindObj, resource)]),
     ],
-    [hpaActions, pdbActions, kindObj, relatedHPAs, resource],
+    [hpaActions, pdbActions, deploymentActionsObject, kindObj, relatedHPAs, resource],
   );
 
   return [deploymentActions, !inFlight, undefined];
