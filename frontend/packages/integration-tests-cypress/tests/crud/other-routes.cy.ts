0497cba540b3f9c1ba226d75cb15c18dcafc8074
logonoff <git@logonoff.co>
2025-07-09 17:01:29 -0400

    OCPBUGS-59163: ResourceSidebar `li` -> `ListItem`

--- frontend/packages/integration-tests-cypress/tests/crud/other-routes.cy.ts
+++ frontend/packages/integration-tests-cypress/tests/crud/other-routes.cy.ts
@@ -49,7 +49,7 @@ describe('Visiting other routes', () => {
     },
     {
       path: '/api-resource/ns/default/core~v1~Pod/schema',
-      waitFor: () => cy.get('li.co-resource-sidebar-item').should('be.visible'),
+      waitFor: () => cy.byTestID('resource-sidebar-item').should('be.visible'),
     },
     {
       path: '/api-resource/ns/default/core~v1~Pod/instances',
