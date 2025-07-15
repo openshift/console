879a9a0fc6621efc8ea07b76d1fee3296a1f7852
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2025-07-15 13:15:31 -0400

    fix: update type from K8sKind to K8sModel in useReplicaSetActions hook since it is deprecated

--- frontend/packages/console-app/src/actions/hooks/useReplicaSetActions.ts
+++ frontend/packages/console-app/src/actions/hooks/useReplicaSetActions.ts
@@ -4,20 +4,20 @@ import { Action } from '@console/dynamic-plugin-sdk';
 import { useDeepCompareMemoize } from '@console/dynamic-plugin-sdk/src/utils/k8s/hooks/useDeepCompareMemoize';
 import { rollbackModal } from '@console/internal/components/modals';
 import { DeploymentModel } from '@console/internal/models';
-import { ReplicaSetKind, K8sKind } from '@console/internal/module/k8s';
+import { ReplicaSetKind, K8sModel } from '@console/internal/module/k8s';
 import { getOwnerNameByKind } from '@console/shared/src';
 import { ReplicaSetActionCreator } from './types';
 
 /**
  * A React hook for retrieving actions related to ReplicaSet resources.
  *
- * @param {K8sKind} kind - The K8s model for the resource.
+ * @param {K8sModel} kind - The K8s model for the resource.
  * @param {ReplicaSetKind} resource - The specific ReplicaSet resource instance.
  * @param {ReplicaSetActionCreator[]} [filterActions] - Optional. If provided, returns only specified actions.
  * @returns {Action[]} An array containing the generated actions.
  */
 export const useReplicaSetActions = (
-  kind: K8sKind,
+  kind: K8sModel,
   resource: ReplicaSetKind,
   filterActions?: ReplicaSetActionCreator[],
 ): Action[] => {
