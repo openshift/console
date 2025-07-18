eb491ebcd3c8cb708b8bfb228f04eb3a8ec4816c
Jon Jackson <jonjacks@redhat.com>
2025-07-18 14:13:31 -0400

    add support for tooltips on CatalogBadges and add badges to operatorhub catalog items

--- frontend/packages/console-shared/src/components/catalog/CatalogBadges.tsx
+++ frontend/packages/console-shared/src/components/catalog/CatalogBadges.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Label } from '@patternfly/react-core';
+import { Label, Tooltip } from '@patternfly/react-core';
 import { CatalogItemBadge } from '@console/dynamic-plugin-sdk/src/extensions';
 import './CatalogBadges.scss';
 
@@ -7,17 +7,25 @@ type CatalogBadgesProps = {
   badges: CatalogItemBadge[];
 };
 
+const Badge = ({ color, icon, variant, text, tooltip }: CatalogItemBadge) => {
+  const badge = (
+    <Label
+      className="odc-catalog-badges__label"
+      color={color}
+      icon={icon}
+      variant={variant}
+      data-test={`${text}-badge`}
+    >
+      {text}
+    </Label>
+  );
+  return tooltip ? <Tooltip content={tooltip}>{badge}</Tooltip> : badge;
+};
+
 const CatalogBadges: React.FC<CatalogBadgesProps> = ({ badges }) => (
   <div className="odc-catalog-badges">
     {badges?.map((badge) => (
-      <Label
-        className="odc-catalog-badges__label"
-        color={badge.color}
-        icon={badge.icon}
-        variant={badge.variant}
-      >
-        {badge.text}
-      </Label>
+      <Badge key={badge.text} {...badge} />
     ))}
   </div>
 );
