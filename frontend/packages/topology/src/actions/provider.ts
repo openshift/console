e2ed02a3d99a077561b10a39c82332674933219e
Vikram Raj <vikrmrj@gmail.com>
2025-08-05 11:46:23 +0530

    drop use of ceateModalLauncher from Topology package

--- frontend/packages/topology/src/actions/provider.ts
+++ frontend/packages/topology/src/actions/provider.ts
@@ -4,19 +4,20 @@ import { modelFor, referenceFor } from '@console/internal/module/k8s';
 import { useK8sModel } from '@console/shared/src/hooks/useK8sModel';
 import { TYPE_WORKLOAD, TYPE_CONNECTS_TO } from '../const';
 import { getResource } from '../utils';
-import { DeleteConnectorAction, MoveConnectorAction } from './edgeActions';
-import { getModifyApplicationAction } from './modify-application';
+import { DeleteConnectorAction, useMoveConnectorAction } from './edgeActions';
+import { useGetModifyApplicationAction } from './modify-application';
 
 export const useTopologyWorkloadActionProvider = (element: GraphElement) => {
   const resource = getResource(element);
+  const k8sKind = resource ? modelFor(referenceFor(resource)) : null;
+  const getModifyApplicationAction = useGetModifyApplicationAction(k8sKind, resource);
   const actions = useMemo(() => {
     if (element.getType() !== TYPE_WORKLOAD) return undefined;
     if (!resource) {
       return [];
     }
-    const k8sKind = modelFor(referenceFor(resource));
-    return [getModifyApplicationAction(k8sKind, resource)];
-  }, [element, resource]);
+    return [getModifyApplicationAction];
+  }, [element, getModifyApplicationAction, resource]);
 
   return useMemo(() => {
     if (!actions) return [[], true, undefined];
@@ -27,10 +28,11 @@ export const useTopologyWorkloadActionProvider = (element: GraphElement) => {
 export const useTopologyVisualConnectorActionProvider = (element: Edge) => {
   const resource = getResource(element.getSource?.());
   const [kindObj, inFlight] = useK8sModel(referenceFor(resource ?? {}));
+  const moveConnectionAction = useMoveConnectorAction(kindObj, element);
   const actions = useMemo(() => {
     if (!kindObj || element.getType() !== TYPE_CONNECTS_TO) return undefined;
-    return [MoveConnectorAction(kindObj, element), DeleteConnectorAction(kindObj, element)];
-  }, [element, kindObj]);
+    return [moveConnectionAction, DeleteConnectorAction(kindObj, element)];
+  }, [element, kindObj, moveConnectionAction]);
 
   return useMemo(() => {
     if (!actions) return [[], true, undefined];
