9cbd722d5930873375ff787c707d4605cad15feb
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2025-08-15 16:38:28 -0400

    feat: Add build provider and actions

---
+++ frontend/packages/console-app/src/actions/providers/build-provider.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { K8sResourceKind } from '@console/internal/module/k8s';
+import { useBuildsActions } from '../hooks/useBuildsActions';
+
+export const useBuildActionsProvider = (resource: K8sResourceKind) => {
+  const buildActions = useBuildsActions(resource);
+
+  const actions = React.useMemo(() => [...buildActions], [buildActions]);
+
+  return [actions, true, false];
+};
