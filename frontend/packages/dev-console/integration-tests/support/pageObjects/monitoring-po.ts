7355b3c05090efe717af1f78b04c2fe40277d44f
PeterYurkovich <pyurkovi@redhat.com>
2025-10-07 11:41:22 -0400

    remove: old dev persepctive observe tests which are already not running

--- frontend/packages/dev-console/integration-tests/support/pageObjects/monitoring-po.ts
+++ frontend/packages/dev-console/integration-tests/support/pageObjects/monitoring-po.ts
@@ -1,57 +1,12 @@
 export const monitoringPO = {
   tabs: {
-    dashboard: '[data-test-id="horizontal-link-Dashboards"]',
-    metrics: '[data-test-id="horizontal-link-Metrics"]',
-    alerts: '[data-test-id="horizontal-link-Alerts"]',
     events: '[data-test-id="horizontal-link-Events"]',
   },
   timeRange: '',
   refreshInterval: '',
-  dashboardTab: {
-    dashboardDropdown: '[data-test="dashboard-dropdown"]',
-    workloadsDropdown: '[data-test="workload-dropdown"]',
-    dashboardFilter: '[aria-label="Options menu"]',
-    workloadsFilter: '[#odc-monitoring-dashboard-workload-filter]',
-    typeDropdown: '[data-test="type-dropdown"]',
-    podDropdown: '[data-test="pod-dropdown"]',
-    dashboard: '[data-test-id="dashboard"]',
-    sections: {
-      cpuUsage: '[data-test-id="panel-cpu-usage"]',
-      memoryUsage: '[data-test-id="panel-memory-usage"]',
-      receiveBandwidth: '[data-test="receive-bandwidth-chart"]',
-      transmitBandwidth: '[data-test="transmit-bandwidth-chart"]',
-      rateOfReceivedPackets: '[data-test="rate-of-received-packets-chart"]',
-      rateOfTransmittedPackets: '[data-test="rate-of-transmitted-packets-chart"]',
-      rateOfReceivedPacketsDropped: '[data-test="rate-of-received-packets-dropped-chart"]',
-      rateOfTransmittedPacketsDropped: '[data-test="rate-of-transmitted-packets-dropped-chart"]',
-    },
-  },
-  metricsTab: {
-    selectQuery: '.odc-metrics-query-input button[type="button"]',
-    queryExpression: 'textarea[placeholder="Expression (press Shift+Enter for newlines)"]',
-    cpuGraph: '.graph-wrapper--query-browser',
-    podsList: 'table[aria-label="query results table"]',
-    showPromQL: '',
-    hidePromQL: '',
-    emptyQueryMessage: '',
-    resetZoom: '',
-  },
   eventsTab: {
     resources: '',
     selectedResource: '.form-group ul li span',
     types: '[data-test="console-select-menu-toggle"]',
-  },
-  alertsTab: {
-    filter: '[data-test-id="filter-dropdown-toggle"] button',
-    search: 'input[data-test-id="item-filter"]',
-    table: '[role="grid"]',
-    alertsTable: {
-      columns: {
-        name: '[data-label="Name"]',
-        severity: '[data-label="Severity"]',
-        alertState: '[data-label="Alert state"]',
-        notifications: '[data-label="Notifications"]',
-      },
-    },
   },
 };
--- frontend/packages/dev-console/integration-tests/support/pages/monitoring/monitoring-page.ts
+++ frontend/packages/dev-console/integration-tests/support/pages/monitoring/monitoring-page.ts
@@ -8,47 +8,6 @@ export const detailsPage = {
 };
 
 export const monitoringPage = {
-  dashboard: {
-    selectWorkload: (workloadName: string = 'All Workloads') => {
-      cy.get(monitoringPO.dashboardTab.workloadsFilter).click();
-      cy.byTestID('console-select-search-input').type(workloadName);
-      cy.get(`[id="${workloadName}-link"]`).click();
-    },
-    selectDashboard: (dashboardName: string) => {
-      cy.get(monitoringPO.dashboardTab.dashboardDropdown).click();
-      cy.get('input[type="search"]').type(dashboardName);
-      cy.get('button.monitoring-dashboards__dashboard_dropdown_item').first().click();
-    },
-    verifySection: () => cy.get(monitoringPO.dashboardTab.dashboard).should('exist'),
-    verifyCpuUsageGraph: () => cy.get(monitoringPO.dashboardTab.sections.cpuUsage).should('exist'),
-    verifyMemoryUsageGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.memoryUsage).should('exist'),
-    verifyReceiveBandwidthGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.receiveBandwidth).should('exist'),
-    verifyTransmitBandwidthGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.transmitBandwidth).should('exist'),
-    verifyRateOfReceivedPacketsGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.rateOfReceivedPackets).should('exist'),
-    verifyRateOfTransmittedPacketsGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.rateOfTransmittedPackets).should('exist'),
-    verifyRateOfReceivedPacketsDroppedGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.rateOfReceivedPacketsDropped).should('exist'),
-    verifyRateOfTransmittedPacketsDroppedGraph: () =>
-      cy.get(monitoringPO.dashboardTab.sections.rateOfTransmittedPacketsDropped).should('exist'),
-  },
-  metrics: {
-    enterQuery: (query: string) => {
-      cy.get(monitoringPO.metricsTab.queryExpression).type(query);
-    },
-    selectQuery: (queryValue: string) => {
-      cy.selectValueFromAutoCompleteDropDown(monitoringPO.metricsTab.selectQuery, queryValue);
-    },
-    verifyGraph: () => {
-      cy.get(monitoringPO.metricsTab.cpuGraph).should('be.visible');
-    },
-    verifyResetZoom: () => cy.byButtonText('Reset Zoom').should('be.visible'),
-    clickResetZoom: () => cy.byButtonText('Reset Zoom').click(),
-  },
   events: {
     selectResources: (resourceName: string) => {
       cy.selectValueFromAutoCompleteDropDown(monitoringPO.eventsTab.resources, resourceName);
@@ -61,16 +20,6 @@ export const monitoringPage = {
   },
   selectTab: (tabName: monitoringTabs | string) => {
     switch (tabName) {
-      case 'Dashboard':
-      case monitoringTabs.Dashboard:
-        detailsPage.selectTab(monitoringPO.tabs.dashboard);
-        cy.url().should('include', 'dev-monitoring/ns/');
-        break;
-      case 'Metrics':
-      case monitoringTabs.Metrics:
-        detailsPage.selectTab(monitoringPO.tabs.metrics);
-        cy.url().should('include', 'metrics');
-        break;
       case 'Events':
       case monitoringTabs.Events:
         detailsPage.selectTab(monitoringPO.tabs.events);
