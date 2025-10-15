2a0f2795715c2b686ee53a1aa433cdfea1f4fed7
Vikram Raj <vikrmrj@gmail.com>
2025-10-15 20:25:11 +0530

    Fix Bare Metal Hosts nav item

--- frontend/packages/metal3-plugin/src/plugin.tsx
+++ frontend/packages/metal3-plugin/src/plugin.tsx
@@ -54,45 +54,6 @@ const METAL3_FLAG = 'METAL3';
 
 const plugin: Plugin<ConsumedExtensions> = [
   {
-    type: 'ModelDefinition',
-    properties: {
-      models: [
-        BareMetalHostModel,
-        NodeMaintenanceModel,
-        NodeMaintenanceKubevirtAlphaModel,
-        NodeMaintenanceKubevirtBetaModel,
-      ],
-    },
-  },
-  {
-    type: 'FeatureFlag/Model',
-    properties: {
-      model: BareMetalHostModel,
-      flag: METAL3_FLAG,
-    },
-  },
-  {
-    type: 'FeatureFlag/Model',
-    properties: {
-      model: NodeMaintenanceModel,
-      flag: NODE_MAINTENANCE_FLAG,
-    },
-  },
-  {
-    type: 'FeatureFlag/Model',
-    properties: {
-      model: NodeMaintenanceKubevirtBetaModel,
-      flag: NODE_MAINTENANCE_KV_BETA_FLAG,
-    },
-  },
-  {
-    type: 'FeatureFlag/Model',
-    properties: {
-      model: NodeMaintenanceKubevirtAlphaModel,
-      flag: NODE_MAINTENANCE_KV_ALPHA_FLAG,
-    },
-  },
-  {
     type: 'FeatureFlag/Custom',
     properties: {
       detect: detectBaremetalPlatform,
@@ -102,54 +63,6 @@ const plugin: Plugin<ConsumedExtensions> = [
     type: 'FeatureFlag/Custom',
     properties: {
       detect: detectBMOEnabled,
-    },
-  },
-  {
-    type: 'Page/Resource/List',
-    properties: {
-      model: BareMetalHostModel,
-      loader: () =>
-        import(
-          './components/baremetal-hosts/BareMetalHostsPage' /* webpackChunkName: "metal3-baremetalhost" */
-        ).then((m) => m.default),
-    },
-  },
-  {
-    type: 'Page/Resource/Details',
-    properties: {
-      model: BareMetalHostModel,
-      loader: () =>
-        import(
-          './components/baremetal-hosts/BareMetalHostDetailsPage' /* webpackChunkName: "metal3-baremetalhost" */
-        ).then((m) => m.default),
-    },
-  },
-  {
-    type: 'Page/Route',
-    properties: {
-      exact: true,
-      path: `/k8s/ns/:ns/${referenceForModel(BareMetalHostModel)}/~new/form`,
-      loader: () =>
-        import(
-          './components/baremetal-hosts/add-baremetal-host/AddBareMetalHostPage' /* webpackChunkName: "metal3-baremetalhost" */
-        ).then((m) => m.default),
-    },
-    flags: {
-      required: [BAREMETAL_FLAG, METAL3_FLAG],
-    },
-  },
-  {
-    type: 'Page/Route',
-    properties: {
-      exact: true,
-      path: `/k8s/ns/:ns/${referenceForModel(BareMetalHostModel)}/:name/edit`,
-      loader: () =>
-        import(
-          './components/baremetal-hosts/add-baremetal-host/AddBareMetalHostPage' /* webpackChunkName: "metal3-baremetalhost" */
-        ).then((m) => m.default),
-    },
-    flags: {
-      required: [BAREMETAL_FLAG, METAL3_FLAG],
     },
   },
   {
