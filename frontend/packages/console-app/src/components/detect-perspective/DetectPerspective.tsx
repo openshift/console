dda94bd9a639b28b43f12588d57c09747e417668
logonoff <git@logonoff.co>
2025-11-17 16:29:29 -0500

    OCPBUGS-65930: Preserve query string in perspective switch

--- frontend/packages/console-app/src/components/detect-perspective/DetectPerspective.tsx
+++ frontend/packages/console-app/src/components/detect-perspective/DetectPerspective.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useLocation } from 'react-router-dom-v5-compat';
+import { FC, useEffect } from 'react';
+import { createPath, useLocation } from 'react-router-dom-v5-compat';
 import { Perspective, PerspectiveContext } from '@console/dynamic-plugin-sdk';
 import { usePerspectives } from '@console/shared/src';
 import PerspectiveDetector from './PerspectiveDetector';
@@ -19,14 +19,14 @@ const getPerspectiveURLParam = (perspectives: Perspective[]) => {
   return perspectiveIDs.includes(perspectiveParam) ? perspectiveParam : null;
 };
 
-const DetectPerspective: React.FC<DetectPerspectiveProps> = ({ children }) => {
+const DetectPerspective: FC<DetectPerspectiveProps> = ({ children }) => {
   const [activePerspective, setActivePerspective, loaded] = useValuesForPerspectiveContext();
   const perspectiveExtensions = usePerspectives();
   const perspectiveParam = getPerspectiveURLParam(perspectiveExtensions);
   const location = useLocation();
-  React.useEffect(() => {
+  useEffect(() => {
     if (perspectiveParam && perspectiveParam !== activePerspective) {
-      setActivePerspective(perspectiveParam, location.pathname);
+      setActivePerspective(perspectiveParam, createPath(location));
     }
   }, [perspectiveParam, activePerspective, setActivePerspective, location]);
   return loaded ? (
