8838da3640b2151bf832ba5d794ab7887abf4ebb
Krish Agarwal <krishagarwal278@gmail.com>
2025-11-20 12:06:57 -0500

    e2e test failure

--- frontend/packages/dev-console/src/components/import/deployImage-validation-utils.ts
+++ frontend/packages/dev-console/src/components/import/deployImage-validation-utils.ts
@@ -18,7 +18,10 @@ export const deployValidationSchema = (t: TFunction) =>
     project: projectNameValidationSchema,
     application: applicationNameValidationSchema,
     name: nameValidationSchema(t),
-    isi: isiValidationSchema(t),
+    isi: yup.object().when('registry', {
+      is: 'internal',
+      then: (schema) => schema.concat(isiValidationSchema(t)),
+    }),
     serverless: serverlessValidationSchema(t),
     deployment: deploymentValidationSchema(t),
     route: routeValidationSchema(t),
