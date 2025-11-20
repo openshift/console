c1e58004e74136b7c200abe12c7e3b5315cdfcfe
Krish Agarwal <krishagarwal278@gmail.com>
2025-11-20 13:11:16 -0500

    switching to original callback style

--- frontend/packages/dev-console/src/components/import/deployImage-validation-utils.ts
+++ frontend/packages/dev-console/src/components/import/deployImage-validation-utils.ts
@@ -18,9 +18,8 @@ export const deployValidationSchema = (t: TFunction) =>
     project: projectNameValidationSchema,
     application: applicationNameValidationSchema,
     name: nameValidationSchema(t),
-    isi: yup.object().when('registry', {
-      is: 'internal',
-      then: (schema) => schema.concat(isiValidationSchema(t)),
+    isi: yup.object().when('registry', ([registry], schema) => {
+      return registry === 'internal' ? schema.concat(isiValidationSchema(t)) : schema;
     }),
     serverless: serverlessValidationSchema(t),
     deployment: deploymentValidationSchema(t),
