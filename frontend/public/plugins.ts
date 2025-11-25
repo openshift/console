692a28b30fe8aa8f7c4e1f20495e9943504111ce
logonoff <git@logonoff.co>
2025-11-25 14:22:42 -0500

    CONSOLE-3769: Swap out plugin store and sync flags
    a

--- frontend/public/plugins.ts
+++ frontend/public/plugins.ts
@@ -1,7 +1,9 @@
 import * as _ from 'lodash';
 import { PluginStore } from '@console/plugin-sdk/src/store';
-import { ActivePlugin } from '@console/plugin-sdk/src/typings/base';
+import type { ActivePlugin } from '@console/plugin-sdk/src/typings/base';
 import { getURLSearchParams } from './components/utils/link';
+import type { Middleware } from 'redux';
+import type { RootState } from './redux';
 
 const getEnabledDynamicPluginNames = () => {
   const allPluginNames = window.SERVER_FLAGS.consolePlugins;
@@ -26,7 +28,30 @@ const activePlugins =
 
 const dynamicPluginNames = getEnabledDynamicPluginNames();
 
-export const pluginStore = new PluginStore(activePlugins, dynamicPluginNames);
+export const pluginStore = new PluginStore();
+
+activePlugins.forEach((plugin) => {
+  pluginStore.addActivePlugin(plugin);
+});
+
+/**
+ * Redux middleware to update plugin store feature flags when actions are dispatched.
+ */
+export const featureFlagMiddleware: Middleware<{}, RootState> = (s) => {
+  let prevFlags: RootState['FLAGS'] | undefined;
+
+  return (next) => (action) => {
+    const result = next(action);
+    const nextFlags = s.getState().FLAGS;
+
+    if (nextFlags !== prevFlags) {
+      prevFlags = nextFlags;
+      pluginStore.setFeatureFlags(nextFlags.toObject());
+    }
+
+    return result;
+  };
+};
 
 if (process.env.NODE_ENV !== 'production') {
   // Expose Console plugin store for debugging
