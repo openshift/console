e80666a0533008d81b99c4b66b4305b28d601790
Yanping Zhang <yanpzhan@redhat.com>
2025-11-25 17:12:06 +0800

    add new auto scenario to upstream

---
+++ frontend/packages/integration-tests-cypress/tests/app/template.cy.ts
@@ -0,0 +1,30 @@
+import { catalog } from '../../views/catalogs';
+import { guidedTour } from '../../views/guided-tour';
+
+describe('template feature', () => {
+  before(() => {
+    cy.log('create template:');
+    cy.exec(`oc create -f ./fixtures/httpd-example-template.yaml -n openshift`).then((result) => {
+      expect(result.stdout).to.include('created');
+    });
+    cy.login();
+    guidedTour.close();
+  });
+
+  after(() => {
+    cy.exec('oc delete template httpd-example-test -n openshift');
+  });
+  it('Allow custom icon using template annotation', () => {
+    cy.clickNavLink(['Ecosystem', 'Software Catalog']);
+    cy.get('.loading-box__loaded', { timeout: 50000 }).should('exist');
+    catalog.filterByKeyword('test apach');
+    cy.byTestID('Template-Test Apache HTTP Server').click();
+    cy.exec(
+      `oc get template httpd-example-test -n openshift -o jsonpath='{.metadata.annotations.iconClass}'`,
+    ).then((output) => {
+      const iconClass = output.stdout;
+      cy.log(`1. icon url: ${iconClass}`);
+      catalog.checkItemImage(`${iconClass}`);
+    });
+  });
+});
