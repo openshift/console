a122f7ed0c4001aa35e665960564ee6b6aba2cda
Vikram Raj <vikrmrj@gmail.com>
2025-11-25 19:40:18 +0530

    Move Helm nav option under Ecosystem nav section

--- frontend/packages/helm-plugin/src/providers/useHelmChartRepositoriesBreadcrumbs.ts
+++ frontend/packages/helm-plugin/src/providers/useHelmChartRepositoriesBreadcrumbs.ts
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { useParams, useLocation } from 'react-router-dom-v5-compat';
-import { useActivePerspective } from '@console/dynamic-plugin-sdk';
 import { K8sKind } from '@console/internal/module/k8s';
 import { useTabbedTableBreadcrumbsFor } from '@console/shared';
 import { HelmChartRepositoryModel, ProjectHelmChartRepositoryModel } from '../models';
@@ -14,13 +13,12 @@ export const useHelmChartRepositoriesBreadcrumbs = (kindObj: K8sKind) => {
   const { t } = useTranslation();
   const params = useParams();
   const location = useLocation();
-  const isAdminPerspective = useActivePerspective()[0] === 'admin';
   return useTabbedTableBreadcrumbsFor(
     kindObj,
     location,
     params,
-    isAdminPerspective ? 'helm-repositories' : 'helm',
-    isAdminPerspective ? '' : 'repositories',
+    'helm',
+    'repositories',
     t('helm-plugin~Repositories'),
     true,
   );
