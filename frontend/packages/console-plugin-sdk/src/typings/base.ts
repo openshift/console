5aa5eb715bf06ed310464540795b1ce799396691
logonoff <git@logonoff.co>
2025-11-26 08:31:35 -0500

    Reapply "CONSOLE-3769: Move `LazyLoader` type to internal"
    This reverts commit 2b444de7473d07d8ad8be7da5c17af317e3ebaf0.

--- frontend/packages/console-plugin-sdk/src/typings/base.ts
+++ frontend/packages/console-plugin-sdk/src/typings/base.ts
@@ -1,17 +1,4 @@
-import * as React from 'react';
-import {
-  ExtensionFlags,
-  Extension,
-  ExtensionTypeGuard,
-  LoadedExtension,
-} from '@console/dynamic-plugin-sdk/src/types';
-
-export type { ExtensionFlags, Extension, ExtensionTypeGuard, LoadedExtension };
-
-/**
- * Common interface for loading async React components.
- */
-export type LazyLoader<T extends {} = {}> = () => Promise<React.ComponentType<Partial<T>>>;
+import type { ExtensionDeclaration } from '@console/dynamic-plugin-sdk/src/types';
 
 /**
  * From Console application perspective, a plugin is a list of extensions
@@ -19,5 +6,5 @@ export type LazyLoader<T extends {} = {}> = () => Promise<React.ComponentType<Pa
  */
 export type ActivePlugin = {
   name: string;
-  extensions: Extension[];
+  extensions: ExtensionDeclaration[];
 };
