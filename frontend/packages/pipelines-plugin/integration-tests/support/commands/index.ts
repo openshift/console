1d1e7f95189cf676016ad878b651df7a3818dc69
Vikram Raj <vikrmrj@gmail.com>
2025-11-28 16:05:56 +0530

    Remove pipelines-plugin from console

--- frontend/packages/pipelines-plugin/integration-tests/support/commands/index.ts
+++
@@ -1,8 +0,0 @@
-// Include the cypress customized commands related files
-import '../../../../integration-tests-cypress/support/selectors';
-import '../../../../integration-tests-cypress/support/a11y';
-import '../../../../integration-tests-cypress/support/login';
-import '../../../../integration-tests-cypress/support/project';
-import '../../../../integration-tests-cypress/support/index';
-import '../../../../dev-console/integration-tests/support/commands/app';
-import './hooks';
--- frontend/packages/pipelines-plugin/src/components/pipelines/detail-page-tabs/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as PipelineDetails } from './pipeline-details/PipelineDetails';
-export { default as PipelineParametersForm } from './PipelineParametersForm';
-export { default as PipelineResources } from './PipelineResources';
-export { default as PipelineResourcesForm } from './PipelineResourcesForm';
-export { default as PipelineWorkspaces } from './PipelineWorkspaces';
-export { default as PipelineRuns } from './PipelineRuns';
-export { default as PipelineForm } from './PipelineForm';
-export * from './pipelineForm-validation-utils';
-export * from './utils';
