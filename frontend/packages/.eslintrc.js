c03d92d5efdf224346cbfeefb4a915afad7d95e7
logonoff <git@logonoff.co>
2026-01-05 11:31:54 -0500

    CONSOLE-4631: Block lodash-es and react default/splat

--- frontend/packages/.eslintrc.js
+++ frontend/packages/.eslintrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   root: true,
   extends: ['plugin:console/react-typescript-prettier'],
-  rules: {
-    'no-restricted-imports': [
-      'error',
-      {
-        name: 'lodash-es',
-        message: 'Use lodash instead. webpack is configured to use lodash-es automatically.',
-      },
-    ],
-  },
 };
