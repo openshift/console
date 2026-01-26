logonoff/openshift-console

OCPBUGS-72585: Do not resolve disabled catalog type extensions

frontend/packages/console-shared/src/components/catalog/hooks/useCatalogExtensions.ts
@@ -15,6 +15,7 @@ import {
   isCatalogCategoriesProvider,
   CatalogCategoriesProvider,
 } from '@console/dynamic-plugin-sdk/src/extensions';
+import { useGetAllDisabledSubCatalogs } from '../utils';
 
 const useCatalogExtensions = (
   catalogId: string,
@@ -27,11 +28,27 @@ const useCatalogExtensions = (
   ResolvedExtension<CatalogCategoriesProvider>[],
   boolean,
 ] => {
+  const [disabledCatalogs] = useGetAllDisabledSubCatalogs();
+
+  const isNotDisabledType = useCallback(
+    (e: { properties: { type?: string } }) => {
+      // If no type is specified, we consider it not disabled
+      if (!e.properties.type) {
+        return true;
+      }
+
+      return !disabledCatalogs.includes(e.properties.type);
+    },
+    [disabledCatalogs],
+  );
+
   const [itemTypeExtensions, itemTypesResolved] = useResolvedExtensions<CatalogItemType>(
     useCallback(
       (e): e is CatalogItemType =>
-        isCatalogItemType(e) && (!catalogType || e.properties.type === catalogType),
-      [catalogType],
+        isCatalogItemType(e) &&
+        isNotDisabledType(e) &&
+        (!catalogType || e.properties.type === catalogType),
+      [catalogType, isNotDisabledType],
     ),
   );
 
@@ -40,8 +57,10 @@ const useCatalogExtensions = (
   >(
     useCallback(
       (e): e is CatalogItemTypeMetadata =>
-        isCatalogItemTypeMetadata(e) && (!catalogType || e.properties.type === catalogType),
-      [catalogType],
+        isCatalogItemTypeMetadata(e) &&
+        isNotDisabledType(e) &&
+        (!catalogType || e.properties.type === catalogType),
+      [catalogType, isNotDisabledType],
     ),
   );
 
@@ -49,9 +68,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemProvider =>
         isCatalogItemProvider(e) &&
+        isNotDisabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isNotDisabledType],
     ),
   );
 
@@ -59,9 +79,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemFilter =>
         isCatalogItemFilter(e) &&
+        isNotDisabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isNotDisabledType],
     ),
   );
 
@@ -71,11 +92,12 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogCategoriesProvider =>
         isCatalogCategoriesProvider(e) &&
+        isNotDisabledType(e) &&
         (!e.properties.catalogId ||
           e.properties.catalogId === catalogId ||
           !e.properties.type ||
           e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isNotDisabledType],
     ),
   );
 
@@ -85,9 +107,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemMetadataProvider =>
         isCatalogItemMetadataProvider(e) &&
+        isNotDisabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isNotDisabledType],
     ),
   );
 
@@ -96,28 +119,30 @@ const useCatalogExtensions = (
       (catalogType
         ? itemTypeExtensions.filter((e) => e.properties.type === catalogType)
         : itemTypeExtensions
-      ).map((e) => {
-        const metadataExts = typeMetadataExtensions.filter(
-          (em) => e.properties.type === em.properties.type,
-        );
-        if (metadataExts.length > 0) {
-          return Object.assign({}, e, {
-            properties: {
-              ...e.properties,
-              filters: [
-                ...(e.properties.filters ?? []),
-                ..._.flatten(metadataExts.map((em) => em.properties.filters).filter((x) => x)),
-              ],
-              groupings: [
-                ...(e.properties.groupings ?? []),
-                ..._.flatten(metadataExts.map((em) => em.properties.groupings).filter((x) => x)),
-              ],
-            },
-          });
-        }
-        return e;
-      }),
-    [catalogType, itemTypeExtensions, typeMetadataExtensions],
+      )
+        .filter(isNotDisabledType)
+        .map((e) => {
+          const metadataExts = typeMetadataExtensions.filter(
+            (em) => e.properties.type === em.properties.type,
+          );
+          if (metadataExts.length > 0) {
+            return Object.assign({}, e, {
+              properties: {
+                ...e.properties,
+                filters: [
+                  ...(e.properties.filters ?? []),
+                  ..._.flatten(metadataExts.map((em) => em.properties.filters).filter((x) => x)),
+                ],
+                groupings: [
+                  ...(e.properties.groupings ?? []),
+                  ..._.flatten(metadataExts.map((em) => em.properties.groupings).filter((x) => x)),
+                ],
+              },
+            });
+          }
+          return e;
+        }),
+    [catalogType, itemTypeExtensions, typeMetadataExtensions, isNotDisabledType],
   );
 
   catalogProviderExtensions.sort((a, b) => {

openshift/console

frontend/packages/console-shared/src/components/catalog/hooks/useCatalogExtensions.ts
@@ -15,6 +15,7 @@ import {
   isCatalogCategoriesProvider,
   CatalogCategoriesProvider,
 } from '@console/dynamic-plugin-sdk/src/extensions';
+import { useGetAllDisabledSubCatalogs } from '../utils';
 
 const useCatalogExtensions = (
   catalogId: string,
@@ -27,11 +28,27 @@ const useCatalogExtensions = (
   ResolvedExtension<CatalogCategoriesProvider>[],
   boolean,
 ] => {
+  const [disabledCatalogs] = useGetAllDisabledSubCatalogs();
+
+  const isEnabledType = useCallback(
+    (e: { properties: { type?: string } }) => {
+      // If no type is specified, we consider it not disabled
+      if (!e.properties.type) {
+        return true;
+      }
+
+      return !disabledCatalogs.includes(e.properties.type);
+    },
+    [disabledCatalogs],
+  );
+
   const [itemTypeExtensions, itemTypesResolved] = useResolvedExtensions<CatalogItemType>(
     useCallback(
       (e): e is CatalogItemType =>
-        isCatalogItemType(e) && (!catalogType || e.properties.type === catalogType),
-      [catalogType],
+        isCatalogItemType(e) &&
+        isEnabledType(e) &&
+        (!catalogType || e.properties.type === catalogType),
+      [catalogType, isEnabledType],
     ),
   );
 
@@ -40,8 +57,10 @@ const useCatalogExtensions = (
   >(
     useCallback(
       (e): e is CatalogItemTypeMetadata =>
-        isCatalogItemTypeMetadata(e) && (!catalogType || e.properties.type === catalogType),
-      [catalogType],
+        isCatalogItemTypeMetadata(e) &&
+        isEnabledType(e) &&
+        (!catalogType || e.properties.type === catalogType),
+      [catalogType, isEnabledType],
     ),
   );
 
@@ -49,9 +68,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemProvider =>
         isCatalogItemProvider(e) &&
+        isEnabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isEnabledType],
     ),
   );
 
@@ -59,9 +79,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemFilter =>
         isCatalogItemFilter(e) &&
+        isEnabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isEnabledType],
     ),
   );
 
@@ -71,11 +92,12 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogCategoriesProvider =>
         isCatalogCategoriesProvider(e) &&
+        isEnabledType(e) &&
         (!e.properties.catalogId ||
           e.properties.catalogId === catalogId ||
           !e.properties.type ||
           e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isEnabledType],
     ),
   );
 
@@ -85,9 +107,10 @@ const useCatalogExtensions = (
     useCallback(
       (e): e is CatalogItemMetadataProvider =>
         isCatalogItemMetadataProvider(e) &&
+        isEnabledType(e) &&
         _.castArray(e.properties.catalogId).includes(catalogId) &&
         (!catalogType || e.properties.type === catalogType),
-      [catalogId, catalogType],
+      [catalogId, catalogType, isEnabledType],
     ),
   );
 
@@ -96,28 +119,30 @@ const useCatalogExtensions = (
       (catalogType
         ? itemTypeExtensions.filter((e) => e.properties.type === catalogType)
         : itemTypeExtensions
-      ).map((e) => {
-        const metadataExts = typeMetadataExtensions.filter(
-          (em) => e.properties.type === em.properties.type,
-        );
-        if (metadataExts.length > 0) {
-          return Object.assign({}, e, {
-            properties: {
-              ...e.properties,
-              filters: [
-                ...(e.properties.filters ?? []),
-                ..._.flatten(metadataExts.map((em) => em.properties.filters).filter((x) => x)),
-              ],
-              groupings: [
-                ...(e.properties.groupings ?? []),
-                ..._.flatten(metadataExts.map((em) => em.properties.groupings).filter((x) => x)),
-              ],
-            },
-          });
-        }
-        return e;
-      }),
-    [catalogType, itemTypeExtensions, typeMetadataExtensions],
+      )
+        .filter(isEnabledType)
+        .map((e) => {
+          const metadataExts = typeMetadataExtensions.filter(
+            (em) => e.properties.type === em.properties.type,
+          );
+          if (metadataExts.length > 0) {
+            return Object.assign({}, e, {
+              properties: {
+                ...e.properties,
+                filters: [
+                  ...(e.properties.filters ?? []),
+                  ..._.flatten(metadataExts.map((em) => em.properties.filters).filter((x) => x)),
+                ],
+                groupings: [
+                  ...(e.properties.groupings ?? []),
+                  ..._.flatten(metadataExts.map((em) => em.properties.groupings).filter((x) => x)),
+                ],
+              },
+            });
+          }
+          return e;
+        }),
+    [catalogType, itemTypeExtensions, typeMetadataExtensions, isEnabledType],
   );
 
   catalogProviderExtensions.sort((a, b) => {