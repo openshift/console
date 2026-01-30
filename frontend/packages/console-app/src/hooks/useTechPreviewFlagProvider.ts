762a07166522429a588ff0a6f4e0c51292e08ba7
logonoff <git@logonoff.co>
2026-01-29 19:13:23 -0500

    NO-JIRA: Convert useTechPreviewFlagProvider to static function

--- frontend/packages/console-app/src/hooks/useTechPreviewFlagProvider.ts
+++
@@ -1,9 +0,0 @@
-import { SetFeatureFlag } from '@console/dynamic-plugin-sdk/src/extensions/feature-flags';
-import { FLAG_TECH_PREVIEW } from '../consts';
-
-type UseTechPreviewFlagProvider = (setFeatureFlag: SetFeatureFlag) => void;
-const useTechPreviewFlagProvider: UseTechPreviewFlagProvider = (setFeatureFlag) => {
-  setFeatureFlag(FLAG_TECH_PREVIEW, !!window.SERVER_FLAGS.techPreview);
-};
-
-export default useTechPreviewFlagProvider;
