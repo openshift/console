f02cc861da2e0260e937cdf100246dcaa13b6a9a
logonoff <git@logonoff.co>
2026-03-13 10:47:24 -0400

    CONSOLE-5114: Demo plugin react-router migration

--- dynamic-demo-plugin/src/components/ProjectOverview/Inventory.tsx
+++ dynamic-demo-plugin/src/components/ProjectOverview/Inventory.tsx
@@ -5,7 +5,7 @@ import {
   InventoryItemTitle,
   InventoryItemLoading,
 } from '@openshift-console/dynamic-plugin-sdk';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import type { FC } from 'react';
 
