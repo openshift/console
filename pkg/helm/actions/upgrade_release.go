dd8f981908026879e775609865e217ee3746a026
sowmya-sl <lsowmyanarayanan@gmail.com>
2026-05-26 16:27:06 +0530

    HELM-728: Apply auth credentials before LocateChart on upgrade
    Credentials were being set after LocateChart, which is the call that
    actually contacts the registry. Move applyBasicAuthFromUserCredentials
    before LocateChart so the registry client has auth headers when pulling.

--- pkg/helm/actions/upgrade_release.go
+++ pkg/helm/actions/upgrade_release.go
@@ -204,6 +204,17 @@ func UpgradeReleaseAsync(
 				}
 			}
 		}
+		if auth_secret != "" {
+			klog.Infof("Found persisted auth secret %s for release %s/%s, applying credentials for upgrade", auth_secret, releaseNamespace, releaseName)
+			userCredentials, err := GetUserCredentials(coreClient, releaseNamespace, auth_secret)
+			if err != nil {
+				klog.Errorf("Failed to get user credentials for release upgrade %s/%s: %v", releaseNamespace, releaseName, err)
+			} else {
+				if err := applyBasicAuthFromUserCredentials(&client.ChartPathOptions, client, userCredentials); err != nil {
+					klog.Errorf("Failed to apply auth from secret %s for release %s/%s: %v", auth_secret, releaseNamespace, releaseName, err)
+				}
+			}
+		}
 		chartLocation = chartUrl
 		client.ChartPathOptions.Version = chartInfo.Version
 		cp, err = client.ChartPathOptions.LocateChart(chartLocation, settings)
@@ -229,19 +240,6 @@ func UpgradeReleaseAsync(
 		}
 		ch.Metadata.Annotations["chart_url"] = chartUrl
 		addAuthSecretAnnotation(ch, auth_secret)
-	}
-	if auth_secret != "" {
-		klog.Infof("Found persisted auth secret %s for release %s/%s, applying credentials for upgrade", auth_secret, releaseNamespace, releaseName)
-		userCredentials, err := GetUserCredentials(coreClient, releaseNamespace, auth_secret)
-		if err != nil {
-			klog.Infof("Failed to get user credentials for release upgrade %s/%s: %v", releaseNamespace, releaseName, err)
-
-		} else {
-			if err := applyBasicAuthFromUserCredentials(&client.ChartPathOptions, client, userCredentials); err != nil {
-				klog.Errorf("Failed to apply auth from secret %s for release %s/%s: %v", auth_secret, releaseNamespace, releaseName, err)
-				// Continue with upgrade but log the error - the upgrade may still work if auth is not required
-			}
-		}
 	}
 	go func() {
 		_, err := client.Run(releaseName, ch, vals)
