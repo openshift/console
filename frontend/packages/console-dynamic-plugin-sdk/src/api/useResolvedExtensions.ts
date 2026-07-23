5e6c1bcd302f7a9e185bfa0d4e35852f4eac4c79
Vojtech Szocs <vszocs@redhat.com>
2026-07-23 18:06:43 +0000

    Align Console useResolvedExtensions hook with upstream plugin SDK

--- frontend/packages/console-dynamic-plugin-sdk/src/api/useResolvedExtensions.ts
+++ frontend/packages/console-dynamic-plugin-sdk/src/api/useResolvedExtensions.ts
@@ -1,21 +1,12 @@
-import type { UseResolvedExtensionsOptions as UseResolvedExtensionsOptionsSDK } from '@openshift/dynamic-plugin-sdk';
+import type { Extension, ExtensionPredicate } from '@openshift/dynamic-plugin-sdk';
 import { useResolvedExtensions as useResolvedExtensionsSDK } from '@openshift/dynamic-plugin-sdk';
 import { useExtensions } from '@console/plugin-sdk/src/api/useExtensions';
 import type { UseResolvedExtensions } from '../extensions/console-types';
-import type { Extension, ExtensionPredicate, ResolvedExtension } from '../types';
-
-const hookOptions: UseResolvedExtensionsOptionsSDK = {
-  useExtensionsImpl: useExtensions,
-};
 
 export const useResolvedExtensions: UseResolvedExtensions = <E extends Extension>(
-  ...predicates: ExtensionPredicate<E>[]
-): [ResolvedExtension<E>[], boolean, any[]] => {
-  const predicate =
-    predicates.length === 1
-      ? predicates[0]
-      : predicates.length > 1
-      ? (extension: Extension): extension is E => predicates.some((guard) => guard(extension))
-      : undefined;
-  return useResolvedExtensionsSDK(predicate, hookOptions);
+  predicate: ExtensionPredicate<E>,
+) => {
+  const extensions = useExtensions(predicate);
+
+  return useResolvedExtensionsSDK(extensions);
 };
