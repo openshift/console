7b384e66a4a9e5042cbdaaa15b566cd17001800c
logonoff <git@logonoff.co>
2026-09-01 21:31:10 -0400

    NO-JIRA: Temporarily disable maxFailures to get the full picture

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -54,7 +54,6 @@ export default defineConfig({
   testMatch: '**/*.spec.ts',
   forbidOnly: isCI,
   globalTimeout: Number(process.env.GLOBAL_TIMEOUT_MS) || 0,
-  maxFailures: isCI ? 10 : 0,
   retries: isCI ? 2 : 0,
   timeout: 120_000,
   reporter: isCI
