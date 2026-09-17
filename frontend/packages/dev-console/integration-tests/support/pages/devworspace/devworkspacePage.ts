4b40cc4d704d07ac5c433f93e429a6e236926672
Sahil Shah <sahshah@redhat.com>
2026-09-17 15:20:13 -0400

    chore: remove Dev Console Cypress sources

--- frontend/packages/dev-console/integration-tests/support/pages/devworspace/devworkspacePage.ts
+++
@@ -1,5 +0,0 @@
-export const devWorkspacePage = {
-  verifyDevWsResourceStatus: (status: string) => {
-    cy.byTestID('resource-status').should('have.text', status);
-  },
-};
--- frontend/scripts/knip.ts
+++ frontend/scripts/knip.ts
@@ -241,7 +241,6 @@ const config: KnipConfig = {
   // Not shipped in production so we don't mind being loosey-goosey
   ignoreWorkspaces: [
     'packages/eslint-plugin-console',
-    'packages/integration-tests',
     'packages/*/integration-tests',
   ],
 
